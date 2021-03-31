3e461f5fd8f9719081965e249e91aeb1f6ee1066
YakaryBovine <kaz9876@gmail.com>
2021-04-01 08:43:11 +1300

    ByName prototype

--- src/AzerothWarsCSharp.Source/Libraries/Team.cs
+++ src/AzerothWarsCSharp.Source/Libraries/Team.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AzerothWarsCSharp.Source.Libraries
 { 
@@ -17,6 +14,16 @@ namespace AzerothWarsCSharp.Source.Libraries
     {
       Name = name;
       All.Add(this);
+    }
+
+    /// <summary>
+    /// Returns the Team with the provided name.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static Team ByName(string name)
+    {
+      throw new NotImplementedException();
     }
 
     public static HashSet<Team> All { get; } = new();
