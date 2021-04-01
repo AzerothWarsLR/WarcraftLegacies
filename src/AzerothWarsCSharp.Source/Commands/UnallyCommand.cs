ac5dd9c79322b56c279c0222df54a144495528aa
Zakary Boven <zakaryboven@gmail.com>
2021-04-01 15:57:51 +1300

    Create UnallyCommand.cs

---
+++ src/AzerothWarsCSharp.Source/Commands/UnallyCommand.cs
@@ -0,0 +1,30 @@
+﻿using AzerothWarsCSharp.Source.Libraries;
+using static War3Api.Common;
+
+namespace AzerothWarsCSharp.Source.Commands
+{
+  /// <summary>
+  /// A command to join a new, independent team.
+  /// </summary>
+  public static class UnallyCommand
+  {
+    public static void Initialize()
+    {
+      Command.Register("unally", (player triggerPlayer, string[] arguments) =>
+      {
+        var triggerFaction = Faction.ByPlayerHandle(triggerPlayer);
+        if (triggerFaction != null)
+        {
+          var targetTeamName = triggerFaction.Name + " Pact";
+          var targetTeam = Team.ByName(targetTeamName);
+          if (targetTeam == null)
+          {
+            targetTeam = new Team(targetTeamName);
+          }
+          triggerFaction.Team = targetTeam;
+        }
+      }
+      );
+    }
+  }
+}
