1984a97bcfe1e29bfd1bd36a4c586ab1c0fc1a52
YakaryBovine <kaz9876@gmail.com>
2021-04-01 19:03:55 +1300

    Create HeroGlowFix.cs

---
+++ src/AzerothWarsCSharp.Source/GameLogic/HeroGlowFix.cs
@@ -0,0 +1,33 @@
+﻿using AzerothWarsCSharp.Source.Libraries;
+using static War3Api.Common;
+using static War3Api.Blizzard;
+
+namespace AzerothWarsCSharp.Source.GameLogic
+{
+  /// <summary>
+  /// Manually resets a hero's color when trained.
+  /// Avoids a bug with hero glows.
+  /// </summary>
+  public static class HeroGlowFix
+  {
+    private static void OnAnyHeroRevived()
+    {
+      var revivedLegend = Legend.ByUnitHandle(GetTriggerUnit());
+      if (revivedLegend.PlayerColor != null)
+      {
+        SetUnitColor(revivedLegend.Unit, revivedLegend.PlayerColor);
+      }
+      else
+      {
+        SetUnitColor(GetTriggerUnit(), Faction.ByPlayerHandle(GetTriggerPlayer()).PlayerColor);
+      }
+    }
+
+    public static void Initialize()
+    {
+      trigger trig = CreateTrigger();
+      TriggerRegisterAnyUnitEventBJ(trig, EVENT_PLAYER_HERO_REVIVE_FINISH);
+      TriggerAddAction(trig, OnAnyHeroRevived);
+    }
+  }
+}
