21cd8ac8c1ca7963a3e03d706aba0ea0d4fe93ad
Zakary Boven <zakaryboven@gmail.com>
2021-04-01 14:53:54 +1300

    final batch of cheats

---
+++ src/AzerothWarsCSharp.Source/Commands/CheatTele.cs
@@ -0,0 +1,53 @@
+﻿using AzerothWarsCSharp.Common.Constants;
+using AzerothWarsCSharp.Source.Libraries;
+using System.Collections.Generic;
+using WCSharp.Events;
+using static War3Api.Common;
+
+namespace AzerothWarsCSharp.Source.Commands
+{
+  /// <summary>
+  /// A cheat that causes the player's units to be teleported whenever they are given the patrol order.
+  /// </summary>
+  public static class CheatTele
+  {
+    public static void Initialize()
+    {
+      //Create cheat
+      CheatCommand.Register("tele", (player triggerPlayer, string[] arguments) =>
+      {
+        var toggleArg = arguments[1];
+        if (toggleArg == "on")
+        {
+          _enabledPlayers.Add(triggerPlayer);
+          CheatCommand.Display(triggerPlayer, "Teleport-on-patrol activated.");
+        }
+        else if (toggleArg == "off")
+        {
+          _enabledPlayers.Remove(triggerPlayer);
+          CheatCommand.Display(triggerPlayer, "Teleport-on-patrol deactivated.");
+        }
+      }
+      );
+      //Create trigger
+      for (int i = 0; i < PlayerConstants.PlayerSlotCount; i++)
+      {
+        PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeReceivesPointOrder, OnAnyUnitIssuedPointOrder);
+      }
+    }
+
+    private static void OnAnyUnitIssuedPointOrder()
+    {
+      if (_enabledPlayers.Count > 0)
+      {
+        if (_enabledPlayers.Contains(GetOwningPlayer(GetTriggerUnit())) && GetIssuedOrderId() == 851990)
+        {
+          SetUnitX(GetTriggerUnit(), GetOrderPointX());
+          SetUnitY(GetTriggerUnit(), GetOrderPointY());
+        }
+      }
+    }
+
+    private static readonly HashSet<player> _enabledPlayers = new();
+  }
+}
---
+++ src/AzerothWarsCSharp.Source/Commands/CheatVision.cs
@@ -0,0 +1,37 @@
+﻿using AzerothWarsCSharp.Source.Libraries;
+using static War3Api.Common;
+using static War3Api.Blizzard;
+using System.Collections.Generic;
+using AzerothWarsCSharp.Common.Constants;
+
+namespace AzerothWarsCSharp.Source.Commands
+{
+  /// <summary>
+  /// A cheat that guves the player vision over the entire map.
+  /// </summary>
+  public static class CheatVision
+  {
+    public static void Initialize()
+    {
+      //Create cheat
+      CheatCommand.Register("vision", (player triggerPlayer, string[] arguments) =>
+      {
+        var toggleArg = arguments[1];
+        if (toggleArg == "on")
+        {
+          _fogModifiersByPlayer[triggerPlayer] = CreateFogModifierRectBJ(true, triggerPlayer, FOG_OF_WAR_VISIBLE, GetPlayableMapRect());
+          CheatCommand.Display(triggerPlayer, "Vision over whole map granted.");
+        }
+        else if (toggleArg == "off")
+        {
+          DestroyFogModifier(_fogModifiersByPlayer[triggerPlayer]);
+          _fogModifiersByPlayer[triggerPlayer] = null;
+          CheatCommand.Display(triggerPlayer, "Vision over whole map revoked.");
+        }
+      }
+      );
+    }
+
+    private static readonly Dictionary<player, fogmodifier> _fogModifiersByPlayer = new();
+  }
+}
