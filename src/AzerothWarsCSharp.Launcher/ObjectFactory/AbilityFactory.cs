598707611e2a9ee7cd07f19322e4dc2fdddcbb48
YakaryBovine <kaz9876@gmail.com>
2021-04-08 11:24:18 +1200

    start of object factory concept

---
+++ src/AzerothWarsCSharp.Launcher/ObjectFactory/AbilityFactory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using War3Api.Object;
+
+namespace AzerothWarsCSharp.Launcher.ObjectFactory
+{
+  public static class AbilityFactory
+  {
+    public static Ability CreateCrushingWave(
+      string newRawcode, char hotkey, float[] damage, float[] maxDamage, float[] distance, int levels, float[] startArea,
+      float[] finalArea, float[] castRange, IEnumerable<Target>[] targetsAllowed, float[] cooldown, int[] manaCost,
+      int missileSpeed = 1100, IEnumerable<string> artSpecial = null, IEnumerable<string> missileArt = null)
+    {
+      var newAbility = new War3Api.Object.Abilities.CrushingWave(newRawcode)
+      {
+        TextHotkeyNormal = hotkey,
+        StatsLevels = levels,
+        ArtMissileSpeed = missileSpeed,
+        ArtSpecial = artSpecial,
+        ArtMissileArt = missileArt
+      };
+      for (var i = 0; i < levels; i++)
+      {
+        newAbility.DataDamage[i] = damage[i];
+        newAbility.DataMaxDamage[i] = damage[i];
+        newAbility.DataDistance[i] = distance[i];
+        newAbility.StatsAreaOfEffect[i] = startArea[i];
+        newAbility.DataFinalArea[i] = finalArea[i];
+        newAbility.StatsCastRange[i] = castRange[i];
+        newAbility.StatsTargetsAllowed[i] = targetsAllowed[i];
+        newAbility.StatsCooldown[i] = cooldown[i];
+        newAbility.StatsManaCost[i] = manaCost[i];
+        newAbility.TextTooltipNormal[i] = $"Deals {damage[i]} damage to each enemy land unit in a line, up to {maxDamage[i]} max damage.";
+      }
+      return newAbility;
+    }
+
+    public static Ability CreateSummonWaterElemental(string newRawcode, string iconName, Unit[] summonedUnit, int[] summonCount, int levels = 1)
+    {
+      //Buff setup
+      var newBuff = new Buff(BuffType.WaterElemental)
+      {
+        ArtIcon = iconName,
+      };
+
+      //Ability setup
+      var newAbility = new War3Api.Object.Abilities.ArchMageWaterElemental(newRawcode)
+      {
+        ArtIconNormal = iconName,
+        ArtIconResearch = iconName
+      };
+      for (var i = 0; i < levels; i++)
+      {
+        newAbility.DataSummonedUnitCount[i] = summonCount[i];
+        newAbility.DataSummonedUnitType[i] = summonedUnit[i];
+        newAbility.StatsBuffs[i] = new Buff[] { newBuff };
+        newAbility.TextTooltipNormal[i] = $"Summon {summonedUnit[i].TextName}";
+      }
+
+      return newAbility;
+    }
+  }
+}
--- src/AzerothWarsCSharp.Launcher/ObjectFactory/BuildingFactory.cs
+++ src/AzerothWarsCSharp.Launcher/ObjectFactory/BuildingFactory.cs
@@ -1,28 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using War3Api.Object;
 
 namespace AzerothWarsCSharp.Launcher.ObjectFactory
 {
   public static class BuildingFactory
   {
-    private static string GenerateExtendedTooltip(string flavourText, IEnumerable<Unit> unitsTrained, IEnumerable<Upgrade> upgradesResearched)
+    private static string GenerateExtendedTooltip(string flavourText, IEnumerable<Unit> unitsTrained = null, IEnumerable<Upgrade> upgradesResearched = null)
     {
-      return null;
+      var stringBuilder = new StringBuilder();
+      stringBuilder.Append(flavourText);
+      stringBuilder.AppendLine("Trains: ");
+      foreach (var unit in unitsTrained)
+      {
+        stringBuilder.Append(unit.TextName);
+      }
+      stringBuilder.AppendLine("Researches: ");
+      foreach (var upgrade in upgradesResearched)
+      {
+        stringBuilder.Append(upgrade.TextName);
+      }
+      return stringBuilder.ToString();
     }
 
-    public static void CreateBuilding(string newRawCode, IEnumerable<Unit> units, IEnumerable<Upgrade> upgrades, 
-      string name, string model, string icon, string soundSet)
+    public static Unit CreateBuilding(UnitType baseType, string newRawCode, string name, IEnumerable<Unit> units = null, IEnumerable<Upgrade> upgrades = null)
     {
-      var newBuilding = new Unit(UnitType.Humanbarracks, newRawCode)
+      var newBuilding = new Unit(baseType, newRawCode)
       {
         TextTooltipBasic = name,
         TechtreeUnitsTrained = units,
         TechtreeResearchesAvailable = upgrades,
-        ArtModelFile = model,
-        ArtIconGameInterface = icon,
-        SoundUnitSoundSet = soundSet,
-        TextTooltipExtended = GenerateExtendedTooltip("Primary troop production building.", units, upgrades);
+        TextTooltipExtended = GenerateExtendedTooltip("Primary troop production building.", units, upgrades),
       };
+      return newBuilding;
     }
   }
 }
