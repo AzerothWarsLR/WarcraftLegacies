b17f896ff6f52a96b8bee6d475e0b8e27ac3af42
YakaryBovine <zakaryboven@gmail.com>
2022-04-11 23:22:55 +1200

    Update ExampleQuestA.cs

--- src/AzerothWarsCSharp.TestSource/Quests/ExampleQuestA.cs
+++ src/AzerothWarsCSharp.TestSource/Quests/ExampleQuestA.cs
@@ -6,10 +6,16 @@ namespace AzerothWarsCSharp.TestSource.Quests
 {
   public sealed class ExampleQuestA : QuestData
   {
-    public ExampleQuestA() : base("Example Quest", "You have to do something fun.", "ReplaceableTextures\\CommandButtons\\BTNAcolyte.blp")
+    public ExampleQuestA() : base("Example Quest", "You have to do something fun.",
+      "ReplaceableTextures\\CommandButtons\\BTNAcolyte.blp")
     {
       var acolyte = CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), FourCC("uaco"), 0, 0, 0);
+      CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), FourCC("uaco"), 0, 0, 0);
+      CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), FourCC("uaco"), 0, 0, 0);
+      CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), FourCC("uaco"), 0, 0, 0);
+      CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), FourCC("uaco"), 0, 0, 0);
       AddQuestItem(new QuestItemKillUnit(acolyte));
+      AddQuestItem(new QuestItemKillXUnit(FourCC("uaco"), 3));
     }
 
     protected override string RewardDescription => "Just the greatest things.";
