45187e1f74d192d356f165ee8ac4c1d21c0b9f1c
YakaryBovine <zakaryboven@gmail.com>
2022-04-13 22:47:24 +1200

    Added Uther for testing

--- src/AzerothWarsCSharp.TestSource/LegendSetup.cs
+++ src/AzerothWarsCSharp.TestSource/LegendSetup.cs
@@ -6,12 +6,17 @@ namespace AzerothWarsCSharp.TestSource
   public static class LegendSetup
   {
     public static Legend Kael { get; private set; }
+    public static Legend Uther { get; private set; }
 
     public static void Setup()
     {
       Kael = new Legend
       {
         Unit = CreateUnit(Player(0), FourCC("Hkal"), 0, 0, 0)
+      };
+      Uther = new Legend
+      {
+        Unit = CreateUnit(Player(3), FourCC("Huth"), 0, 0, 0)
       };
     }
   }
