b220b584adb63e6394a3c6b81a0a19bbb0ac70a0
YakaryBovine <zakaryboven@gmail.com>
2022-04-15 16:18:36 +1200

    ExampleQuestC changes completer's name to Zerg

--- src/AzerothWarsCSharp.TestSource/Quests/ExampleQuestC.cs
+++ src/AzerothWarsCSharp.TestSource/Quests/ExampleQuestC.cs
@@ -1,39 +1,45 @@
-﻿using AzerothWarsCSharp.MacroTools.FactionSystem;
+﻿using AzerothWarsCSharp.MacroTools.ArtifactSystem;
+using AzerothWarsCSharp.MacroTools.FactionSystem;
 using AzerothWarsCSharp.MacroTools.Powers;
 using AzerothWarsCSharp.MacroTools.QuestSystem;
 using AzerothWarsCSharp.MacroTools.QuestSystem.UtilityStructs;
 using AzerothWarsCSharp.TestSource.Setup;
 using static War3Api.Common;
 
-public sealed class ExampleQuestC : QuestData
+namespace AzerothWarsCSharp.TestSource.Quests
 {
-  private Power _zerglingPower;
+  public sealed class ExampleQuestC : QuestData
+  {
+    private readonly Power _zerglingPower;
   
-  public ExampleQuestC(QuestData otherQuest) : base("Free Zergling", "We really need a free Zergling.",
-    "ReplaceableTextures\\CommandButtons\\BTNZergling.blp")
-  {
-    AddQuestItem(new QuestItemAcquireArtifact(ArtifactSetup.Killmaim));
-    AddQuestItem(new QuestItemSelfExists());
-    _zerglingPower = new DummyPower("Zerglings", "Spawn zerglings constantly.", "Zergling");
-  }
+    public ExampleQuestC(QuestData otherQuest) : base("Free Zergling", "We really need a free Zergling.",
+      "ReplaceableTextures\\CommandButtons\\BTNZergling.blp")
+    {
+      AddQuestItem(new QuestItemAcquireArtifact(ArtifactSetup.Killmaim));
+      AddQuestItem(new QuestItemSelfExists());
+      _zerglingPower = new DummyPower("Zerglings", "Spawn zerglings constantly.", "Zergling");
+    }
 
-  protected override string RewardDescription => "A free Zergling";
-  protected override string CompletionPopup => "Congratulations on your free Zergling!";
-  protected override string PenaltyDescription => "A hostile Peasant spawns";
+    protected override string RewardDescription => "A free Zergling, and Orb of Darkness";
+    protected override string CompletionPopup => "Congratulations on your free Zergling!";
+    protected override string PenaltyDescription => "A hostile Peasant spawns";
 
-  protected override void OnAdd()
-  {
-    Holder.AddPower(_zerglingPower);
-  }
+    protected override void OnAdd()
+    {
+      Holder.AddPower(_zerglingPower);
+    }
 
-  protected override void OnFail()
-  {
-    CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), FourCC("hpea"), 0, 0, 0);
-  }
+    protected override void OnFail()
+    {
+      CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), FourCC("hpea"), 0, 0, 0);
+    }
   
-  protected override void OnComplete()
-  {
-    CreateUnit(Holder.Player, FourCC("zzrg"), 0, 0, 0);
-    Holder.RemovePower(_zerglingPower);
+    protected override void OnComplete()
+    {
+      CreateUnit(Holder.Player, FourCC("zzrg"), 0, 0, 0);
+      Holder.RemovePower(_zerglingPower);
+      Holder.Name = "Zerg";
+      Holder.Icon = "ReplaceableTextures\\CommandButtons\\BTNZergling.blp";
+    }
   }
 }
