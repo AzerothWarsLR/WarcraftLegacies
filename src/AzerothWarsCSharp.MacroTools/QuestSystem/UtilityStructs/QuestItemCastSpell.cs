cfc3fc9a4c221e9adab3ea76f19d28fab5e3d370
YakaryBovine <zakaryboven@gmail.com>
2022-04-18 00:35:51 +1200

    QuestItemCastSpell properly expects a specific spell

--- src/AzerothWarsCSharp.MacroTools/QuestSystem/UtilityStructs/QuestItemCastSpell.cs
+++ src/AzerothWarsCSharp.MacroTools/QuestSystem/UtilityStructs/QuestItemCastSpell.cs
@@ -1,20 +1,35 @@
 using WCSharp.Events;
-
 using static War3Api.Common;
 
 namespace AzerothWarsCSharp.MacroTools.QuestSystem.UtilityStructs
 {
   /// <summary>
-  /// Completes when a unit casts a specific spell.
+  ///   Completes when a unit casts a specific spell.
   /// </summary>
   public sealed class QuestItemCastSpell : QuestItemData
   {
     private readonly bool _holderOnly;
 
     /// <summary>
-    /// The unit that was used to complete the quest.
+    ///   Completes when a unit casts a specific spell.
     /// </summary>
-    public unit Caster { get; private set; }
+    /// <param name="spellId">The spell that needs to be casted for completion.</param>
+    /// <param name="holderOnly">If true, the quest holder must cast the spell themselves.</param>
+    public QuestItemCastSpell(int spellId, bool holderOnly)
+    {
+      PlayerUnitEvents.Register(PlayerUnitEvent.SpellFinish, OnCast, spellId);
+      if (holderOnly)
+        Description = "Cast " + GetObjectName(spellId);
+      else
+        Description = "Anyone casts " + GetObjectName(spellId);
+
+      _holderOnly = holderOnly;
+    }
+
+    /// <summary>
+    ///   The unit that was used to complete the quest.
+    /// </summary>
+    public unit? Caster { get; private set; }
 
     private void OnCast()
     {
@@ -23,26 +38,6 @@ namespace AzerothWarsCSharp.MacroTools.QuestSystem.UtilityStructs
         Caster = GetTriggerUnit();
         Progress = QuestProgress.Complete;
       }
-    }
-
-    /// <summary>
-    /// Completes when a unit casts a specific spell.
-    /// </summary>
-    /// <param name="spellId">The spell that needs to be casted for completion.</param>
-    /// <param name="holderOnly">If true, the quest holder must cast the spell themselves.</param>
-    public QuestItemCastSpell(int spellId, bool holderOnly)
-    {
-      PlayerUnitEvents.Register(PlayerUnitEvent.SpellFinish, OnCast);
-      if (holderOnly)
-      {
-        Description = "Cast " + GetObjectName(spellId);
-      }
-      else
-      {
-        Description = "Anyone casts " + GetObjectName(spellId);
-      }
-      
-      _holderOnly = holderOnly;
     }
   }
 }
