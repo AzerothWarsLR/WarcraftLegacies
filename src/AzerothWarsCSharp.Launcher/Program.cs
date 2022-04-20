45d4115fca764dfab365a5c6596924b83f41465d
YakaryBovine <zakaryboven@gmail.com>
2022-04-20 13:49:07 +1200

    Updated all dependencies

--- src/AzerothWarsCSharp.Launcher/Program.cs
+++ src/AzerothWarsCSharp.Launcher/Program.cs
@@ -107,7 +107,7 @@ namespace AzerothWarsCSharp.Launcher
       const string csc = DEBUG ? "-debug -define:DEBUG" : null;
       var csproj = Directory.EnumerateFiles(projectFolderPath, "*.csproj", SearchOption.TopDirectoryOnly).Single();
       var compiler = new Compiler(csproj, OUTPUT_FOLDER_PATH, string.Empty, null,
-        "War3Api.Blizzard.*;War3Api.Common.*;WCSharp.*;AzerothWarsCSharp.MacroTools.*", "", csc, false, null,
+        "WCSharp.*;AzerothWarsCSharp.MacroTools.*", "", csc, false, null,
         string.Empty)
       {
         IsExportMetadata = true,
