f09261d53fb7f70dc166ba7f931e55d58832c4e0
YakaryBovine <zakaryboven@gmail.com>
2022-04-22 22:26:16 +1200

    Moved back to verbose compiler setup

--- src/AzerothWarsCSharp.Launcher/Program.cs
+++ src/AzerothWarsCSharp.Launcher/Program.cs
@@ -107,7 +107,7 @@ namespace AzerothWarsCSharp.Launcher
       const string csc = DEBUG ? "-debug -define:DEBUG" : null;
       var csproj = Directory.EnumerateFiles(projectFolderPath, "*.csproj", SearchOption.TopDirectoryOnly).Single();
       var compiler = new Compiler(csproj, OUTPUT_FOLDER_PATH, string.Empty, null,
-        "WCSharp.*", "", csc, false, null,
+        "War3Api.Common.*;War3Api.Blizzard.*;WCSharp.*;AzerothWarsCSharp.MacroTools.*", "", csc, false, null,
         string.Empty)
       {
         IsExportMetadata = true,
