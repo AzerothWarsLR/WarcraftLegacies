9f325d122588c519424e93f35a32e0bc67e6eee1
YakaryBovine <zakaryboven@gmail.com>
2022-04-23 23:25:23 +1200

    Create UnitEffect.cs

---
+++ src/AzerothWarsCSharp.MacroTools/SpellSystem/UnitEffect.cs
@@ -0,0 +1,25 @@
+﻿namespace AzerothWarsCSharp.MacroTools.SpellSystem
+{
+  public abstract class UnitEffect
+  {
+    /// <summary>
+    /// The unit type that gets this effect.
+    /// </summary>
+    public int UnitTypeId { get; }
+
+    public virtual void OnDealsDamage()
+    {
+      
+    }
+
+    public virtual void OnCreated()
+    {
+      
+    }
+
+    protected UnitEffect(int unitTypeId)
+    {
+      UnitTypeId = unitTypeId;
+    }
+  }
+}
