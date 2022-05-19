cd58218c61d1984b12d345b145a0405f3efd91bf
YakaryBovine <zakaryboven@gmail.com>
2022-05-19 16:37:40 +1200

    Removed usages of SetPlayerAllianceStateBJ

--- src/AzerothWarsCSharp.MacroTools/FactionSystem/PlayerExtensions.cs
+++ src/AzerothWarsCSharp.MacroTools/FactionSystem/PlayerExtensions.cs
@@ -4,11 +4,66 @@ namespace AzerothWarsCSharp.MacroTools.FactionSystem
 {
   public static class PlayerExtensions
   {
+    public static void SetAllianceState(this player sourcePlayer, player otherPlayer, AllianceState allianceState)
+    {
+      // Prevent players from attempting to ally with themselves.
+      if (sourcePlayer == otherPlayer)
+      {
+        return;
+      }
+
+      if (allianceState == AllianceState.UnalliedVision)
+      {
+        sourcePlayer.SetPlayerAllianceStateAlly(otherPlayer, false);
+        sourcePlayer.SetPlayerAllianceStateVision(otherPlayer, true);
+        sourcePlayer.SetPlayerAllianceStateControl(otherPlayer, false);
+        sourcePlayer.SetPlayerAllianceStateFullControl(otherPlayer, false);
+      }
+      else if (allianceState == AllianceState.AlliedVision)
+      {
+        sourcePlayer.SetPlayerAllianceStateAlly(otherPlayer, true);
+        sourcePlayer.SetPlayerAllianceStateVision(otherPlayer, true);
+        sourcePlayer.SetPlayerAllianceStateControl(otherPlayer, false);
+        sourcePlayer.SetPlayerAllianceStateFullControl(otherPlayer, false);
+      }
+      else if (allianceState == AllianceState.AlliedAdvUnits)
+      {
+        sourcePlayer.SetPlayerAllianceStateAlly(otherPlayer, true);
+        sourcePlayer.SetPlayerAllianceStateVision(otherPlayer, true);
+        sourcePlayer.SetPlayerAllianceStateControl(otherPlayer, true);
+        sourcePlayer.SetPlayerAllianceStateFullControl(otherPlayer, true);
+      }
+    }
+
+    public static void SetPlayerAllianceStateAlly(this player sourcePlayer, player otherPlayer, bool flag)
+    {
+      SetPlayerAlliance(sourcePlayer, otherPlayer, ALLIANCE_PASSIVE, flag);
+      SetPlayerAlliance(sourcePlayer, otherPlayer, ALLIANCE_HELP_REQUEST, flag);
+      SetPlayerAlliance(sourcePlayer, otherPlayer, ALLIANCE_HELP_RESPONSE, flag);
+      SetPlayerAlliance(sourcePlayer, otherPlayer, ALLIANCE_SHARED_XP, flag);
+      SetPlayerAlliance(sourcePlayer, otherPlayer, ALLIANCE_SHARED_SPELLS, flag);
+    }
+
+    public static void SetPlayerAllianceStateVision(this player sourcePlayer, player otherPlayer, bool flag)
+    {
+      SetPlayerAlliance(sourcePlayer, otherPlayer, ALLIANCE_SHARED_VISION, flag);
+    }
+
+    public static void SetPlayerAllianceStateControl(this player sourcePlayer, player otherPlayer, bool flag)
+    {
+      SetPlayerAlliance(sourcePlayer, otherPlayer, ALLIANCE_SHARED_CONTROL, flag);
+    }
+
+    public static void SetPlayerAllianceStateFullControl(this player sourcePlayer, player otherPlayer, bool flag)
+    {
+      SetPlayerAlliance(sourcePlayer, otherPlayer, ALLIANCE_SHARED_ADVANCED_CONTROL, flag);
+    }
+
     public static void AdjustPlayerState(this player player, playerstate playerState, int value)
     {
       SetPlayerState(player, playerState, GetPlayerState(player, playerState) + value);
     }
-    
+
     public static int GetObjectLimit(this player player, int objectId)
     {
       return PlayerData.ByHandle(player).GetObjectLimit(objectId);
