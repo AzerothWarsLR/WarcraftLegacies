cd7377848a6123c74e2cc3d98471fc25102fee10
YakaryBovine <zakaryboven@gmail.com>
2022-05-20 00:07:48 +1200

    Removed unused usings

--- src/AzerothWarsCSharp.Source/Mechanics/Scourge/Plague/DarkConversionPeriodicAction.cs
+++ src/AzerothWarsCSharp.Source/Mechanics/Scourge/Plague/DarkConversionPeriodicAction.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using AzerothWarsCSharp.MacroTools.Buffs;
-using AzerothWarsCSharp.MacroTools.Libraries;
 using AzerothWarsCSharp.MacroTools.Wrappers;
 using WCSharp.Buffs;
 using WCSharp.Events;
--- src/AzerothWarsCSharp.Source/Mechanics/Scourge/Plague/PlaguePower.cs
+++ src/AzerothWarsCSharp.Source/Mechanics/Scourge/Plague/PlaguePower.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using AzerothWarsCSharp.MacroTools.FactionSystem;
 using AzerothWarsCSharp.MacroTools.Libraries;
 using WCSharp.Buffs;
