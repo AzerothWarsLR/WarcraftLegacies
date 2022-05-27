b6b67c556a9f8d10618d46c05d14c0611bcc52b4
YakaryBovine <zakaryboven@gmail.com>
2022-05-27 22:30:15 +1200

    Implemented Forsaken's Corrupt Building mechanic

---
+++ src/AzerothWarsCSharp.Source/Buffs/CorruptBuildingBuff.cs
@@ -0,0 +1,27 @@
+﻿using AzerothWarsCSharp.MacroTools.FactionSystem;
+using WCSharp.Buffs;
+using static War3Api.Common;
+
+namespace AzerothWarsCSharp.Source.Buffs
+{
+  public sealed class CorruptBuildingBuff : PassiveBuff
+  {
+    public CorruptBuildingBuff(unit caster, unit target) : base(caster, target)
+    {
+      EffectString = @"Units\Undead\PlagueCloud\PlagueCloudtarget.mdl";
+      EffectAttachmentPoint = "overhead";
+    }
+
+    public int BonusIncome { get; init; }
+
+    public override void OnApply()
+    {
+      CastingPlayer.AddBonusIncome(BonusIncome);
+    }
+
+    public override void OnDispose()
+    {
+      CastingPlayer.AddBonusIncome(-BonusIncome);
+    }
+  }
+}
---
+++ src/AzerothWarsCSharp.Source/Spells/CorruptBuildingSpell.cs
@@ -0,0 +1,26 @@
+﻿using AzerothWarsCSharp.MacroTools.SpellSystem;
+using AzerothWarsCSharp.Source.Buffs;
+using WCSharp.Buffs;
+using static War3Api.Common;
+
+namespace AzerothWarsCSharp.Source.Spells
+{
+  public sealed class CorruptBuildingSpell : Spell
+  {
+    public CorruptBuildingSpell(int id) : base(id)
+    {
+    }
+
+    public int BonusIncome { get; init; }
+
+    public override void OnCast(unit caster, unit target, float targetX, float targetY)
+    {
+      var buff = new CorruptBuildingBuff(caster, target)
+      {
+        BonusIncome = BonusIncome,
+        Duration = float.MaxValue
+      };
+      BuffSystem.Add(buff, StackBehaviour.Stack);
+    }
+  }
+}
