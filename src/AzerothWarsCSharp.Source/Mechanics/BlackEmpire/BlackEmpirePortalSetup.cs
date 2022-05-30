171e1ac3ad63b6be899d4ee2acbcc0fadae24bdf
YakaryBovine <zakaryboven@gmail.com>
2022-05-30 22:38:42 +1200

    Implemented BlackEmpirePortalSetup

--- src/AzerothWarsCSharp.Source/Mechanics/BlackEmpire/BlackEmpirePortalSetup.cs
+++ src/AzerothWarsCSharp.Source/Mechanics/BlackEmpire/BlackEmpirePortalSetup.cs
@@ -1,18 +1,43 @@
-﻿namespace AzerothWarsCSharp.Source.Mechanics.BlackEmpire
+﻿using AzerothWarsCSharp.MacroTools;
+using AzerothWarsCSharp.MacroTools.ControlPointSystem;
+using WCSharp.Shared.Data;
+
+namespace AzerothWarsCSharp.Source.Mechanics.BlackEmpire
 {
   public static class BlackEmpirePortalSetup
   {
-    public static BlackEmpirePortal TwilightHighlandsPortal;
-    public static BlackEmpirePortal NorthrendPortal;
-    public static BlackEmpirePortal TanarisPortal;
-    public static BlackEmpirePortal IllidanPortal;
+    public static BlackEmpirePortal TwilightHighlandsPortal { get; private set; }
+    public static BlackEmpirePortal NorthrendPortal { get; private set; }
+    public static BlackEmpirePortal TanarisPortal { get; private set; }
 
     public static void Setup()
     {
-      TwilightHighlandsPortal = new BlackEmpirePortal();
-      NorthrendPortal = new BlackEmpirePortal();
-      TanarisPortal = new BlackEmpirePortal();
-      IllidanPortal = new BlackEmpirePortal();
+      BlackEmpirePortalManager.RegisterPortals(new[]
+      {
+        TwilightHighlandsPortal = new BlackEmpirePortal(
+          PreplacedUnitSystem.GetUnit(Constants.UNIT_H03V_ENTRANCE_PORTAL, new Point(-24805, 1872)),
+          null,
+          Regions.Ny_Twilight_Highlands_Interior.Center,
+          PreplacedUnitSystem.GetUnit(Constants.UNIT_N07F_NY_ALOTHA_PORTAL_GREEN_TWILIGHT_HIGHLANDS,
+            Regions.NyHighland.Center), "Twilight",
+          ControlPointManager.GetFromUnitType(Constants.UNIT_N04V_NORTHERN_HIGHLANDS_10GOLD_MIN)),
+
+        NorthrendPortal = new BlackEmpirePortal(
+          PreplacedUnitSystem.GetUnit(Constants.UNIT_H03V_ENTRANCE_PORTAL, new Point(-25513, -1379)),
+          null,
+          Regions.NyNorth.Center,
+          PreplacedUnitSystem.GetUnit(Constants.UNIT_N07F_NY_ALOTHA_PORTAL_GREEN_TWILIGHT_HIGHLANDS,
+            Regions.Ny_Ulduar_Interior.Center), "Northrend",
+          ControlPointManager.GetFromUnitType(Constants.UNIT_N04V_NORTHERN_HIGHLANDS_10GOLD_MIN)),
+
+        TanarisPortal = new BlackEmpirePortal(
+          PreplacedUnitSystem.GetUnit(Constants.UNIT_H03V_ENTRANCE_PORTAL, new Point(-26208, -1945)),
+          null,
+          Regions.Ny_Silithus_Interior.Center,
+          PreplacedUnitSystem.GetUnit(Constants.UNIT_N07F_NY_ALOTHA_PORTAL_GREEN_TWILIGHT_HIGHLANDS,
+            Regions.NyTanaris.Center), "Ul'dum",
+          ControlPointManager.GetFromUnitType(Constants.UNIT_N04V_NORTHERN_HIGHLANDS_10GOLD_MIN))
+      });
     }
   }
 }
--- src/AzerothWarsCSharp.Source/Setup/QuestSetup/BlackEmpireQuestSetup.cs
+++ src/AzerothWarsCSharp.Source/Setup/QuestSetup/BlackEmpireQuestSetup.cs
@@ -1,17 +1,25 @@
+using System.Collections.Generic;
+using AzerothWarsCSharp.Source.Mechanics.BlackEmpire;
+using AzerothWarsCSharp.Source.Quests.BlackEmpire;
+using AzerothWarsCSharp.Source.Setup.FactionSetup;
+using War3Api;
+
 namespace AzerothWarsCSharp.Source.Setup.QuestSetup
 {
   public static class BlackEmpireQuestSetup
   {
     public static void Setup()
     {
-      // var blackempire = BlackEmpireSetup.FactionBlackempire;
-      // blackempire.StartingQuest = blackempire.AddQuest(new QuestFirstObelisk(Regions.NyalothaUnlock1.Rect));
-      // blackempire.AddQuest(new QuestSecondObelisk(new List<rect>
-      //   {Regions.NyalothaUnlock2.Rect, Regions.NyalothaUnlock3.Rect}));
-      // blackempire.AddQuest(new QuestThirdObelisk(new List<rect>
-      //   {Regions.NyalothaUnlock1.Rect, Regions.NyalothaUnlock2.Rect, Regions.NyalothaUnlock3.Rect})));
-      // blackempire.AddQuest(new QuestYoggSaron());
-      // blackempire.AddQuest(new QuestBladeOfTheBlackEmpire(new QuestIntoTheVoid()));
+      BlackEmpirePortalSetup.Setup();
+
+      var blackempire = BlackEmpireSetup.FactionBlackempire;
+      blackempire.StartingQuest = blackempire.AddQuest(new QuestFirstObelisk(Regions.NyalothaUnlock1.Rect));
+      blackempire.AddQuest(new QuestSecondObelisk(new List<Common.rect>
+        {Regions.NyalothaUnlock2.Rect, Regions.NyalothaUnlock3.Rect}));
+      blackempire.AddQuest(new QuestThirdObelisk(new List<Common.rect>
+        {Regions.NyalothaUnlock1.Rect, Regions.NyalothaUnlock2.Rect, Regions.NyalothaUnlock3.Rect}));
+      blackempire.AddQuest(new QuestYoggSaron());
+      blackempire.AddQuest(new QuestBladeOfTheBlackEmpire(new QuestIntoTheVoid()));
     }
   }
 }
