643806f52487a24cad07b1af5f330198f3899b48
YakaryBovine <zakaryboven@gmail.com>
2022-06-02 14:36:27 +1200

    Dialogue for when Kel'thuzad starts summoning the Legion

--- src/AzerothWarsCSharp.Source/Setup/DialogueSetup.cs
+++ src/AzerothWarsCSharp.Source/Setup/DialogueSetup.cs
@@ -20,6 +20,17 @@ namespace AzerothWarsCSharp.Source.Setup
           "Naive fool. My death will make little difference in the long run. For now, the scourging of this land... begins.",
           "Kel'thuzad"
         ));
+
+        DialogueManager.Add(new Dialogue(
+          new[]
+          {
+            new ObjectiveStartSpell(Constants.ABILITY_A00J_SUMMON_THE_BURNING_LEGION_ALL_FACTIONS, false,
+              LegendScourge.LegendKelthuzad)
+          }
+          , @"Sound\Dialogue\UndeadCampaign\Undead08\U08Kelthuzad18.flac",
+          "Come forth, Lord Archimonde! Enter this world, and let us bask in your power!",
+          "Kel'thuzad"
+        ));
       }
       catch (Exception ex)
       {
--- src/AzerothWarsCSharp.TestSource/Setup/DialogueSetup.cs
+++ src/AzerothWarsCSharp.TestSource/Setup/DialogueSetup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using AzerothWarsCSharp.MacroTools.DialogueSystem;
 using AzerothWarsCSharp.MacroTools.QuestSystem.UtilityStructs;
+using static War3Api.Common;
 
 namespace AzerothWarsCSharp.TestSource.Setup
 {
@@ -17,6 +18,15 @@ namespace AzerothWarsCSharp.TestSource.Setup
           }
           , @"Sound\Dialogue\HumanCampaign\Human04\H04Kelthuzad28.flac",
           "Naive fool. My death will make little difference in the long run. For now, the scourging of this land... begins.",
+          "Kel'thuzad"
+        ));
+        DialogueManager.Add(new Dialogue(
+          new[]
+          {
+            new ObjectiveStartSpell(FourCC("AHfs"), false, LegendSetup.Kael)
+          }
+          , @"Sound\Dialogue\UndeadCampaign\Undead08\U08Kelthuzad18.flac",
+          "Come forth, Lord Archimonde! Enter this world, and let us bask in your power!",
           "Kel'thuzad"
         ));
       }
