ca4634b126d818a3a9e8e0906674ff9565c95ef0
YakaryBovine <zakaryboven@gmail.com>
2022-06-04 15:03:00 +1200

    Fixed instance system and included Scarlet Monastery in it

--- src/AzerothWarsCSharp.Source/Setup/InstanceSetup.cs
+++ src/AzerothWarsCSharp.Source/Setup/InstanceSetup.cs
@@ -7,102 +7,61 @@ namespace AzerothWarsCSharp.Source.Setup
   {
     public static void Setup()
     {
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Barrow Deeps",
-      //     Rectangles = {Regions.InstanceBarrowDeeps}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Twisting Nether",
-      //     Rectangles = {Regions.TwistingNether}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Dire Maul",
-      //     Rectangles = {Regions.InstanceDireMaul}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Scholomance",
-      //     Rectangles = {Regions.InstanceScholomance}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Blackrock Depths",
-      //     Rectangles = {Regions.InstanceBlackrock}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Tomb of Sargeras",
-      //     Rectangles = {Regions.InstanceSargerasTomb}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Azjol'nerub",
-      //     Rectangles = {Regions.InstanceAzjolNerub}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Outland",
-      //     Rectangles = {Regions.InstanceOutland}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Nazjatar",
-      //     Rectangles = {Regions.InstanceNazjatar}
-      //   }
-      // );
-      //
-      // InstanceSystem.Register(
-      //   new Instance
-      //   {
-      //     Name = "Dalaran Dungeons",
-      //     Rectangles =
-      //     {
-      //       Regions.InstanceDalaranDungeon1,
-      //       Regions.InstanceDalaranDungeon2,
-      //       Regions.InstanceDalaranDungeon3
-      //     }
-      //   }
-      // );
-      //
-      // var naxxramas = new Instance
-      // {
-      //   Name = "Naxxramas",
-      //   Rectangles =
-      //   {
-      //     Regions.NaxxramasInside
-      //   }
-      // };
-      // naxxramas.AddDependency(PreplacedUnitSystem.GetUnit(Constants.UNIT_U01X_HEART_OF_NAXXRAMAS));
-      // naxxramas.AddDependency(PreplacedUnitSystem.GetUnit(Constants.UNIT_E013_NAXXRAMAS_SCOURGE));
-      // InstanceSystem.Register(naxxramas);
+      InstanceSystem.Register(
+        new Instance("Barrow Deeps", Regions.InstanceBarrowDeeps)
+      );
+
+      InstanceSystem.Register(
+        new Instance("Twisting Nether", Regions.TwistingNether)
+      );
+
+      InstanceSystem.Register(
+        new Instance("Dire Maul", Regions.InstanceDireMaul)
+      );
+
+      InstanceSystem.Register(
+        new Instance("Scholomance", Regions.InstanceScholomance)
+      );
+
+      InstanceSystem.Register(
+        new Instance("Blackrock Depths", Regions.InstanceBlackrock)
+      );
+
+      InstanceSystem.Register(
+        new Instance("Tomb of Sargeras", Regions.InstanceSargerasTomb)
+      );
+
+      InstanceSystem.Register(
+        new Instance("Azjol'nerub", Regions.InstanceAzjolNerub)
+      );
+
+
+      InstanceSystem.Register(
+        new Instance("Outland", Regions.InstanceOutland)
+      );
+
+
+      InstanceSystem.Register(
+        new Instance("Nazjatar", Regions.InstanceNazjatar)
+      );
+
+      InstanceSystem.Register(
+        new Instance("Dalaran Dungeons", new[]
+        {
+          Regions.InstanceDalaranDungeon1,
+          Regions.InstanceDalaranDungeon2,
+          Regions.InstanceDalaranDungeon3
+        })
+      );
+
+      var naxxramas = new Instance("Naxxramas", Regions.NaxxramasInside);
+      naxxramas.AddDependency(PreplacedUnitSystem.GetUnit(Constants.UNIT_U01X_HEART_OF_NAXXRAMAS));
+      naxxramas.AddDependency(PreplacedUnitSystem.GetUnit(Constants.UNIT_E013_NAXXRAMAS_SCOURGE));
+      InstanceSystem.Register(naxxramas);
+
+      var scarletMonastery = new Instance("Scarlet Monastery", Regions.ScarletAmbient);
+      scarletMonastery.AddDependency(PreplacedUnitSystem.GetUnit(Constants.UNIT_H00T_SCARLET_MONASTERY_LORDAERON));
+      InstanceSystem.Register(scarletMonastery);
     }
   }
 }
