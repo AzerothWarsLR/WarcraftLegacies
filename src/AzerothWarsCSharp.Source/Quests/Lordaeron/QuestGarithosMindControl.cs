5d15d381426f4c8ea922155a121de0b6a0595161
YakaryBovine <zakaryboven@gmail.com>
2022-06-15 23:03:41 +1200

    More cleanup

--- src/AzerothWarsCSharp.Source/Quests/Lordaeron/QuestGarithosMindControl.cs
+++ src/AzerothWarsCSharp.Source/Quests/Lordaeron/QuestGarithosMindControl.cs
@@ -16,7 +16,7 @@ namespace AzerothWarsCSharp.Source.Quests.Lordaeron
       "Garithos has always had a distrust of other races, he might be tempted to join the Scarlet MindControl.",
       "ReplaceableTextures\\CommandButtons\\BTNGarithos.blp")
     {
-      AddObjective(new ObjectiveResearch(FourCC("R08F"), FourCC("hbla")));
+      AddObjective(new ObjectiveResearch(Constants.UPGRADE_R08F_GARITHOS_MIND_CONTROL_LORDAERON, Constants.UNIT_HBLA_BLACKSMITH_LORDAERON));
     }
 
     protected override string CompletionPopup => "Garithos weak mind is an easy pray to Sylvanas mind control, ";
@@ -26,16 +26,16 @@ namespace AzerothWarsCSharp.Source.Quests.Lordaeron
 
     protected override void OnComplete(Faction completingFaction)
     {
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_H00F_PALADIN_LORDAERON, -Faction.UNLIMITED);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UPGRADE_R06Q_PALADIN_MASTER_TRAINING_LORDAERON, -Faction.UNLIMITED);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_H012_CAPTAIN_FALRIC_LORDAERON_DEMI, -Faction.UNLIMITED);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_HART_CROWN_PRINCE_OF_LORDAERON_LORDAERON, -Faction.UNLIMITED);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_HUTH_LEADER_OF_THE_SILVER_HAND_LORDAERON, -Faction.UNLIMITED);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_H01J_THE_ASHBRINGER_LORDAERON, -Faction.UNLIMITED);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_HARF_HIGH_KING_LORDAERON_HIGH_KING, -Faction.UNLIMITED);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_H009_DARK_KNIGHT_GARITHOS, 6);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_H049_RANGER_LORD_FORSAKEN, 1);
-      LordaeronSetup.FactionLordaeron.ModObjectLimit(Constants.UNIT_HLGR_GRAND_MARSHAL_SCARLET, 1);
+      completingFaction.ModObjectLimit(Constants.UNIT_H00F_PALADIN_LORDAERON, -Faction.UNLIMITED);
+      completingFaction.ModObjectLimit(Constants.UPGRADE_R06Q_PALADIN_MASTER_TRAINING_LORDAERON, -Faction.UNLIMITED);
+      completingFaction.ModObjectLimit(Constants.UNIT_H012_CAPTAIN_FALRIC_LORDAERON_DEMI, -Faction.UNLIMITED);
+      completingFaction.ModObjectLimit(Constants.UNIT_HART_CROWN_PRINCE_OF_LORDAERON_LORDAERON, -Faction.UNLIMITED);
+      completingFaction.ModObjectLimit(Constants.UNIT_HUTH_LEADER_OF_THE_SILVER_HAND_LORDAERON, -Faction.UNLIMITED);
+      completingFaction.ModObjectLimit(Constants.UNIT_H01J_THE_ASHBRINGER_LORDAERON, -Faction.UNLIMITED);
+      completingFaction.ModObjectLimit(Constants.UNIT_HARF_HIGH_KING_LORDAERON_HIGH_KING, -Faction.UNLIMITED);
+      completingFaction.ModObjectLimit(Constants.UNIT_H009_DARK_KNIGHT_GARITHOS, 6);
+      completingFaction.ModObjectLimit(Constants.UNIT_H049_RANGER_LORD_FORSAKEN, 1);
+      completingFaction.ModObjectLimit(Constants.UNIT_HLGR_GRAND_MARSHAL_SCARLET, 1);
 
       completingFaction.Player?.SetTeam(TeamSetup.Forsaken);
       completingFaction.Name = "Garithos";
