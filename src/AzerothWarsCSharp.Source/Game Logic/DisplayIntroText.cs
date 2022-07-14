69accf72d4d46785fb64844a8abdb2e868518d23
Zakary Boven <zakaryboven@gmail.com>
2022-07-14 16:36:54 +1200

    Intro text is timed

--- src/AzerothWarsCSharp.Source/Game Logic/DisplayIntroText.cs
+++ src/AzerothWarsCSharp.Source/Game Logic/DisplayIntroText.cs
@@ -25,7 +25,7 @@ namespace AzerothWarsCSharp.Source.Game_Logic
          TimerStart(timer, timeout, false, Display);
          FogEnable(false);
          FogMaskEnable(false);
-         DisplayTextToPlayer(GetLocalPlayer(), 0, 0, @"|cffffcc00Warcraft Legacies|r
+         DisplayTimedTextToPlayer(GetLocalPlayer(), 0, 0, 51,@"|cffffcc00Warcraft Legacies|r
 |cffaaaaaaJoin our Discord:|r discord.gg/pnWZs69
 |cffff0000Support our Patreon:|r https://www.patreon.com/lordsebas
 ");
