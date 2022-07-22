a0a139d0902422f258b4ef14f4e7dd64708a5153
Zakary Boven <zakaryboven@gmail.com>
2022-07-22 17:04:05 +1200

    Players cannot build shipyards in specific areas of the map (#25)
    * Gave all shipyards the Shipyard category
    
    * Shipyards can't be constructed in some areas
    
    * Style fix

---
+++ src/AzerothWarsCSharp.Source/Game Logic/ShipyardBanZones.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using AzerothWarsCSharp.MacroTools;
+using AzerothWarsCSharp.MacroTools.FactionSystem;
+using WCSharp.Events;
+using WCSharp.Shared.Data;
+using static War3Api.Common;
+
+namespace AzerothWarsCSharp.Source.Game_Logic
+{
+  public static class ShipyardBanZones
+  {
+    /// <summary>
+    /// Prevents Shipyards from being constructed in the provided areas.
+    /// </summary>
+    public static void Setup(IEnumerable<Rectangle> banZones)
+    {
+      foreach (var unitType in UnitType.GetAll())
+      {
+        if (unitType.Category == UnitCategory.Shipyard)
+        {
+          PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesConstruction, () =>
+          {
+            var constructedStructure = GetConstructedStructure();
+            foreach (var banZone in banZones)
+            {
+              if (!banZone.Contains(GetUnitX(constructedStructure), GetUnitY(constructedStructure))) continue;
+              GetOwningPlayer(constructedStructure).AddGold(GetUnitGoldCost(GetUnitTypeId(constructedStructure)));
+              GetOwningPlayer(constructedStructure).AddLumber(GetUnitWoodCost(GetUnitTypeId(constructedStructure)));
+              KillUnit(constructedStructure);
+            }
+          });
+        }
+      }
+    }
+  }
+}
