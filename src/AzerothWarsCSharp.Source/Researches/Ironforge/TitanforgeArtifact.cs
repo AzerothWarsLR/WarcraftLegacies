9be073f1fb68cd4b2e27ea15c5e8dd014f7c489a
Zakary Boven <zakaryboven@gmail.com>
2022-09-30 17:33:26 +1300

    Cleanup

--- src/AzerothWarsCSharp.Source/Researches/Ironforge/TitanforgeArtifact.cs
+++ src/AzerothWarsCSharp.Source/Researches/Ironforge/TitanforgeArtifact.cs
@@ -1,33 +1,34 @@
- using AzerothWarsCSharp.MacroTools.FactionSystem;
- using static War3Api.Common;  
- using WCSharp.Events;
- using AzerothWarsCSharp.MacroTools.ArtifactSystem;
- namespace AzerothWarsCSharp.Source.Researches.Ironforge
- {
-   public static class TitanForgeArtifact
-   {
-     private static readonly int RESEARCH_ID = Constants.UPGRADE_R08K_TITANFORGE_ARTIFACT_IRONFORGE;
+using AzerothWarsCSharp.MacroTools.FactionSystem;
+using static War3Api.Common;
+using WCSharp.Events;
+using AzerothWarsCSharp.MacroTools.ArtifactSystem;
 
-     private static void Research()
-     {
-       var heldItem = UnitItemInSlot(GetTriggerUnit(), 0);
-       Artifact heldArtifact = ArtifactManager.GetFromTypeId(GetItemTypeId(heldItem));
-       if (heldItem != null && heldArtifact != null && heldArtifact.Titanforged == false)
-       {
-         heldArtifact.Titanforge();
-       }
-       else
-       {
-         GetTriggerPlayer().AdjustPlayerState(PLAYER_STATE_RESOURCE_GOLD, 1000);
-         GetTriggerPlayer().AdjustPlayerState(PLAYER_STATE_RESOURCE_LUMBER, 750);
-       }
+namespace AzerothWarsCSharp.Source.Researches.Ironforge
+{
+  public static class TitanForgeArtifact
+  {
+    private const int ResearchId = Constants.UPGRADE_R08K_TITANFORGE_ARTIFACT_IRONFORGE;
 
-       SetPlayerTechResearched(GetTriggerPlayer(), RESEARCH_ID, 1);
-     }
+    private static void Research()
+    {
+      var heldItem = UnitItemInSlot(GetTriggerUnit(), 0);
+      var heldArtifact = ArtifactManager.GetFromTypeId(GetItemTypeId(heldItem));
+      if (heldItem != null && heldArtifact != null && heldArtifact.Titanforged == false)
+      {
+        heldArtifact.Titanforge();
+      }
+      else
+      {
+        GetTriggerPlayer().AdjustPlayerState(PLAYER_STATE_RESOURCE_GOLD, 1000);
+        GetTriggerPlayer().AdjustPlayerState(PLAYER_STATE_RESOURCE_LUMBER, 750);
+      }
 
-     public static void Setup()
-     {
-       PlayerUnitEvents.Register(PlayerUnitEvent.ResearchIsFinished, Research, Constants.UPGRADE_R08K_TITANFORGE_ARTIFACT_IRONFORGE);
-     }
-   }
- }
+      SetPlayerTechResearched(GetTriggerPlayer(), ResearchId, 0);
+    }
+
+    public static void Setup()
+    {
+      PlayerUnitEvents.Register(PlayerUnitEvent.ResearchIsFinished, Research, ResearchId);
+    }
+  }
+}
