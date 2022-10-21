007e06a493f15e4e635658641fc8b217e443b017
Zakary Boven <zakaryboven@gmail.com>
2022-10-22 11:05:45 +1300

    Documented Spell, SpellSystem, and CorruptBuildingSpell

--- src/AzerothWarsCSharp.MacroTools/SpellSystem/SpellSystem.cs
+++ src/AzerothWarsCSharp.MacroTools/SpellSystem/SpellSystem.cs
@@ -5,6 +5,10 @@ using static War3Api.Common;
 
 namespace AzerothWarsCSharp.MacroTools.SpellSystem
 {
+  /// <summary>
+  /// Provides a way to extend the functionality of existing Warcraft 3 spells and passive abilities
+  /// by adding extra code on top of their normal effects.
+  /// </summary>
   public static class SpellSystem
   {
     private static readonly Dictionary<int, Spell> SpellsByAbilityId = new();
@@ -15,11 +19,53 @@ namespace AzerothWarsCSharp.MacroTools.SpellSystem
         () => GetUnitTypeId(GetTrainedUnit()));
     }
 
+    /// <summary>
+    /// Returns the registered <see cref="Spell"/> with the provided ability ID.
+    /// </summary>
+    /// <exception cref="Exception">Thrown if there is no ability with the provided ID.</exception>
     public static Spell GetSpellByAbilityId(int abilityId)
     {
       if (!SpellsByAbilityId.ContainsKey(abilityId))
         throw new Exception($"There is no Spell with abilityId {abilityId}.");
       return SpellsByAbilityId[abilityId];
+    }
+    
+        /// <summary>
+    /// Registeres the provided passive ability to the <see cref="SpellSystem"/>, causing its functionality
+    /// to be invoked when specific Warcraft 3 events occur.
+    /// </summary>
+    public static void Register(TakeDamageEffect takeDamageEffect)
+    {
+      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeDamages, takeDamageEffect.OnTakesDamage,
+        takeDamageEffect.DamagedUnitTypeId);
+    }
+
+    /// <summary>
+    /// Registeres the provided passive ability to the <see cref="SpellSystem"/>, causing its functionality
+    /// to be invoked when specific Warcraft 3 events occur.
+    /// </summary>
+    public static void Register(UnitEffect unitEffect)
+    {
+      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeDamages, unitEffect.OnDealsDamage, unitEffect.UnitTypeId);
+      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeIsCreated, unitEffect.OnCreated, unitEffect.UnitTypeId);
+      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesBeingTrained, unitEffect.OnTrained, unitEffect.UnitTypeId);
+      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesTraining, unitEffect.OnTrainedUnit, unitEffect.UnitTypeId);
+      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesBeingConstructed, unitEffect.OnConstruction,
+        unitEffect.UnitTypeId);
+      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesUpgrade, unitEffect.OnUpgrade, unitEffect.UnitTypeId);
+      PlayerUnitEvents.Register(PlayerUnitEvent.HeroTypeFinishesRevive, unitEffect.OnCreated, unitEffect.UnitTypeId);
+    }
+
+    /// <summary>
+    ///   Registers the provided <see cref="Spell"/> to the <see cref="SpellSystem"/>, causing its functionality
+    /// to be invoked when a Warcraft 3 spell matching its IDs is used.
+    /// </summary>
+    public static void Register(Spell spell)
+    {
+      PlayerUnitEvents.Register(PlayerUnitEvent.SpellEffect, OnCast, spell.Id);
+      PlayerUnitEvents.Register(PlayerUnitEvent.SpellEndCast, OnStop, spell.Id);
+      PlayerUnitEvents.Register(PlayerUnitEvent.SpellLearnedByHeroType, OnLearn, spell.Id);
+      SpellsByAbilityId.Add(spell.Id, spell);
     }
 
     private static void OnStop()
@@ -37,38 +83,6 @@ namespace AzerothWarsCSharp.MacroTools.SpellSystem
     private static void OnLearn()
     {
       SpellsByAbilityId[GetLearnedSkill()].OnLearn(GetTriggerUnit());
-    }
-
-    public static void Register(TakeDamageEffect takeDamageEffect)
-    {
-      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeDamages, takeDamageEffect.OnTakesDamage,
-        takeDamageEffect.DamagedUnitTypeId);
-    }
-
-    /// <summary>
-    ///   Registers an <see cref="UnitEffect" /> to the <see cref="SpellSystem" />.
-    /// </summary>
-    public static void Register(UnitEffect unitEffect)
-    {
-      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeDamages, unitEffect.OnDealsDamage, unitEffect.UnitTypeId);
-      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeIsCreated, unitEffect.OnCreated, unitEffect.UnitTypeId);
-      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesBeingTrained, unitEffect.OnTrained, unitEffect.UnitTypeId);
-      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesTraining, unitEffect.OnTrainedUnit, unitEffect.UnitTypeId);
-      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesBeingConstructed, unitEffect.OnConstruction,
-        unitEffect.UnitTypeId);
-      PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesUpgrade, unitEffect.OnUpgrade, unitEffect.UnitTypeId);
-      PlayerUnitEvents.Register(PlayerUnitEvent.HeroTypeFinishesRevive, unitEffect.OnCreated, unitEffect.UnitTypeId);
-    }
-
-    /// <summary>
-    ///   Registers a spell to the SpellSystem.
-    /// </summary>
-    public static void Register(Spell spell)
-    {
-      PlayerUnitEvents.Register(PlayerUnitEvent.SpellEffect, OnCast, spell.Id);
-      PlayerUnitEvents.Register(PlayerUnitEvent.SpellEndCast, OnStop, spell.Id);
-      PlayerUnitEvents.Register(PlayerUnitEvent.SpellLearnedByHeroType, OnLearn, spell.Id);
-      SpellsByAbilityId.Add(spell.Id, spell);
     }
   }
 }
--- src/AzerothWarsCSharp.Source/Spells/CorruptBuildingSpell.cs
+++ src/AzerothWarsCSharp.Source/Spells/CorruptBuildingSpell.cs
@@ -5,17 +5,28 @@ using static War3Api.Common;
 
 namespace AzerothWarsCSharp.Source.Spells
 {
+  /// <summary>
+  /// Corrupts a building, granting it hit extra hit points and
+  /// causing it to grant the caster income for as long as it exists.
+  /// </summary>
   public sealed class CorruptBuildingSpell : Spell
   {
     private readonly int _bonusIncome;
     private readonly int _bonusHealth;
 
+    /// <summary>
+    /// Initializes an instance of the <see cref="CorruptBuildingSpell"/> class.
+    /// </summary>
+    /// <param name="id"><inheritdoc /></param>
+    /// <param name="bonusIncome">The casting player gets this much extra income as long as the corrupted building is alive.</param>
+    /// <param name="bonusHealth">The corrupted building gets this much extra hit points.</param>
     public CorruptBuildingSpell(int id, int bonusIncome, int bonusHealth) : base(id)
     {
       _bonusIncome = bonusIncome;
       _bonusHealth = bonusHealth;
     }
     
+    /// <inheritdoc />
     public override void OnCast(unit caster, unit target, float targetX, float targetY)
     {
       SetUnitState(target, UNIT_STATE_LIFE, GetUnitState(target, UNIT_STATE_MAX_LIFE));
