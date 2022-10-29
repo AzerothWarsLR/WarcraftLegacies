aeea7ce8778255615b2246d5270a65ea1fe3826f
Zakary Boven <zakaryboven@gmail.com>
2022-10-29 23:17:35 +1300

    Added documentation to QuestOpenScholomance.cs

--- src/AzerothWarsCSharp.Source/Quests/Forsaken/QuestOpenScholomance.cs
+++ src/AzerothWarsCSharp.Source/Quests/Forsaken/QuestOpenScholomance.cs
@@ -6,12 +6,19 @@ using static War3Api.Common;
 
 namespace AzerothWarsCSharp.Source.Quests.Forsaken
 {
+  /// <summary>
+  /// A <see cref="QuestData"/> to open the way into Scholomance.
+  /// </summary>
   public sealed class QuestOpenScholomance : QuestData
   {
     private unit? _scholomanceInner;
     private unit? _scholomanceOuter;
 
-    public QuestOpenScholomance(QuestData questPlague) : base("Open Scholomance",
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuestOpenScholomance"/> class.
+    /// </summary>
+    /// <param name="parentQuest">The <see cref="QuestData"/> which must be completed before this one can be.</param>
+    public QuestOpenScholomance(QuestData parentQuest) : base("Open Scholomance",
       "Scholomance remains hidden deep within the ancient crypts beneath Caer Darrow, away from the prying eyes of the Alliance. Soon it will be time to open its doors and unleash the necromantic horrors within.",
       @"ReplaceableTextures\CommandButtons\BTNNecropolis.blp")
     {
@@ -19,14 +26,17 @@ namespace AzerothWarsCSharp.Source.Quests.Forsaken
       _scholomanceOuter = PreplacedUnitSystem.GetUnit(Constants.UNIT_N035_SCHOLOMANCE);
       AddObjective(new ObjectiveEitherOf(
         new ObjectiveResearch(Constants.UPGRADE_R02X_OPEN_THE_SCHOLOMANCE_SCOURGE, Constants.UNIT_U012_SCHOLOMANCE),
-        new ObjectiveCompleteQuest(questPlague)));
+        new ObjectiveCompleteQuest(parentQuest)));
       Required = true;
     }
 
+    /// <inheritdoc />
     protected override string CompletionPopup => "Scholomance has been opened!";
 
+    /// <inheritdoc />
     protected override string RewardDescription => "Units can be moved in and out of Scholomance";
 
+    /// <inheritdoc />
     protected override void OnComplete(Faction completingFaction)
     {
       completingFaction.SetObjectLevel(Constants.UPGRADE_R02X_OPEN_THE_SCHOLOMANCE_SCOURGE, 1);
