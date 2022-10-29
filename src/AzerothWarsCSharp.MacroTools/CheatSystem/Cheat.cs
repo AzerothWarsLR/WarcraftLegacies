67f5e18527d268f698a78dae9ba169d012c06216
Zakary Boven <zakaryboven@gmail.com>
2022-10-29 19:12:58 +1300

    Implemented CheatManager for more easily managing Cheats

---
+++ src/AzerothWarsCSharp.MacroTools/CheatSystem/Cheat.cs
@@ -0,0 +1,28 @@
+﻿using static War3Api.Common;
+
+namespace AzerothWarsCSharp.MacroTools.CheatSystem
+{
+  /// <summary>
+  /// A function that a <see cref="player"/> can execute via the chat, which is only enabled in testing scenarios.
+  /// </summary>
+  public abstract class Cheat
+  {
+    /// <summary>
+    /// The command that a player can execute to run the <see cref="Cheat"/>.
+    /// </summary>
+    public abstract string Command { get; }
+    
+    /// <summary>
+    /// How many parameters should be supplied to this <see cref="Cheat"/> command.
+    /// </summary>
+    public abstract int ParameterCount { get; }
+
+    /// <summary>
+    /// What happens when the <see cref="Cheat"/> gets executed by a player.
+    /// </summary>
+    /// <param name="cheater">The <see cref="player"/> typing the <see cref="Cheat"/> command in.</param>
+    /// <param name="parameters">The parameters specified by the <see cref="player"/> entering the sheet, seperated by spaces.</param>
+    /// <returns>A message describing what the <see cref="Cheat"/> is doing to the executing <see cref="player"/>.</returns>
+    public abstract string Execute(player cheater, params string[] parameters);
+  }
+}
---
+++ src/AzerothWarsCSharp.MacroTools/CheatSystem/CheatManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AzerothWarsCSharp.MacroTools.Wrappers;
+using static AzerothWarsCSharp.MacroTools.Libraries.GeneralHelpers;
+using static War3Api.Common;
+
+namespace AzerothWarsCSharp.MacroTools.CheatSystem
+{
+  /// <summary>
+  /// Responsible for managing all <see cref="Cheat"/>s in the game.
+  /// </summary>
+  public static class CheatManager
+  {
+    private static readonly Dictionary<string, Cheat> CheatsByCommand = new();
+    private static readonly TriggerWrapper CheatTrigger = new();
+    
+    /// <summary>
+    /// A piece of text to be appended before any message relating to cheats.
+    /// </summary>
+    private const string CheatMessagePrefix = "|cffD27575CHEAT:|r";
+    
+    /// <summary>
+    /// All <see cref="Cheat"/>s must be prefixed with this when entered into the chat.
+    /// </summary>
+    private const string Prefix = "-";
+    
+    /// <summary>
+    /// Registers a <see cref="Cheat"/>, allowing it to be fired when a player executes its command in the chat.
+    /// </summary>
+    public static void Register(Cheat cheat)
+    {
+      CheatsByCommand.Add(Prefix + cheat.Command, cheat);
+      foreach (var player in GetAllPlayers()) 
+        CheatTrigger.RegisterPlayerChatEvent(player, Prefix + cheat.Command, false);
+    }
+
+    private static void ExecuteCheat()
+    {
+      try
+      {
+        var split = GetEventPlayerChatString().Split(" ");
+        var command = split[0];
+        if (!CheatsByCommand.TryGetValue(command, out var cheat)) 
+          return;
+        var triggerPlayer = GetTriggerPlayer();
+        var parameters = split.Skip(1).ToArray();
+        if (parameters.Length != cheat.ParameterCount)
+        {
+          DisplayTextToPlayer(triggerPlayer, 0, 0, $"{CheatMessagePrefix} You must supply exactly {cheat.ParameterCount} parameters.");
+          return;
+        }
+        var message = cheat.Execute(triggerPlayer, parameters);
+        DisplayTextToPlayer(triggerPlayer, 0, 0, $"{CheatMessagePrefix} {message}");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Failed to execute cheat: {ex}");
+      }
+    }
+
+    static CheatManager()
+    {
+      CheatTrigger.AddAction(ExecuteCheat);
+    }
+  }
+}
