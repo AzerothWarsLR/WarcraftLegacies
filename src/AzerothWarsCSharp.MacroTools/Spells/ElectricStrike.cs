697e3737ded7bc6ba5f456f2af02b9f011c9bdd9
Zakary Boven <zakaryboven@gmail.com>
2022-10-29 21:11:01 +1300

    Cleanup

--- src/AzerothWarsCSharp.MacroTools/Spells/ElectricStrike.cs
+++ src/AzerothWarsCSharp.MacroTools/Spells/ElectricStrike.cs
@@ -6,30 +6,58 @@ using WCSharp.Shared.Data;
 
 namespace AzerothWarsCSharp.MacroTools.Spells
 {
+  /// <summary>
+  /// Casts Storm Bolt and Purge on each unit in the target radius.
+  /// </summary>
   public sealed class ElectricStrike : Spell
   {
-    public int StunId { get; init; }//The ability that gets cast on each unit in the radius
-    public int PurgeId { get; init; } //The ability that gets cast on each unit in the radius
+    /// <summary>
+    /// Gets cast on each unit in the radius. Should be based off Storm Bolt or Fire Bolt.
+    /// </summary>
+    public int StunId { get; init; }
+    
+    /// <summary>
+    /// Gets cast on each unit in the radius. Should be based off Purge.
+    /// </summary>
+    public int PurgeId { get; init; }
+    
+    /// <summary>
+    /// The order ID for casting the specified Purge ability on targets.
+    /// </summary>
     public string PurgeOrder { get; init; } = string.Empty;
+    
+    /// <summary>
+    /// The order ID for casting the specified Storm Bolt ability on targets.
+    /// </summary>
     public string StunOrder { get; init; } = string.Empty;
+    
+    /// <summary>
+    /// The radius in which to cast Purge and Storm Bolt on units.
+    /// </summary>
     public float Radius { get; init; }
+    
+    /// <summary>
+    /// The visual effect to create at the center of the cast location.
+    /// </summary>
     public string Effect { get; init; } = string.Empty;
 
-    private group TempGroup = CreateGroup();
+    private readonly group _tempGroup = CreateGroup();
 
+    /// <inheritdoc />
     public ElectricStrike(int id) : base(id)
     {
     }
 
+    /// <inheritdoc />
     public override void OnCast(unit caster, unit target, Point targetPoint)
     {
       try
       {
-        GroupEnumUnitsInRange(TempGroup, targetPoint.X, targetPoint.Y, Radius, null);
+        GroupEnumUnitsInRange(_tempGroup, targetPoint.X, targetPoint.Y, Radius, null);
         EffectSystem.Add(AddSpecialEffect(Effect, targetPoint.X, targetPoint.Y));
         while (true)
         {
-          unit u = FirstOfGroup(TempGroup);
+          var u = FirstOfGroup(_tempGroup);
           if (u == null)
           {
             return;
@@ -41,7 +69,7 @@ namespace AzerothWarsCSharp.MacroTools.Spells
             DummyCast.DummyCastUnit(GetOwningPlayer(caster), PurgeId, PurgeOrder, 1, u);
           }
           
-          GroupRemoveUnit(TempGroup, u);
+          GroupRemoveUnit(_tempGroup, u);
         }
       }
       catch (Exception ex)
