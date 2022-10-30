ace58d699cab322be2103aaf200e83d6658050f9
Zakary Boven <zakaryboven@gmail.com>
2022-10-30 15:10:03 +1300

    Added documentation to Stomp.cs

--- src/AzerothWarsCSharp.MacroTools/Spells/Stomp.cs
+++ src/AzerothWarsCSharp.MacroTools/Spells/Stomp.cs
@@ -5,15 +5,45 @@ using static War3Api.Common;
 
 namespace AzerothWarsCSharp.MacroTools.Spells
 {
-  public class Stomp : Spell
+  /// <summary>
+  /// Deals damage and casts a dummy spell at each unit in the area.
+  /// </summary>
+  public sealed class Stomp : Spell
   {
+    /// <summary>
+    /// How much base damage to deal.
+    /// </summary>
     public float DamageBase { get; init; } = 20;
+    
+    /// <summary>
+    /// How much additional damage to deal per level.
+    /// </summary>
     public float DamageLevel { get; init; } = 30;
+    
+    /// <summary>
+    /// How long the stun should last.
+    /// </summary>
     public int DurationBase { get; init; }
+    
+    /// <summary>
+    /// How much extra duration the stun should get per level.
+    /// </summary>
     public int DurationLevel { get; init; }
+    
+    /// <summary>
+    /// The radius in which units are affected.
+    /// </summary>
     public float Radius { get; init; } = 300;
+    
+    /// <summary>
+    /// The ID of an ability that stuns units. Should be based on Storm Bolt.
+    /// </summary>
     public int StunAbilityId { get; init; }
-    public string StunOrderString { get; init; }
+
+    /// <summary>
+    /// The order string for <see cref="StunAbilityId"/>.
+    /// </summary>
+    public string StunOrderString { get; init; } = "";
     
     private void DamageUnit(unit caster, widget target)
     {
@@ -24,13 +54,14 @@ namespace AzerothWarsCSharp.MacroTools.Spells
     private void StunUnit(unit caster, unit target)
     {
       var duration = DurationBase + DurationLevel * GetAbilityLevel(caster);
-      if (StunAbilityId == 0 || StunOrderString == null || duration <= 0)
+      if (StunAbilityId == 0 || StunOrderString == "" || duration <= 0)
       {
         return;
       }
       DummyCast.CastOnUnit(caster, StunAbilityId, StunOrderString, duration, target);
     }
 
+    /// <inheritdoc />
     public override void OnCast(unit caster, unit target, Point targetPoint)
     {
       var tempGroup = new GroupWrapper();
@@ -47,6 +78,7 @@ namespace AzerothWarsCSharp.MacroTools.Spells
         GetUnitY(caster)));
     }
 
+    /// <inheritdoc />
     public Stomp(int id) : base(id)
     {
       
