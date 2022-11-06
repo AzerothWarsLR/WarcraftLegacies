1e74e31196c147f60be7eaea39f004adda64f1e5
TracyTheBoy <93258020+TracyTheBoy@users.noreply.github.com>
2022-11-06 23:49:46 +0100

    When Scourge leaves the game, the Helm of Domination is dropped to an accessible location (#137)

--- src/WarcraftLegacies.Source/Mechanics/Scourge/HelmOfDominationDropsWhenScourgeLeaves.cs
+++ src/WarcraftLegacies.Source/Mechanics/Scourge/HelmOfDominationDropsWhenScourgeLeaves.cs
@@ -1,22 +1,31 @@
-// using WarcraftLegacies.Source.Legends;
-// using WarcraftLegacies.Source.Setup;
-// using WarcraftLegacies.Source.Setup.FactionSetup;
-//
-// using static War3Api.Common;  using static MacroTools.GeneralHelpers;
-// {
-//   public class HelmOfDominationDropsWhenScourgeLeaves{
-//
-//     private static void Actions( ){
-//       if (GetTriggerFaction() == ScourgeSetup.FactionScourge && ArtifactSetup.ArtifactHelmofdomination.OwningUnit == LegendScourge.LegendLichking.Unit){
-//         SetItemPosition(ArtifactSetup.ArtifactHelmofdomination.Item, GetRectCenterX(gg_rct_LichKing), GetRectCenterY(gg_rct_LichKing));
-//       }
-//     }
-//
-//     public static void Setup( ){
-//       trigger trig = CreateTrigger();
-//       OnFactionGameLeave.register(trig);
-//       TriggerAddAction(trig,  Actions);
-//     }
-//
-//   }
-// }
+using WarcraftLegacies.Source.Setup.Legends;
+using WarcraftLegacies.Source.Setup;
+using WarcraftLegacies.Source.Setup.FactionSetup;
+using MacroTools.FactionSystem;
+using MacroTools.Extensions;
+using WCSharp.Shared.Data;
+
+/// <summary>
+///  When Scourge leaves the game, drop the Helm of Domination to an easily accessible position since the default position is hard to see.
+/// </summary>
+public class HelmOfDominationDropsWhenScourgeLeaves
+{
+    public static void Setup()
+    {
+        Faction.GameLeave += Faction_GameLeave;
+    }
+
+    private static void Faction_GameLeave(object? sender, Faction leavingFaction)
+    {
+        if (ArtifactSetup.ArtifactHelmofdomination == null || LegendScourge.LegendLichking?.Unit == null)
+            return;
+        
+        if (leavingFaction == ScourgeSetup.Scourge && ArtifactSetup.ArtifactHelmofdomination.OwningUnit == LegendScourge.LegendLichking.Unit)
+        {
+            var lichKingPosition = LegendScourge.LegendLichking.Unit.GetPosition();
+            LegendScourge.LegendLichking.Unit.DropAllItems();
+            ArtifactSetup.ArtifactHelmofdomination.Item.SetPosition(new Point(lichKingPosition.X - 55, lichKingPosition.Y + 30));// Move item in front of the LK 
+        }
+    }
+}
+
