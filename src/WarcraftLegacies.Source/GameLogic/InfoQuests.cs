f264f76a2b92d282917f49d269da8cc6f746fc89
Zakary Boven <zakaryboven@gmail.com>
2022-11-07 21:41:22 +1300

    Updated credits

--- src/WarcraftLegacies.Source/GameLogic/InfoQuests.cs
+++ src/WarcraftLegacies.Source/GameLogic/InfoQuests.cs
@@ -34,9 +34,9 @@ Camera:
          QuestSetIconPath(creditsQuest, @"ReplaceableTextures\CommandButtons\BTNManual3.blp");
          QuestSetDescription(creditsQuest, @"An enormous thank you to...
 
-The Azeroth wars past developers: Augur, Crusader793, Bhaal_Spawn., EagleMan, Avrion, Railen, Thurr, Rhemar, SteakonSpear, Talinn, LuneLune. Dave_Rolf and Richardik.
-
-And the artists who have created the swathe of custom icons and models that we use: Ujimasa Hojo. General Frank, Shiv, Malvodion, nGy, UgoUgo, JetFangInferno, -Grendel,  SpasMaster, Eagle XI, Mythic, Hamsta, assasin_lord, Hayate, R.A.N.G.I.T.,  Stefan.K,  Eusira, 00110000, Pyritie, Sunchips, Dojo, PeeKay, nhocklanhox6, dhguardianes, HappyTauren, HerrDave, Shyster, takakenji, JesusHipster, Boogles, Kwaliti, Tauer, WhiteDeath, and Mechanical Man");
+Previous Azeroth Wars developers: Augur, Crusader793, Bhaal_Spawn., EagleMan, Avrion, Railen, Thurr, Rhemar, SteakonSpear, Talinn, LuneLune, Dave_Rolf and Richardik,
+And to our code contributors, Naowsx and Tracy,
+and to the artists who have created the swathe of custom icons and models that we use: Ujimasa Hojo, General Frank, Shiv, Malvodion, nGy, UgoUgo, JetFangInferno, -Grendel,  SpasMaster, Eagle XI, Mythic, Hamsta, assasin_lord, Hayate, R.A.N.G.I.T.,  Stefan.K,  Eusira, 00110000, Pyritie, Sunchips, Dojo, PeeKay, nhocklanhox6, dhguardianes, HappyTauren, HerrDave, Shyster, takakenji, JesusHipster, Boogles, Kwaliti, Tauer, WhiteDeath, and Mechanical Man");
       }
    }
 }
