c24c8a55b8d775df01824ddc107ab95d8f12acc2
Zakary Boven <zakaryboven@gmail.com>
2022-11-10 12:15:59 +1300

    scaffolding

---
+++ src/MacroTools/DemonGateSystem/DemonGateManager.cs
@@ -0,0 +1,10 @@
+﻿namespace MacroTools.DemonGateSystem
+{
+  public static class DemonGateManager
+  {
+    public static void Register(DemonGateType demonGateType)
+    {
+      
+    }
+  }
+}
---
+++ src/MacroTools/DemonGateSystem/DemonGateType.cs
@@ -0,0 +1,10 @@
+﻿namespace MacroTools.DemonGateSystem
+{
+  public sealed class DemonGateType
+  {
+    public DemonGateType(int gateUnitTypeId, int demonUnitTypeId, float spawnInterval, int spawnCount)
+    {
+      
+    }
+  }
+}
