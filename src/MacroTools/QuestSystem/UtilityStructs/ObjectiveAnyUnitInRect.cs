fb52dbea2cc8682db06edf925317cc825058f9f6
Zakary Boven <zakaryboven@gmail.com>
2022-11-12 12:41:55 +1300

    Made ObjectiveAnyUnitInRect more efficient (#272)

--- src/MacroTools/QuestSystem/UtilityStructs/ObjectiveAnyUnitInRect.cs
+++ src/MacroTools/QuestSystem/UtilityStructs/ObjectiveAnyUnitInRect.cs
@@ -1,3 +1,4 @@
+using MacroTools.Extensions;
 using MacroTools.FactionSystem;
 using MacroTools.Wrappers;
 using WCSharp.Shared.Data;
@@ -5,28 +6,53 @@ using static War3Api.Common;
 
 namespace MacroTools.QuestSystem.UtilityStructs
 {
+  /// <summary>
+  /// Completed when an eligible player moves a unit into the specified <see cref="Rectangle"/>.
+  /// </summary>
   public sealed class ObjectiveAnyUnitInRect : Objective
   {
-    private static readonly trigger EntersRectTrig = CreateTrigger();
-    private static readonly trigger ExitsRectTrig = CreateTrigger();
     private readonly bool _heroOnly;
     private readonly rect _targetRect;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ObjectiveAnyUnitInRect"/> class.
+    /// </summary>
+    /// <param name="targetRect">Where the player has to move a unit.</param>
+    /// <param name="rectName">A user-friendly name for the area.</param>
+    /// <param name="heroOnly">If true, can only be completed with a hero.</param>
     public ObjectiveAnyUnitInRect(Rectangle targetRect, string rectName, bool heroOnly)
     {
       _targetRect = targetRect.Rect;
-      if (heroOnly)
-        Description = "You have a hero at " + rectName;
-      else
-        Description = "You have a unit at " + rectName;
-      region target = RectToRegion(_targetRect);
+      Description = heroOnly ? $"You have a hero at {rectName}" : $"You have a unit at {rectName}";
       _heroOnly = heroOnly;
       DisplaysPosition = true;
-      TriggerRegisterEnterRegion(EntersRectTrig, target, null);
-      TriggerRegisterLeaveRegion(ExitsRectTrig, target, null);
       PingPath = "MinimapQuestTurnIn";
+
+      CreateTrigger()
+        .RegisterEnterRegion(targetRect)
+        .AddAction(() =>
+        {
+          var triggerUnit = GetTriggerUnit();
+          if (IsValidUnitInRect())
+          {
+            TriggerUnit = triggerUnit;
+            Progress = QuestProgress.Complete;
+          }
+          else
+          {
+            Progress = QuestProgress.Incomplete;
+          }
+        });
+      CreateTrigger()
+        .RegisterLeaveRegion(targetRect)
+        .AddAction(() =>
+        {
+          if (!IsValidUnitInRect()) 
+            Progress = QuestProgress.Incomplete;
+        });
     }
 
+    /// <inheritdoc />
     public override Point Position => new(GetRectCenterX(_targetRect), GetRectCenterY(_targetRect));
 
     /// <summary>
@@ -34,47 +60,18 @@ namespace MacroTools.QuestSystem.UtilityStructs
     /// </summary>
     public unit? TriggerUnit { get; private set; }
 
-    private static region RectToRegion(rect whichRect)
-    {
-      region rectRegion = CreateRegion();
-      RegionAddRect(rectRegion, whichRect);
-      return rectRegion;
-    }
-
     private bool IsValidUnitInRect()
     {
       foreach (var unit in new GroupWrapper().EnumUnitsInRect(_targetRect).EmptyToList())
-        if (EligibleFactions.Contains(GetOwningPlayer(unit)) && UnitAlive(unit) &&
+        if (EligibleFactions.Contains(unit.OwningPlayer()) && unit.IsAlive() &&
             (IsUnitType(unit, UNIT_TYPE_HERO) || !_heroOnly))
           return true;
       return false;
     }
 
-    private void OnRegionEnter()
-    {
-      var triggerUnit = GetTriggerUnit();
-      if (EligibleFactions.Contains(GetOwningPlayer(triggerUnit)) && UnitAlive(triggerUnit) &&
-        (IsUnitType(triggerUnit, UNIT_TYPE_HERO) || !_heroOnly) || IsValidUnitInRect())
-      {
-        TriggerUnit = triggerUnit;
-        Progress = QuestProgress.Complete;
-      }
-      else
-      {
-        Progress = QuestProgress.Incomplete;
-      }
-    }
-
-    private void OnRegionExit()
-    {
-      Progress = IsValidUnitInRect() ? QuestProgress.Complete : QuestProgress.Incomplete;
-    }
-
     internal override void OnAdd(Faction whichFaction)
     {
       Progress = IsValidUnitInRect() ? QuestProgress.Complete : QuestProgress.Incomplete;
-      TriggerAddAction(EntersRectTrig, OnRegionEnter);
-      TriggerAddAction(ExitsRectTrig, OnRegionExit);
     }
   }
 }
