fb25ebf394c6e0d639189fdcf8d915f04bc174f5
Zakary Boven <zakaryboven@gmail.com>
2022-11-12 12:58:48 +1300

    Fix ObjectiveLegendNotPermanentlyDead.cs

--- src/MacroTools/QuestSystem/UtilityStructs/ObjectiveLegendNotPermanentlyDead.cs
+++ src/MacroTools/QuestSystem/UtilityStructs/ObjectiveLegendNotPermanentlyDead.cs
@@ -4,17 +4,23 @@ using static War3Api.Common;
 
 namespace MacroTools.QuestSystem.UtilityStructs
 {
+  /// <summary>
+  /// Completed while a specific <see cref="Legend"/> is not permanently dead, failed otherwise.
+  /// </summary>
   public sealed class ObjectiveLegendNotPermanentlyDead : Objective
   {
     private readonly Legend _target;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ObjectiveLegendNotPermanentlyDead"/> class.
+    /// </summary>
+    /// <param name="target">The <see cref="Legend"/> to check death status for.</param>
     public ObjectiveLegendNotPermanentlyDead(Legend target)
     {
       _target = target;
-      if (IsUnitType(target.Unit, UNIT_TYPE_STRUCTURE))
-        Description = target.Name + " is intact";
-      else
-        Description = target.Name + " is alive";
+      Description = IsUnitType(target.Unit, UNIT_TYPE_STRUCTURE)
+        ? $"{target.Name} is intact"
+        : $"{target.Name} is alive";
 
       target.PermanentlyDied += OnAnyUnitDeath;
       PlayerUnitEvents.Register(PlayerUnitEvent.UnitTypeFinishesTraining, OnAnyUnitTrain);
@@ -27,7 +33,7 @@ namespace MacroTools.QuestSystem.UtilityStructs
 
     private void OnAnyUnitTrain()
     {
-      if (!ProgressLocked && _target.UnitType == GetUnitTypeId(GetTriggerUnit()))
+      if (!ProgressLocked && _target.Unit == GetTrainedUnit())
         Progress = QuestProgress.Complete;
     }
 
