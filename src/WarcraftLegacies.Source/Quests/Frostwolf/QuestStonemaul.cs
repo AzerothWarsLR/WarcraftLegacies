1ced242eb2d636b0610e708cc7954c3c1199cb8a
Zakary Boven <zakaryboven@gmail.com>
2022-11-12 16:02:43 +1300

    Cleaned up QuestStonemaul.cs

--- src/WarcraftLegacies.Source/Quests/Frostwolf/QuestStonemaul.cs
+++ src/WarcraftLegacies.Source/Quests/Frostwolf/QuestStonemaul.cs
@@ -5,52 +5,56 @@ using MacroTools.Extensions;
 using MacroTools.FactionSystem;
 using MacroTools.QuestSystem;
 using MacroTools.QuestSystem.UtilityStructs;
-using MacroTools.Wrappers;
 using WCSharp.Shared.Data;
 using static War3Api.Common;
 
-
 namespace WarcraftLegacies.Source.Quests.Frostwolf
 {
+  /// <summary>
+  /// Quest for capturing Stonemaul Keep.
+  /// </summary>
   public sealed class QuestStonemaul : QuestData
   {
-    private readonly List<unit> _rescueUnits = new();
+    private readonly List<unit> _rescueUnits;
 
+    //Todo: bad flavour
+    /// <inheritdoc />
+    protected override string CompletionPopup =>
+      "Stonemaul has been liberated, and its military is now free to assist the Frostwolf Clan.";
+
+    /// <inheritdoc />
+    protected override string RewardDescription => "Control of all units in Stonemaul and 3000 lumber";
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuestStonemaul"/> class.
+    /// </summary>
+    /// <param name="rescueRect">Units in this area will be made invulnerable, then rescued when the quest is completed.</param>
     public QuestStonemaul(Rectangle rescueRect) : base("The Chieftain's Challenge",
       "The Ogres of Stonemaul follow the strongest, slay the Chieftain to gain control of the base.",
       "ReplaceableTextures\\CommandButtons\\BTNOneHeadedOgre.blp")
     {
-      AddObjective(new ObjectiveKillUnit(PreplacedUnitSystem.GetUnit(FourCC("noga")))); //Korgall
-      AddObjective(new ObjectiveControlPoint(ControlPointManager.GetFromUnitType(FourCC("n022"))));
+      AddObjective(new ObjectiveKillUnit(PreplacedUnitSystem.GetUnit(Constants.UNIT_NOGA_STONEMAUL_WARCHIEF_KOR_GALL)));
+      AddObjective(new ObjectiveControlPoint(ControlPointManager.GetFromUnitType(Constants.UNIT_N022_STONEMAUL_20GOLD_MIN)));
       AddObjective(new ObjectiveExpire(1505));
       AddObjective(new ObjectiveSelfExists());
-      ResearchId = FourCC("R03S");
+      ResearchId = Constants.UPGRADE_R03S_QUEST_COMPLETED_THE_CHIEFTAIN_S_CHALLENGE_FROSTWOLF;
 
-      foreach (var unit in new GroupWrapper().EnumUnitsInRect(rescueRect.Rect).EmptyToList())
-        if (GetOwningPlayer(unit) == Player(PLAYER_NEUTRAL_PASSIVE))
-        {
-          SetUnitInvulnerable(unit, true);
-          _rescueUnits.Add(unit);
-        }
+      _rescueUnits = rescueRect.PrepareUnitsForRescue(Player(PLAYER_NEUTRAL_PASSIVE));
 
       Required = true;
     }
-
-    //Todo: bad flavour
-    protected override string CompletionPopup =>
-      "Stonemaul has been liberated, and its military is now free to assist the Frostwolf Clan.";
-
-    protected override string RewardDescription => "Control of all units in Stonemaul and 3000 lumber";
-
+    
+    /// <inheritdoc />
     protected override void OnFail(Faction completingFaction)
     {
-      foreach (var unit in _rescueUnits) unit.Rescue(Player(PLAYER_NEUTRAL_AGGRESSIVE));
+      Player(PLAYER_NEUTRAL_AGGRESSIVE).RescueGroup(_rescueUnits);
     }
 
+    /// <inheritdoc />
     protected override void OnComplete(Faction completingFaction)
     {
-      foreach (var unit in _rescueUnits) unit.Rescue(completingFaction.Player);
-      completingFaction.Player.AdjustPlayerState(PLAYER_STATE_RESOURCE_LUMBER, 3000);
+      completingFaction.Player?.RescueGroup(_rescueUnits);
+      completingFaction.Player?.AdjustPlayerState(PLAYER_STATE_RESOURCE_LUMBER, 3000);
     }
   }
 }
