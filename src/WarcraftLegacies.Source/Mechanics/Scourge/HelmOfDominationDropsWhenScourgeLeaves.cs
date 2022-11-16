373f41e09fb498b3c78c973b649ad77d46edbf4b
Zakary Boven <zakaryboven@gmail.com>
2022-11-16 23:10:24 +1300

    Helm of Domination drops nicely when Frozen Throne dies

--- src/WarcraftLegacies.Source/Mechanics/Scourge/HelmOfDominationDropsWhenScourgeLeaves.cs
+++ src/WarcraftLegacies.Source/Mechanics/Scourge/HelmOfDominationDropsWhenScourgeLeaves.cs
@@ -1,31 +1,61 @@
-using WarcraftLegacies.Source.Setup.Legends;
+using MacroTools;
+using MacroTools.Extensions;
+using MacroTools.FactionSystem;
 using WarcraftLegacies.Source.Setup;
 using WarcraftLegacies.Source.Setup.FactionSetup;
-using MacroTools.FactionSystem;
-using MacroTools.Extensions;
+using WarcraftLegacies.Source.Setup.Legends;
 using WCSharp.Shared.Data;
 
-/// <summary>
-///  When Scourge leaves the game, drop the Helm of Domination to an easily accessible position since the default position is hard to see.
-/// </summary>
-public class HelmOfDominationDropsWhenScourgeLeaves
+namespace WarcraftLegacies.Source.Mechanics.Scourge
 {
+  /// <summary>
+  ///  When Scourge leaves the game, drop the Helm of Domination to an easily accessible position since the default position is hard to see.
+  /// </summary>
+  public static class HelmOfDominationDropsWhenScourgeLeaves
+  {
+    /// <summary>
+    /// Sets up <see cref="HelmOfDominationDropsWhenScourgeLeaves"/>.
+    /// </summary>
     public static void Setup()
     {
-        Faction.GameLeave += Faction_GameLeave;
+      if (LegendScourge.LegendLichking == null)
+        throw new SystemNotInitializedException(nameof(LegendScourge));
+      if (ScourgeSetup.Scourge == null)
+        throw new SystemNotInitializedException(nameof(ScourgeSetup));
+      LegendScourge.LegendLichking.PermanentlyDied += OnLichKingDeath;
+      ScourgeSetup.Scourge.LeftGame += OnScourgeLeaveGame;
     }
 
-    private static void Faction_GameLeave(object? sender, Faction leavingFaction)
+    private static void OnLichKingDeath(object? sender, Legend legend)
     {
-        if (ArtifactSetup.ArtifactHelmofdomination == null || LegendScourge.LegendLichking?.Unit == null)
-            return;
-        
-        if (leavingFaction == ScourgeSetup.Scourge && ArtifactSetup.ArtifactHelmofdomination.OwningUnit == LegendScourge.LegendLichking.Unit)
-        {
-            var lichKingPosition = LegendScourge.LegendLichking.Unit.GetPosition();
-            LegendScourge.LegendLichking.Unit.DropAllItems();
-            ArtifactSetup.ArtifactHelmofdomination.Item.SetPosition(new Point(lichKingPosition.X - 55, lichKingPosition.Y + 30));// Move item in front of the LK 
-        }
+      CheckHelmOfDomination();
+      UnregisterEvents();
     }
-}
 
+    private static void OnScourgeLeaveGame(object? sender, Faction faction)
+    {
+      CheckHelmOfDomination();
+      UnregisterEvents();
+    }
+
+    private static void UnregisterEvents()
+    {
+      if (LegendScourge.LegendLichking != null) 
+        LegendScourge.LegendLichking.PermanentlyDied -= OnLichKingDeath;
+      if (ScourgeSetup.Scourge != null) 
+        ScourgeSetup.Scourge.LeftGame -= OnScourgeLeaveGame;
+    }
+    
+    private static void CheckHelmOfDomination()
+    {
+      if (ArtifactSetup.ArtifactHelmofdomination == null
+          || LegendScourge.LegendLichking?.Unit == null)
+        return;
+
+      var lichKingPosition = LegendScourge.LegendLichking.Unit.GetPosition();
+      LegendScourge.LegendLichking.Unit.DropAllItems();
+      ArtifactSetup.ArtifactHelmofdomination.Item.SetPosition(new Point(lichKingPosition.X - 55,
+        lichKingPosition.Y + 30));
+    }
+  }
+}
