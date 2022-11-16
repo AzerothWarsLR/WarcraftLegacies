95449ddd2cb3351f42eb1d960224d0b11acf3ea7
Zakary Boven <zakaryboven@gmail.com>
2022-11-16 17:26:28 +1300

    Corrected the Dire Wolf target for the quest "Shadows of Silverpine Forest"

--- src/WarcraftLegacies.Source/Quests/Dalaran/QuestShadowfang.cs
+++ src/WarcraftLegacies.Source/Quests/Dalaran/QuestShadowfang.cs
@@ -4,46 +4,47 @@ using MacroTools.Extensions;
 using MacroTools.FactionSystem;
 using MacroTools.QuestSystem;
 using MacroTools.QuestSystem.UtilityStructs;
-using MacroTools.Wrappers;
 using WCSharp.Shared.Data;
 using static War3Api.Common;
 
 namespace WarcraftLegacies.Source.Quests.Dalaran
 {
+  /// <summary>
+  /// Kill a wolf to unlock a base.
+  /// </summary>
   public sealed class QuestShadowfang : QuestData
   {
-    private readonly List<unit> _rescueUnits = new();
+    private readonly List<unit> _rescueUnits;
 
-    public QuestShadowfang(Rectangle rescueRect, unit DirewolfToKill) : base("Shadows of Silverspine Forest",
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuestShadowfang"/> class.
+    /// </summary>
+    /// <param name="rescueRect">Units in this region will start invulnerable and be rescued when the quest completes.</param>
+    /// <param name="direwolfToKill">This unit must be killed to complete the quest.</param>
+    public QuestShadowfang(Rectangle rescueRect, unit direwolfToKill) : base("Shadows of Silverspine Forest",
       "The woods of Silverspine are unsafe for travellers, they need to be investigated",
       "ReplaceableTextures\\CommandButtons\\BTNworgen.blp")
     {
-      AddObjective(new ObjectiveKillUnit(DirewolfToKill)); //Worgen
-      AddObjective(new ObjectiveControlPoint(ControlPointManager.GetFromUnitType(FourCC("n01D"))));
+      AddObjective(new ObjectiveKillUnit(direwolfToKill));
+      AddObjective(new ObjectiveControlPoint(ControlPointManager.GetFromUnitType(Constants.UNIT_N01D_SILVERPINE_FOREST_15GOLD_MIN)));
       AddObjective(new ObjectiveExpire(1444));
       AddObjective(new ObjectiveSelfExists());
-
-      foreach (var unit in new GroupWrapper().EnumUnitsInRect(rescueRect).EmptyToList())
-        if (GetOwningPlayer(unit) == Player(PLAYER_NEUTRAL_PASSIVE))
-        {
-          SetUnitInvulnerable(unit, true);
-          _rescueUnits.Add(unit);
-        }
+      _rescueUnits = rescueRect.PrepareUnitsForRescue(RectangleExtensions.RescueMode.HideUnits);
     }
 
+    /// <inheritdoc />
     protected override string CompletionPopup =>
       "Shadowfang has been liberated, and its military is now free to assist Dalaran.";
 
+    /// <inheritdoc />
     protected override string RewardDescription => "Control of all units in Shadowfang";
 
-    protected override void OnFail(Faction completingFaction)
-    {
-      foreach (var unit in _rescueUnits) unit.Rescue(Player(PLAYER_NEUTRAL_AGGRESSIVE));
-    }
+    /// <inheritdoc />
+    protected override void OnFail(Faction completingFaction) => 
+      Player(PLAYER_NEUTRAL_AGGRESSIVE).RescueGroup(_rescueUnits);
 
-    protected override void OnComplete(Faction completingFaction)
-    {
-      foreach (var unit in _rescueUnits) unit.Rescue(completingFaction.Player);
-    }
+    /// <inheritdoc />
+    protected override void OnComplete(Faction completingFaction) =>
+      completingFaction.Player?.RescueGroup(_rescueUnits);
   }
 }
