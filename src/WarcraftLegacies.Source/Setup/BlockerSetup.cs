17019ca5f479e042b81c9a9e726851efaa65f79e
Zakary Boven <zakaryboven@gmail.com>
2022-11-17 18:49:09 +1300

    Revert "Remove BlockerSetup.cs"
    This reverts commit 6425ab6281e2627ec8008e97f0190a099947bcd1.

---
+++ src/WarcraftLegacies.Source/Setup/BlockerSetup.cs
@@ -0,0 +1,37 @@
+﻿using static War3Api.Common;
+
+namespace WarcraftLegacies.Source.Setup
+{
+  /// <summary>
+  /// Hides all pathing blockers on the map.
+  /// </summary>
+  public static class BlockerSetup
+  {
+    public static void Setup()
+    {
+      var pathingBlockers = new[]
+      {
+        FourCC("YTlb"),
+        FourCC("YTlc"),
+        FourCC("B009"),
+        FourCC("B00A"),
+        FourCC("YTab"),
+        FourCC("YTac"),
+        FourCC("YTfb"),
+        FourCC("YTfc"),
+        FourCC("YTpb"),
+        FourCC("YTpc"),
+        FourCC("B011"),
+        FourCC("OTip")
+      };
+
+      EnumDestructablesInRect(WCSharp.Shared.Data.Rectangle.WorldBounds.Rect, null, () =>
+      {
+        foreach (var pathingBlocker in pathingBlockers)
+        {
+          if (GetDestructableTypeId(GetEnumDestructable()) == pathingBlocker) RemoveDestructable(GetEnumDestructable());
+        }
+      });
+    }
+  }
+}
