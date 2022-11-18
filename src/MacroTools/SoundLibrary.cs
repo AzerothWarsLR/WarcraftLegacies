edd12402c338809c7304a84dac536c223d1ffd77
Zakary Boven <zakaryboven@gmail.com>
2022-11-18 16:48:47 +1300

    SoundLibrary starts populated instead of lazy loading

--- src/MacroTools/SoundLibrary.cs
+++ src/MacroTools/SoundLibrary.cs
@@ -4,22 +4,16 @@ namespace MacroTools
 {
   public static class SoundLibrary
   {
-    private static sound? _rescue;
-    public static sound Rescue => _rescue ??= CreateSoundFromLabel("Rescue", false, false, false, 10000, 10000);
-    
-    private static sound? _hint;
-    public static sound Hint => _hint ??= CreateSoundFromLabel("Hint", false, false, false, 10000, 10000);
-    
-    private static sound? _completed;
-    public static sound Completed => _completed ??= CreateSoundFromLabel("QuestCompleted", false, false, false, 10000, 10000);
+    public static sound Rescue { get; } = CreateSoundFromLabel("Rescue", false, false, false, 10000, 10000);
 
-    private static sound? _failed;
-    public static sound Failed => _failed ??= CreateSoundFromLabel("QuestFailed", false, false, false, 10000, 10000);
-    
-    private static sound? _warning;
-    public static sound Warning => _warning ??= CreateSoundFromLabel("Warning", false, false, false, 10000, 10000);
-    
-    private static sound? _discovered;
-    public static sound Discovered => _discovered ??= CreateSoundFromLabel("QuestNew", false, false, false, 10000, 10000);
+    public static sound Hint { get; } = CreateSoundFromLabel("Hint", false, false, false, 10000, 10000);
+
+    public static sound Completed { get; } = CreateSoundFromLabel("QuestCompleted", false, false, false, 10000, 10000);
+
+    public static sound Failed { get; } = CreateSoundFromLabel("QuestFailed", false, false, false, 10000, 10000);
+
+    public static sound Warning { get; } = CreateSoundFromLabel("Warning", false, false, false, 10000, 10000);
+
+    public static sound Discovered { get; } = CreateSoundFromLabel("QuestNew", false, false, false, 10000, 10000);
   }
 }
