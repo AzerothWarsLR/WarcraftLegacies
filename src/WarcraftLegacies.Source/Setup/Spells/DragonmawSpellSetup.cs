9cccb0ecdf28bfa119e3d75da4dcaaeca0149d35
Zakary Boven <zakaryboven@gmail.com>
2022-11-20 21:52:20 +1300

    Implement a passive ability which causes the unit to gain mana when dealing damage (#406)

--- src/WarcraftLegacies.Source/Setup/Spells/DragonmawSpellSetup.cs
+++ src/WarcraftLegacies.Source/Setup/Spells/DragonmawSpellSetup.cs
@@ -1,4 +1,6 @@
-﻿using MacroTools.PassiveAbilitySystem;
+﻿using MacroTools;
+using MacroTools.PassiveAbilities;
+using MacroTools.PassiveAbilitySystem;
 using MacroTools.Spells;
 using MacroTools.SpellSystem;
 
@@ -26,6 +28,16 @@ namespace WarcraftLegacies.Source.Setup.Spells
       SpellSystem.Register(new UnholyArmor(Constants.ABILITY_A0HG_UNHOLY_ARMOR_DRAGONMAW_GORFAX)
       {
         PercentageDamage = 0.06f
+      });
+
+      PassiveAbilityManager.Register(new RestoreManaFromDamage(Constants.UNIT_O06F_BLOOD_WARRIOR_DRAGONMAW, Constants.ABILITY_A0FR_TRANSFUSION_GORFAX)
+      {
+        ManaPerDamage = new LeveledAbilityField<float>
+        {
+          Base = 0.8f,
+          PerLevel = 0.2f
+        },
+        Effect = "Abilities\\Spells\\Undead\\ReplenishMana\\SpiritTouchTarget.mdl"
       });
     }
   }
