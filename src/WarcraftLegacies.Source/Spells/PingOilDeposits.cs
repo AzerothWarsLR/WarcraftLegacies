9967526728f0442210fee0ed5a7c8549acdf8725
Zakary Boven <zakaryboven@gmail.com>
2022-11-20 17:23:54 +1300

    Add a way for Goblins to ping oil deposits (#404)

---
+++ src/WarcraftLegacies.Source/Spells/PingOilDeposits.cs
@@ -0,0 +1,40 @@
+﻿using MacroTools.Extensions;
+using MacroTools.FactionSystem;
+using MacroTools.Powers;
+using MacroTools.SpellSystem;
+using WCSharp.Shared.Data;
+using static War3Api.Common;
+
+namespace WarcraftLegacies.Source.Spells
+{
+  /// <summary>
+  /// Pings all oil deposits.
+  /// </summary>
+  public sealed class PingOilDeposits : Spell
+  {
+    /// <summary>
+    /// How long the ping should last.
+    /// </summary>
+    public float Duration { get; init; }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PingOilDeposits"/> class.
+    /// </summary>
+    /// <param name="id"><inheritdoc /></param>
+    public PingOilDeposits(int id) : base(id)
+    {
+    }
+
+    /// <inheritdoc />
+    public override void OnCast(unit caster, unit target, Point targetPoint)
+    {
+      var oilPower = GetTriggerUnit().OwningPlayer().GetFaction()?.GetPowerByType<OilPower>();
+      if (oilPower == null) 
+        return;
+      foreach (var oilDeposit in oilPower.GetAllOilPools())
+      {
+        PingMinimapEx(oilDeposit.Position.X, oilDeposit.Position.Y, Duration, 255, 255, 255, false);
+      }
+    }
+  }
+}
