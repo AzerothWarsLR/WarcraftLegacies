126741efbe772745b5215628b85ac452689519b5
Zakary Boven <zakaryboven@gmail.com>
2022-11-21 22:22:21 +1300

    Remove the ObjectFactory project

--- src/WarcraftLegacies.ObjectFactory/IObjectFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using War3Api.Object;
-
-namespace WarcraftLegacies.ObjectFactory
-{
-  public interface IObjectFactory<T>
-  {
-    public T Generate(string newRawCode, ObjectDatabase objectDatabase);
-  }
-}
--- src/WarcraftLegacies.ObjectFactory/MovementSoundDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WarcraftLegacies.ObjectFactory
-{
-  public class MovementSoundDetails
-  {
-    public string Path { get; set; }
-    public int LoopingFadeInRate { get; set; }
-    public int LoopingFadeOutRate { get; set; }
-  }
-}
