adfcace65f2fc59e0cd0100a887d46a0987c34c4
Zakary Boven <zakaryboven@gmail.com>
2022-11-21 18:04:32 +1300

    Resized the Artifact menu

--- src/MacroTools/BookSystem/ArtifactSystem/ArtifactPage.cs
+++ src/MacroTools/BookSystem/ArtifactSystem/ArtifactPage.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using MacroTools.ArtifactSystem;
 
-namespace MacroTools.BookSystem.Artifacts
+namespace MacroTools.BookSystem.ArtifactSystem
 {
   /// <summary>
   ///   Represents a limited number of Artifacts in a rectangular grid.
   /// </summary>
   public sealed class ArtifactPage : Page
   {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArtifactPage"/> class.
+    /// </summary>
     public ArtifactPage()
     {
       Rows = 3;
-      Columns = 4;
+      Columns = 5;
       YOffsetTop = 0.025f;
       YOffsetBot = 0.05f;
     }
