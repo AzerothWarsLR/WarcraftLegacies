76ff708e77b38de1fb0718d6048b4ff25e3bb521
Zakary Boven <zakaryboven@gmail.com>
2022-11-23 22:27:51 +1300

    Fixed Forsaken building corruption income

--- src/WarcraftLegacies.Source/Spells/CorruptBuildingSpell.cs
+++ src/WarcraftLegacies.Source/Spells/CorruptBuildingSpell.cs
@@ -12,18 +12,21 @@ namespace WarcraftLegacies.Source.Spells
   /// </summary>
   public sealed class CorruptBuildingSpell : Spell
   {
-    private readonly int _bonusIncome;
+    private readonly int _bonusGoldIncome;
+    private readonly int _bonusLumberIncome;
     private readonly int _bonusHealth;
 
     /// <summary>
     /// Initializes an instance of the <see cref="CorruptBuildingSpell"/> class.
     /// </summary>
     /// <param name="id"><inheritdoc /></param>
-    /// <param name="bonusIncome">The casting player gets this much extra income as long as the corrupted building is alive.</param>
+    /// <param name="bonusGoldIncome">The casting player gets this much extra gold income as long as the corrupted building is alive.</param>
+    /// <param name="bonusLumberIncome">The casting player gets this much extra lumber income as long as the corrupted building is alive.</param>
     /// <param name="bonusHealth">The corrupted building gets this much extra hit points.</param>
-    public CorruptBuildingSpell(int id, int bonusIncome, int bonusHealth) : base(id)
+    public CorruptBuildingSpell(int id, int bonusGoldIncome, int bonusLumberIncome, int bonusHealth) : base(id)
     {
-      _bonusIncome = bonusIncome;
+      _bonusGoldIncome = bonusGoldIncome;
+      _bonusLumberIncome = bonusLumberIncome;
       _bonusHealth = bonusHealth;
     }
     
@@ -31,7 +34,7 @@ namespace WarcraftLegacies.Source.Spells
     public override void OnCast(unit caster, unit target, Point targetPoint)
     {
       SetUnitState(target, UNIT_STATE_LIFE, GetUnitState(target, UNIT_STATE_MAX_LIFE));
-      var buff = new CorruptBuildingBuff(caster, target, _bonusIncome, _bonusHealth)
+      var buff = new CorruptBuildingBuff(caster, target, _bonusGoldIncome, _bonusLumberIncome, _bonusHealth)
       {
         Duration = float.MaxValue
       };
