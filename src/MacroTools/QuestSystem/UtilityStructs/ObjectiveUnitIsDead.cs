06a2d16563234b7d98ea30f6e5faa555f5326b08
Mark Ferguson <mjferguson404@gmail.com>
2022-11-24 10:24:42 +0000

    Stonemaul fix
    Added ObjectiveUnitIsDead

---
+++ src/MacroTools/QuestSystem/UtilityStructs/ObjectiveUnitIsDead.cs
@@ -0,0 +1,43 @@
+﻿using MacroTools.Extensions;
+using MacroTools.FactionSystem;
+using WCSharp.Events;
+using WCSharp.Shared.Data;
+using static War3Api.Common;
+
+namespace MacroTools.QuestSystem.UtilityStructs
+{
+  public class ObjectiveUnitIsDead : Objective
+  {
+    public ObjectiveUnitIsDead(unit unitToKill)
+    {
+      trigger trig = CreateTrigger();
+      TriggerRegisterUnitEvent(trig, unitToKill, EVENT_UNIT_DEATH);
+      TriggerAddAction(trig, OnUnitDeath);
+      Target = unitToKill;
+      TargetWidget = Target;
+      InitializeDescription();
+      DisplaysPosition = IsUnitType(Target, UNIT_TYPE_STRUCTURE) ||
+                         GetOwningPlayer(Target) == Player(PLAYER_NEUTRAL_AGGRESSIVE);
+    }
+
+    public override Point Position => new(GetUnitX(Target), GetUnitY(Target));
+
+    public unit Target { get; }
+
+    private void OnUnitDeath()
+    {
+      Progress = QuestProgress.Complete;
+    }
+
+    private void InitializeDescription()
+    {
+      if (IsUnitType(Target, UNIT_TYPE_STRUCTURE) || IsUnitType(Target, UNIT_TYPE_ANCIENT))
+      {
+        Description = GetUnitName(Target) + "has been destroyed.";
+        return;
+      }
+
+      Description = GetUnitName(Target) + "is dead.";
+    }
+  }
+}
