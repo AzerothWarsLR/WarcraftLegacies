622174614fd5c77d8b63582d3e6fea1e06552499
Zakary Boven <zakaryboven@gmail.com>
2022-11-25 22:35:27 +1300

    Cleaned up QuestADestinyofFlameandSorrow.cs

--- src/WarcraftLegacies.Source/Quests/Naga/QuestADestinyofFlameandSorrow.cs
+++ src/WarcraftLegacies.Source/Quests/Naga/QuestADestinyofFlameandSorrow.cs
@@ -1,16 +1,19 @@
-﻿using MacroTools.Extensions;
-using MacroTools.FactionSystem;
-using MacroTools.QuestSystem;
+﻿using MacroTools.QuestSystem;
 using MacroTools.QuestSystem.UtilityStructs;
 using WarcraftLegacies.Source.Setup;
 using WarcraftLegacies.Source.Setup.Legends;
 using static War3Api.Common;
 
-
 namespace WarcraftLegacies.Source.Quests.Naga
 {
+  /// <summary>
+  /// Illidan gets the Skull of Gul'dan and does cool stuff with it.
+  /// </summary>
   public sealed class QuestADestinyofFlameandSorrow : QuestData
   {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuestADestinyofFlameandSorrow"/> class.
+    /// </summary>
     public QuestADestinyofFlameandSorrow() : base("A Destiny of Flame and Sorrow",
       "The Skull of Gul'dan calls for Illidan. If he could harness it's power, he would gain immeasureable power",
       "ReplaceableTextures\\CommandButtons\\BTNGuldanSkull.blp")
@@ -19,9 +22,11 @@ namespace WarcraftLegacies.Source.Quests.Naga
       ResearchId = FourCC("R095");
     }
 
+    /// <inheritdoc/>
     protected override string CompletionPopup =>
       "The Skull of Gul'dan has given Illidan Demonic power beyond measure";
 
+    /// <inheritdoc/>
     protected override string RewardDescription =>
       "You can now cast Metamorphosis with Illidan";
   }
