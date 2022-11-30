dbb1e526ad56aaf4e92229ae6d66f7e9fc61f84e
Zakary Boven <zakaryboven@gmail.com>
2022-12-01 02:04:12 +1300

    made a good start

---
+++ src/MacroTools/Missiles/DefensiveOrbMissile.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using MacroTools.Extensions;
+using MacroTools.Wrappers;
+using WCSharp.Events;
+using static War3Api.Common;
+using WCSharp.Missiles;
+using WCSharp.Shared.Data;
+
+namespace MacroTools.Missiles
+{
+  /// <summary>
+  /// Arcane orb that rotates around the caster.
+  /// </summary>
+  public sealed class DefensiveOrbMissile : OrbitalMissile
+  {
+    /// <summary>
+    /// The radius in which orbs do damage.
+    /// </summary>
+    public float DamageRadius { get; init; }
+    
+    /// <summary>
+    /// The amount of damage the orb deals.
+    /// </summary>
+    public float Damage { get; init; }
+    
+    /// <inheritdoc />
+    public DefensiveOrbMissile(unit caster, unit target) : base(caster, target)
+    {
+      TargetImpactZ = 50;
+      EffectScale = 1.0f;
+      Interval = PeriodicEvents.SYSTEM_INTERVAL;
+    }
+
+    /// <inheritdoc />
+    public override void OnCollision(unit unit)
+    {
+      if (IsUnitEnemy(unit, CastingPlayer)) 
+        Detonate();
+    }
+
+    private bool IsValidTarget(unit whichUnit)
+    {
+      return IsUnitAlly(whichUnit, Caster.OwningPlayer()) || !UnitAlive(whichUnit) ||
+             BlzIsUnitInvulnerable(whichUnit) || IsUnitType(whichUnit, UNIT_TYPE_STRUCTURE) ||
+             IsUnitType(whichUnit, UNIT_TYPE_ANCIENT);
+    }
+    
+    private void Detonate()
+    {
+      foreach (var unit in new GroupWrapper().EnumUnitsInRange(new Point(MissileX, MissileY), DamageRadius).EmptyToList().Where(IsValidTarget))
+        unit.TakeDamage(Caster, Damage, attackType: ATTACK_TYPE_MAGIC, damageType: DAMAGE_TYPE_MAGIC);
+    }
+  }
+}
---
+++ src/MacroTools/PassiveAbilities/DefensiveOrbs.cs
@@ -0,0 +1,63 @@
+﻿using MacroTools.Missiles;
+using MacroTools.PassiveAbilitySystem;
+using WCSharp.Missiles;
+using static War3Api.Common;
+
+namespace MacroTools.PassiveAbilities
+{
+  /// <summary>
+  /// The hero gains arcane orbs when casting abilities, which then collide with enemy units to deal damage.
+  /// </summary>
+  public sealed class DefensiveOrbs : PassiveAbility
+  {
+    /// <summary>
+    /// The radius in which the orbs orbit.
+    /// </summary>
+    public float OrbitRadius { get; init; }
+    
+    /// <summary>
+    /// The time it takes for the orbs to complete a full rotation.
+    /// </summary>
+    public float OrbitalPeriod { get; init; }
+
+    /// <summary>
+    /// The appearance of the orbs.
+    /// </summary>
+    public string OrbEffectPath { get; init; } = "";
+
+    /// <summary>
+    /// The amount of damage dealt by each orb when they explode.
+    /// </summary>
+    public LeveledAbilityField<float> Damage { get; init; } = new();
+
+    /// <summary>
+    /// The radius in which orbs deal damage when they explode.
+    /// </summary>
+    public LeveledAbilityField<float> Radius { get; init; } = new();
+
+    private readonly int _abilityTypeId;
+    
+    /// <inheritdoc />
+    public DefensiveOrbs(int unitTypeId, int abilityTypeId) : base(unitTypeId)
+    {
+      _abilityTypeId = abilityTypeId;
+    }
+
+    /// <inheritdoc />
+    public override void OnSpellFinish()
+    {
+      var caster = GetTriggerUnit();
+      var abilityLevel = GetUnitAbilityLevel(caster, _abilityTypeId);
+      var newOrb = new DefensiveOrbMissile(caster, caster)
+      {
+        OrbitalPeriod = OrbitalPeriod,
+        CollisionRadius = Radius.Base + Radius.PerLevel * abilityLevel,
+        EffectString = OrbEffectPath,
+        Range = OrbitRadius,
+        Damage = Damage.Base + Damage.PerLevel * abilityLevel,
+        DamageRadius = Radius.Base + Radius.PerLevel * abilityLevel
+      };
+      MissileSystem.Add(newOrb);
+    }
+  }
+}
