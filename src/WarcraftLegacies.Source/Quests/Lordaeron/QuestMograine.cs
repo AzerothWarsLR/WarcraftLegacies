216be616e2c5e213409994427c3196b17d4660b7
Zakary Boven <zakaryboven@gmail.com>
2022-12-01 13:58:07 +1300

    Cleaned up QuestMograine.cs

--- src/WarcraftLegacies.Source/Quests/Lordaeron/QuestMograine.cs
+++ src/WarcraftLegacies.Source/Quests/Lordaeron/QuestMograine.cs
@@ -9,8 +9,7 @@ namespace WarcraftLegacies.Source.Quests.Lordaeron
   /// </summary>
   public sealed class QuestMograine : QuestData
   {
-    private static readonly int AltarId = Constants.UNIT_HALT_ALTAR_OF_KINGS;
-    private static readonly int HeroId = Constants.UNIT_H01J_THE_ASHBRINGER_LORDAERON;
+    private const int AltarId = Constants.UNIT_HALT_ALTAR_OF_KINGS;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="QuestMograine"/> class.
@@ -26,10 +25,9 @@ namespace WarcraftLegacies.Source.Quests.Lordaeron
     /// <inheritdoc/>
     protected override string CompletionPopup =>
       "With the Lich King eliminated, the Kingdom of Lordaeron is free of its greatest threat. Alexandros Mograine gains recognition as a champion of the war, and prepares himself to aid Lordaeron in future conflicts in a greater capacity.";
+
     /// <inheritdoc/>
     protected override string RewardDescription =>
-      "You can summon Alexandros Mograine from the " + GetObjectName(AltarId);
-
-
+      $"You can summon Alexandros Mograine from the {GetObjectName(AltarId)}";
   }
 }
