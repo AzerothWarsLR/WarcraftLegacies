9225854356837fc24648ec0ab7451a7adb43ccea
Zakary Boven <zakaryboven@gmail.com>
2022-12-10 02:04:54 +1300

    registered stuff

---
+++ src/WarcraftLegacies.Source/Setup/Spells/FelHordeSpellSetup.cs
@@ -0,0 +1,55 @@
+﻿using MacroTools.PassiveAbilities;
+using MacroTools.PassiveAbilitySystem;
+using MacroTools.Spells;
+using MacroTools.SpellSystem;
+
+namespace WarcraftLegacies.Source.Setup.Spells
+{
+  /// <summary>
+  /// Responsible for setting up all Fel Horde <see cref="Spell"/>s and <see cref="PassiveAbility"/>s.
+  /// </summary>
+  public static class FelHordeSpellSetup
+  {
+    /// <summary>
+    /// Sets up <see cref="FelHordeSpellSetup"/>.
+    /// </summary>
+    public static void Setup()
+    {
+      var ascendance = new Ascendance(Constants.ABILITY_AEME_ASCENDANCE_TEAL_ZULUHED)
+      {
+        DurationBase = 15,
+        DurationLevel = 15,
+        HealBase = 50,
+        HealLevel = 100,
+        Radius = 600,
+        AbilitiesToRemove = new[]
+        {
+          Constants.ABILITY_HEAL_HEALING_WAVE_TEAL_ZULUHED,
+          Constants.ABILITY_A0B4_BLOODLUST_TOTEM_TEAL_ZULUHED,
+          Constants.ABILITY_AHAB_BRILLIANCE_AURA_ZULUHED_JAINA_MALFURION_VOL_JIN_JERGOSH,
+          Constants.ABILITY_AEME_ASCENDANCE_TEAL_ZULUHED
+        }
+      };
+      SpellSystem.Register(ascendance);
+      
+      var warStompKazzak = new Stomp(Constants.ABILITY_A0AW_WAR_STOMP_BLUE_DOOM_GUARD_TEAL_KAZZAK)
+      {
+        Radius = 300,
+        DamageBase = 25,
+        DurationBase = 3,
+        StunAbilityId = Constants.ABILITY_A0WN_STUN_UNIT_DUMMY,
+        StunOrderString = "thunderbolt",
+        SpecialEffect = @"Abilities\Spells\Orc\WarStomp\WarStompCaster.mdl"
+      };
+      SpellSystem.Register(warStompKazzak);
+
+      PassiveAbilityManager.Register(new SummonUnitOnDeath(Constants.UNIT_NCHG_FEL_GRUNT_FEL_HORDE)
+      {
+        Duration = 40,
+        SummonUnitTypeId = Constants.UNIT_N00O_SKELETAL_GRUNT_FEL_HORDE,
+        SummonCount = 1,
+        SpecialEffectPath = @"Abilities\Spells\Undead\RaiseSkeletonWarrior\RaiseSkeleton.mdl"
+      });
+    }
+  }
+}
--- src/WarcraftLegacies.Source/Setup/SpellsSetup.cs
+++ src/WarcraftLegacies.Source/Setup/SpellsSetup.cs
@@ -22,17 +22,6 @@ namespace WarcraftLegacies.Source.Setup
       };
       SpellSystem.Register(warStompImmoltar);
 
-      var warStompKazzak = new Stomp(Constants.ABILITY_A0AW_WAR_STOMP_BLUE_DOOM_GUARD_TEAL_KAZZAK)
-      {
-        Radius = 300,
-        DamageBase = 25,
-        DurationBase = 3,
-        StunAbilityId = FourCC("A0WN"),
-        StunOrderString = "thunderbolt",
-        SpecialEffect = @"Abilities\Spells\Orc\WarStomp\WarStompCaster.mdl"
-      };
-      SpellSystem.Register(warStompKazzak);
-
       var thunderClap = new Stomp(FourCC("A0QC"))
       {
         Radius = 225,
@@ -194,23 +183,6 @@ namespace WarcraftLegacies.Source.Setup
       var overclock = new CooldownReset(Constants.ABILITY_A0RA_OVERCLOCK_GAZLOWEE);
       SpellSystem.Register(overclock);
 
-      var ascendance = new Ascendance(Constants.ABILITY_AEME_ASCENDANCE_TEAL_ZULUHED)
-      {
-        DurationBase = 15,
-        DurationLevel = 15,
-        HealBase = 50,
-        HealLevel = 100,
-        Radius = 600,
-        AbilitiesToRemove = new[]
-        {
-          Constants.ABILITY_HEAL_HEALING_WAVE_TEAL_ZULUHED,
-          Constants.ABILITY_A0B4_BLOODLUST_TOTEM_TEAL_ZULUHED,
-          Constants.ABILITY_AHAB_BRILLIANCE_AURA_ZULUHED_JAINA_MALFURION_VOL_JIN_JERGOSH,
-          Constants.ABILITY_AEME_ASCENDANCE_TEAL_ZULUHED
-        }
-      };
-      SpellSystem.Register(ascendance);
-
       var unholyArmor = new UnholyArmor(Constants.ABILITY_A0F8_UNHOLY_ARMOR_FEL_HORDE_FEL_WARLOCK)
       {
         PercentageDamage = 0.06f
@@ -257,6 +229,7 @@ namespace WarcraftLegacies.Source.Setup
       ForsakenSpellSetup.Setup();
       DragonmawSpellSetup.Setup();
       IllidariSpellSetup.Setup();
+      FelHordeSpellSetup.Setup();
     }
   }
 }
