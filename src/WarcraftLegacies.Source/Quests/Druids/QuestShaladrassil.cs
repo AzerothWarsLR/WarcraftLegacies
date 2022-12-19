7b1ac253fac527375a3e38c65e679ef68b0e9745
Zakary Boven <zakaryboven@gmail.com>
2022-12-20 00:34:36 +1300

    QuestShaladrassil displays a popup when it grants its Power reward

--- src/WarcraftLegacies.Source/Quests/Druids/QuestShaladrassil.cs
+++ src/WarcraftLegacies.Source/Quests/Druids/QuestShaladrassil.cs
@@ -1,4 +1,5 @@
-﻿using MacroTools.FactionSystem;
+﻿using MacroTools.Extensions;
+using MacroTools.FactionSystem;
 using MacroTools.QuestSystem;
 using MacroTools.QuestSystem.UtilityStructs;
 using WarcraftLegacies.Source.Powers;
@@ -28,11 +29,13 @@ namespace WarcraftLegacies.Source.Quests.Druids
     /// <inheritdoc />
     protected override void OnComplete(Faction completingFaction)
     {
-      completingFaction.AddPower(new ShaladrassilsBlessing(LegendNeutral.Shaladrassil.Unit,
+      var power = new ShaladrassilsBlessing(LegendNeutral.Shaladrassil.Unit,
         Constants.UNIT_EFON_TREANT_DRUIDS_SUMMONED, 60, 12, 50)
       {
         IconName = "TreeOfEternity"
-      });
+      };
+      completingFaction.AddPower(power);
+      completingFaction.Player?.DisplayPowerAcquired(power);
     }
   }
 }
