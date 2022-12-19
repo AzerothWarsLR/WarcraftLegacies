65fe3c57189e2063017bff47fae333851a1de941
Zakary Boven <zakaryboven@gmail.com>
2022-12-19 16:50:54 +1300

    registered quests

---
+++ src/WarcraftLegacies.Source/Setup/QuestSetup/IllidariQuestSetup.cs
@@ -0,0 +1,14 @@
+﻿using WarcraftLegacies.Source.Quests;
+using WarcraftLegacies.Source.Setup.FactionSetup;
+
+namespace WarcraftLegacies.Source.Setup.QuestSetup
+{
+  public static class NagaQuestSetup
+  {
+    public static void Setup(ArtifactSetup artifactSetup)
+    {
+      var illidari = IllidariSetup.Illidari;
+      illidari.AddQuest(new QuestBlackrookHold());
+    }
+  }
+}
