ac43307ef22df82647ef298bee6c47cc8e9f16dc
TracyTheBoy <Tristan.Schraepler@gmail.com>
2022-12-20 07:36:13 +0100

    2 new objectives and 2 Draenai Quests for #638
    Added an objective to bring a unit to full health
    Added an objective to build something inside a specific area
    Added a draenai quest to repair the exodar
    Added a quest to establish a base on azuremyst isles

---
+++ src/MacroTools/QuestSystem/UtilityStructs/ObjectiveBuildInRect.cs
@@ -0,0 +1,84 @@
+﻿using MacroTools.Extensions;
+using System;
+using WCSharp.Events;
+using WCSharp.Shared.Data;
+using static War3Api.Common;
+
+namespace MacroTools.QuestSystem.UtilityStructs
+{
+  /// <summary>
+  /// Completed when an eligible player completes construction on a building in the specified <see cref="Rectangle"/>.
+  /// </summary>
+  public class ObjectiveBuildInRect : Objective
+  {
+    private readonly int _objectId;
+    private readonly Rectangle _targetRect;
+    private int _currentBuildCount;
+    private readonly int _targetBuildCount;
+    private string _rectName;
+
+    private int CurrentBuildCount
+    {
+      set
+      {
+        _currentBuildCount = value;
+        Description = $"Build {GetObjectName(_objectId)} ( {I2S(_currentBuildCount)} / {I2S(_targetBuildCount)} ) at {_rectName}";
+      }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ObjectiveBuildInRect"/> class.  /// 
+    /// </summary>
+    /// <param name="targetRect"></param>
+    /// <param name="rectName"></param>
+    /// <param name="objectId"></param>
+    /// <param name="count"></param>
+    public ObjectiveBuildInRect(Rectangle targetRect, string rectName, int objectId, int count = 1)
+    {
+      _rectName = rectName;
+      _targetBuildCount = count;
+      _targetRect = targetRect;
+      _objectId = objectId;
+      DisplaysPosition = true;
+      CurrentBuildCount = 0;
+      PingPath = "MinimapQuestTurnIn";
+      PlayerUnitEvents.Register(UnitTypeEvent.Dies, OnDeath, objectId);
+      CreateTrigger()
+        .RegisterEnterRegion(targetRect)
+        .AddAction(() =>
+        {
+          var triggerUnit = GetTriggerUnit();
+          if (!IsUnitValid(triggerUnit))
+            return;
+          CurrentBuildCount = _currentBuildCount + 1;
+          if (_currentBuildCount == _targetBuildCount)
+          {
+            Progress = QuestProgress.Complete;
+          }
+        });
+    }
+
+    private void OnDeath()
+    {
+      if (Progress == QuestProgress.Complete)
+        return;
+
+      var triggerUnit = GetTriggerUnit();
+      if (!IsUnitValid(triggerUnit))
+        return;
+      var point = triggerUnit.GetPosition();
+      if (point.X > _targetRect.Left && point.X < _targetRect.Right && point.Y > _targetRect.Bottom && point.Y < _targetRect.Top)
+      {
+        CurrentBuildCount = _currentBuildCount - 1;
+        Progress = QuestProgress.Incomplete;
+      }
+    }
+
+    /// <inheritdoc />
+    public override Point Position => new(GetRectCenterX(_targetRect.Rect), GetRectCenterY(_targetRect.Rect));
+
+    private bool IsUnitValid(unit whichUnit) =>
+      EligibleFactions.Contains(whichUnit.OwningPlayer()) &&
+      (IsUnitType(whichUnit, UNIT_TYPE_STRUCTURE) && whichUnit.GetTypeId() == _objectId);
+  }
+}
---
+++ src/MacroTools/QuestSystem/UtilityStructs/ObjectiveUnitReachesFullHealth.cs
@@ -0,0 +1,51 @@
+﻿using MacroTools.Extensions;
+using MacroTools.FactionSystem;
+using WCSharp.Shared.Data;
+using static War3Api.Common;
+
+namespace MacroTools.QuestSystem.UtilityStructs
+{
+  /// <summary>
+  /// An objective that completes when a unit reaches full health and fails if it dies
+  /// </summary>
+  public class ObjectiveUnitReachesFullHealth : Objective
+  {
+    private unit _objectiveUnit { get; }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ObjectiveUnitReachesFullHealth"/> class.
+    /// </summary>
+    /// <param name="objectiveUnit">The unit that must be brought to full hp for this objective to complete</param>
+    public ObjectiveUnitReachesFullHealth(unit objectiveUnit)
+    {
+      _objectiveUnit = objectiveUnit;
+      TargetWidget = objectiveUnit;
+      Description = $"{GetUnitName(objectiveUnit)} brought to full health";
+      DisplaysPosition = IsUnitType(objectiveUnit, UNIT_TYPE_STRUCTURE);
+      CreateTrigger()
+        .RegisterUnitEvent(objectiveUnit, EVENT_UNIT_SELECTED)
+        .AddAction(() =>
+        {
+          if (GetUnitState(_objectiveUnit, UNIT_STATE_LIFE) == GetUnitState(_objectiveUnit, UNIT_STATE_MAX_LIFE))
+            Progress = QuestProgress.Complete;
+        });
+      CreateTrigger()
+        .RegisterUnitEvent(objectiveUnit, EVENT_UNIT_DESELECTED)
+        .AddAction(() =>
+        {
+          if (GetUnitState(_objectiveUnit, UNIT_STATE_LIFE) == GetUnitState(_objectiveUnit, UNIT_STATE_MAX_LIFE))
+            Progress = QuestProgress.Complete;
+        });
+    }
+
+    /// <inheritdoc/>
+    public override Point Position => new(GetUnitX(_objectiveUnit), GetUnitY(_objectiveUnit));
+
+    internal override void OnAdd(Faction faction)
+    {
+      if (!UnitAlive(_objectiveUnit))
+        Progress = QuestProgress.Failed;
+      else if (GetUnitState(_objectiveUnit, UNIT_STATE_LIFE) == GetUnitState(_objectiveUnit, UNIT_STATE_MAX_LIFE))
+        Progress = QuestProgress.Complete;
+    }
+  }
+}
