97f3b63583f3c97821f4b73cd84a598e1f3980b8
Zakary Boven <zakaryboven@gmail.com>
2022-12-20 23:56:38 +1300

    add example to lordaeron

--- src/WarcraftLegacies.Source/Setup/FactionSetup/LordaeronSetup.cs
+++ src/WarcraftLegacies.Source/Setup/FactionSetup/LordaeronSetup.cs
@@ -18,6 +18,7 @@ namespace WarcraftLegacies.Source.Setup.FactionSetup
         StartingLumber = 500,
         UndefeatedResearch = FourCC("R05M"),
         CinematicMusic = "Comradeship",
+        ControlPointDefenderUnitTypeId = Constants.UNIT_H03F_CONTROL_POINT_DEFENDER_SHARED,
         IntroText = @"You are playing as the royal |cff4242ebKingdom of Lordaeron|r.
 
 You start the game with the largest territory, but the Plague of Undeath is coming.
