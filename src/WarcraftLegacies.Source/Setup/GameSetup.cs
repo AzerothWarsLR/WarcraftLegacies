74681374389303cdbc7d2ecb58826a263be7f204
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2022-12-23 18:55:58 -0500

    Update GameSetup.cs

--- src/WarcraftLegacies.Source/Setup/GameSetup.cs
+++ src/WarcraftLegacies.Source/Setup/GameSetup.cs
@@ -35,8 +35,8 @@ namespace WarcraftLegacies.Source.Setup
         ControlLevelSettings = new ControlLevelSettings
         {
           DefaultDefenderUnitTypeId = Constants.UNIT_H03W_CONTROL_POINT_DEFENDER_LORDAERON,
-          DamageBase = 15,
-          DamagePerControlLevel = 3,
+          DamageBase = 8,
+          DamagePerControlLevel = 2,
           ArmorPerControlLevel = 1,
           HitPointsPerControlLevel = 100,
           ControlLevelMaximum = 20
