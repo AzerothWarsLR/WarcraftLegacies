3536468fdd4f14d51ef8dd8645642acfeb4ae448
Zakary Boven <zakaryboven@gmail.com>
2023-01-02 23:03:25 +1300

    Units inside the Exodar start the game hidden

--- src/WarcraftLegacies.Source/Quests/Draenei/QuestRepairExodarHull.cs
+++ src/WarcraftLegacies.Source/Quests/Draenei/QuestRepairExodarHull.cs
@@ -14,13 +14,14 @@ namespace WarcraftLegacies.Source.Quests.Draenei
   /// </summary>
   public class QuestRepairExodarHull : QuestData
   {
-    private QuestData _questToFailOnFail { get; }
+    private readonly QuestData _questToFailOnFail;
+    private readonly List<unit> _rescueUnits;
 
     /// <summary>
-    /// 
+    /// Initializes a new instance of the <see cref="QuestRepairExodarHull"/> class.
     /// </summary>
-    /// <param name="rescueRect"></param>
-    /// <param name="questToFailOnFail">This quest will fail upon failing <see cref="QuestRepairExodarHull"/></param>
+    /// <param name="rescueRect">Units in this area start invulnerable and are rescued when the quest is completed.</param>
+    /// <param name="questToFailOnFail">This quest will fail upon failing <see cref="QuestRepairExodarHull"/>.</param>
     public QuestRepairExodarHull(Rectangle rescueRect, QuestData questToFailOnFail) : base("A New Home",
       "After the disastrous voyage through the Twisting Nether, the Exodar crash-landed on Azuremyst Isle. Its hull must be repaired in order to get inside.",
       "ReplaceableTextures\\CommandButtons\\BTNDraeneiVaultOfRelics.blp")
@@ -30,22 +31,20 @@ namespace WarcraftLegacies.Source.Quests.Draenei
       AddObjective(new ObjectiveUnitReachesFullHealth(LegendDraenei.LegendExodar.Unit));
       AddObjective(new ObjectiveKillAllInArea(new List<Rectangle> { Regions.AzuremystAmbient }, "on Azuremyst Isle"));
       AddObjective(new ObjectiveSelfExists());
-      _rescueUnits = rescueRect.PrepareUnitsForRescue(RescuePreparationMode.HideNonStructures);
+      _rescueUnits = rescueRect.PrepareUnitsForRescue(RescuePreparationMode.HideAll);
       ResearchId = Constants.UPGRADE_R099_QUEST_COMPLETED_A_NEW_HOME; // Change this to current quest;
       _questToFailOnFail = questToFailOnFail;
     }
 
-    private List<unit> _rescueUnits { get; }
-
     /// <inheritdoc/>
-    protected override string CompletionPopup => "The Exodar's hull is repaired. It can now be entered again";
+    protected override string CompletionPopup => "The Exodar's hull is repaired. It can now be entered again.";
 
     /// <inheritdoc/>
     protected override string FailurePopup => "The Exodar is destroyed. It can never be repaired again.";
 
     /// <inheritdoc/>
     protected override string RewardDescription =>
-      "You are now able to enter the Exodar and gain access to everything inside.";
+      "Gain access to the Exodar's interior";
 
     /// <inheritdoc/>
     protected override void OnComplete(Faction whichFaction)
@@ -57,9 +56,6 @@ namespace WarcraftLegacies.Source.Quests.Draenei
     }
 
     /// <inheritdoc/>
-    protected override void OnFail(Faction whichFaction)
-    {
-      _questToFailOnFail.Progress = QuestProgress.Failed;
-    }
+    protected override void OnFail(Faction whichFaction) => _questToFailOnFail.Progress = QuestProgress.Failed;
   }
 }
