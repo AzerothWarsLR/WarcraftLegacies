c2100b1ae702e71f97d50f6482ab418d2d14b80d
Zakary Boven <zakaryboven@gmail.com>
2023-01-05 22:00:45 +1300

    Removed Infusions from SharedFactionConfigSetup.cs as they are actually faction-specific

--- src/WarcraftLegacies.Source/Setup/SharedFactionConfigSetup.cs
+++ src/WarcraftLegacies.Source/Setup/SharedFactionConfigSetup.cs
@@ -16,11 +16,6 @@ namespace WarcraftLegacies.Source.Setup
       {
         faction.ModObjectLimit(Constants.UPGRADE_RHME_PYRITE_FORGED_WEAPONRY_UNIVERSAL_UPGRADE, 8);
         faction.ModObjectLimit(Constants.UPGRADE_RHAR_PYRITE_ARMOR_PLATING_UNIVERSAL_UPGRADE, 8);
-        faction.ModObjectLimit(Constants.UPGRADE_R00K_POWER_INFUSION_4_SHARED, Faction.UNLIMITED);
-        faction.ModObjectLimit(Constants.UPGRADE_R01H_MALORNE_S_INFUSION_DRUIDS, Faction.UNLIMITED);
-        faction.ModObjectLimit(Constants.UPGRADE_R026_ELUNE_S_INFUSION_SENTINELS, Faction.UNLIMITED);
-        faction.ModObjectLimit(Constants.UPGRADE_R02A_CHAOS_INFUSION_SCOURGE_LEGION, Faction.UNLIMITED);
-        faction.ModObjectLimit(Constants.UPGRADE_R023_SPIRITUAL_INFUSION_WARSONG_FROSTWOLF_FEL_HORDE, Faction.UNLIMITED);
         faction.ModObjectLimit(Constants.UPGRADE_R04R_FORTIFIED_HULLS_UNIVERSAL_UPGRADE, Faction.UNLIMITED); //Actually Full Scale Escalation
         faction.ModObjectLimit(Constants.UPGRADE_R00C_IMPROVED_CANNONS_ALL_TEAMS, Faction.UNLIMITED);
         faction.ModObjectLimit(Constants.UPGRADE_R006_FORTIFIED_HULLS_UNIVERSAL_UPGRADE, Faction.UNLIMITED);
