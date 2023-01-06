52a2e305163a097846c072a49ea36e40b86f2bd7
Zakary Boven <zakaryboven@gmail.com>
2023-01-07 01:07:57 +1300

    QuestBlackrock properly allows the quest holder to research Blackrock Unification

--- src/WarcraftLegacies.Source/Quests/Fel Horde/QuestBlackrock.cs
+++ src/WarcraftLegacies.Source/Quests/Fel Horde/QuestBlackrock.cs
@@ -8,17 +8,25 @@ using static War3Api.Common;
 
 namespace WarcraftLegacies.Source.Quests.Fel_Horde
 {
+  /// <summary>
+  /// A <see cref="QuestData"/> that unlocks Blackrock when completed.
+  /// </summary>
   public sealed class QuestBlackrock : QuestData
   {
-    private readonly List<unit> _rescueUnits = new();
+    private readonly List<unit> _rescueUnits;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuestBlackrock"/> class.
+    /// </summary>
+    /// <param name="rescueRect"></param>
+    /// <param name="prequisites"></param>
     public QuestBlackrock(Rectangle rescueRect, IEnumerable<QuestData> prequisites) : base("Blackrock Unification",
       "Make contact with the Blackrock clan and convince them to join Magtheridon",
       "ReplaceableTextures\\CommandButtons\\BTNBlackhand.blp")
     {
       foreach (var prequisite in prequisites) 
         AddObjective(new ObjectiveCompleteQuest(prequisite));
-      AddObjective(new ObjectiveResearch(FourCC("R090"), FourCC("o008")));
+      AddObjective(new ObjectiveResearch(Constants.UPGRADE_R090_ACTIVATE_THE_BLACKROCK_CLAN_FEL, Constants.UNIT_O008_HELLFIRE_CITADEL_FEL_HORDE));
       AddObjective(new ObjectiveExpire(1451));
       AddObjective(new ObjectiveSelfExists());
       ResearchId = Constants.UPGRADE_R03C_QUEST_COMPLETED_BLACKROCK_UNIFICATION;
@@ -27,16 +35,24 @@ namespace WarcraftLegacies.Source.Quests.Fel_Horde
     }
 
     //Todo: bad flavour
+    /// <inheritdoc />
     protected override string CompletionPopup =>
       "Blackrock Citadel has been subjugated, and its military is now free to assist the Fel Horde.";
 
+    /// <inheritdoc />
     protected override string RewardDescription =>
       "Control of all units in Blackrock Citadel and enable Dal'rend Blackhand to be trained at the altar";
 
+    /// <inheritdoc />
     protected override void OnFail(Faction completingFaction) => 
       Player(PLAYER_NEUTRAL_AGGRESSIVE).RescueGroup(_rescueUnits);
 
+    /// <inheritdoc />
     protected override void OnComplete(Faction completingFaction) => 
       completingFaction.Player?.RescueGroup(_rescueUnits);
+
+    /// <inheritdoc />
+    protected override void OnAdd(Faction whichFaction) => 
+      whichFaction.ModObjectLimit(Constants.UPGRADE_R090_ACTIVATE_THE_BLACKROCK_CLAN_FEL, Faction.UNLIMITED);
   }
 }
