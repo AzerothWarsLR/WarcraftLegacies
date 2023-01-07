6fae1be9490238145a0727559127c530a6b51cbd
Zakary Boven <zakaryboven@gmail.com>
2023-01-08 02:11:37 +1300

    QuestArgusControl no longer rescues Legion Nexuses

--- src/WarcraftLegacies.Source/Quests/Legion/QuestArgusControl.cs
+++ src/WarcraftLegacies.Source/Quests/Legion/QuestArgusControl.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections.Generic;
-using MacroTools.ControlPointSystem;
-using MacroTools.Extensions;
-using MacroTools.FactionSystem;
+﻿using MacroTools.ControlPointSystem;
 using MacroTools.ObjectiveSystem.Objectives;
 using MacroTools.QuestSystem;
-using WarcraftLegacies.Source.Setup.Legends;
-using static War3Api.Common;
 
 namespace WarcraftLegacies.Source.Quests.Legion
 {
   public sealed class QuestArgusControl : QuestData
   {
-    private readonly List<unit> _rescueUnits = new();
-
     public QuestArgusControl() : base("Argus Incursion",
       "The planet of Argus is not fully under the control of the Legion. Bring it under control!",
       "ReplaceableTextures\\CommandButtons\\BTNMastersLodge.blp")
@@ -23,17 +16,10 @@ namespace WarcraftLegacies.Source.Quests.Legion
       AddObjective(new ObjectiveSelfExists());
       ResearchId = Constants.UPGRADE_R055_QUEST_COMPLETED_ARGUS_INCURSION;
       Required = true;
-      _rescueUnits.Add(LegendLegion.LegionNexusAlterac.Unit);
-      _rescueUnits.Add(LegendLegion.LegionNexusNorthrend.Unit);
     }
 
     protected override string RewardDescription => "With Argus finally under the Legion's control, the invasion of Azeroth can begin!";
     
     protected override string CompletionPopup => "Enable to research Astral Walk from the Burning Citadel and build the Unholy Reliquary";
-
-    protected override void OnComplete(Faction completingFaction)
-    {
-      completingFaction.Player?.RescueGroup(_rescueUnits);
-    }
   }
 }
