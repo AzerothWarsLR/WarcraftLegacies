77c4b5cd02618236a774c4624b412a26dea1fb02
Zakary Boven <zakaryboven@gmail.com>
2023-01-12 20:34:43 +1300

    Expedition quest for Ironforge (#1045)

--- src/WarcraftLegacies.Source/Setup/QuestSetup/IronforgeQuestSetup.cs
+++ src/WarcraftLegacies.Source/Setup/QuestSetup/IronforgeQuestSetup.cs
@@ -1,6 +1,7 @@
 ﻿using MacroTools;
 using WarcraftLegacies.Source.Quests.Ironforge;
 using WarcraftLegacies.Source.Setup.FactionSetup;
+using static War3Api.Common;
 
 namespace WarcraftLegacies.Source.Setup.QuestSetup
 {
@@ -16,6 +17,17 @@ namespace WarcraftLegacies.Source.Setup.QuestSetup
       ironforge.AddQuest(new QuestGnomeregan(Regions.Gnomergan, preplacedUnitSystem));
       ironforge.AddQuest(new QuestDarkIron(Regions.Shadowforge_City));
       ironforge.AddQuest(new QuestWildhammer());
+
+      var missingArtifacts = new int[]
+      {
+        Constants.ITEM_I012_ASHBRINGER,
+        Constants.ITEM_I01A_DEMON_SOUL,
+        Constants.ITEM_I00F_GLOVES_OF_AHN_QIRAJ,
+        Constants.ITEM_I00Z_THUNDERFURY,
+        Constants.ITEM_I015_XAL_ATATH_BLADE_OF_THE_BLACK_EMPIRE,
+        Constants.ITEM_I01T_FANDRAL_S_FLAMESCYTHE
+      };
+      ironforge.AddQuest(new QuestExpedition(missingArtifacts[GetRandomInt(0, missingArtifacts.Length - 1)]));
     }
   }
 }
