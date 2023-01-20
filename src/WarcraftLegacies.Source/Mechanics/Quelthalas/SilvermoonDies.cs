da2de2527495b422750d2af9e56465de7295d3bb
Zakary Boven <zakaryboven@gmail.com>
2023-01-20 14:18:17 +1300

    fix

--- src/WarcraftLegacies.Source/Mechanics/Quelthalas/SilvermoonDies.cs
+++ src/WarcraftLegacies.Source/Mechanics/Quelthalas/SilvermoonDies.cs
@@ -1,3 +1,4 @@
+using MacroTools.Extensions;
 using WarcraftLegacies.Source.Setup.Legends;
 using static War3Api.Common;
 
@@ -5,16 +6,14 @@ namespace WarcraftLegacies.Source.Mechanics.Quelthalas
 {
   public static class SilvermoonDies
   {
-    private static void Dies()
-    {
-      SetUnitInvulnerable(LegendQuelthalas.LegendSunwell.Unit, false);
-    }
-
-    public static void Setup()
+    public static void Setup(LegendQuelthalas legendQuelthalas)
     {
       var trig = CreateTrigger();
-      TriggerRegisterUnitEvent(trig, LegendQuelthalas.LegendSunwell.Unit, EVENT_UNIT_DEATH);
-      TriggerAddAction(trig, Dies);
+      TriggerRegisterUnitEvent(trig, legendQuelthalas.LegendSunwell.Unit, EVENT_UNIT_DEATH);
+      TriggerAddAction(trig, () =>
+      {
+        legendQuelthalas.LegendSunwell.Unit?.SetInvulnerable(false);
+      });
     }
   }
 }
