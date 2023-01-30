022954efcc889ff86409c8ec0d2123e4579aeb6c
Zakary Boven <zakaryboven@gmail.com>
2023-01-30 20:14:13 +1300

    Updated QuestDarkPortal's flavour and cleaned up unusable functionality

--- src/WarcraftLegacies.Source/Quests/Fel Horde/QuestDarkPortal.cs
+++ src/WarcraftLegacies.Source/Quests/Fel Horde/QuestDarkPortal.cs
@@ -3,6 +3,7 @@ using MacroTools.FactionSystem;
 using MacroTools.ObjectiveSystem.Objectives.FactionBased;
 using MacroTools.ObjectiveSystem.Objectives.TimeBased;
 using MacroTools.QuestSystem;
+using WarcraftLegacies.Source.Setup.FactionSetup;
 using static War3Api.Common;
 
 namespace WarcraftLegacies.Source.Quests.Fel_Horde
@@ -25,8 +26,11 @@ namespace WarcraftLegacies.Source.Quests.Fel_Horde
     /// <param name="outerWaygate1">A Waygate outside outland, next to the Dark Portal.</param>
     /// <param name="outerWaygate2">A Waygate outside outland, next to the Dark Portal.</param>
     /// <param name="outerWaygate3">A Waygate outside outland, next to the Dark Portal.</param>
-    public QuestDarkPortal(unit innerWaygate1, unit innerWaygate2, unit innerWaygate3, unit outerWaygate1, unit outerWaygate2, unit outerWaygate3)
-      : base("The Dark Portal opens", "The Dark Portal has been opened.", "ReplaceableTextures\\CommandButtons\\BTNDarkPortal.blp")
+    public QuestDarkPortal(unit innerWaygate1, unit innerWaygate2, unit innerWaygate3, unit outerWaygate1,
+      unit outerWaygate2, unit outerWaygate3)
+      : base("The Dark Portal",
+        "Following the Second War, the archmage Khadgar and his fellow magi sealed the Dark Portal so that it would never again be used to threaten Azeroth. Little did they know that their magicks were only temporary, and that the portal would open again, in time.",
+        "ReplaceableTextures\\CommandButtons\\BTNDarkPortal.blp")
     {
       _innerWaygate1 = innerWaygate1.Show(false);
       _innerWaygate2 = innerWaygate2.Show(false);
@@ -35,19 +39,31 @@ namespace WarcraftLegacies.Source.Quests.Fel_Horde
       _outerWaygate2 = outerWaygate2.Show(false);
       _outerWaygate3 = outerWaygate3.Show(false);
       AddObjective(new ObjectiveTime(420));
-      AddObjective(new ObjectiveExpire(540));
-      AddObjective(new ObjectiveSelfExists());
       Global = true;
     }
 
     /// <inheritdoc />
-    protected override string RewardFlavour => "The way to Azeroth will open itself to the Fel Horde";
+    protected override string RewardFlavour
+    {
+      get
+      {
+        var flavour = "The Dark Portal, previously thought to have been sealed forever, has been opened once more.";
+        if (StormwindSetup.Stormwind?.ScoreStatus != ScoreStatus.Defeated)
+          flavour +=
+            " The people of Stormwind are about to relive their worst nightmares, as the demonic Fel Horde spills forth from Outland to resume their slaughterous rampage.";
+        else if (IllidariSetup.Illidari?.ScoreStatus != ScoreStatus.Defeated)
+          flavour +=
+            " Illidan's forces brace themselves, ready to visit destruction upon Azeroth in the name of their new master.";
+        else
+          flavour +=
+            " Intrepid explorers may now move freely between the otherwise distant worlds of Azeroth and Outland.";
+        return flavour;
+      }
+    }
 
     /// <inheritdoc />
-    protected override string RewardDescription => "The Dark Portal portals are now open";
-
-    /// <inheritdoc />
-    protected override void OnFail(Faction completingFaction) => OpenPortal();
+    protected override string RewardDescription =>
+      "The Dark Portal can be used to teleport units between the Eastern Kingdoms and Outland";
 
     /// <inheritdoc />
     protected override void OnComplete(Faction completingFaction) => OpenPortal();
