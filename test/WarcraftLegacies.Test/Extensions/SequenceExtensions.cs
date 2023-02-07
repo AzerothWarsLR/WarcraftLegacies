521afae0444a37bc61dd28440080d91258e9de7d
Zakary Boven <zakaryboven@gmail.com>
2023-02-07 23:38:15 +1300

    Corrected token generation

--- test/WarcraftLegacies.Test/Extensions/SequenceExtensions.cs
+++ test/WarcraftLegacies.Test/Extensions/SequenceExtensions.cs
@@ -4,6 +4,6 @@ namespace WarcraftLegacies.Test.Extensions
 {
   public static class SequenceExtensions
   {
-    public static string GetToken(this Sequence sequence) => sequence.Name.ToLower().Trim().Split('-')[0];
+    public static string GetToken(this Sequence sequence) => sequence.Name.ToLower().Trim().Split('-')[0].Split(' ')[0];
   }
 }
