d3124382db0c6d6977c8eb7f73829353e46a0415
Zakary Boven <zakaryboven@gmail.com>
2023-02-11 17:46:48 +1300

    QuestSapphiron is completed even if someone snipes Sapphiron

--- src/MacroTools/ObjectiveSystem/Objectives/UnitBased/ObjectiveUnitIsDead.cs
+++ src/MacroTools/ObjectiveSystem/Objectives/UnitBased/ObjectiveUnitIsDead.cs
@@ -5,23 +5,43 @@ using static War3Api.Common;
 
 namespace MacroTools.ObjectiveSystem.Objectives.UnitBased
 {
+  /// <summary>
+  /// An <see cref="Objective"/> that is completed when a specific unit dies.
+  /// </summary>
   public sealed class ObjectiveUnitIsDead : Objective
   {
+    /// <inheritdoc />
+    public override Point Position => new(GetUnitX(Target), GetUnitY(Target));
+
+    /// <summary>
+    /// The unit that has to die to complete the objective.
+    /// </summary>
+    public unit Target { get; }
+    
+    /// <summary>
+    /// The unit that killed the <see cref="Target"/>.
+    /// </summary>
+    public unit? KillingUnit { get; private set; }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ObjectiveUnitIsDead"/> class.
+    /// </summary>
+    /// <param name="unitToKill"></param>
     public ObjectiveUnitIsDead(unit unitToKill)
     {
       CreateTrigger()
         .RegisterUnitEvent(unitToKill, EVENT_UNIT_DEATH)
-        .AddAction(() => { Progress = QuestProgress.Complete; });
+        .AddAction(() =>
+        {
+          KillingUnit = GetKillingUnit();
+          Progress = QuestProgress.Complete;
+        });
       Target = unitToKill;
       TargetWidget = Target;
       InitializeDescription();
       DisplaysPosition = IsUnitType(Target, UNIT_TYPE_STRUCTURE) ||
                          GetOwningPlayer(Target) == Player(PLAYER_NEUTRAL_AGGRESSIVE);
     }
-
-    public override Point Position => new(GetUnitX(Target), GetUnitY(Target));
-
-    public unit Target { get; }
     
     private void InitializeDescription()
     {
--- src/WarcraftLegacies.Source/Quests/Scourge/QuestSapphiron.cs
+++ src/WarcraftLegacies.Source/Quests/Scourge/QuestSapphiron.cs
@@ -1,4 +1,5 @@
-﻿using MacroTools.FactionSystem;
+﻿using MacroTools.Extensions;
+using MacroTools.FactionSystem;
 using MacroTools.LegendSystem;
 using MacroTools.ObjectiveSystem.Objectives.LegendBased;
 using MacroTools.ObjectiveSystem.Objectives.UnitBased;
@@ -7,31 +8,45 @@ using static War3Api.Common;
 
 namespace WarcraftLegacies.Source.Quests.Scourge
 {
+  /// <inheritdoc/>
   public sealed class QuestSapphiron : QuestData
   {
+    private readonly ObjectiveUnitIsDead _unitIsDeadObjective;
     private const int SapphironId = Constants.UNIT_UBDD_SAPPHIRON_SCOURGE_DEMI;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuestSapphiron"/> class.
+    /// </summary>
+    /// <param name="sapphiron"></param>
+    /// <param name="kelthuzad"></param>
     public QuestSapphiron(unit sapphiron, LegendaryHero kelthuzad) : base("Sapphiron",
       "Kill Sapphiron the Blue Dragon to have Kel'Tuzad reanimate her as a Frost Wyrm. Sapphiron can be found in Northrend.",
       "ReplaceableTextures\\CommandButtons\\BTNFrostWyrm.blp")
     {
-      AddObjective(new ObjectiveKillUnit(sapphiron));
+      _unitIsDeadObjective = new ObjectiveUnitIsDead(sapphiron);
+      AddObjective(_unitIsDeadObjective);
       AddObjective(new ObjectiveControlLegend(kelthuzad, false));
       ResearchId = Constants.UPGRADE_R025_QUEST_COMPLETED_SAPPHIRON_SCOURGE;
     }
-    
+
     /// <inheritdoc/>
     protected override string RewardFlavour =>
       "Sapphiron has been slain, and has been reanimated as a mighty Frost Wyrm under the command of the Scourge.";
 
     /// <inheritdoc/>
-    protected override string RewardDescription => "The demihero Sapphiron and unlocks one of the requirements to train Frost Wyrms";
+    protected override string RewardDescription => "Unlock one of the requirements to train Frost Wyrms. " +
+                                                   "If your team killed Sapphiron, gain her in an undead form; " +
+                                                   $"otherwise, learn to train her from the {GetObjectName(Constants.UNIT_UAOD_ALTAR_OF_DARKNESS)}";
 
     /// <inheritdoc/>
     protected override void OnComplete(Faction completingFaction)
     {
-      CreateUnit(completingFaction.Player, SapphironId, GetUnitX(GetTriggerUnit()), GetUnitY(GetTriggerUnit()),
-        GetUnitFacing(GetTriggerUnit()));
+      var killingPlayer = _unitIsDeadObjective.KillingUnit?.OwningPlayer();
+      if (killingPlayer == null)
+        return;
+      if (completingFaction.Player?.GetTeam()?.Contains(killingPlayer) == true)
+        CreateUnit(completingFaction.Player, SapphironId, GetUnitX(GetTriggerUnit()), GetUnitY(GetTriggerUnit()),
+          GetUnitFacing(GetTriggerUnit()));
     }
   }
 }
