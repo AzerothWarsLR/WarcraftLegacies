daea0dbbc6ef82ea6b428ea6f234ede260efb1c8
TracyTheBoy <Tristan.Schraepler@gmail.com>
2023-02-19 04:34:10 +0100

    replaced four cc functions with constants for control points

--- src/WarcraftLegacies.Source/Setup/ControlPointSetup.cs
+++ src/WarcraftLegacies.Source/Setup/ControlPointSetup.cs
@@ -10,12 +10,12 @@ namespace WarcraftLegacies.Source.Setup
   {
     private static readonly Dictionary<int, int> ControlPointValues = new()
     {
-      {FourCC("B025"), 10},
-      {FourCC("B050"), 15},
-      {FourCC("B051"), 20},
-      {FourCC("B052"), 25},
-      {FourCC("B053"), 30},
-      {FourCC("B054"), 50}
+      {Constants.BUFF_B025_COMMAND_POINT_10_GOLD_PER_MINUTE, 10},
+      {Constants.BUFF_B050_COMMAND_POINT_15_GOLD_PER_MINUTE, 15},
+      {Constants.BUFF_B051_COMMAND_POINT_20_GOLD_PER_MINUTE, 20},
+      {Constants.BUFF_B052_COMMAND_POINT_25_GOLD_PER_MINUTE, 25},
+      {Constants.BUFF_B053_COMMAND_POINT_30_GOLD_PER_MINUTE, 30},
+      {Constants.BUFF_B054_COMMAND_POINT_50_GOLD_PER_MINUTE, 50},
     };
 
     private static void InitializeControlPoint(unit unit)
