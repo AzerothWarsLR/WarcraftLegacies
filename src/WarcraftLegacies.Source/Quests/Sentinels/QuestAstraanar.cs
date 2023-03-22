bba14192b69686c2926b590c26eea33d35d544ff
Technopig1992 <127978232+Technopig1992@users.noreply.github.com>
2023-03-22 00:43:21 +0000

    a'dal q tooltip, and couple typos in quests (#1505)

--- src/WarcraftLegacies.Source/Quests/Sentinels/QuestAstraanar.cs
+++ src/WarcraftLegacies.Source/Quests/Sentinels/QuestAstraanar.cs
@@ -48,7 +48,7 @@ namespace WarcraftLegacies.Source.Quests.Sentinels
       "Auberdine has been reached and has joined the Sentinels in their war effort";
 
     /// <inheritdoc />
-    protected override string RewardDescription => "Control of all units in Astranaar Outpost and Auberdine. Tyrande, Maiev and Naisha will de trainable at Altar";
+    protected override string RewardDescription => "Control of all units in Astranaar Outpost and Auberdine. Tyrande, Maiev and Naisha will be trainable at Altar";
 
     /// <inheritdoc />
     protected override void OnFail(Faction completingFaction)
