4cd60bbbae90ddeb3a5cd61e8c109c8a7150c6de
Technopig1992 <albertfairfield@icloud.com>
2023-04-21 01:23:18 +0100

    Nzoth Units & building faction setup
    Nzoth faction and unit constants added for faction setup - Heroes & CP Defender tower variant still missing

--- src/WarcraftLegacies.Source/Setup/FactionSetup/NzothSetup.cs
+++ src/WarcraftLegacies.Source/Setup/FactionSetup/NzothSetup.cs
@@ -18,47 +18,55 @@ namespace WarcraftLegacies.Source.Setup.FactionSetup
         ControlPointDefenderUnitTypeId = Constants.UNIT_N0B6_CONTROL_POINT_DEFENDER_FROSTWOLF,
       };
 
-      Nzoth.ModObjectLimit(Constants.UNIT_OGRE_GREAT_HALL_FROSTWOLF_T1, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OSTR_STRONGHOLD_FROSTWOLF_T2, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OFRT_FORTRESS_FROSTWOLF_T3, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OALT_ALTAR_OF_STORMS_FROSTWOLF_ALTAR, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OBAR_WAR_CAMP_FROSTWOLF_BARRACKS, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OFOR_WAR_MILL_FROSTWOLF_RESEARCH, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OTTO_TAUREN_TOTEM_FROSTWOLF_SPECIALIST, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OSLD_SPIRIT_LODGE_FROSTWOLF_MAGIC, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OTRB_BURROW_FROSTWOLF_FARM, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OWTW_WATCH_TOWER_FROSTWOLF_TOWER, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_O002_IMPROVED_WATCH_TOWER_FROSTWOLF_TOWER_2, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OVLN_VOODOO_LOUNGE_FROSTWOLF_SHOP, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OSHY_HORDE_PIER_FROSTWOLF_SHIPYARD, Faction.UNLIMITED);
-      Nzoth.ModObjectLimit(Constants.UNIT_OOSC_PACK_KODO_FROSTWOLF, Faction.UNLIMITED);
+      /// All Nzoth buildings listed below - with total limits 
+     
+      Nzoth.ModObjectLimit(Constants.UNIT_N0C2_PILLAR_OF_WAVES_NZOTH_T1, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0C8_MONUMENT_OF_THE_DEEP_NZOTH_T2, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0C9_TEMPLE_OF_TIDES_NZOTH_T3, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0C1_CORAL_BED_NZOTH_FARM, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_H0A4_CORAL_FORGE_NZOTH_RESEARCH, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0C4_SPAWNING_GROUNDS_NZOTH_BARRACKS, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0C7_ROYAL_TEMPLE_NZOTH_MAGIC, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0C3_SHRINE_OF_AZSHARA_NZOTH_SPECIALIST, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N08V_PORTAL_OF_THE_DEEP_NZOTH_SPECIALIST, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0BZ_DEEP_VAULT_NZOTH_SHOP, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0C0_ALTAR_OF_THE_QUEEN_NZOTH_ALTAR, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N08L_TEMPLE_PROTECTOR_TOLVIR_TOWER, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N08N_IMPROVED_TEMPLE_PROTECTOR_TOLVIR_TOWER, Faction.UNLIMITED);
 
-      Nzoth.ModObjectLimit(FourCC("opeo"), Faction.UNLIMITED); //Peon
-      Nzoth.ModObjectLimit(FourCC("ogru"), Faction.UNLIMITED); //Grunt
-      Nzoth.ModObjectLimit(FourCC("otau"), Faction.UNLIMITED); //Tauren
-      Nzoth.ModObjectLimit(FourCC("ohun"), Faction.UNLIMITED); //Troll Headhunter
-      Nzoth.ModObjectLimit(FourCC("ocat"), 6); //Catapult
-      Nzoth.ModObjectLimit(FourCC("otbr"), 12); //Troll Batrider
-      Nzoth.ModObjectLimit(FourCC("odoc"), Faction.UNLIMITED); //Troll Witch Doctor
-      Nzoth.ModObjectLimit(FourCC("oshm"), Faction.UNLIMITED); //Shaman
-      Nzoth.ModObjectLimit(FourCC("ospw"), Faction.UNLIMITED); //Spirit Walker
-      Nzoth.ModObjectLimit(FourCC("o00A"), 6); //Far Seer
-      Nzoth.ModObjectLimit(FourCC("obot"), Faction.UNLIMITED); //Transport Ship
+      /// All Nzoth Units listed below - with total limits 
 
-      //Ship
-      Nzoth.ModObjectLimit(FourCC("obot"), Faction.UNLIMITED); //Transport Ship
-      Nzoth.ModObjectLimit(FourCC("h0AS"), Faction.UNLIMITED); //Scout
-      Nzoth.ModObjectLimit(FourCC("h0AP"), Faction.UNLIMITED); //Frigate
-      Nzoth.ModObjectLimit(FourCC("h0B2"), Faction.UNLIMITED); //Fireship
-      Nzoth.ModObjectLimit(FourCC("h0AY"), Faction.UNLIMITED); //Galley
-      Nzoth.ModObjectLimit(FourCC("h0B5"), Faction.UNLIMITED); //Boarding
-      Nzoth.ModObjectLimit(FourCC("h0BC"), Faction.UNLIMITED); //Juggernaut
-      Nzoth.ModObjectLimit(FourCC("h0AO"), 6); //Bombard
+      Nzoth.ModObjectLimit(Constants.UNIT_N0D7_DEEP_FORAGER_N_ZOTH_WORKER, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0DT_TIDEMISTRESS_NZOTH, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0CA_NAGA_INCURSOR_NZOTH, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0CE_HATCHLING_NZOTH, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0CB_DEEPSEER_NZOTH, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0CD_BLOODKIN_NZOTH, Faction.UNLIMITED);
+      Nzoth.ModObjectLimit(Constants.UNIT_O060_RIPTIDE_DRAKE_NZOTH, 6);
+      Nzoth.ModObjectLimit(Constants.UNIT_U02J_TIDAL_TERROR_NZOTH, 4);
+      Nzoth.ModObjectLimit(Constants.UNIT_H01Q_IMMORTAL_GUARDIAN_NZOTH, 4);
+      Nzoth.ModObjectLimit(Constants.UNIT_N0CO_TRENCH_HYDRA_NZOTH, 2);
+      
 
-      Nzoth.ModObjectLimit(FourCC("Othr"), 1); //Thrall
-      Nzoth.ModObjectLimit(FourCC("Ocbh"), 1); //Cairne
-      Nzoth.ModObjectLimit(FourCC("Orkn"), 1); //Voljin
-      Nzoth.ModObjectLimit(FourCC("Orex"), 1); //Rexxar
+
+      
+      
+
+      /// All Nzoth Ships
+
+      Nzoth.ModObjectLimit(FourCC("ubot"), Faction.UNLIMITED); //Undead Transport Ship
+      Nzoth.ModObjectLimit(FourCC("h0AT"), Faction.UNLIMITED); //Scout
+      Nzoth.ModObjectLimit(FourCC("h0AW"), Faction.UNLIMITED); //Frigate
+      Nzoth.ModObjectLimit(FourCC("h0AM"), Faction.UNLIMITED); //Fireship
+      Nzoth.ModObjectLimit(FourCC("h0AZ"), Faction.UNLIMITED); //Galley
+      Nzoth.ModObjectLimit(FourCC("h0AQ"), Faction.UNLIMITED); //Boarding
+      Nzoth.ModObjectLimit(FourCC("h0BB"), Faction.UNLIMITED); //Juggernaut
+      Nzoth.ModObjectLimit(FourCC("h0B9"), 6); //Bombard
+
+
+      /// All Nzoth Heroes listed below - with total limits
+
+     
 
       
       FactionManager.Register(Nzoth);
