bf4d6a206d292b387b417d1d619bf8ec30a3024f
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2023-04-23 22:27:56 -0400

    Black Empire config

--- src/WarcraftLegacies.Source/Setup/Legends/LegendBlackEmpire.cs
+++ src/WarcraftLegacies.Source/Setup/Legends/LegendBlackEmpire.cs
@@ -7,6 +7,9 @@ namespace WarcraftLegacies.Source.Setup.Legends
   public sealed class LegendBlackEmpire
   {
     public LegendaryHero Nzoth { get; }
+    public LegendaryHero Vezax { get; }
+    public LegendaryHero Volazj { get; }
+    public LegendaryHero Yorsahj { get; }
 
     public LegendBlackEmpire()
     {
@@ -15,10 +18,31 @@ namespace WarcraftLegacies.Source.Setup.Legends
         UnitType = Constants.UNIT_U01Z_OLD_GOD_NZOTH,
          StartingXp = 10000,
       };
+
+      Vezax = new LegendaryHero("General Vezax")
+      {
+        UnitType = Constants.UNIT_U02B_YOGG_SARON_CHAMPION,
+        StartingXp = 7000,
+      };
+
+      Volazj = new LegendaryHero("Herald Volazj")
+      {
+        UnitType = Constants.UNIT_E01D_N_RAQI_ARCANIST_YOGG,
+        StartingXp = 7000,
+      };
+
+      Yorsahj = new LegendaryHero("Yor'sahj")
+      {
+        UnitType = Constants.UNIT_U02A_N_RAQI_ABERRATION_YOGG,
+        StartingXp = 7000,
+      };
     }
     public void RegisterLegends()
     {
       LegendaryHeroManager.Register(Nzoth);
+      LegendaryHeroManager.Register(Vezax);
+      LegendaryHeroManager.Register(Volazj);
+      LegendaryHeroManager.Register(Yorsahj);
     }
   }
 }
