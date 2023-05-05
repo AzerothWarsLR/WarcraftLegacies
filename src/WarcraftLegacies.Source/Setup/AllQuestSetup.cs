81a41526e4a8d20e67a727c52dd815ce321c4397
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2023-05-05 12:58:44 -0400

    3.1.0 some fixes

--- src/WarcraftLegacies.Source/Setup/AllQuestSetup.cs
+++ src/WarcraftLegacies.Source/Setup/AllQuestSetup.cs
@@ -24,7 +24,7 @@ namespace WarcraftLegacies.Source.Setup
       GoblinQuestSetup.Setup(allLegendSetup);
       DraeneiQuestSetup.Setup(preplacedUnitSystem, allLegendSetup);
       ZandalarQuestSetup.Setup(artifactSetup, preplacedUnitSystem, allLegendSetup);
-      SunfuryQuestSetup.Setup(preplacedUnitSystem, allLegendSetup);
+      SunfuryQuestSetup.Setup(artifactSetup, preplacedUnitSystem, allLegendSetup);
       SharedQuestSetup.Setup(preplacedUnitSystem, artifactSetup, allLegendSetup);
       CrisisQuestSetup.Setup(preplacedUnitSystem, allLegendSetup);
       
