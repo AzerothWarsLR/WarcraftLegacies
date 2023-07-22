b91a054bba1055bb4f12557c6601ac007da17c19
Technopig1992 <127978232+Technopig1992@users.noreply.github.com>
2023-07-22 22:00:32 +0100

    Locking Greater Carrion Swarm upgrade behind quest
    new quest for legion

---
+++ src/WarcraftLegacies.Source/Quests/Legion/QuestKillCapitalPalace.cs
@@ -0,0 +1,33 @@
+﻿using MacroTools.FactionSystem;
+using MacroTools.LegendSystem;
+using MacroTools.ObjectiveSystem.Objectives.LegendBased;
+using MacroTools.QuestSystem;
+using static War3Api.Common;
+
+namespace WarcraftLegacies.Source.Quests.Legion
+{
+  public sealed class QuestKillCapitalPalace : QuestData
+  {
+   
+    public QuestKillCapitalPalace(Capital CapitalPalace) : base("Unleash the Swarm",
+      "Unlock your Dreadlords' full potential. Weaken the pitiful human resistance by destroying their beloved capital!",
+      "ReplaceableTextures\\CommandButtons\\BTNCarrionSwarm.blp")
+    {
+      AddObjective(new ObjectiveCapitalDead(CapitalPalace));
+      ResearchId = Constants.UPGRADE_R09T_QUEST_COMPLETED_UNLEASH_THE_SWARM;
+    }
+    
+    /// <inheritdoc/>
+    protected override string RewardFlavour =>
+      "The precious human capital has crumbled, and your Dreadlords feel their strength growing as the fate of the humanity is all but sealed.";
+
+    /// <inheritdoc/>
+    protected override string RewardDescription => "You are now able to research the Greater Carrion Swarm upgrade.";
+    /// <inheritdoc/>
+    protected override void OnComplete(Faction whichFaction)
+    {
+
+
+    }
+  }
+}
