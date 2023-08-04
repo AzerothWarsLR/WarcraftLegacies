9ed665b02dd5ff425da1425ef04be116cab3cd72
Zakary Boven <zakaryboven@gmail.com>
2023-08-05 01:31:41 +1200

    Fixed more DTOs

--- src/Launcher/DataTransferObjects/MapDoodadsDto.cs
+++ src/Launcher/DataTransferObjects/MapDoodadsDto.cs
@@ -1,27 +1,14 @@
-﻿namespace Launcher.DataTransferObjects
+﻿using War3Net.Build.Widget;
+
+namespace Launcher.DataTransferObjects
 {
-  public class MapDoodadsDataDto
+  public class MapDoodadsDto
   {
     public int FormatVersion { get; set; }
     public int SubVersion { get; set; }
     public bool UseNewFormat { get; set; }
-    public MapDoodadsDto[] Doodads { get; set; }
+    public DoodadData[] Doodads { get; set; }
     public int SpecialDoodadVersion { get; set; }
     public object[] SpecialDoodads { get; set; }
-  }
-
-  public class MapDoodadsDto
-  {
-    public int State { get; set; }
-    public int Life { get; set; }
-    public int TypeId { get; set; }
-    public int Variation { get; set; }
-    public Position Position { get; set; }
-    public double Rotation { get; set; }
-    public Scale Scale { get; set; }
-    public int SkinId { get; set; }
-    public int MapItemTableId { get; set; }
-    public object[] ItemTableSets { get; set; }
-    public int CreationNumber { get; set; }
   }
 }
