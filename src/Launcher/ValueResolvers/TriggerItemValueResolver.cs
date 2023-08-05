22bf36ce27546e82e2b21e52e0752b5cc2b2e7e3
Zakary Boven <zakaryboven@gmail.com>
2023-08-05 18:23:57 +1200

    Triggers seem to export and import ok

--- src/Launcher/ValueResolvers/TriggerItemValueResolver.cs
+++ src/Launcher/ValueResolvers/TriggerItemValueResolver.cs
@@ -10,9 +10,12 @@ namespace Launcher.ValueResolvers
   public sealed class TriggerItemValueResolver : IValueResolver<MapTriggersDto, MapTriggers, List<TriggerItem>>
   {
     /// <inheritdoc />
-    public List<TriggerItem> Resolve(MapTriggersDto source, MapTriggers destination, List<TriggerItem> destMember, ResolutionContext context)
-    {
-      return source.TriggerItems.Select(x => x.ToTriggerItem()).ToList();
-    }
+    public List<TriggerItem> Resolve(MapTriggersDto source, MapTriggers destination, List<TriggerItem> destMember, ResolutionContext context) =>
+      source.TriggerItems
+        .Where(IsValid)
+        .Select(x => x.ToTriggerItem())
+        .ToList();
+
+    private static bool IsValid(TriggerItemDto triggerItemDto) => triggerItemDto.Name != "<DELETED>";
   }
 }
