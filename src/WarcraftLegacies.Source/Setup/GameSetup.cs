c3a64c7ea3a58e158c243a9ccc5bef0179c09211
Zakary Boven <zakaryboven@gmail.com>
2023-08-05 23:06:18 +1200

    Temporarily disabled scripts that CSharpLua has difficulty compiling

--- src/WarcraftLegacies.Source/Setup/GameSetup.cs
+++ src/WarcraftLegacies.Source/Setup/GameSetup.cs
@@ -75,32 +75,32 @@ namespace WarcraftLegacies.Source.Setup
       RockSetup.Setup();
       TurnResearchSetup.Setup();
       UnitTypeConfig.Setup();
-      ShipyardBanZones.Setup(new[]
-      {
-        Regions.CaerDarrowShipyard,
-        Regions.Arathi_Ships,
-        Regions.Auberdine_Ships,
-        Regions.Kali_Ships,
-        Regions.Dustwallow_Ships,
-        Regions.STV_Ships,
-        Regions.Fenris_ships,
-        Regions.Auberdine_Ships_2,
-        Regions.Outland_Ships,
-        Regions.Northern_Kali_Ships,
-        Regions.Stromwind_antiship,
-        Regions.StratholmeShipyard,
-        Regions.Gilneas_Canals,
-        Regions.TwistingNether,
-        Regions.Dun_Morogh_Ships,
-        Regions.Northrend_ships,
-        Regions.Desolace_Ships,
-        Regions.South_EK_Ships,
-        Regions.IcecrownShipyard,
-        Regions.Loch_Modan_Ships,
-        Regions.Quel_Ships_1,
-        Regions.Quel_Ships_2,
-        Regions.Quel_Ships_3
-      });
+      // ShipyardBanZones.Setup(new[]
+      // {
+      //   Regions.CaerDarrowShipyard,
+      //   Regions.Arathi_Ships,
+      //   Regions.Auberdine_Ships,
+      //   Regions.Kali_Ships,
+      //   Regions.Dustwallow_Ships,
+      //   Regions.STV_Ships,
+      //   Regions.Fenris_ships,
+      //   Regions.Auberdine_Ships_2,
+      //   Regions.Outland_Ships,
+      //   Regions.Northern_Kali_Ships,
+      //   Regions.Stromwind_antiship,
+      //   Regions.StratholmeShipyard,
+      //   Regions.Gilneas_Canals,
+      //   Regions.TwistingNether,
+      //   Regions.Dun_Morogh_Ships,
+      //   Regions.Northrend_ships,
+      //   Regions.Desolace_Ships,
+      //   Regions.South_EK_Ships,
+      //   Regions.IcecrownShipyard,
+      //   Regions.Loch_Modan_Ships,
+      //   Regions.Quel_Ships_1,
+      //   Regions.Quel_Ships_2,
+      //   Regions.Quel_Ships_3
+      // });
       BlockerSetup.Setup();
       NeutralVictimAndPassiveSetup.Setup();
       GateSetup.Setup();
--- src/WarcraftLegacies.Source/UnitTypes/UnitTypeConfig.cs
+++ src/WarcraftLegacies.Source/UnitTypes/UnitTypeConfig.cs
@@ -6,95 +6,95 @@ namespace WarcraftLegacies.Source.UnitTypes
   {
     public static void Setup()
     {
-      UnitType.Register(new UnitType(Constants.UNIT_HSHY_SHIPYARD_LORDAERON_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_H076_SHIPYARD_DALARAN_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_H07W_SHIPYARD_KUL_TIRAS_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_H077_SHIPYARD_QUEL_THALAS_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_H03H_SHIPYARD_GILNEAS_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_H06D_ROYAL_HARBOUR_STORMWIND_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_H07D_SHIPYARD_IRONFORGE_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_OSHY_HORDE_PIER_FROSTWOLF_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_O02T_SHIPYARD_WARSONG_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_O032_SHIPYARD_FEL_HORDE_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_O03I_SHIPYARD_TWILIGHT_DOCK)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_O049_GOLDEN_DOCK_ZANDALARI_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_O03V_BILGEWATER_HARBOR_GOBLIN_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_USHP_HAUNTED_HARBOR_SCOURGE)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_U009_SHIPYARD_LEGION_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_U01A_SHIPYARD_FORSAKEN_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_ESHY_KALDOREI_DOCKS)
-      {
-        Category = UnitCategory.Shipyard
-      });
-
-      UnitType.Register(new UnitType(Constants.UNIT_E011_KALDOREI_DOCKS_SENTINEL_SHIPYARD)
-      {
-        Category = UnitCategory.Shipyard
-      });
+      // UnitType.Register(new UnitType(Constants.UNIT_HSHY_SHIPYARD_LORDAERON_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_H076_SHIPYARD_DALARAN_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_H07W_SHIPYARD_KUL_TIRAS_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_H077_SHIPYARD_QUEL_THALAS_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_H03H_SHIPYARD_GILNEAS_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_H06D_ROYAL_HARBOUR_STORMWIND_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_H07D_SHIPYARD_IRONFORGE_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_OSHY_HORDE_PIER_FROSTWOLF_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_O02T_SHIPYARD_WARSONG_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_O032_SHIPYARD_FEL_HORDE_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_O03I_SHIPYARD_TWILIGHT_DOCK)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_O049_GOLDEN_DOCK_ZANDALARI_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_O03V_BILGEWATER_HARBOR_GOBLIN_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_USHP_HAUNTED_HARBOR_SCOURGE)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_U009_SHIPYARD_LEGION_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_U01A_SHIPYARD_FORSAKEN_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_ESHY_KALDOREI_DOCKS)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
+      //
+      // UnitType.Register(new UnitType(Constants.UNIT_E011_KALDOREI_DOCKS_SENTINEL_SHIPYARD)
+      // {
+      //   Category = UnitCategory.Shipyard
+      // });
     }
   }
 }
