16cab144b95fb3adb8a5643a7f4dcb3dbab154da
Zakary Boven <zakaryboven@gmail.com>
2023-08-09 16:36:10 +1200

    Fix

--- src/Launcher/Services/MapBuilderService.cs
+++ src/Launcher/Services/MapBuilderService.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using CSharpLua;
+using Launcher.Extensions;
 using Launcher.Settings;
 using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Configuration;
