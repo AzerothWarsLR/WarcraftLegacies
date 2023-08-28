621b6bc879c94aee7c3904367bc33d7c0750911f
Technopig1992 <127978232+Technopig1992@users.noreply.github.com>
2023-08-28 13:04:01 +0100

    3.6.2 balance changes multiple factions v3 (#2038)
    * new abilities, misc changes,
    
    Misc
    Fountains of Health are now Neutral Passive.
    Fountains of Health now heal friendly and enemies units.
    Nerfed Hoggers attack speed 1.55>1.85.
    added Mass Frost Armour ability
    added Mass Frost Armour Dummy Ability
    
    VS
    added Mass Frost Armour in Scourge spell setup.
    
    * 3.62 Legion Changes
    
    Increased Charm mana cost 125>300.
    Infiltrators now have the Shadow Veil ability.
    Removed permanent invisibility on infiltrators.
    Added the Shadow Veil ability
    Added icon BTNShadow
    added icon DISBTNShadow
    edited tooltip for infiltrators
    change infiltrator ability slots for vamp aura
    
    * 3.6.2 Scourge Changes
    
    Necromancers no longer have Dispel.
    Necromancers now have Necromantic Cleansing.
    Banshee now has the Freezing Shield ability.
    Banshee Master Training now goes up to Grandmaster.
    Kel'Thuzad's Animate Dead ability number of Corpses Raised increased 6>6 / 9>11 / 12>15 /  15>17.
    Plague Titans now hold more corpses 4>6.
    Buffed Anti Magic Shield 600>675.
    Kel'Thuzad's strength per level increased 1.5>1.8.
    Dark Ritual casting range 800>900.
    Increased Mana converted from Dark Ritual 33%>35% / 55%>65% / 80%>90% / 133%>133%.
    Increased Crypt Fiend’s number of damage dice 1>2.
    Crypt Fiends now have the Slow Poison ability.
    Increased Crypt Fiends attack speed 2.0>1.8.
    
    Misc
    Added Icons
    
    * Tooltips
    
    Tooltip Corrections
    
    AMS scourge
    Necromantic Cleansing
    
    * Gameplay Constants Changes + Misc
    
    Frost Movement Speed reduction reduced in Gameplay Constants 50%>30%.
    Frost Attack Speed reduction 25%>20%.
    
    Misc
    Mana cost reduced on all Soul Burns for unlimited caster units to be 75.

--- src/WarcraftLegacies.Source/Regions.cs
+++ src/WarcraftLegacies.Source/Regions.cs
@@ -258,7 +258,7 @@ public static class Regions
 	public static Rectangle GilneasUnlock6 { get; set; } = new Rectangle(6080f, -928f, 8672f, 1696f);
 	public static Rectangle GilneasUnlock1 { get; set; } = new Rectangle(8288f, -2368f, 9824f, -832f);
 	public static Rectangle GilneasUnlock2 { get; set; } = new Rectangle(6624f, -3264f, 8032f, -2400f);
-	public static Rectangle GilneasUnlock3 { get; set; } = new Rectangle(3968f, 416f, 5376f, 2656f);
+	public static Rectangle GilneasUnlock3 { get; set; } = new Rectangle(3968f, 416f, 5408f, 2656f);
 	public static Rectangle GilneasUnlock4 { get; set; } = new Rectangle(3392f, -2656f, 5504f, -320f);
 	public static Rectangle GilneasUnlock5 { get; set; } = new Rectangle(6432f, 2112f, 7456f, 2816f);
 	public static Rectangle TwilightUnlock { get; set; } = new Rectangle(18592f, -6848f, 19456f, -6048f);
--- src/WarcraftLegacies.Source/Setup/Spells/ScourgeSpellSetup.cs
+++ src/WarcraftLegacies.Source/Setup/Spells/ScourgeSpellSetup.cs
@@ -39,6 +39,16 @@ namespace WarcraftLegacies.Source.Setup.Spells
         TargetType = SpellTargetType.Point
       };
       SpellSystem.Register(massUnholyFrenzy);
+
+      var massFrostArmour = new MassAnySpell(Constants.ABILITY_A13R_MASS_FROST_ARMOR_SCOURGE)
+      {
+        DummyAbilityId = Constants.ABILITY_A13S_MASS_FROST_ARMOUR_SCOURGE_DUMMY,
+        DummyAbilityOrderString = "frostarmoron",
+        Radius = 250,
+        CastFilter = CastFilters.IsTargetOrganicAndAlive,
+        TargetType = SpellTargetType.Point
+      };
+      SpellSystem.Register(massFrostArmour);
     }
   }
 }
