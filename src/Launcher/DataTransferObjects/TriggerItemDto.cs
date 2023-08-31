fdb706bcc8f9a4576dbad57dd165637f3c2375f2
Zakary Boven <zakaryboven@gmail.com>
2023-08-31 23:09:36 +1200

    Split files

--- src/Launcher/DataTransferObjects/TriggerItemDto.cs
+++ src/Launcher/DataTransferObjects/TriggerItemDto.cs
@@ -3,17 +3,7 @@ using War3Net.Build.Script;
 
 namespace Launcher.DataTransferObjects
 {
-  public class MapTriggersDto
-  {
-    public int FormatVersion { get; set; }
-    public int SubVersion { get; set; }
-    public int GameVersion { get; set; }
-    public object[] Variables { get; set; }
-    public TriggerItemDto[] TriggerItems { get; set; }
-    public Dictionary<TriggerItemType, int> TriggerItemCounts { get; set; }
-  }
-
-  public class TriggerItemDto
+  public sealed class TriggerItemDto
   {
     public string Name { get; set; }
     public int Id { get; set; }
