49d99808ba4bd18fe867d506b1a376a8541df549
Zakary Boven <zakaryboven@gmail.com>
2023-09-02 12:34:25 +1200

    Don't generate properties for which there is a Raw equivalent

--- analyzers/War3Api.Object.SourceGenerator/BaseObjectDtoSourceGenerator.cs
+++ analyzers/War3Api.Object.SourceGenerator/BaseObjectDtoSourceGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
@@ -11,7 +12,7 @@ namespace War3Api.Object.SourceGenerator;
 /// Generates a data transfer object for all objects inheriting from War3Api.Object.BaseObject.
 /// </summary>
 [Generator]
-public class BaseObjectDtoSourceGenerator : ISourceGenerator
+public sealed class BaseObjectDtoSourceGenerator : ISourceGenerator
 {
   private const string BaseObjectTypeName = "War3Api.Object.BaseObject";
   private const string OutputNamespace = "War3Api.Object.DataTransferObjects";
@@ -69,8 +70,11 @@ internal static class RoslynGeneratorExtensions{
   /// </summary>
   internal static ClassDeclarationSyntax AddSimplifiedCopiedProperties(this ClassDeclarationSyntax classDeclarationSyntax, INamespaceOrTypeSymbol classSymbol)
   {
-    return classSymbol.GetMembers()
-      .OfType<IPropertySymbol>()
+    var classProperties = classSymbol.GetMembers().OfType<IPropertySymbol>().ToList();
+    var classPropertyNames = classProperties.Select(x => x.Name).ToList();
+    
+    return classProperties
+      .Where(property => IsValidPropertyToCopy(property, classPropertyNames))
       .Select(property => PropertyDeclaration(ParseTypeName(property.Type.ToDisplayString()), property.Name)
         .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword)))
         .WithAccessorList(AccessorList(List(new[]
@@ -81,5 +85,15 @@ internal static class RoslynGeneratorExtensions{
             .WithSemicolonToken(Token(SyntaxKind.SemicolonToken))
         }))))
       .Aggregate(classDeclarationSyntax, (current, propertySyntax) => current.AddMembers(propertySyntax));
+  }
+
+  /// <summary>
+  /// A property is valid to copy if it doesn't end with "Modified",
+  /// and there are no other Properties representing a raw version of itself.
+  /// </summary>
+  private static bool IsValidPropertyToCopy(ISymbol property, ICollection<string> otherPropertyNames)
+  {
+    var propertyName = property.Name;
+    return !propertyName.EndsWith("Modified") && !otherPropertyNames.Contains($"{propertyName}Raw");
   }
 }
