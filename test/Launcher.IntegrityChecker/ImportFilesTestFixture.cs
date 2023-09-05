508a5df5500a89e5327efb45ca769c3845cdac66
Zakary Boven <zakaryboven@gmail.com>
2023-09-05 23:22:38 +1200

    cleanup

--- test/Launcher.IntegrityChecker/ImportFilesTestFixture.cs
+++ test/Launcher.IntegrityChecker/ImportFilesTestFixture.cs
@@ -9,14 +9,12 @@ namespace Launcher.IntegrityChecker
   public sealed class ImportFilesTestFixture
   {
     public Map Map { get; }
-
-    public IEnumerable<PathData> ImportedFiles { get; }
     
     public HashSet<string> ModelsUsedInMap { get; }
 
     public ImportFilesTestFixture()
     {
-      (Map, ImportedFiles) = MapDataProvider.GetMapData;
+      (Map, _) = MapDataProvider.GetMapData;
       AdvancedMapBuilder.AddCSharpCode(Map, @"..\..\..\..\..\src\WarcraftLegacies.Source\", new CompilerSettings());
       ModelsUsedInMap = GetModelsUsedInMap(Map).ToHashSet();
     }
@@ -33,60 +31,50 @@ namespace Launcher.IntegrityChecker
 
     private static IEnumerable<string> GetModelsUsedByUnits(UnitObjectData unitObjectData)
     {
-      var modelFields = new[] { 1818520949, 1831952757 };
-
       return unitObjectData.BaseUnits
         .Concat(unitObjectData.NewUnits)
         .SelectMany(x => x.Modifications)
-        .Where(x => modelFields.Contains(x.Id))
+        .Where(IsModelField)
         .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
         .ToList();
     }
 
     private static IEnumerable<string> GetModelsUsedByAbilities(AbilityObjectData abilityObjectData)
     {
-      var modelFields = new[] { 1952543585, 1952543841, 1952542049 };
-
       return abilityObjectData.BaseAbilities
         .Concat(abilityObjectData.NewAbilities)
         .SelectMany(x => x.Modifications)
-        .Where(x => modelFields.Contains(x.Id))
+        .Where(IsModelField)
         .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
         .ToList();
     }
 
     private static IEnumerable<string> GetModelsUsedByDoodads(DoodadObjectData doodadObjectData)
     {
-      var modelFields = new[] { 1818846820 };
-
       return doodadObjectData.BaseDoodads
         .Concat(doodadObjectData.NewDoodads)
         .SelectMany(x => x.Modifications)
-        .Where(x => modelFields.Contains(x.Id))
+        .Where(IsModelField)
         .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
         .ToList();
     }
     
     private static IEnumerable<string> GetModelsUsedByDestructables(DestructableObjectData destructableObjectData)
     {
-      var modelFields = new[] { 1818846818 };
-
       return destructableObjectData.BaseDestructables
         .Concat(destructableObjectData.NewDestructables)
         .SelectMany(x => x.Modifications)
-        .Where(x => modelFields.Contains(x.Id))
+        .Where(IsModelField)
         .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
         .ToList();
     }
 
     private static IEnumerable<string> GetModelsUsedByBuffs(BuffObjectData buffObjectData)
     {
-      var modelFields = new[] { 1952543846 };
-
       return buffObjectData.BaseBuffs
         .Concat(buffObjectData.NewBuffs)
         .SelectMany(x => x.Modifications)
-        .Where(x => modelFields.Contains(x.Id))
+        .Where(IsModelField)
         .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
         .ToList();
     }
@@ -101,5 +89,10 @@ namespace Launcher.IntegrityChecker
           .Replace(@"""", "")
           .Replace(".mdl", ".mdx");
     }
+
+    private static bool IsModelField(ObjectDataModification modification) =>
+      modification.Type == ObjectDataType.String && IsModelPath(modification.ValueAsString);
+
+    private static bool IsModelPath(string text) => text.EndsWith(".mdx") || text.EndsWith(".mdl");
   }
 }
