db8386123a78b962505c95c0d3483507c283eaee
Zakary Boven <zakaryboven@gmail.com>
2023-09-05 20:46:32 +1200

    good start

---
+++ test/Launcher.IntegrityChecker/ImportFileTests.cs
@@ -0,0 +1,47 @@
+using FluentAssertions;
+using War3Net.Build.Object;
+
+namespace Launcher.IntegrityChecker;
+
+public sealed class ImportFileTests : IClassFixture<MapTestFixture>
+{
+  private readonly MapTestFixture _mapTestFixture;
+
+  public ImportFileTests(MapTestFixture mapTestFixture)
+  {
+    _mapTestFixture = mapTestFixture;
+  }
+  
+  [Theory]
+  [MemberData(nameof(GetAllModels))]
+  public void AllModels_AreInActiveUse(string relativePath)
+  {
+    var activeModels = GetModelsUsedByUnits(_mapTestFixture.Map.UnitSkinObjectData);
+    activeModels.Should().Contain(relativePath);
+  }
+
+  public static IEnumerable<object[]> GetAllModels()
+  {
+    foreach (var pathData in MapDataProvider.GetMapData.AdditionalFiles)
+    {
+      if (pathData.RelativePath.EndsWith(".mdx"))
+      {
+        yield return new object[]
+        {
+          pathData.RelativePath
+        };
+      }
+    }
+  }
+
+  private static IEnumerable<string> GetModelsUsedByUnits(UnitObjectData unitObjectData)
+  {
+    var modelFields = new[] { 1818520949, 1831952757 };
+    
+    return unitObjectData.BaseUnits
+      .SelectMany(x => x.Modifications)
+      .Where(x => modelFields.Contains(x.Id))
+      .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
+      .ToList();
+  }
+}
---
+++ test/Launcher.IntegrityChecker/MapTestFixture.cs
@@ -0,0 +1,17 @@
+﻿using Launcher.Services;
+using War3Net.Build;
+
+namespace Launcher.IntegrityChecker
+{
+  public sealed class MapTestFixture
+  {
+    public Map Map { get; }
+
+    public IEnumerable<PathData> ImportedFiles { get; }
+
+    public MapTestFixture()
+    {
+      (Map, ImportedFiles) = MapDataProvider.GetMapData;
+    }
+  }
+}
