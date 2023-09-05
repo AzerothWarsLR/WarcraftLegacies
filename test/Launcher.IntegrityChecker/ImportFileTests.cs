1b6dd7b06a7aefca4be4f79562f84ceaf6ff40ea
Zakary Boven <zakaryboven@gmail.com>
2023-09-05 22:43:38 +1200

    this should make it run faster

--- test/Launcher.IntegrityChecker/ImportFileTests.cs
+++ test/Launcher.IntegrityChecker/ImportFileTests.cs
@@ -1,24 +1,21 @@
-using System.Text.RegularExpressions;
 using FluentAssertions;
-using War3Net.Build;
-using War3Net.Build.Object;
 
 namespace Launcher.IntegrityChecker;
 
-public sealed class ImportFileTests : IClassFixture<MapTestFixture>
+public sealed class ImportFileTests : IClassFixture<ImportFilesTestFixture>
 {
-  private readonly MapTestFixture _mapTestFixture;
+  private readonly ImportFilesTestFixture _importFilesTestFixture;
 
-  public ImportFileTests(MapTestFixture mapTestFixture)
+  public ImportFileTests(ImportFilesTestFixture importFilesTestFixture)
   {
-    _mapTestFixture = mapTestFixture;
+    _importFilesTestFixture = importFilesTestFixture;
   }
   
   [Theory]
   [MemberData(nameof(GetAllModels))]
   public void AllModels_AreInActiveUse(string relativePath)
   {
-    var activeModels = GetModelsUsedInMap(_mapTestFixture.Map);
+    var activeModels = _importFilesTestFixture.ModelsUsedInMap;
     activeModels.Should().Contain(relativePath);
   }
 
@@ -34,73 +31,5 @@ public sealed class ImportFileTests : IClassFixture<MapTestFixture>
         };
       }
     }
-  }
-  
-  private static IEnumerable<string> GetModelsUsedInMap(Map map)
-  {
-    return GetModelsUsedByUnits(map.UnitSkinObjectData)
-      .Concat(GetModelsUsedByAbilities(map.AbilitySkinObjectData))
-      .Concat(GetModelsUsedByDoodads(map.DoodadObjectData))
-      .Concat(GetModelsUsedByBuffs(map.BuffSkinObjectData))
-      .Concat(GetModelsUsedByScript(map.Script));
-  }
-
-  private static IEnumerable<string> GetModelsUsedByUnits(UnitObjectData unitObjectData)
-  {
-    var modelFields = new[] { 1818520949, 1831952757 };
-    
-    return unitObjectData.BaseUnits
-      .Concat(unitObjectData.NewUnits)
-      .SelectMany(x => x.Modifications)
-      .Where(x => modelFields.Contains(x.Id))
-      .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
-      .ToList();
-  }
-  
-  private static IEnumerable<string> GetModelsUsedByAbilities(AbilityObjectData abilityObjectData)
-  {
-    var modelFields = new[] { 1952543585, 1952543841, 1952543585 };
-    
-    return abilityObjectData.BaseAbilities
-      .Concat(abilityObjectData.NewAbilities)
-      .SelectMany(x => x.Modifications)
-      .Where(x => modelFields.Contains(x.Id))
-      .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
-      .ToList();
-  }
-  
-  private static IEnumerable<string> GetModelsUsedByDoodads(DoodadObjectData doodadObjectData)
-  {
-    var modelFields = new[] { 1818846820 };
-    
-    return doodadObjectData.BaseDoodads
-      .Concat(doodadObjectData.NewDoodads)
-      .SelectMany(x => x.Modifications)
-      .Where(x => modelFields.Contains(x.Id))
-      .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
-      .ToList();
-  }
-  
-  private static IEnumerable<string> GetModelsUsedByBuffs(BuffObjectData buffObjectData)
-  {
-    var modelFields = new[] { 1952543846 };
-    
-    return buffObjectData.BaseBuffs
-      .Concat(buffObjectData.NewBuffs)
-      .SelectMany(x => x.Modifications)
-      .Where(x => modelFields.Contains(x.Id))
-      .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
-      .ToList();
-  }
-  
-  private static IEnumerable<string> GetModelsUsedByScript(string? mapScript)
-  {
-    var matches = Regex.Matches(mapScript, @"[\[""].*\.md[xl][\]""]");
-    foreach (var model in matches.ToList())
-      yield return model.Value
-        .Replace("[", "")
-        .Replace("]", "")
-        .Replace(@"""", "")
-        .Replace(".mdl", ".mdx");
   }
 }
---
+++ test/Launcher.IntegrityChecker/ImportFilesTestFixture.cs
@@ -0,0 +1,92 @@
+﻿using System.Text.RegularExpressions;
+using Launcher.Services;
+using Launcher.Settings;
+using War3Net.Build;
+using War3Net.Build.Object;
+
+namespace Launcher.IntegrityChecker
+{
+  public sealed class ImportFilesTestFixture
+  {
+    public Map Map { get; }
+
+    public IEnumerable<PathData> ImportedFiles { get; }
+    
+    public IEnumerable<string> ModelsUsedInMap { get; }
+
+    public ImportFilesTestFixture()
+    {
+      (Map, ImportedFiles) = MapDataProvider.GetMapData;
+      AdvancedMapBuilder.AddCSharpCode(Map, @"..\..\..\..\..\src\WarcraftLegacies.Source\", new CompilerSettings());
+      ModelsUsedInMap = GetModelsUsedInMap(Map);
+    }
+
+    private static IEnumerable<string> GetModelsUsedInMap(Map map)
+    {
+      return GetModelsUsedByUnits(map.UnitSkinObjectData)
+        .Concat(GetModelsUsedByAbilities(map.AbilitySkinObjectData))
+        .Concat(GetModelsUsedByDoodads(map.DoodadObjectData))
+        .Concat(GetModelsUsedByBuffs(map.BuffSkinObjectData))
+        .Concat(GetModelsUsedByScript(map.Script));
+    }
+
+    private static IEnumerable<string> GetModelsUsedByUnits(UnitObjectData unitObjectData)
+    {
+      var modelFields = new[] { 1818520949, 1831952757 };
+
+      return unitObjectData.BaseUnits
+        .Concat(unitObjectData.NewUnits)
+        .SelectMany(x => x.Modifications)
+        .Where(x => modelFields.Contains(x.Id))
+        .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
+        .ToList();
+    }
+
+    private static IEnumerable<string> GetModelsUsedByAbilities(AbilityObjectData abilityObjectData)
+    {
+      var modelFields = new[] { 1952543585, 1952543841, 1952543585 };
+
+      return abilityObjectData.BaseAbilities
+        .Concat(abilityObjectData.NewAbilities)
+        .SelectMany(x => x.Modifications)
+        .Where(x => modelFields.Contains(x.Id))
+        .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
+        .ToList();
+    }
+
+    private static IEnumerable<string> GetModelsUsedByDoodads(DoodadObjectData doodadObjectData)
+    {
+      var modelFields = new[] { 1818846820 };
+
+      return doodadObjectData.BaseDoodads
+        .Concat(doodadObjectData.NewDoodads)
+        .SelectMany(x => x.Modifications)
+        .Where(x => modelFields.Contains(x.Id))
+        .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
+        .ToList();
+    }
+
+    private static IEnumerable<string> GetModelsUsedByBuffs(BuffObjectData buffObjectData)
+    {
+      var modelFields = new[] { 1952543846 };
+
+      return buffObjectData.BaseBuffs
+        .Concat(buffObjectData.NewBuffs)
+        .SelectMany(x => x.Modifications)
+        .Where(x => modelFields.Contains(x.Id))
+        .Select(x => x.ValueAsString.Replace(".mdl", ".mdx"))
+        .ToList();
+    }
+
+    private static IEnumerable<string> GetModelsUsedByScript(string? mapScript)
+    {
+      var matches = Regex.Matches(mapScript, @"[\[""].*\.md[xl][\]""]");
+      foreach (var model in matches.ToList())
+        yield return model.Value
+          .Replace("[", "")
+          .Replace("]", "")
+          .Replace(@"""", "")
+          .Replace(".mdl", ".mdx");
+    }
+  }
+}
