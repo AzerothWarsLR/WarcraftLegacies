2c6ac5291f8c60ebe39e7c907e7f7c06574da40e
Zakary Boven <zakaryboven@gmail.com>
2023-09-07 11:33:30 +1200

    should be a method

--- test/Launcher.IntegrityChecker/MapDataProvider.cs
+++ test/Launcher.IntegrityChecker/MapDataProvider.cs
@@ -7,20 +7,19 @@ namespace Launcher.IntegrityChecker
   public static class MapDataProvider
   {
     private static (Map, IEnumerable<PathData>)? _mapData;
-    
-    public static (Map Map, IEnumerable<PathData> AdditionalFiles) GetMapData
+
+    public static (Map Map, IEnumerable<PathData> AdditionalFiles) GetMapData()
     {
-      get
       {
         if (_mapData != null)
           return _mapData.Value;
-        
+
         const string mapDataDirectory = "mapdata/WarcraftLegacies";
         var autoMapperConfig = new AutoMapperConfigurationProvider().GetConfiguration();
         var mapper = new Mapper(autoMapperConfig);
         var conversionService = new MapDataToMapConverter(mapper, new JsonModifierProvider());
         _mapData = conversionService.ConvertToMapAndAdditionalFiles(mapDataDirectory);
-          
+
         return _mapData.Value;
       }
     }
