094ad43bafc92105240d59f814df286a749e191c
Zakary Boven <zakaryboven@gmail.com>
2023-09-09 00:26:29 +1200

    fix

--- src/MacroTools/Instances/Instance.cs
+++ src/MacroTools/Instances/Instance.cs
@@ -9,13 +9,15 @@ using static War3Api.Common;
 namespace MacroTools.Instances
 {
   /// <summary>
-  ///   An instance is a region that is physically seperate from all other instances irrespective of its actual geographical
-  ///   location on the Warcraft map.
+  /// An instance is a region that is physically seperate from all other instances irrespective of its actual geographical
+  /// location on the Warcraft map.
   /// </summary>
   public sealed class Instance
   {
+    /// <summary>The entire region that constitutes the physical area of the <see cref="Instance"/>.</summary>
     public region Region { get; }
 
+    /// <summary>The number of <see cref="Gate"/>s that can be used to enter or exit the <see cref="Instance"/>.</summary>
     public int GateCount => _gates.Count;
     
     private readonly trigger _dependencyDiesTrigger;
@@ -24,6 +26,7 @@ namespace MacroTools.Instances
     private readonly Rectangle[] _rectangles;
     private readonly List<unit> _dependencies = new();
     
+    /// <summary>Initializes a new instance of the <see cref="Instance"/> class.</summary>
     public Instance(string name, IEnumerable<Rectangle> areas)
     {
       Region = CreateRegion();
@@ -35,6 +38,7 @@ namespace MacroTools.Instances
         .AddAction(Destroy);
     }
 
+    /// <summary>Initializes a new instance of the <see cref="Instance"/> class.</summary>
     public Instance(string name, Rectangle area) : this(name, new[] {area})
     {
     }
@@ -42,11 +46,8 @@ namespace MacroTools.Instances
     /// <summary>
     /// Adds a <see cref="Gate"/> to the <see cref="Instance"/>, indicating where it can be entered and exited.
     /// </summary>
-    public void AddGate(Gate gate)
-    {
-      _gates.Add(gate);
-    }
-    
+    public void AddGate(Gate gate) => _gates.Add(gate);
+
     /// <summary>
     ///   Gets the <see cref="Gate" /> nearest the given position, if any.
     /// </summary>
@@ -72,8 +73,17 @@ namespace MacroTools.Instances
     }
 
     /// <summary>
-    ///   Kills all <see cref="unit" />s in the instance and moves all <see cref="item" />s to the nearest exterior
-    ///   <see cref="Gate" />.
+    ///   Makes the <see cref="Instance" /> dependent on the given <see cref="unit" /> being alive.
+    ///   When any of the dependent <see cref="unit" />s die, every unit in the <see cref="Instance" /> is killed.
+    /// </summary>
+    public void AddDependency(unit dependency)
+    {
+      _dependencies.Add(dependency);
+      _dependencyDiesTrigger.RegisterUnitEvent(dependency, EVENT_UNIT_DEATH);
+    }
+    
+    /// <summary>
+    /// Kills all units in the instance, then evacuates all units and items to the nearest exterior <see cref="Gate"/>.
     /// </summary>
     private void Destroy()
     {
@@ -82,22 +92,10 @@ namespace MacroTools.Instances
         _dependencyDiesTrigger.Destroy();
         foreach (var rect in _rectangles)
         {
-          foreach (var unit in CreateGroup().EnumUnitsInRect(rect).EmptyToList())
-            KillUnit(unit);
-
-          EnumItemsInRect(rect.Rect, null, () =>
-          {
-            try
-            {
-              var enumItem = GetEnumItem();
-              var exteriorPosition = GetNearestGate(enumItem.GetPosition()).ExteriorPosition;
-              enumItem.SetPosition(exteriorPosition);
-            }
-            catch (Exception ex)
-            {
-              Logger.LogError(ex.ToString());
-            }
-          });
+          var unitsInRect = CreateGroup().EnumUnitsInRect(rect).EmptyToList();
+          KillUnits(unitsInRect);
+          EvacuateUnits(unitsInRect);
+          EvacuateItemsInRect(rect);
         }
 
         foreach (var unit in _dependencies)
@@ -109,14 +107,36 @@ namespace MacroTools.Instances
       }
     }
 
-    /// <summary>
-    ///   Makes the <see cref="Instance" /> dependent on the given <see cref="unit" /> being alive.
-    ///   When any of the dependent <see cref="unit" />s die, every unit in the <see cref="Instance" /> is killed.
-    /// </summary>
-    public void AddDependency(unit dependency)
+    private static void KillUnits(List<unit> unitsToKill)
     {
-      _dependencies.Add(dependency);
-      _dependencyDiesTrigger.RegisterUnitEvent(dependency, EVENT_UNIT_DEATH);
+      foreach (var unit in unitsToKill)
+        KillUnit(unit);
+    }
+    
+    private void EvacuateUnits(List<unit> unitsInRect)
+    {
+      foreach (var unit in unitsInRect)
+      {
+        var exteriorPosition = GetNearestGate(unit.GetPosition()).ExteriorPosition;
+        unit.SetPosition(exteriorPosition);
+      }
+    }
+
+    private void EvacuateItemsInRect(Rectangle rect)
+    {
+      EnumItemsInRect(rect.Rect, null, () =>
+      {
+        try
+        {
+          var enumItem = GetEnumItem();
+          var exteriorPosition = GetNearestGate(enumItem.GetPosition()).ExteriorPosition;
+          enumItem.SetPosition(exteriorPosition);
+        }
+        catch (Exception ex)
+        {
+          Logger.LogError(ex.ToString());
+        }
+      });
     }
   }
 }
