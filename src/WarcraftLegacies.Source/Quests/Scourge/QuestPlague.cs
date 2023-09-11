8a1f77c1498194eadc35e8532645d3725754c5f4
Zakary Boven <zakaryboven@gmail.com>
2023-09-11 18:49:29 +1200

    remove factionnames dependency from QuestPlague

--- src/WarcraftLegacies.Source/Quests/Scourge/QuestPlague.cs
+++ src/WarcraftLegacies.Source/Quests/Scourge/QuestPlague.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using MacroTools;
 using MacroTools.Extensions;
 using MacroTools.FactionSystem;
@@ -9,7 +8,6 @@ using MacroTools.ObjectiveSystem.Objectives.TimeBased;
 using MacroTools.ObjectiveSystem.Objectives.UnitBased;
 using MacroTools.QuestSystem;
 using WarcraftLegacies.Source.Mechanics.Scourge.Plague;
-using WarcraftLegacies.Source.Setup.FactionSetup;
 using WCSharp.Buffs;
 using WCSharp.Shared.Data;
 using static War3Api.Common;
@@ -22,7 +20,6 @@ namespace WarcraftLegacies.Source.Quests.Scourge
   public sealed class QuestPlague : QuestData
   {
     private readonly Faction _plagueVictim;
-    private readonly float _duration;
 
     private readonly unit _portalController1;
     private readonly unit _portalController2;
@@ -30,12 +27,10 @@ namespace WarcraftLegacies.Source.Quests.Scourge
     private readonly unit _innerWaygate2;
     private readonly unit _outerWaygate1;
     private readonly unit _outerWaygate2;
-
-    private readonly List<PlagueCauldronSummonParameter> _plagueCauldronSummonParameters;
-    private readonly int _plagueCauldronUnitTypeId;
-    private readonly List<Rectangle> _plagueRects;
-    private readonly List<Point> _attackTargets;
+    
     private readonly unit _gilneasDoor;
+    private readonly Faction _secondaryPlagueFaction;
+    private readonly PlagueParameters _plagueParameters;
 
     /// <summary>
     /// When completed, the quest holder initiates the Plague, creating Plague Cauldrons around Lordaeron
@@ -44,18 +39,17 @@ namespace WarcraftLegacies.Source.Quests.Scourge
     /// <param name="plagueParameters">Provides information about how the Plague should work.</param>
     /// <param name="preplacedUnitSystem">A system for finding preplaced units.</param>
     /// <param name="plagueVictim">The faction that the plague will primarily affect.</param>
-    public QuestPlague(PlagueParameters plagueParameters, PreplacedUnitSystem preplacedUnitSystem, Faction plagueVictim) : base(
+    /// <param name="secondaryPlagueFaction">The faction that will gain some of the fringe benefits of the plague.</param>
+    public QuestPlague(PlagueParameters plagueParameters, PreplacedUnitSystem preplacedUnitSystem, Faction plagueVictim,
+      Faction secondaryPlagueFaction) : base(
       "Plague of Undeath",
       "The Cult of the Damned is prepared to unleash a devastating zombifying plague across the lands of Lordaeron.",
-      "ReplaceableTextures\\CommandButtons\\BTNPlagueBarrel.blp")
+      @"ReplaceableTextures\CommandButtons\BTNPlagueBarrel.blp")
     {
       _gilneasDoor = preplacedUnitSystem.GetUnit(Constants.UNIT_H02K_GREYMANE_S_GATE_CLOSED).SetInvulnerable(true);
       _plagueVictim = plagueVictim;
-      _plagueRects = plagueParameters.PlagueRects;
-      _plagueCauldronUnitTypeId = plagueParameters.PlagueCauldronUnitTypeId;
-      _plagueCauldronSummonParameters = plagueParameters.PlagueCauldronSummonParameters;
-      _duration = plagueParameters.Duration;
-      _attackTargets = plagueParameters.AttackTargets;
+      _plagueParameters = plagueParameters;
+      _secondaryPlagueFaction = secondaryPlagueFaction;
       AddObjective(new ObjectiveEitherOf(
         new ObjectiveResearch(Constants.UPGRADE_R06I_PLAGUE_OF_UNDEATH_SCOURGE, FourCC("u000")),
         new ObjectiveTime(660)));
@@ -78,44 +72,7 @@ namespace WarcraftLegacies.Source.Quests.Scourge
 
     /// <inheritdoc />
     protected override string RewardDescription =>
-      "All villagers in Lordaeron are transformed into Zombies, and several Plague Cauldrons spawn throughout Lordaeron, which periodically spawn Zombies. A portal opens between Dragonblight and Scholomance. You can now build Necropolis.";
-
-    private void CreatePlagueCauldrons(Faction completingFaction)
-    {
-      var plaguePlayer = (completingFaction.ScoreStatus != ScoreStatus.Defeated
-        ? completingFaction.Player
-        : Player(PLAYER_NEUTRAL_AGGRESSIVE))
-                         ?? Player(PLAYER_NEUTRAL_AGGRESSIVE);
-
-      var legion = FactionManager.GetFromName(FactionNames.Legion);
-
-      var plaguePlayer2 = (legion != null && legion.Player != null && legion.ScoreStatus != ScoreStatus.Defeated) ? legion.Player : Player(PLAYER_NEUTRAL_AGGRESSIVE) ?? Player(PLAYER_NEUTRAL_AGGRESSIVE);
-
-      foreach (var plagueRect in _plagueRects)
-      {
-
-        var position = plagueRect.GetRandomPoint();
-        var plagueCauldron = CreateUnit(plaguePlayer, _plagueCauldronUnitTypeId, position.X, position.Y, 0).SetTimedLife(_duration);
-
-        CreateUnit(plaguePlayer2, Constants.UNIT_U00D_LEGION_HERALD_LEGION_WORKER, position.X, position.Y, 0);
-
-        var attackTarget = _attackTargets.OrderBy(x => MathEx.GetDistanceBetweenPoints(position, x)).First();
-
-        var plagueCauldronBuff = new PlagueCauldronBuff(plagueCauldron, plagueCauldron, attackTarget)
-        {
-          ZombieUnitTypeId = Constants.UNIT_NZOM_ZOMBIE_SCOURGE
-        };
-        BuffSystem.Add(plagueCauldronBuff);
-
-        foreach (var parameter in _plagueCauldronSummonParameters)
-          foreach (var unit in GeneralHelpers.CreateUnits(plaguePlayer, parameter.SummonUnitTypeId,
-                   position.X, position.Y, 0, parameter.SummonCount))
-          {
-            if (!unit.IsType(UNIT_TYPE_PEON))
-              unit.IssueOrder("attack", attackTarget);
-          }
-      }
-    }
+      "All villagers in Lordaeron are transformed into Zombies, several Zombie-spawning Plague Cauldrons spawn throughout Lordaeron, a portal opens between Dragonblight and Scholomance, you learn to build Necropoli, and Lordaeron's Control Points reset to level 0";
 
     /// <inheritdoc />
     protected override void OnComplete(Faction completingFaction)
@@ -127,7 +84,7 @@ namespace WarcraftLegacies.Source.Quests.Scourge
       completingFaction.AddPower(plaguePower);
       _gilneasDoor
         .SetInvulnerable(false);
-      ResetLordaeronCpLevel();
+      ResetVictimControlPointLevel();
       _portalController1.SetInvulnerable(false);
       _portalController2.SetInvulnerable(false);
       _innerWaygate1
@@ -144,18 +101,53 @@ namespace WarcraftLegacies.Source.Quests.Scourge
         .SetWaygateDestination(Regions.Scholomance_Exterior_2.Center);
     }
 
-    private static void ResetLordaeronCpLevel()
-    {
-      var lordaeron = FactionManager.GetFromName(FactionNames.Lordaeron);
-      if (lordaeron != null && lordaeron.Player != null)
-      {
-        foreach (var cp in lordaeron.Player.GetControlPoints())
-          cp.ControlLevel = 0;
-      }
-    }
-
     /// <inheritdoc />
     protected override void OnAdd(Faction whichFaction) =>
       whichFaction.ModObjectLimit(Constants.UPGRADE_R06I_PLAGUE_OF_UNDEATH_SCOURGE, Faction.UNLIMITED);
+    
+    private void CreatePlagueCauldrons(Faction completingFaction)
+    {
+      var primaryPlaguePlayer = completingFaction.ScoreStatus != ScoreStatus.Defeated && completingFaction.Player != null
+        ? completingFaction.Player
+        : Player(PLAYER_NEUTRAL_AGGRESSIVE);
+      
+      var secondaryPlaguePlayer = _secondaryPlagueFaction.ScoreStatus != ScoreStatus.Defeated && _secondaryPlagueFaction.Player != null
+        ? _secondaryPlagueFaction.Player
+        : Player(PLAYER_NEUTRAL_AGGRESSIVE);
+
+      foreach (var plagueRect in _plagueParameters.PlagueRects)
+      {
+        var position = plagueRect.GetRandomPoint();
+        var plagueCauldron = CreateUnit(primaryPlaguePlayer, _plagueParameters.PlagueCauldronUnitTypeId, position.X, position.Y, 0)
+          .SetTimedLife(_plagueParameters.Duration);
+
+        CreateUnit(secondaryPlaguePlayer, Constants.UNIT_U00D_LEGION_HERALD_LEGION_WORKER, position.X, position.Y, 0);
+
+        var attackTarget = _plagueParameters.AttackTargets.OrderBy(x => MathEx.GetDistanceBetweenPoints(position, x)).First();
+
+        var plagueCauldronBuff = new PlagueCauldronBuff(plagueCauldron, plagueCauldron, attackTarget)
+        {
+          ZombieUnitTypeId = Constants.UNIT_NZOM_ZOMBIE_SCOURGE
+        };
+        BuffSystem.Add(plagueCauldronBuff);
+
+        foreach (var parameter in _plagueParameters.PlagueCauldronSummonParameters)
+        foreach (var unit in GeneralHelpers.CreateUnits(primaryPlaguePlayer, parameter.SummonUnitTypeId,
+                   position.X, position.Y, 0, parameter.SummonCount))
+        {
+          if (!unit.IsType(UNIT_TYPE_PEON))
+            unit.IssueOrder("attack", attackTarget);
+        }
+      }
+    }
+    
+    private void ResetVictimControlPointLevel()
+    {
+      if (_plagueVictim.Player == null) 
+        return;
+      
+      foreach (var controlPoint in _plagueVictim.Player.GetControlPoints())
+        controlPoint.ControlLevel = 0;
+    }
   }
 }
