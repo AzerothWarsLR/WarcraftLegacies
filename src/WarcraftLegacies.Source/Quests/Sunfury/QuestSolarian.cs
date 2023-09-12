115622b3834028e0df2d2a8cd68e90ed59b52f96
Zakary Boven <zakaryboven@gmail.com>
2023-09-13 00:42:21 +1200

    fix high astromancer

--- src/WarcraftLegacies.Source/Quests/Sunfury/QuestSolarian.cs
+++ src/WarcraftLegacies.Source/Quests/Sunfury/QuestSolarian.cs
@@ -1,6 +1,7 @@
 ﻿using MacroTools.ArtifactSystem;
 using MacroTools.ObjectiveSystem.Objectives.ArtifactBased;
 using MacroTools.QuestSystem;
+using static War3Api.Common;
 
 namespace WarcraftLegacies.Source.Quests.Sunfury
 {
@@ -9,13 +10,12 @@ namespace WarcraftLegacies.Source.Quests.Sunfury
   /// </summary>
   public sealed class QuestSolarian : QuestData
   {
-
     /// <summary>
     /// Initializes a new instance of the <see cref="QuestSolarian"/> class.
     /// </summary>
     /// <param name="essence">Required to complete the quest.</param>
     public QuestSolarian(Artifact essence) : base("The High Astromancer",
-      "The Astromancers research the Void. There would be no better artifact to study than the Essence of Murmur, a powerful Void Lord",
+      "High Astromancer Solarion has long had a fascination with the void, much to the chagrin of her colleagues. With the right research materials in hand, she could become a force to be reckoned with.",
       @"ReplaceableTextures\CommandButtons\BTNSpell_Shadow_SummonVoidWalker.blp")
     {
       AddObjective(new ObjectiveAcquireArtifact(essence));
@@ -24,10 +24,11 @@ namespace WarcraftLegacies.Source.Quests.Sunfury
     }
 
     /// <inheritdoc />
-    protected override string RewardFlavour => "The Essence of Murmur has been studied.";
+    protected override string RewardFlavour =>
+      "Extensive study of Murmur's essence has granted Solarion the power to channel void energies.";
 
     /// <inheritdoc />
-    protected override string RewardDescription => "Unlock Solarian, the High Astromancer at the altar.";
-
+    protected override string RewardDescription =>
+      $"Learn to train High Astromancer Solarion from the {GetObjectName(Constants.UNIT_H0C6_ALTAR_OF_BLOOD_SUNFURY_ALTAR)}";
   }
 }
