6bdbf4a17b755522a78a3df74c65b5d634339d89
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2023-09-19 19:42:42 -0400

    navigation quest removed

--- src/WarcraftLegacies.Source/Setup/QuestSetup/SharedQuestSetup.cs
+++ src/WarcraftLegacies.Source/Setup/QuestSetup/SharedQuestSetup.cs
@@ -24,10 +24,8 @@ namespace WarcraftLegacies.Source.Setup.QuestSetup
       var tombOfSargerasQuest = CreateTombOfSargerasQuest(preplacedUnitSystem);
       var ragnarosQuest = CreateRagnarosQuest(preplacedUnitSystem, allLegendSetup);
       var dragonsOfNightmareOne = CreateDragonsOfNightmareQuestOne(preplacedUnitSystem);
-      var navigation = new QuestNavigation();
       foreach (var faction in FactionManager.GetAllFactions())
       {
-        faction.AddQuest(navigation);
         faction.AddQuest(tombOfSargerasQuest);
         faction.AddQuest(new QuestZinrokhAssembly(new List<Artifact>
         {
