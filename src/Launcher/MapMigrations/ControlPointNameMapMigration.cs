d213273abbaf6c9d287562f81487bfeccafb5062
Zakary Boven <zakaryboven@gmail.com>
2023-09-21 00:33:34 +1200

    really fix

--- src/Launcher/MapMigrations/ControlPointNameMapMigration.cs
+++ src/Launcher/MapMigrations/ControlPointNameMapMigration.cs
@@ -15,7 +15,7 @@ namespace Launcher.MapMigrations
       if (unitSkins == null)
         return;
 
-      const int textNameId = 1835101796;
+      const int textNameId = 1835101813;
       foreach (var unitSkin in unitSkins)
       {
         var textNameModification = unitSkin.GetDataModification(textNameId);
