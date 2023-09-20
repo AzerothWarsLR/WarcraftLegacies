0668df9851b664179a0008868a00f3a65e4f42ed
Zakary Boven <zakaryboven@gmail.com>
2023-09-20 23:20:08 +1200

    Automatically migrate creep gold bounty values based on a formula (#2187)

---
+++ src/Launcher/Extensions/SimpleObjectModificationExtensions.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using System.Linq;
+using War3Net.Build.Object;
+
+namespace Launcher.Extensions
+{
+  public static class SimpleObjectModificationExtensions
+  {
+    public static bool TryGetDataModification(this SimpleObjectModification simpleObjectModification,
+      int key, out SimpleObjectDataModification? dataModification)
+    {
+      dataModification = simpleObjectModification.Modifications.FirstOrDefault(x => x.Id == key);
+      return dataModification != null;
+    }
+  }
+}
---
+++ src/Launcher/MapDataMigrations/GoldBountyMigration.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Launcher.Extensions;
+using War3Api.Object;
+using War3Api.Object.Enums;
+using War3Net.Build;
+
+namespace Launcher.MapDataMigrations
+{
+  /// <summary>
+  /// Assigns gold bounties based on level to all units in the map.
+  /// </summary>
+  public sealed class GoldBountyMigration
+  {
+    public void Migrate(Map map, ObjectDatabase objectDatabase)
+    {
+      foreach (var unit in objectDatabase.GetUnits().Where(IsCreep))
+      {
+        var level = unit.StatsLevel;
+        unit.StatsGoldBountyAwardedBase = level;
+        unit.StatsGoldBountyAwardedNumberOfDice = level;
+        unit.StatsGoldBountyAwardedSidesPerDie = 4;
+      }
+
+      map.UnitObjectData = objectDatabase.GetAllData().UnitData;
+    }
+    
+    private static bool IsCreep(Unit unit) => unit.StatsRace == UnitRace.Creeps && !unit.StatsIsABuilding;
+  }
+}
