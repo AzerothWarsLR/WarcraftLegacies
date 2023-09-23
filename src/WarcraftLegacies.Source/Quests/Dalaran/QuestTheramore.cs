de8bca31dcf3bcbf37eb2ffe7326e671bd5139d2
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2023-09-23 14:57:22 -0400

    Update QuestTheramore.cs

--- src/WarcraftLegacies.Source/Quests/Dalaran/QuestTheramore.cs
+++ src/WarcraftLegacies.Source/Quests/Dalaran/QuestTheramore.cs
@@ -30,7 +30,7 @@ namespace WarcraftLegacies.Source.Quests.Dalaran
       "The distant lands of Kalimdor remain untouched by human civilization. If the Third War proceeds poorly, it may become necessary to establish a forward base there.",
       @"ReplaceableTextures\CommandButtons\BTNHumanArcaneTower.blp")
     {
-      AddObjective(new ObjectiveResearch(Constants.UPGRADE_R0A7, Constants.UNIT_H002_THE_VIOLET_CITADEL_DALARAN_OTHER));
+      AddObjective(new ObjectiveResearch(Constants.UPGRADE_R0A7_ESCAPE_TO_THERAMORE_DALARAN, Constants.UNIT_H002_THE_VIOLET_CITADEL_DALARAN_OTHER));
       AddObjective(new ObjectiveSelfExists());
       _rescueUnits = theramoreRect.PrepareUnitsForRescue(RescuePreparationMode.HideNonStructures);
     }
