861ba56ff8bba16d510055e3044853d1750c2bfe
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2023-09-23 11:16:12 -0400

    Thunderbluff is now capturable
    Taurensand spirit walkers now requires thunderbluff

--- src/WarcraftLegacies.Source/Quests/Druids/QuestDruidsKillFrostwolf.cs
+++ src/WarcraftLegacies.Source/Quests/Druids/QuestDruidsKillFrostwolf.cs
@@ -1,4 +1,4 @@
-using MacroTools.Extensions;
+﻿using MacroTools.Extensions;
 using MacroTools.FactionSystem;
 using MacroTools.LegendSystem;
 using MacroTools.ObjectiveSystem.Objectives.LegendBased;
@@ -22,7 +22,7 @@ namespace WarcraftLegacies.Source.Quests.Druids
       "The Frostwolf Clan has arrived on the shores of Kalimdor. Though their respect of the wild spirits is to be admired, their presence cannot be tolerated.",
       @"ReplaceableTextures\CommandButtons\BTNHeroTaurenChieftain.blp")
     {
-      AddObjective(new ObjectiveCapitalDead(thunderBluff));
+      AddObjective(new ObjectiveControlCapital(thunderBluff, false));
       ResearchId = Constants.UPGRADE_R044_QUEST_COMPLETED_NATURAL_CONTEST_DRUIDS;
     }
     
