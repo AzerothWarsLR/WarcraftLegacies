7ce005a1d20a298bf90f3eccb83e0e58700e32e5
B.Towner <59606542+vampirika@users.noreply.github.com>
2023-09-24 01:56:09 +0100

    Update text and objectives of Ratchet Quest
    Removed T3 objective.
    Updated flavour text and reward description.
    Removed music on completion.

--- src/WarcraftLegacies.Source/Quests/Goblin/QuestRatchet.cs
+++ src/WarcraftLegacies.Source/Quests/Goblin/QuestRatchet.cs
@@ -25,12 +25,11 @@ namespace WarcraftLegacies.Source.Quests.Goblin
     /// </summary>
     /// <param name="noggenfogger">Must be brought somewhere to complete the quest.</param>
     public QuestRatchet(LegendaryHero noggenfogger) : base("Ratchet Port",
-      "The Port of Ratchet would be a great expansion of our trade empire.",
+      "The port of Ratchet would be a great expansion of our trade empire.",
       @"ReplaceableTextures\CommandButtons\BTNGoblinShop2.blp")
     {
       AddObjective(new ObjectiveControlPoint(ControlPointManager.Instance.GetFromUnitType(Constants.UNIT_N0A6_RATCHET)));
       AddObjective(new ObjectiveLegendInRect(noggenfogger, Regions.Ratchet_Unlock_1, "Ratchet"));
-      AddObjective(new ObjectiveUpgrade(Constants.UNIT_O03N_FORTRESS_GOBLIN_T3, Constants.UNIT_O03L_GREAT_HALL_GOBLIN_T1));
       AddObjective(new ObjectiveSelfExists());
       Required = true;
       ResearchId = Constants.UPGRADE_VQ01_QUEST_COMPLETED_RATCHET_PORT;
@@ -39,10 +38,10 @@ namespace WarcraftLegacies.Source.Quests.Goblin
     }
 
     /// <inheritdoc />
-    protected override string RewardFlavour => "We have succesfully established a port at Ratchet!";
+    protected override string RewardFlavour => "With a port now established near Orgrimmar, we now have an outpost to support the Horde war effort in the North!";
 
     /// <inheritdoc />
-    protected override string RewardDescription => "You have unlocked Ratchet.";
+    protected override string RewardDescription => "You gain control of a small outpost in Ratchet";
 
     /// <inheritdoc />
     protected override void OnFail(Faction completingFaction)
@@ -57,9 +56,6 @@ namespace WarcraftLegacies.Source.Quests.Goblin
     /// <inheritdoc />
     protected override void OnComplete(Faction completingFaction)
     {
-      completingFaction.Player.RescueGroup(_rescueUnits);
-      if (GetLocalPlayer() == completingFaction.Player) 
-        PlayThematicMusic("war3mapImported\\GoblinTheme.mp3");
     }
   }
 }
