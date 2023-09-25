ab7ea7d8546e3028e5c29172071dc170e122c83b
Zakary Boven <zakaryboven@gmail.com>
2023-09-26 01:48:08 +1300

    Added the Immortality power for Druids and Sentinels (#2244)

--- src/WarcraftLegacies.Source/Setup/AllFactionSetup.cs
+++ src/WarcraftLegacies.Source/Setup/AllFactionSetup.cs
@@ -5,15 +5,15 @@ namespace WarcraftLegacies.Source.Setup
 {
   public static class AllFactionSetup
   {
-    public static void Setup(PreplacedUnitSystem preplacedUnitSystem, ArtifactSetup artifactSetup)
+    public static void Setup(PreplacedUnitSystem preplacedUnitSystem, ArtifactSetup artifactSetup, AllLegendSetup allLegendSetup)
     {
       ScourgeSetup.Setup(preplacedUnitSystem, artifactSetup.HelmOfDomination);
       LegionSetup.Setup(preplacedUnitSystem);
       LordaeronSetup.Setup(preplacedUnitSystem);
       DalaranSetup.Setup(preplacedUnitSystem);
       QuelthalasSetup.Setup(preplacedUnitSystem);
-      SentinelsSetup.Setup(preplacedUnitSystem);
-      DruidsSetup.Setup(preplacedUnitSystem);
+      SentinelsSetup.Setup(preplacedUnitSystem, allLegendSetup);
+      DruidsSetup.Setup(preplacedUnitSystem, allLegendSetup);
       FelHordeSetup.Setup(preplacedUnitSystem);
       FrostwolfSetup.Setup(preplacedUnitSystem);
       WarsongSetup.Setup(preplacedUnitSystem);
