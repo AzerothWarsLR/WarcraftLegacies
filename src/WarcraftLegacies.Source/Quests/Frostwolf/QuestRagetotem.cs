ded6ad689509c204f78e49be07e613a14cd65f67
Sebastian Tanoni <47048283+SebastianTanoni@users.noreply.github.com>
2023-09-29 19:36:26 -0400

    Update src/WarcraftLegacies.Source/Quests/Frostwolf/QuestRagetotem.cs
    Co-authored-by: Zakary Boven <zakaryboven@gmail.com>

--- src/WarcraftLegacies.Source/Quests/Frostwolf/QuestRagetotem.cs
+++ src/WarcraftLegacies.Source/Quests/Frostwolf/QuestRagetotem.cs
@@ -24,7 +24,7 @@ namespace WarcraftLegacies.Source.Quests.Frostwolf
 
     /// <inheritdoc />
     protected override string RewardFlavour =>
-      "Impressed by Cairne's prowess, the Ragetotem tribe has joined the Bloodhoof!";
+      "Tales of Cairne's strength and wisdom reverberate throughout Kalimdor. As strength is drawn to strength, the Ragetotem are drawn to the Bloodhoof.";
 
     /// <inheritdoc />
     protected override string RewardDescription =>
