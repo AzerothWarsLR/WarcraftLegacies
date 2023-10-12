9284370ba2bfbca061ccb35d1e800e8021c817fe
Zakary Boven <zakaryboven@gmail.com>
2023-10-12 15:09:00 +1300

    Refactor game mode selection (#2375)

--- src/WarcraftLegacies.Source/Commands/InviteCommand.cs
+++ src/WarcraftLegacies.Source/Commands/InviteCommand.cs
@@ -16,46 +16,40 @@ namespace WarcraftLegacies.Source.Commands
     {
       var enteredString = GetEventPlayerChatString();
       var triggerPlayer = GetTriggerPlayer();
+      
+      if (SubString(enteredString, 0, StringLength(Command)) != Command) return;
+      var content = SubString(enteredString, StringLength(Command), StringLength(enteredString));
+      content = StringCase(content, false);
 
-      if (OpenAllianceVote.AreAlliancesOpen)
+      if (!FactionManager.FactionWithNameExists(content))
       {
-        if (SubString(enteredString, 0, StringLength(Command)) != Command) return;
-        var content = SubString(enteredString, StringLength(Command), StringLength(enteredString));
-        content = StringCase(content, false);
-
-        if (!FactionManager.FactionWithNameExists(content))
-        {
-          DisplayTextToPlayer(triggerPlayer, 0, 0, $"There is no Faction with the name {content}.");
-          return;
-        }
-
-        var targetFaction = FactionManager.GetFromName(content);
-
-        if (triggerPlayer.GetFaction() == targetFaction)
-        {
-          DisplayTextToPlayer(triggerPlayer, 0, 0, "You can'invite yourself to your own team.");
-          return;
-        }
-
-        if (targetFaction.Player == null)
-        {
-          DisplayTextToPlayer(triggerPlayer, 0, 0,
-            $"There is no player with the Faction {targetFaction.PrefixCol} {targetFaction.Name}|r.");
-          return;
-        }
-
-        if (targetFaction.Player != null) triggerPlayer.GetTeam()?.Invite(targetFaction.Player);
+        DisplayTextToPlayer(triggerPlayer, 0, 0, $"There is no Faction with the name {content}.");
+        return;
       }
-      else
+
+      var targetFaction = FactionManager.GetFromName(content);
+
+      if (triggerPlayer.GetFaction() == targetFaction)
       {
-        DisplayTextToPlayer(triggerPlayer, 0, 0, "You can!ally yet");
+        DisplayTextToPlayer(triggerPlayer, 0, 0, "You can'invite yourself to your own team.");
+        return;
       }
+
+      if (targetFaction.Player == null)
+      {
+        DisplayTextToPlayer(triggerPlayer, 0, 0,
+          $"There is no player with the Faction {targetFaction.PrefixCol} {targetFaction.Name}|r.");
+        return;
+      }
+
+      if (targetFaction.Player != null) triggerPlayer.GetTeam()?.Invite(targetFaction.Player);
     }
 
     public static void Setup()
     {
       var trig = CreateTrigger();
-      foreach (var player in WCSharp.Shared.Util.EnumeratePlayers()) TriggerRegisterPlayerChatEvent(trig, player, Command, false);
+      foreach (var player in WCSharp.Shared.Util.EnumeratePlayers())
+        TriggerRegisterPlayerChatEvent(trig, player, Command, false);
       TriggerAddAction(trig, Actions);
     }
   }
---
+++ src/WarcraftLegacies.Source/GameModes/OpenAlliance.cs
@@ -0,0 +1,37 @@
+﻿using MacroTools.GameModes;
+using MacroTools.HintSystem;
+using WarcraftLegacies.Source.Commands;
+using WarcraftLegacies.Source.GameLogic.GameEnd;
+using static War3Api.Common;
+
+namespace WarcraftLegacies.Source.GameModes
+{
+  public sealed class OpenAlliance : IGameMode
+  {
+    /// <inheritdoc />
+    public string Name => "Open Alliance";
+
+    /// <inheritdoc />
+    public void OnChoose()
+    {
+      DisplayTextToPlayer(GetLocalPlayer(), 0, 0, "Alliances are open!");
+      SetupAllianceCommands();
+      SetupControlPointVictory();
+    }
+    
+    private static void SetupAllianceCommands()
+    {
+      Hint.Register(new Hint("You can change alliances by using the commands -invite, -uninvite, -join, and -unally."));
+      InviteCommand.Setup();
+      JoinCommand.Setup();
+      UnallyCommand.Setup();
+      UninviteCommand.Setup();
+    }
+
+    private static void SetupControlPointVictory()
+    {
+      ControlPointVictory.Setup();
+      Hint.Register(new Hint($"Win the game by capturing {ControlPointVictory.CpsVictory} Control Points."));
+    }
+  }
+}
