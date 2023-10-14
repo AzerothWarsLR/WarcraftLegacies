15c4464873d08b74ba8d45a98d9df26e9a8da2e8
Zakary Boven <zakaryboven@gmail.com>
2023-10-14 18:37:46 +1300

    More object editor stuff

---
+++ src/WarcraftLegacies.Source/Spells/RendSoul.cs
@@ -0,0 +1,34 @@
+﻿using MacroTools.SpellSystem;
+using static War3Api.Common;
+using WCSharp.Shared.Data;
+
+namespace WarcraftLegacies.Source.Spells
+{
+  /// <summary>
+  /// Kill the target unit to restore hit points and mana based on its maximum hit points, and create a summoned unit at
+  /// its position.
+  /// </summary>
+  public sealed class RendSoul : Spell
+  {
+    public float HitPointsPerTargetMaximumHitPoints { get; init; }
+    
+    public float ManaPointsPerTargetMaximumHitPoints { get; init; }
+    
+    public int UnitTypeSummoned { get; init; }
+
+    public string EffectTarget { get; init; } = "";
+
+    public string EffectCaster { get; init; } = "";
+    
+    /// <inheritdoc />
+    public RendSoul(int id) : base(id)
+    {
+    }
+
+    /// <inheritdoc />
+    public override void OnCast(unit caster, unit target, Point targetPoint)
+    {
+      
+    }
+  }
+}
