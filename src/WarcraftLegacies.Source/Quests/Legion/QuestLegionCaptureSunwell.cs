613602530ebe051c302e966a5d6a26fce111101a
Sebastian Tanoni <47048283+SebastianTanoni@users.noreply.github.com>
2023-10-19 22:07:06 -0400

    Implemented Legion rework (#2467)
    Dreadlord balance changes, Nathrezim balance, Alterac and shrines at plague bases

--- src/WarcraftLegacies.Source/Quests/Legion/QuestLegionCaptureSunwell.cs
+++ src/WarcraftLegacies.Source/Quests/Legion/QuestLegionCaptureSunwell.cs
@@ -1,4 +1,4 @@
-using MacroTools.Extensions;
+﻿using MacroTools.Extensions;
 using MacroTools.FactionSystem;
 using MacroTools.LegendSystem;
 using MacroTools.ObjectiveSystem.Objectives.LegendBased;
@@ -8,31 +8,23 @@ namespace WarcraftLegacies.Source.Quests.Legion
 {
   public sealed class QuestLegionCaptureSunwell : QuestData
   {
-    private const int RewardResearchTypeId = Constants.UPGRADE_R054_STOLEN_POWER_LEGION_QUEST;
+    private const int RewardResearchTypeId = Constants.UPGRADE_R054_QUEST_COMPLETED_FALL_OF_SILVERMOON;
     
     public QuestLegionCaptureSunwell(Capital sunwell) : base("Fall of Silvermoon",
       "The Sunwell is the source of the High Elves' immortality and magical prowess. Under control of the Scourge, it would be the source of immense necromantic power.",
       @"ReplaceableTextures\CommandButtons\BTNOrbOfCorruption.blp")
     {
       AddObjective(new ObjectiveControlCapital(sunwell, false));
+      ResearchId = Constants.UPGRADE_R054_QUEST_COMPLETED_FALL_OF_SILVERMOON;
     }
 
     /// <inheritdoc/>
     protected override string RewardFlavour =>
-      "The Sunwell has been captured by the Scourge. It now writhes with necromantic energy.";
+      "The Sunwell has been captured by the Legion. It now writhes with demonic energy.";
 
     /// <inheritdoc/>
-    protected override string RewardDescription => "A research improving your Dreadlords";
+    protected override string RewardDescription => "Improves Dreadlords and Nathrezim by increasing their attack damage by 20, movement speed by 20, hit points by 200, and granting them the ability to cast Astral Walk";
 
-    /// <inheritdoc/>
-    protected override void OnAdd(Faction whichFaction) => 
-      whichFaction.ModObjectLimit(RewardResearchTypeId, Faction.UNLIMITED);
 
-    /// <inheritdoc />
-    protected override void OnComplete(Faction whichFaction)
-    {
-      whichFaction.SetObjectLevel(RewardResearchTypeId, 1);
-      whichFaction.Player?.DisplayResearchAcquired(RewardResearchTypeId, 1);
-    }
   }
 }
