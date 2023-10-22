8a5b3977f48253bed1c597d29759416fb994f341
Mark Ferguson <mjferguson404@gmail.com>
2023-10-22 18:56:07 +0100

    Update MultiTargetRecall.cs

--- src/MacroTools/Spells/MultiTargetRecall.cs
+++ src/MacroTools/Spells/MultiTargetRecall.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using MacroTools.Extensions;
 using MacroTools.SpellSystem;
-using War3Api;
 using WCSharp.Shared.Data;
 using static War3Api.Common;
 
