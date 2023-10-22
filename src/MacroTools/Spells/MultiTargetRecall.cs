f2adba675dde858b9a359b9facde8bffb8f059b2
Mark Ferguson <mjferguson404@gmail.com>
2023-10-22 18:52:27 +0100

    Update MultiTargetRecall.cs
    Don't move units that are resistant
    Move units when done casting instead of on cast

--- src/MacroTools/Spells/MultiTargetRecall.cs
+++ src/MacroTools/Spells/MultiTargetRecall.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using MacroTools.Extensions;
 using MacroTools.SpellSystem;
+using War3Api;
 using WCSharp.Shared.Data;
 using static War3Api.Common;
 
@@ -16,6 +17,8 @@ namespace MacroTools.Spells
     private DummyCast.CastFilter CastFilter { get; }
     
     public SpellTargetType TargetType { get; init; } = SpellTargetType.None;
+
+    private Point? Center { get; set; }
     
     public MultiTargetRecall(int id, DummyCast.CastFilter castFilter) : base(id)
     {
@@ -24,15 +27,21 @@ namespace MacroTools.Spells
     
     public override void OnCast(unit caster, unit target, Point targetPoint)
     {
-      base.OnCast(caster, target, targetPoint);
-      var center = TargetType == SpellTargetType.None ? new Point(GetUnitX(caster), GetUnitY(caster)) : targetPoint;
+      Center = TargetType == SpellTargetType.None ? new Point(GetUnitX(caster), GetUnitY(caster)) : targetPoint;
+    }
+
+    public override void OnStop(unit caster)
+    {
+      if (Center == null) return;
       foreach (var unit in CreateGroup()
-                 .EnumUnitsInRange(center, Radius).EmptyToList()
-                 .FindAll(unit => CastFilter(caster, unit))
+                 .EnumUnitsInRange(Center, Radius).EmptyToList()
+                 .FindAll(unit => CastFilter(caster, unit) && !unit.IsResistant())
                  .Take(AmountToTarget))
       {
         SetUnitPosition(unit, GetUnitX(caster), GetUnitY(caster));
       }
     }
+
+    
   }
 }
