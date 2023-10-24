a7668f53a62b5326df0526da14f62209139e6810
Ripzss <105180704+Ripzss@users.noreply.github.com>
2023-10-24 17:13:46 +0200

    New Gilneas Quest to unlock Darius Crowley. The Rebel
    Changed tooltips on heroes
    Changed Darius Crowly Healing Spray -> Howl of Terror
    New Quest The Rebel: to unlock Darius Crowley
    Darius Crowley now starts lvl 7
    Expanded some regions to hopefully fix shipsyards in gilneas not unlocking properly

---
+++ src/WarcraftLegacies.Source/Quests/Gilneas/QuestCrowley.cs
@@ -0,0 +1,33 @@
+﻿using MacroTools.ObjectiveSystem.Objectives.TimeBased;
+using MacroTools.QuestSystem;
+using static War3Api.Common;
+
+namespace WarcraftLegacies.Source.Quests.Gilneas
+{
+  /// <summary>
+  /// Darius gets released from Prison " 
+  /// </summary>
+  public sealed class QuestCrowley : QuestData
+  {
+    private const int AltarId = Constants.UNIT_H02X_ALTAR_OF_KINGS_GILNEAS_ALTAR;
+
+    /// <summary>
+    /// IniInitializes a new instance of the <see cref="QuestCrowley"/> class.
+    /// </summary>
+    public QuestCrowley() : base("The Rebel",
+      "Darius Crowley has been imprisoned for a long time, if Gilneas would be in great peril, an early release might be the soloution!",
+      @"ReplaceableTextures/CommandButtons/BTNWorgenHunt.blp")
+    {
+      AddObjective(new ObjectiveTime(900));
+      ResearchId = Constants.UPGRADE_MD05_QUEST_COMPLETED_THE_REBEL;
+    }
+
+    /// <inheritdoc/>
+    protected override string RewardFlavour =>
+      "With the threat of the Scourge and the Plague, Genn Greymane has decided to forgive Darius Crowly for the Northgate Rebellion so that he can help gilneas in their dire times.";
+
+    /// <inheritdoc/>
+    protected override string RewardDescription =>
+      $"Learn to train Darius Crowley from the {GetObjectName(AltarId)}";
+  }
+}
