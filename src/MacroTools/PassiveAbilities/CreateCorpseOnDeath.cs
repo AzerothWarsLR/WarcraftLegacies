c90656592467fad69a0d3040173997743463f43b
Sebastian Tanoni <47048283+SebastianTanoni@users.noreply.github.com>
2023-10-26 16:46:30 -0400

    Implemented Storm Cloud and Undying Frenzy, removed Skeletal Frame, and fixed Dark Summoning (#2513)

---
+++ src/MacroTools/PassiveAbilities/CreateCorpseOnDeath.cs
@@ -0,0 +1,45 @@
+﻿using MacroTools.Extensions;
+using MacroTools.PassiveAbilitySystem;
+using static War3Api.Common;
+
+namespace MacroTools.PassiveAbilities
+{
+  /// <summary>
+  /// Causes the unit to summon a unit upon death.
+  /// </summary>
+  public sealed class CreateCorpseOnDeath : PassiveAbility
+  {
+    
+    /// <summary>
+    /// The unit type corpse to summon on death.
+    /// </summary>
+    public int CorpseUnitTypeId { get; init; }
+
+    /// <summary>
+    /// How many corpses to summon.
+    /// </summary>
+    public int CorpseCount { get; init; } = 1;
+    
+    /// <summary>
+    /// The player must have this research for the ability to take effect.
+    /// </summary>
+    public int RequiredResearch { get; init; }
+    
+    /// <inheritdoc />
+    public CreateCorpseOnDeath(int unitTypeId) : base(unitTypeId)
+    {
+    }
+    
+    /// <inheritdoc />
+    public override void OnDeath()
+    {
+      var triggerUnit = GetTriggerUnit();
+      if (RequiredResearch != 0 && (GetPlayerTechCount(triggerUnit.OwningPlayer(), RequiredResearch, false) == 0))
+        return;
+      var pos = triggerUnit.GetPosition();
+      for (var i = 0; i < CorpseCount; i++)
+        CreateCorpse(triggerUnit.OwningPlayer(), CorpseUnitTypeId, pos.X, pos.Y, triggerUnit.GetFacing());
+      triggerUnit.Remove();
+    }
+  }
+}
