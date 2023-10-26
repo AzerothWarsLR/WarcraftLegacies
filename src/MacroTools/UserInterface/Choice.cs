8fda71f7c9127fd9133651edfcb5f70ab0c48c77
Zakary Boven <zakaryboven@gmail.com>
2023-10-26 23:38:05 +1300

    Fix nullability

--- src/MacroTools/UserInterface/Choice.cs
+++ src/MacroTools/UserInterface/Choice.cs
@@ -4,12 +4,12 @@
   public sealed class Choice<T>
   {
     /// <summary>Internal data that the choice needs to know about.</summary>
-    public T? Data { get; }
+    public T Data { get; }
 
     /// <summary>The text shown to the player.</summary>
     public string Name { get; }
 
-    public Choice(T? data, string name)
+    public Choice(T data, string name)
     {
       Data = data;
       Name = name;
