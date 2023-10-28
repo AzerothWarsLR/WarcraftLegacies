7a08f441b2be68f913237e665213803d39ed1598
B.Towner <59606542+vampirika@users.noreply.github.com>
2023-10-28 14:20:09 +0100

    Further unit tweaks / fixes
    Wrapped level formula in check if is building so buildings do not get level assigned.
    Normalised boss dragon health to 1800 and adjusted formula so they are all level 10.
    Added hp to Murmur to make level 10.

--- src/Launcher/MapMigrations/CreepLevelMapMigration.cs
+++ src/Launcher/MapMigrations/CreepLevelMapMigration.cs
@@ -14,40 +14,44 @@ namespace Launcher.MapMigrations
     {
       foreach (var unit in objectDatabase.GetUnits())
       {
-        var level = 0;
-        var unitHealth = unit.StatsHitPointsMaximumBase;
-        var remainder = unitHealth % healthFactor;
-
-        if (remainder > 0)
-          unitHealth -= remainder;
-
-        var healthDividedByFactor = unitHealth / healthFactor;
-
-        if (healthDividedByFactor < 8)
+        if (!unit.StatsIsABuilding)
         {
-          level += healthDividedByFactor;
-        } else
-        {
-          level += 7;
+          var level = 0;
+          var unitHealth = unit.StatsHitPointsMaximumBase;
+          var remainder = unitHealth % healthFactor;
+
+          if (remainder > 0)
+            unitHealth -= remainder;
+
+          var healthDividedByFactor = unitHealth / healthFactor;
+
+          if (healthDividedByFactor < 8)
+          {
+            level += healthDividedByFactor;
+          }
+          else
+          {
+            level += 7;
+          }
+
+          if (unitHealth >= 1800)
+            level += 1;
+
+          if (unitHealth >= 2400)
+            level += 1;
+
+          if (unitHealth >= 3000)
+            level += 1;
+
+          if (unit.CombatAttack1AttackType.Equals(War3Api.Object.Enums.AttackType.Chaos) || unit.CombatAttack1AttackType.Equals(War3Api.Object.Enums.AttackType.Hero))
+            level += 1;
+
+          // is a flying unit
+          if (unit.MovementHeight > 100)
+            level += 1;
+
+          unit.StatsLevel = level;
         }
-
-        if (unitHealth >= 2000)
-          level += 1;
-
-        if (unitHealth >= 2500)
-          level += 1;
-
-        if (unitHealth >= 3000)
-          level += 1;
-
-        if (unit.CombatAttack1AttackType.Equals(War3Api.Object.Enums.AttackType.Chaos) || unit.CombatAttack1AttackType.Equals(War3Api.Object.Enums.AttackType.Hero))
-          level += 1;
-
-        // is a flying unit
-        if (unit.MovementHeight > 100)
-          level += 1;
-
-        unit.StatsLevel = level;
       }
 
       map.UnitObjectData = objectDatabase.GetAllData().UnitData;
