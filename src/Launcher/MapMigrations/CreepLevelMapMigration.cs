458e4d0aea0ff4148b5bcebb676cc8b6d02bf7df
B.Towner <59606542+vampirika@users.noreply.github.com>
2023-11-11 10:31:32 +0000

    GoldAndLevelChanges
    Added level formula.
    Updated gold formula

---
+++ src/Launcher/MapMigrations/CreepLevelMapMigration.cs
@@ -0,0 +1,60 @@
+﻿using War3Api.Object;
+using War3Net.Build;
+
+namespace Launcher.MapMigrations
+{
+  /// <summary>
+  /// Assigns level to all units in the map based on health, attack type and whether it is flying.
+  /// </summary>
+  public sealed class CreepLevelMapMigration : IMapMigration
+  {
+    private readonly int healthFactor = 200;
+
+    public void Migrate(Map map, ObjectDatabase objectDatabase)
+    {
+      foreach (var unit in objectDatabase.GetUnits())
+      {
+        if (!unit.StatsIsABuilding)
+        {
+          var level = 0;
+          var unitHealth = unit.StatsHitPointsMaximumBase;
+          var remainder = unitHealth % healthFactor;
+
+          if (remainder > 0)
+            unitHealth -= remainder;
+
+          var healthDividedByFactor = unitHealth / healthFactor;
+
+          if (healthDividedByFactor < 8)
+          {
+            level += healthDividedByFactor;
+          }
+          else
+          {
+            level += 7;
+          }
+
+          if (unitHealth >= 1800)
+            level += 1;
+
+          if (unitHealth >= 2400)
+            level += 1;
+
+          if (unitHealth >= 3000)
+            level += 1;
+
+          if (unit.CombatAttack1AttackType.Equals(War3Api.Object.Enums.AttackType.Chaos) || unit.CombatAttack1AttackType.Equals(War3Api.Object.Enums.AttackType.Hero))
+            level += 1;
+
+          // is a flying unit
+          if (unit.MovementHeight > 100)
+            level += 1;
+
+          unit.StatsLevel = level;
+        }
+      }
+
+      map.UnitObjectData = objectDatabase.GetAllData().UnitData;
+    }
+  }
+}
--- src/Launcher/MapMigrations/GoldBountyMapMigration.cs
+++ src/Launcher/MapMigrations/GoldBountyMapMigration.cs
@@ -13,14 +13,32 @@ namespace Launcher.MapMigrations
       foreach (var unit in objectDatabase.GetUnits())
       {
         var level = unit.StatsLevel;
-        if (unit.StatsGoldBountyAwardedBase != level)
-          unit.StatsGoldBountyAwardedBase = level;
-        
-        if (unit.StatsGoldBountyAwardedNumberOfDice != level)
-          unit.StatsGoldBountyAwardedNumberOfDice = level;
-        
-        if (unit.StatsGoldBountyAwardedNumberOfDice != 4)
-          unit.StatsGoldBountyAwardedSidesPerDie = 4;
+        var scalingFactor = 4;
+        var numberOfDiceConstant = 1;
+        var maxValueOnDice = 2;
+        var baseConstant = -2;
+        var baseGold = (level * scalingFactor) + baseConstant;
+
+        if (level >= 7)
+          baseGold += 5;
+
+        if (unit.StatsHitPointsMaximumBase >= 1500)
+          baseGold += 7;
+
+        if (unit.StatsHitPointsMaximumBase >= 2000)
+          baseGold += 10;
+
+        if (unit.StatsHitPointsMaximumBase >= 2500)
+          baseGold += 15;
+
+        if (unit.StatsGoldBountyAwardedBase != baseGold)
+          unit.StatsGoldBountyAwardedBase = baseGold;
+
+        if (unit.StatsGoldBountyAwardedNumberOfDice != level + numberOfDiceConstant)
+          unit.StatsGoldBountyAwardedNumberOfDice = level + numberOfDiceConstant;
+
+        if (unit.StatsGoldBountyAwardedNumberOfDice != maxValueOnDice)
+          unit.StatsGoldBountyAwardedSidesPerDie = maxValueOnDice;
       }
 
       map.UnitObjectData = objectDatabase.GetAllData().UnitData;
