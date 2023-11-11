6db88d856d9ca7cfd1d23c67d832308171941c29
Savantic <mjferguson404@gmail.com>
2023-11-11 22:03:16 +0000

    Added a delay to Dark Summoning (#2653)
    Changed MultiTargetRecall to DelayedMultiTargetRecall
    DelayedMultiTargetRecall uses a buff with a dynamic duration based on distance from the caster, clamped between 3 and 30
    If the caster dies before the buff runs out then only a percentage of units are transported, based on remaining buff duration, and the rest are killed.
    
    There doesn't seem to be a good way to convey remaining time/how long for summon. Maybe one to keep in mind for potential solutions.
    
    For issue #2629

---
+++ src/MacroTools/Buffs/DelayedRecallBuff.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MacroTools.Extensions;
+using WCSharp.Buffs;
+using WCSharp.Shared.Data;
+using static War3Api.Common;
+
+namespace MacroTools.Buffs
+{
+  public sealed class DelayedRecallBuff : PassiveBuff
+  {
+    /// <summary>
+    /// The units that should be moved when the buff is disposed off.
+    /// </summary>
+    private List<unit> UnitsToMove { get;}
+    
+    /// <summary>
+    /// The point of the casting unit when first cast.
+    /// </summary>
+    private Point TargetPosition { get; }
+    
+    /// <summary>The percentage of units to lose when the caster dies (rounded down)</summary>
+    public float DeathPenalty { get; init; }
+    
+    /// <summary>
+    /// The effect that is created when the buff is applied.
+    /// </summary>
+    private new effect? Effect { get; set; }
+
+    /// <inheritdoc />
+    public DelayedRecallBuff(unit caster, unit target, List<unit> unitsToMove) : base(caster, target)
+    {
+      UnitsToMove = unitsToMove;
+      TargetPosition =  new Point(GetUnitX(caster), GetUnitY(caster));
+    }
+    
+    public override void OnApply()
+    {
+      foreach (var unit in UnitsToMove)
+      {
+        unit.Show(false).SetInvulnerable(true);
+      }
+
+      Effect = AddSpecialEffect(@"Abilities\Spells\Undead\Darksummoning\DarkSummonTarget.mdl", TargetPosition.X,
+        TargetPosition.Y);
+    }
+
+    public override void OnDispose()
+    {
+      DestroyEffect(Effect);
+      if (Math.Floor(Duration) > 0)
+      {
+        var amountToMove = (int)Math.Floor(UnitsToMove.Count * DeathPenalty);
+        foreach (var unit in UnitsToMove.Take(UnitsToMove.Count - amountToMove))
+        {
+          unit.Kill();
+        }
+      }
+      
+      foreach (var unit in UnitsToMove)
+      {
+        unit.Show(true).SetPosition(TargetPosition).SetInvulnerable(false);
+      }
+    }
+  }
+}
