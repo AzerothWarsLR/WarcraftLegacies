966f581603def1964e7266733a58529da2b3c657
Zakary Boven <zakaryboven@gmail.com>
2023-11-25 10:27:49 +1300

    Fix compilation errors

--- src/TestMap.Source/Setup/PlayerSetup.cs
+++ src/TestMap.Source/Setup/PlayerSetup.cs
@@ -1,21 +1,10 @@
-﻿using MacroTools.Extensions;
-using TestMap.Source.Setup.FactionSetup.FactionSetup;
-using static War3Api.Common;
-
-namespace TestMap.Source.Setup
+﻿namespace TestMap.Source.Setup
 {
   public static class PlayerSetup
   {
     public static void Setup()
     {
-      Player(0).SetFaction(DalaranSetup.Dalaran);
-      Player(0).SetTeam(TeamSetup.TeamAlliance);
-      
-      Player(1).SetFaction(DraeneiSetup.Draenei);
-      Player(1).SetTeam(TeamSetup.TeamHorde);
-      
-      Player(2).SetFaction(DruidsSetup.FactionDruids);
-      Player(2).SetTeam(TeamSetup.TeamAlliance);
+
     }
   }
 }
