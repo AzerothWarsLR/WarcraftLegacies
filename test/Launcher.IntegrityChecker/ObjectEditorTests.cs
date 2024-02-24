f28ab3b6bb0413cc53e872d9637c550c15b89baf
Zakary Boven <zakaryboven@gmail.com>
2024-02-24 22:25:20 +1300

    Remove preplaced units from inaccessible units list

--- test/Launcher.IntegrityChecker/ObjectEditorTests.cs
+++ test/Launcher.IntegrityChecker/ObjectEditorTests.cs
@@ -5,11 +5,11 @@ using Xunit.Sdk;
 
 namespace Launcher.IntegrityChecker
 {
-  public sealed class UnitTests : IClassFixture<MapTestFixture>
+  public sealed class ObjectEditorTests : IClassFixture<MapTestFixture>
   {
     private readonly MapTestFixture _mapTestFixture;
 
-    public UnitTests(MapTestFixture mapTestFixture)
+    public ObjectEditorTests(MapTestFixture mapTestFixture)
     {
       _mapTestFixture = mapTestFixture;
     }
@@ -19,19 +19,21 @@ namespace Launcher.IntegrityChecker
     {
       var preplacedUnitIds = _mapTestFixture.Map.Units!.Units.Select(x => x.TypeId).ToHashSet();
       var preplacedUnitTypes = _mapTestFixture.ObjectDatabase.GetUnits().Where(x => preplacedUnitIds.Contains(x.NewId));
-      
       var inaccessibleUnits = _mapTestFixture.ObjectDatabase.GetUnits().ToList();
 
       foreach (var preplacedUnit in preplacedUnitTypes)
+      {
+        inaccessibleUnits.Remove(preplacedUnit);
         foreach (var preplacedUnitChild in GetChildren(preplacedUnit))
           inaccessibleUnits.Remove(preplacedUnitChild);
+      }
 
       if (inaccessibleUnits.Count <= 0) 
         return;
       
       var exceptionMessageBuilder = new StringBuilder();
       exceptionMessageBuilder.AppendLine(
-        "There is no way to train, summon, or spawn the following units. Remove them from the map or add a way for the player to get them.");
+        $"There is no way to train, summon, or spawn the following {inaccessibleUnits.Count} units. Remove them from the map or add a way for the player to get them.");
       foreach (var unit in inaccessibleUnits)
         exceptionMessageBuilder.AppendLine(GetReadableId(unit));
       
@@ -46,6 +48,14 @@ namespace Launcher.IntegrityChecker
           yield return trainedUnit;
           foreach (var trainedUnitChildren in GetChildren(trainedUnit))
             yield return trainedUnitChildren;
+        }
+
+      if (unit.IsTechtreeStructuresBuiltModified)
+        foreach (var builtStructure in unit.TechtreeStructuresBuilt)
+        {
+          yield return builtStructure;
+          // foreach (var builtStructureChildren in GetChildren(builtStructure))
+          //   yield return builtStructureChildren;
         }
     }
 
