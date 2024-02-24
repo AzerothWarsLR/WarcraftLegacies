900798ab9021fbdf8d19882e6d81bf3c4bf31030
Zakary Boven <zakaryboven@gmail.com>
2024-02-24 20:53:36 +1300

    refactor

---
+++ test/Launcher.IntegrityChecker/MapTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using Launcher.Services;
+using Launcher.Settings;
+using War3Net.Build;
+
+namespace Launcher.IntegrityChecker
+{
+  /// <summary>
+  /// Provides a fully constructed Warcraft Legacies map.
+  /// </summary>
+  public sealed class MapTestFixture
+  {
+    public Map Map { get; }
+
+    public MapTestFixture()
+    {
+      (Map, _) = MapDataProvider.GetMapData();
+      AdvancedMapBuilder.AddCSharpCode(Map, "../../../../../src/WarcraftLegacies.Source/", new CompilerSettings());
+    }
+  }
+}
