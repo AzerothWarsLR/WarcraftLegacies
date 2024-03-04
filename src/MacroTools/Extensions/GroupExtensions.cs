c07054551c5f6133b1e75640c7ef51a1698bf4ef
Zakary Boven <zakaryboven@gmail.com>
2024-03-05 08:06:05 +1300

    Introduced GroupUtils as a replacement for GroupExtensions

--- src/MacroTools/Extensions/GroupExtensions.cs
+++ src/MacroTools/Extensions/GroupExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using WCSharp.Shared.Data;
+using WCSharp.Shared.Extensions;
 
 
 namespace MacroTools.Extensions
@@ -11,83 +12,13 @@ namespace MacroTools.Extensions
   {
     /// <summary>
     ///   Empties the units in this group into a List,
-    ///   then destroys it.
+    ///   then clears it.
     /// </summary>
     public static List<unit> EmptyToList(this group whichGroup)
     {
-      var list = new List<unit>();
-      var firstOfGroup = FirstOfGroup(whichGroup);
-      while (firstOfGroup != null)
-      {
-        list.Add(firstOfGroup);
-        GroupRemoveUnit(whichGroup, firstOfGroup);
-        firstOfGroup = FirstOfGroup(whichGroup);
-      }
-      DestroyGroup(whichGroup);
+      var list = whichGroup.ToList();
+      list.Clear();
       return list;
-    }
-
-    /// <summary>
-    /// Adds an additional unit to the group.
-    /// </summary>
-    public static group Add(group whichGroup, unit unit)
-    {
-      GroupAddUnit(whichGroup, unit);
-      return whichGroup;
-    }
-
-    /// <summary>
-    /// Adds all units selected by the provided player to the group.
-    /// </summary> 
-    public static  group EnumSelectedUnits(this group whichGroup, player whichPlayer)
-    {
-      SyncSelections();
-      GroupEnumUnitsSelected(whichGroup, whichPlayer, null);
-      return whichGroup;
-    }
-
-    /// <summary>
-    /// Adds all units of the provided type to the group.
-    /// </summary>
-    public static  group EnumUnitsOfType(this group whichGroup, int unitType)
-    {
-      GroupEnumUnitsOfType(whichGroup, GetObjectName(unitType), null);
-      return whichGroup;
-    }
-
-    /// <summary>
-    /// Adds all units owned by the provided player to the group.
-    /// </summary>
-    public static  group EnumUnitsOfPlayer(this group whichGroup, player player)
-    {
-      GroupEnumUnitsOfPlayer(whichGroup, player, null);
-      return whichGroup;
-    }
-
-    /// <summary>
-    /// Adds all units positioned in the provided <see cref="Rectangle"/> to the group>.
-    /// </summary>
-    public static  group EnumUnitsInRect(this group whichGroup, Rectangle rect)
-    {
-      return EnumUnitsInRect(whichGroup, rect.Rect);
-    }
-
-    /// <summary>
-    /// Adds all units positioned in the provided <see cref="rect"/> to the group.
-    /// </summary>
-    public static  group EnumUnitsInRect(this group whichGroup, rect rect)
-    {
-      GroupEnumUnitsInRect(whichGroup, rect, null);
-      return whichGroup;
-    }
-
-    /// <summary>
-    /// Adds all units within range of the specified <see cref="Point"/> to the group.
-    /// </summary>
-    public static  group EnumUnitsInRange(this group whichGroup, Point point, float radius)
-    {
-      GroupEnumUnitsInRange(whichGroup, point.X, point.Y, radius, null);
-      return whichGroup;
     }
 
     /// <summary>
---
+++ src/MacroTools/Utils/GroupUtils.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using MacroTools.Extensions;
+using WCSharp.Shared.Data;
+
+namespace MacroTools.Utils
+{
+  public static class GroupUtils
+  {
+    private static readonly group UniversalGroup = CreateGroup();
+
+    /// <summary>
+    /// Adds all units selected by the provided player to the group.
+    /// </summary> 
+    public static List<unit> GetSelectedUnits(player whichPlayer)
+    {
+      SyncSelections();
+      GroupEnumUnitsSelected(UniversalGroup, whichPlayer, null);
+      return UniversalGroup.EmptyToList();
+    }
+
+    /// <summary>
+    /// Adds all units of the provided type to the group.
+    /// </summary>
+    public static List<unit> GetUnitsOfType(int unitType)
+    {
+      GroupEnumUnitsOfType(UniversalGroup, GetObjectName(unitType), null);
+      return UniversalGroup.EmptyToList();
+    }
+
+    /// <summary>
+    /// Adds all units owned by the provided player to the group.
+    /// </summary>
+    public static List<unit> GetUnitsOfPlayer(player player)
+    {
+      GroupEnumUnitsOfPlayer(UniversalGroup, player, null);
+      return UniversalGroup.EmptyToList();
+    }
+
+    /// <summary>
+    /// Adds all units positioned in the provided <see cref="Rectangle"/> to the group>.
+    /// </summary>
+    public static List<unit> GetUnitsInRect(Rectangle rect)
+    {
+      return GetUnitsInRect(rect.Rect);
+    }
+
+    /// <summary>
+    /// Adds all units positioned in the provided <see cref="rect"/> to the group.
+    /// </summary>
+    public static List<unit> GetUnitsInRect(rect rect)
+    {
+      GroupEnumUnitsInRect(UniversalGroup, rect, null);
+      return UniversalGroup.EmptyToList();
+    }
+
+    /// <summary>
+    /// Adds all units within range of the specified <see cref="Point"/> to the group.
+    /// </summary>
+    public static List<unit> GetUnitsInRange(Point point, float radius)
+    {
+      GroupEnumUnitsInRange(UniversalGroup, point.X, point.Y, radius, null);
+      return UniversalGroup.EmptyToList();
+    }
+  }
+}
