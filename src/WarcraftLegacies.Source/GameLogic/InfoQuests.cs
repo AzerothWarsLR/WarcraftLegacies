d3379c9d2571db2662762c6437aa9e9e36d09505
Zakary Boven <zakaryboven@gmail.com>
2024-03-15 16:25:38 +1300

    Merged credits and Discord quests

--- src/WarcraftLegacies.Source/GameLogic/InfoQuests.cs
+++ src/WarcraftLegacies.Source/GameLogic/InfoQuests.cs
@@ -12,21 +12,18 @@ namespace WarcraftLegacies.Source.GameLogic
      /// </summary>
       public static void Setup()
       {
-         var discordQuest = CreateQuest();
-         QuestSetTitle(discordQuest, "Discord");
-         QuestSetIconPath(discordQuest, @"ReplaceableTextures\CommandButtons\BTNDrum.blp");
-         QuestSetDescription(discordQuest, "Please visit our Discord at: https://discord.gg/4eGZn");
-         QuestSetRequired(discordQuest, false);
+         var creditsAndDiscordQuest = CreateQuest();
+         QuestSetTitle(creditsAndDiscordQuest, "Discord & Credits");
+         QuestSetIconPath(creditsAndDiscordQuest, @"ReplaceableTextures\CommandButtons\BTNManual3.blp");
+         QuestSetDescription(creditsAndDiscordQuest, @"Please visit our Discord at: https://discord.gg/4eGZn
 
-         var creditsQuest = CreateQuest();
-         QuestSetTitle(creditsQuest, "Credits");
-         QuestSetIconPath(creditsQuest, @"ReplaceableTextures\CommandButtons\BTNManual3.blp");
-         QuestSetDescription(creditsQuest, @"An enormous thank you to...
+An enormous thank you to...
 
 Previous Azeroth Wars developers: Augur, Crusader793, Bhaal_Spawn., EagleMan, Avrion, Railen, Thurr, Rhemar, SteakonSpear, Talinn, LuneLune, Dave_Rolf and Richardik,
 And to our code contributors, Naowsx, Savantic, Headhunter and Tracy,
-and to the artists who have created the swathe of custom icons and models that we use: Ujimasa Hojo, General Frank, Shiv, Malvodion, nGy, UgoUgo, JetFangInferno, -Grendel,  SpasMaster, Eagle XI, Mythic, Hamsta, assasin_lord, Hayate, R.A.N.G.I.T.,  Stefan.K,  Eusira, 00110000, Pyritie, Sunchips, Dojo, PeeKay, nhocklanhox6, dhguardianes, HappyTauren, HerrDave, Shyster, takakenji, JesusHipster, Boogles, Kwaliti, Tauer, WhiteDeath, and Mechanical Man");
-         QuestSetRequired(creditsQuest, false);
+and to the artists who have created the swathe of custom icons and models that we use: Ujimasa Hojo, General Frank, Shiv, Malvodion, nGy, UgoUgo, JetFangInferno, -Grendel,  SpasMaster, Eagle XI, Mythic, Hamsta, assasin_lord, Hayate, R.A.N.G.I.T.,  Stefan.K,  Eusira, 00110000, Pyritie, Sunchips, Dojo, PeeKay, nhocklanhox6, dhguardianes, HappyTauren, HerrDave, Shyster, takakenji, JesusHipster, Boogles, Kwaliti, Tauer, WhiteDeath, and Mechanical Man"
+);
+         QuestSetRequired(creditsAndDiscordQuest, false);
       }
    }
 }
