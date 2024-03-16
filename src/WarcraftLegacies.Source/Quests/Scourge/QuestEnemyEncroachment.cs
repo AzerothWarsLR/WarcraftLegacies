a9a695872a31753a383b4a4c5fef627dfb63e92d
Zakary Boven <zakaryboven@gmail.com>
2024-03-17 09:35:36 +1300

    cleanup code

--- src/WarcraftLegacies.Source/Quests/Scourge/QuestEnemyEncroachment.cs
+++ src/WarcraftLegacies.Source/Quests/Scourge/QuestEnemyEncroachment.cs
@@ -5,9 +5,10 @@ namespace WarcraftLegacies.Source.Quests.Scourge
 {
   public sealed class QuestEnemyEncroachment : QuestData
   {
-    
     /// <inheritdoc />
-    public QuestEnemyEncroachment() : base("The Slumbering King", "Ner'zhul commands the undead hordes from his throne atop Icecrown, waiting patiently for the inevitable day that interlopers will come to invade his frozen lands.", @"ReplaceableTextures\CommandButtons\BTNAnimateDead.blp")
+    public QuestEnemyEncroachment() : base("The Slumbering King",
+      "Ner'zhul commands the undead hordes from his throne atop Icecrown, waiting patiently for the inevitable day that interlopers will come to invade his frozen lands.",
+      @"ReplaceableTextures\CommandButtons\BTNAnimateDead.blp")
     {
       AddObjective(new ObjectiveAnyEnemyUnitInRects(new[]
       {
@@ -24,9 +25,10 @@ namespace WarcraftLegacies.Source.Quests.Scourge
       }, "Northrend"));
       ResearchId = Constants.UPGRADE_R04V_QUEST_COMPLETED_THE_SLUMBERING_KING;
     }
-    
+
     /// <inheritdoc/>
-    public override string RewardFlavour => "For the first time in years, outlanders have set foot on the shores of Northrend. Soon they will feel the biting chill of death.";
+    public override string RewardFlavour =>
+      "For the first time in years, outlanders have set foot on the shores of Northrend. Soon they will feel the biting chill of death.";
 
     /// <inheritdoc/>
     protected override string RewardDescription => "Learn to cast Frost Nova and Animate Dead from the Frozen Throne";
