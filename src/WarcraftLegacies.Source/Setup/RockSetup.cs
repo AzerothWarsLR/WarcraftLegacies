b50729082ad4d833411d6a853ef2a629da766df6
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2024-03-19 15:06:43 -0400

    Island changes
    -Removed waterways connecting islands
    -removed creeps that are not connected to a control point in the maelstrom
    -removed Crestfall island, as well as the control point there

--- src/WarcraftLegacies.Source/Setup/RockSetup.cs
+++ src/WarcraftLegacies.Source/Setup/RockSetup.cs
@@ -24,14 +24,6 @@ namespace WarcraftLegacies.Source.Setup
       RockSystem.Register(new RockGroup(Regions.KaliRock12, RockChunkId, 300));
       RockSystem.Register(new RockGroup(Regions.KaliRock14, RockChunkId, 480));
 
-      //Island rocks
-      RockSystem.Register(new RockGroup(Regions.IslandBlocker1, IslandChunkId, 1200));
-      RockSystem.Register(new RockGroup(Regions.IslandBlocker2, IslandChunkId, 1200));
-      RockSystem.Register(new RockGroup(Regions.IslandBlocker3, IslandChunkId, 1200));
-      RockSystem.Register(new RockGroup(Regions.IslandBlocker4, IslandChunkId, 1200));
-      RockSystem.Register(new RockGroup(Regions.IslandBlocker5, IslandChunkId, 1200));
-      RockSystem.Register(new RockGroup(Regions.IslandBlocker6, IslandChunkId, 1200));
-
       //Kali Ashenvale rocks
       RockSystem.Register(new RockGroup(Regions.KaliRock7, ForestChunkId, 480));
       RockSystem.Register(new RockGroup(Regions.KaliRock10, ForestChunkId, 480));
