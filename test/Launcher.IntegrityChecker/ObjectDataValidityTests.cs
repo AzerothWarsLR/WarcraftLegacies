742377d1ccdc00d1392cb3cac92d2ca75097f274
Zakary Boven <zakaryboven@gmail.com>
2024-03-24 10:21:38 +1300

    Added test for checking unit research requirements

--- test/Launcher.IntegrityChecker/ObjectDataValidityTests.cs
+++ test/Launcher.IntegrityChecker/ObjectDataValidityTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using Launcher.Extensions;
 using War3Api.Object;
 using Xunit.Sdk;
@@ -18,18 +19,16 @@ namespace Launcher.IntegrityChecker
     public void AllUnits_HaveValidFields()
     {
       var issues = new List<string>();
-
-      foreach (var unit in _mapTestFixture.ObjectDatabase.GetUnits())
-        if (unit.IsTechtreeResearchesAvailableModified)
-          try
-          {
-            _ = unit.TechtreeResearchesAvailable;
-          }
-          catch (KeyNotFoundException)
-          {
-            issues.Add($"{GetReadableId(unit)} has an invalid Researches Available field.");
-          }
-
+      var objectDatabase = _mapTestFixture.ObjectDatabase;
+      
+      foreach (var unit in objectDatabase.GetUnits())
+      {
+        if (VerifyResearchesAvailable(unit, out var researchesAvailableIssue)) 
+          issues.Add(researchesAvailableIssue);
+        
+        if (VerifyResearchesRequired(unit, objectDatabase, out var researchesRequiredIssue)) 
+          issues.Add(researchesRequiredIssue);
+      }
       if (issues.Count == 0)
         return;
       
@@ -38,6 +37,56 @@ namespace Launcher.IntegrityChecker
         exceptionMessageBuilder.AppendLine(issue);
       
       throw new XunitException(exceptionMessageBuilder.ToString());
+    }
+
+    private static bool VerifyResearchesAvailable(Unit unit, [NotNullWhen(true)] out string? issue)
+    {
+      issue = null;
+      
+      if (!unit.IsTechtreeResearchesAvailableModified)
+        return false;
+
+      try
+      {
+        _ = unit.TechtreeResearchesAvailable;
+      }
+      catch (KeyNotFoundException)
+      {
+        issue = $"{GetReadableId(unit)} has an invalid Researches Available field.";
+        return true;
+      }
+      
+      return false;
+    }
+    
+    private static bool VerifyResearchesRequired(Unit unit, ObjectDatabase objectDatabase,
+      [NotNullWhen(true)] out string? issue)
+    {
+      issue = null;
+      
+      if (!unit.IsTechtreeRequirementsModified)
+        return false;
+      
+      var requirements = unit.TechtreeRequirements;
+      if (requirements.All(requirement => IsTechValid(requirement, objectDatabase))) 
+        return false;
+      
+      issue = $"{GetReadableId(unit)} has an invalid Requirements field.";
+      return true;
+    }
+
+    private static bool IsTechValid(Tech tech, ObjectDatabaseBase objectDatabase)
+    {
+      try
+      {
+        _ = objectDatabase.GetObject(tech.Key);
+      }
+      catch
+      {
+        return false;
+      }
+
+      return true;
     }
     
     private static string GetReadableId(BaseObject baseObject) =>
