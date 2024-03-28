964ac6cac5a40717b11d1627435397510e235452
Sebastian Tanoni <47048283+SebastianTanoni@users.noreply.github.com>
2024-03-28 17:56:52 -0400

    Removed lumber abilities and replaced them with alternatives (#2809)
    Eat Tree removed for ancients and mountain giant
    War club removed from mountain giant
    Nature Vitality given to mountain giant
    
    Eat Tree removed for ancients and mountain giant
    War club removed from mountain giant
    Nature Vitality given to mountain giant
    Removed regrow tree from sunfury dome, world trees and Druid t2 and t3
    Removed gather dupplicate from wisp
    gave a replacement spell for malf and druid t3 and t2

--- src/WarcraftLegacies.Source/Setup/Spells/DruidsSpellSetup.cs
+++ src/WarcraftLegacies.Source/Setup/Spells/DruidsSpellSetup.cs
@@ -9,20 +9,7 @@ namespace WarcraftLegacies.Source.Setup.Spells
   {
     public static void Setup()
     {
-      SpellSystem.Register(new RegrowTrees(ABILITY_A01A_REGROW_TREES_BROWN_NORDRASSIL)
-      {
-        Radius = 8500
-      });
       
-      SpellSystem.Register(new RegrowTrees(ABILITY_A0G8_SACRED_GROUND_BROWN_MAIN_BUILDINGS)
-      {
-        Radius = 1500
-      });
-      
-      SpellSystem.Register(new RegrowTrees(ABILITY_A04C_SEED_GROWTH)
-      {
-        Radius = 1200
-      });
 
       SpellSystem.Register(new Devour(ABILITY_A0NP_DEVOUR_TORTOLLA)
       {
