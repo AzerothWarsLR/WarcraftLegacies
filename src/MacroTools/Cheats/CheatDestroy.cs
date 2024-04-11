4eb35a66fdc15bc3172859864567bfd97b45360c
Zakary Boven <zakaryboven@gmail.com>
2024-04-11 21:20:21 +1200

    fix destroy cheat

--- src/MacroTools/Cheats/CheatDestroy.cs
+++ src/MacroTools/Cheats/CheatDestroy.cs
@@ -24,10 +24,11 @@ namespace MacroTools.Cheats
     /// <inheritdoc />
     public override string Execute(player cheater, params string[] parameters)
     {
-      var artifact = ArtifactManager.GetFromName(parameters[0]);
-      if (artifact == null)
+      if (!ArtifactManager.TryGetByName(parameters[0], out var artifact))
+      {
         return $"You must specify the name of a registered {nameof(Artifact)} as the first parameter.";
-      
+      }
+
       var artifactName = GetItemName(artifact.Item);
       ArtifactManager.Destroy(artifact);
       return $"Destroyed {artifactName}";
