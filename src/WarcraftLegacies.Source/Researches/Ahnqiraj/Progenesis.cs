df50b8c34d0c20a3beae91566a88c0dfe5dc4744
Zakary Boven <zakaryboven@gmail.com>
2024-04-18 22:39:15 +1200

    better filtering and better blood

--- src/WarcraftLegacies.Source/Researches/Ahnqiraj/Progenesis.cs
+++ src/WarcraftLegacies.Source/Researches/Ahnqiraj/Progenesis.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using MacroTools.Extensions;
 using MacroTools.ResearchSystems;
 
@@ -17,19 +17,29 @@ namespace WarcraftLegacies.Source.Researches.Ahnqiraj
     /// <inheritdoc />
     public override void OnResearch(player researchingPlayer)
     {
-      var workers = CreateGroup().EnumUnitsOfType(UNIT_U019_DRONE_C_THUN_WORKER).EmptyToList();
+      var workers = CreateGroup()
+        .EnumUnitsOfPlayer(researchingPlayer)
+        .EmptyToList()
+        .Where(IsValidTarget);
+      
       foreach (var worker in workers)
       {
-        Console.WriteLine("YOSA");
         var position = worker.GetPosition();
         var facing = worker.GetFacing();
         var hitPointsPercentage = worker.GetLifePercent();
         var owner = worker.OwningPlayer();
+
+        AddSpecialEffect(@"Objects\Spawnmodels\Critters\Albatross\CritterBloodAlbatross.mdl", position.X, position.Y)
+          .SetLifespan();
         
         worker.SafelyRemove();
         var soldier = CreateUnit(owner, UNIT_N06I_SILITHID_WARRIOR_C_THUN_SILITHID_WARRIOR, position.X, position.Y, facing);
         soldier.SetLifePercent(hitPointsPercentage);
       }
     }
+
+    private static bool IsValidTarget(unit target) => GetUnitTypeId(target) == UNIT_U019_DRONE_C_THUN_WORKER &&
+                                                      !IsUnitType(target, UNIT_TYPE_DEAD) &&
+                                                      !IsUnitType(target, UNIT_TYPE_SUMMONED);
   }
 }
