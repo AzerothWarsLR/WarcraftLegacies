9fa14b3c03c1c52fdedeb2e84a7b0942f6b50c46
Zakary Boven <zakaryboven@gmail.com>
2024-04-25 02:21:30 +1200

    Implement Incubate ability (#2951)
    **Incubate:** Raises an Immature Egg from a nearby corpse. After 1 minutes the egg matures. Mature eggs can be hatched into permanent Silithid Soldiers, and will automatically hatch if they are destroyed.
    **Rapid Incubation:** Causes Immature Eggs incubated by Vile Corrupters to mature in 1 minute instead of 5.

---
+++ src/WarcraftLegacies.Source/PassiveAbilities/Incubate/ImmatureEggBuff.cs
@@ -0,0 +1,47 @@
+﻿using MacroTools.Extensions;
+using WCSharp.Buffs;
+
+namespace WarcraftLegacies.Source.PassiveAbilities.Incubate
+{
+  /// <summary>
+  /// Causes the egg to mature when the buff expires, allowing it to hatch.
+  /// </summary>
+  public sealed class ImmatureEggBuff : PassiveBuff
+  {
+    /// <summary>The unit type ID of the unit that gets hatched.</summary>
+    public required int HatchedUnitTypeId { get; init; }
+    
+    /// <summary>The unit type ID of the mature egg version to transform into.</summary>
+    public required int MatureEggUnitTypeId { get; init; }
+    
+    /// <inheritdoc />
+    public ImmatureEggBuff(unit caster) : base(caster, caster)
+    {
+    }
+
+    /// <inheritdoc />
+    public override void OnApply()
+    {
+      Target.SetTimedLife(Duration + 1);
+    }
+
+    /// <inheritdoc />
+    public override void OnExpire()
+    {
+      var formerLifePercent = Target.GetLifePercent();
+      var formerPosition = Target.GetPosition();
+      var formerOwner = Target.OwningPlayer();
+      
+      Target.Kill().Remove();
+      
+      var matureEgg = CreateUnit(formerOwner, MatureEggUnitTypeId, formerPosition.X, formerPosition.Y, 270)
+        .SetLifePercent(formerLifePercent);
+      
+      var matureEggBuff = new MatureEggBuff(matureEgg)
+      {
+        HatchedUnitTypeId = HatchedUnitTypeId
+      };
+      BuffSystem.Add(matureEggBuff);
+    }
+  }
+}
