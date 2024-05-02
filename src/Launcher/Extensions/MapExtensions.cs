cf6a63910d5bf9b6b1cdb22563acf7ecf83422b8
Zakary Boven <zakaryboven@gmail.com>
2024-05-02 16:46:01 +1200

    fixed exporting of unit skin object data

--- src/Launcher/Extensions/MapExtensions.cs
+++ src/Launcher/Extensions/MapExtensions.cs
@@ -26,13 +26,15 @@ namespace Launcher.Extensions
     public static ObjectDatabase GetObjectDatabaseFromMap(this Map map)
     {
       var objectDatabase = new ObjectDatabase();
-      if (map.AbilityObjectData != null) objectDatabase.AddObjects(map.AbilityObjectData);
-      if (map.BuffObjectData != null) objectDatabase.AddObjects(map.BuffObjectData);
+      if (map.AbilityObjectData != null) objectDatabase.AddObjects(map.AbilityObjectData, map.AbilitySkinObjectData);
+      // if (map.BuffObjectData != null) objectDatabase.AddObjects(map.BuffObjectData, map.BuffSkinObjectData);
       //if (map.DestructableObjectData != null) objectDatabase.AddObjects(map.DestructableObjectData);
-      if (map.DoodadObjectData != null) objectDatabase.AddObjects(map.DoodadObjectData);
-      if (map.ItemObjectData != null) objectDatabase.AddObjects(map.ItemObjectData);
-      if (map.UnitObjectData != null) objectDatabase.AddObjects(map.UnitObjectData);
-      if (map.UpgradeObjectData != null) objectDatabase.AddObjects(map.UpgradeObjectData);
+      //if (map.DoodadObjectData != null) objectDatabase.AddObjects(map.DoodadObjectData, map.DoodadSkinObjectData);
+      //if (map.ItemObjectData != null) objectDatabase.AddObjects(map.ItemObjectData, map.ItemSkinObjectData);
+      if (map.UnitObjectData != null) objectDatabase.AddObjects(map.UnitObjectData, map.UnitSkinObjectData);
+      //if (map.UpgradeObjectData != null) objectDatabase.AddObjects(map.UpgradeObjectData, map.UpgradeSkinObjectData);
+
+      var test = objectDatabase.GetUnits();
       return objectDatabase;
     }
 
