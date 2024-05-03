a26f679fc7a8377ccd300cae906eaabca0197d53
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2024-05-03 15:17:45 -0400

    BlackEmpireTech tree work

--- src/WarcraftLegacies.Source/Factions/BlackEmpire.cs
+++ src/WarcraftLegacies.Source/Factions/BlackEmpire.cs
@@ -6,7 +6,7 @@ namespace WarcraftLegacies.Source.Factions
   public class BlackEmpire : Faction
   {
     /// <inheritdoc />
-    public BlackEmpire() : base("BlackEmpire", PLAYER_COLOR_TURQUOISE, "|C0000FFFF",
+    public BlackEmpire() : base("BlackEmpire", PLAYER_COLOR_MAROON, "|C0000FFFF",
       @"ReplaceableTextures\CommandButtons\BTNNzothIcon.blp")
     {
       ControlPointDefenderUnitTypeId = UNIT_N0DV_CONTROL_POINT_DEFENDER_BLACK_EMPIRE_TOWER;
@@ -29,7 +29,8 @@ namespace WarcraftLegacies.Source.Factions
       ModObjectLimit(UNIT_H06U_SIPHONING_CRYSTAL_YOGG_RESEARCH, UNLIMITED);
       ModObjectLimit(UNIT_N0AW_INCUBATION_POOL_YOGG_BARRACKS, UNLIMITED);
       ModObjectLimit(UNIT_N0B3_SHADOW_LAIR_YOGG_MAGIC, UNLIMITED);
-      ModObjectLimit(UNIT_N0AX_PIT_OF_TORMENT_YOGG_SPECIALIST, UNLIMITED);
+      ModObjectLimit(UNIT_N0AX_MUTATION_CIRCLE_YOGG_SPECIALIST, UNLIMITED);
+      ModObjectLimit(UNIT_LS08_PIT_OF_TORMENT_NIGHTMARE_SPECIALIST, UNLIMITED);
       ModObjectLimit(UNIT_N0B2_OMINOUS_VAULT_YOGG_SHOP, UNLIMITED);
       ModObjectLimit(UNIT_N0AV_ALTAR_OF_MADNESS_YOGG_ALTAR, UNLIMITED);
       ModObjectLimit(UNIT_H09E_MADNESS_POOL_YOGG_TOWER, UNLIMITED);
@@ -45,26 +46,31 @@ namespace WarcraftLegacies.Source.Factions
 
       ModObjectLimit(UNIT_O01G_FACELESS_MARAUDER_YOGG, UNLIMITED);
       ModObjectLimit(UNIT_O04V_SOUL_HUNTER_YOGG, UNLIMITED);
-      ModObjectLimit(UNIT_U029_STYGIAN_HULK_YOGG, 12);
+      ModObjectLimit(UNIT_H09F_GLADIATOR_YOGG, 12);
 
       ModObjectLimit(UNIT_N077_SORCERER_YOGG, UNLIMITED);
       ModObjectLimit(UNIT_O04Y_FATEWEAVER_YOGG, UNLIMITED);
+      ModObjectLimit(UNIT_BHN1_HERALD_OF_NYALOTHA_TOLVIR, 6);
+
       ModObjectLimit(UNIT_U02F_FORGOTTEN_ONE_YOGG, 2);
+      ModObjectLimit(UNIT_SHZ5_AQIR_BLACK_EMPIRE, 6);
+
       ModObjectLimit(UNIT_O04Z_FLYING_HORROR_YOGG, 12);
       ModObjectLimit(UNIT_N0AH_DEFORMED_CHIMERA_YOGG, 4);
-      ModObjectLimit(UNIT_H09F_GLADIATOR_YOGG, UNLIMITED  );
+      ModObjectLimit(UNIT_U029_STYGIAN_HULK_YOGG, 12);
 
-     // All Nzoth Ships
 
-      ModObjectLimit(FourCC("ushp"), UNLIMITED); //Undead Shipyard
-      ModObjectLimit(FourCC("ubot"), UNLIMITED); //Undead Transport Ship
-      ModObjectLimit(FourCC("h0AT"), UNLIMITED); //Scout
-      ModObjectLimit(FourCC("h0AW"), UNLIMITED); //Frigate
-      ModObjectLimit(FourCC("h0AM"), UNLIMITED); //Fireship
-      ModObjectLimit(FourCC("h0AZ"), UNLIMITED); //Galley
-      ModObjectLimit(FourCC("h0AQ"), UNLIMITED); //Boarding
-      ModObjectLimit(FourCC("h0BB"), UNLIMITED); //Juggernaut
-      ModObjectLimit(FourCC("h0B9"), 6); //Bombard
+      // All Nzoth Ships
+
+      ModObjectLimit(UNIT_USHP_HAUNTED_HARBOR_SCOURGE_SHIPYARD, UNLIMITED);
+      ModObjectLimit(UNIT_H0AT_SCOUT_SHIP_UNDEAD, UNLIMITED);
+      ModObjectLimit(UNIT_H0AW_FRIGATE_UNDEAD, UNLIMITED);
+      ModObjectLimit(UNIT_UBOT_HAUNTED_TRANSPORT_SHIP_SCOURGE_LEGION, UNLIMITED);
+      ModObjectLimit(UNIT_H0AM_FIRESHIP_UNDEAD, UNLIMITED);
+      ModObjectLimit(UNIT_H0AZ_GALLEY_UNDEAD, UNLIMITED);
+      ModObjectLimit(UNIT_H0AQ_BOARDING_VESSEL_UNDEAD, UNLIMITED);
+      ModObjectLimit(UNIT_H0BB_JUGGERNAUT_UNDEAD, UNLIMITED);
+      ModObjectLimit(UNIT_H0B9_BOMBARD_UNDEAD, 6);
       
       // All Nzoth Heroes listed below - with total limits
 
