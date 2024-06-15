85a79201123f963bf4f8157fe75657624e97fd13
Zakary Boven <zakaryboven@gmail.com>
2024-06-15 22:11:04 +1200

    Implemented quest to get C'thun ultimate

--- src/WarcraftLegacies.Source/Factions/Ahnqiraj.cs
+++ src/WarcraftLegacies.Source/Factions/Ahnqiraj.cs
@@ -56,10 +56,9 @@ namespace WarcraftLegacies.Source.Factions
       AddQuest(new QuestTanarisOutpost(Regions.QirajOutpost5));
       AddQuest(new QuestEmperorConstruct());
       AddQuest(new QuestMockeryOfLife());
-      AddQuest(new QuestWarOfTheShiftingSand(_allLegendSetup.Ahnqiraj.Cthun, 
-        _allLegendSetup.Druids.Nordrassil));
       AddQuest(new QuestFreshMeat(_allLegendSetup.Ahnqiraj.Cthun));
-      AddQuest(new QuestLitheMeat());
+      AddQuest(new QuestAwakening(_allLegendSetup.Ahnqiraj.Cthun));
+      AddQuest(new QuestWarOfTheShiftingSand(_allLegendSetup.Ahnqiraj.Cthun, _allLegendSetup.Druids.Nordrassil));
       AddQuest(new QuestFiendThousandFaces(_allLegendSetup.Neutral.YoggSaron));
     }
 
