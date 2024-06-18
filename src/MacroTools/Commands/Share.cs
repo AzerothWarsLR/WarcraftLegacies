826cb91d574d64ab563dfd6692b985a4bb1ac4d2
Technopig1992 <127978232+Technopig1992@users.noreply.github.com>
2024-06-18 21:06:14 +0100

    Share All Command
    Added a -share all command that checks if the first parameter is "all" if so share control with all factions that have a player active and is on the same team.

--- src/MacroTools/Commands/Share.cs
+++ src/MacroTools/Commands/Share.cs
@@ -2,6 +2,7 @@
 using MacroTools.Extensions;
 using MacroTools.FactionSystem;
 using static War3Api.Common;
+using System;
 
 namespace MacroTools.Commands
 {
@@ -12,7 +13,7 @@ namespace MacroTools.Commands
     public override string CommandText => "share";
 
     /// <inheritdoc />
-    public override ExpectedParameterCount ExpectedParameterCount => new(0,1);
+    public override ExpectedParameterCount ExpectedParameterCount => new(0, 1);
 
     /// <inheritdoc />
     public override CommandType Type => CommandType.Normal;
@@ -23,19 +24,42 @@ namespace MacroTools.Commands
     /// <inheritdoc />
     public override string Execute(player cheater, params string[] parameters)
     {
+      var cheaterTeam = cheater.GetTeam(); // Declare cheaterTeam once at the beginning
+
+      // Check if the first parameter is 'all', regardless of any additional parameters
+      if (parameters.Length >= 1 && parameters[0].ToLower() == "all")
+      {
+        var factions = FactionManager.GetAllFactions();
+
+        foreach (var faction in factions)
+        {
+          if (faction.Player != null && faction.Player.GetTeam() == cheaterTeam)
+          {
+            SetPlayerAlliance(cheater, faction.Player, ALLIANCE_SHARED_CONTROL, true);
+          }
+        }
+
+        return $"Shared control with all factions on your team.";
+      }
+
+      // If not 'all', then proceed with previous share logic from Yak's code
+      if (parameters.Length == 0 || parameters[0].ToLower() == "")
+      {
+        return $"Please specify a faction name or 'all' to share control.";
+      }
+
       var targetFaction = FactionManager.GetFactionByName(parameters[0]);
-      if (targetFaction == null) 
+      if (targetFaction == null)
         return $"There is no faction named {parameters[0]}.";
 
       if (targetFaction.Player == null)
         return $"There is nobody playing the {targetFaction.Name} faction.";
 
-      var cheaterTeam = cheater.GetTeam();
       if (cheaterTeam != targetFaction.Player.GetTeam())
         return $"{targetFaction.Name} isn't on your team, so you can't share control with them.";
-      
+
       SetPlayerAlliance(cheater, targetFaction.Player, ALLIANCE_SHARED_CONTROL, true);
-      
+
       return $"Shared control with {targetFaction.Name}.";
     }
   }
