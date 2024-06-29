6ff1a81c6fc146b6e9087d7fab5ba67e553bd182
Zakary Boven <zakaryboven@gmail.com>
2024-06-29 12:38:42 +1200

    Prevent Legion Teleporter from casting multiple portals at once (#3110)
    Legion Teleporter Slipstreams now all share a cooldown. Once the user has created a portal, they cannot create any more until they cancel the existing one using the Close Portal  button. This prevents a difficult pathing issue that occurs when two portals occupy a similar location at once.

---
+++ src/WarcraftLegacies.Source/Spells/Slipstream/SlipstreamSpellLegionTeleporter.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using MacroTools;
+using MacroTools.ChannelSystem;
+using MacroTools.Extensions;
+using MacroTools.SpellSystem;
+using WCSharp.Shared.Data;
+
+namespace WarcraftLegacies.Source.Spells.Slipstream
+{
+  /// <summary>
+  /// Channel a portal from a predetermined location to another one.
+  /// <para><see cref="SlipstreamSpellLegionTeleporter"/>s on the same unit share a cooldown with each other.
+  /// Their cooldown ends only when a portal is closed.</para>
+  /// </summary>
+  public sealed class SlipstreamSpellLegionTeleporter : Spell
+  {
+    /// <summary>
+    /// The unit to spawn to act as the actual portal.
+    /// </summary>
+    public required int PortalUnitTypeId { get; init; }
+
+    /// <summary>
+    /// An ability that can be used to close the portal early.
+    /// </summary>
+    public required int CloseAbilityId { get; init; }
+
+    /// <summary>
+    /// Any instance of <see cref="SlipstreamSpellLegionTeleporter"/> that might exist on the same unit.
+    /// <para>Their cooldowns will start at the same time, and end together when a portal closes.</para>
+    /// </summary>
+    public List<SlipstreamSpellLegionTeleporter> RelatedSlipstreams { get; } = new();
+    
+    /// <summary>
+    /// The portal won't be useable until the caster has channeled for this long.
+    /// </summary>
+    public required float OpeningDelay { get; init; }
+
+    /// <summary>
+    /// It takes this long for the portal to close after the caster stops channeling.
+    /// </summary>
+    public required float ClosingDelay { get; init; }
+
+    /// <summary>
+    /// The origin of any portal created by this spell.
+    /// </summary>
+    public required Point OriginLocation { get; init; }
+    
+    /// <summary>
+    /// The destination of any portal created by this spell.
+    /// </summary>
+    public required Point TargetLocation { get; init; }
+
+    /// <summary>
+    /// The color of the created portals.
+    /// </summary>
+    public Color Color { get; init; } = new(255, 255, 255, 255);
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SlipstreamSpell"/> class.
+    /// </summary>
+    /// <param name="id"><inheritdoc /></param>
+    public SlipstreamSpellLegionTeleporter(int id) : base(id)
+    {
+    }
+
+    /// <inheritdoc/>
+    public override void OnCast(unit caster, unit target, Point targetPoint)
+    {
+      ChannelManager.Add(new SlipstreamPortalChannel(caster, Id, OriginLocation, TargetLocation)
+      {
+        Active = true,
+        PortalUnitTypeId = PortalUnitTypeId,
+        OpeningDelay = OpeningDelay,
+        ClosingDelay = ClosingDelay,
+        Color = Color,
+        CloseAbilityId = CloseAbilityId,
+        RelatedSlipstreams = RelatedSlipstreams,
+        RefundFunc = Refund
+      });
+
+      foreach (var relatedSlipstream in RelatedSlipstreams)
+        caster.StartAbilityCooldown(relatedSlipstream.Id);
+    }
+
+    /// <inheritdoc/>
+    public override void OnStartCast(unit caster, unit target, Point targetPoint)
+    {
+      caster.IssueOrder("stop");
+      Refund(caster);
+    }
+
+    private void Refund(unit whichUnit)
+    {
+      whichUnit.RestoreMana(BlzGetUnitAbilityManaCost(whichUnit, Id, GetAbilityLevel(whichUnit)));
+      BlzEndUnitAbilityCooldown(whichUnit, Id);
+    }
+  }
+}
