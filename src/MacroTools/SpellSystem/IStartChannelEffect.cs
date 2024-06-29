397bedbbb260cd48a68c565e61dfdaed454ff333
Zakary Boven <zakaryboven@gmail.com>
2024-06-29 16:08:05 +1200

    y

---
+++ src/MacroTools/SpellSystem/IStartChannelEffect.cs
@@ -0,0 +1,13 @@
+﻿using WCSharp.Shared.Data;
+using static War3Api.Common;
+
+namespace MacroTools.SpellSystem
+{
+  public interface IStartChannelEffect
+  {
+    /// <summary>
+    /// An effect that occurs when the spell starts channeling.
+    /// </summary>
+    public void OnStartChannel(unit caster, Point targetPoint);
+  }
+}
--- src/WarcraftLegacies.Source/FactionMechanics/Goblins/NuclearLaunch.cs
+++ src/WarcraftLegacies.Source/FactionMechanics/Goblins/NuclearLaunch.cs
@@ -1,4 +1,5 @@
 ﻿using MacroTools.Extensions;
+using MacroTools.Sound;
 using MacroTools.SpellSystem;
 using WCSharp.Shared.Data;
 
@@ -7,26 +8,45 @@ namespace WarcraftLegacies.Source.FactionMechanics.Goblins
   /// <summary>
   /// Fire a giant delayed nuke, with a warning signal placed down beforehand.
   /// </summary>
-  public sealed class NuclearLaunch : Spell
+  public sealed class NuclearLaunch : Spell, IStartChannelEffect
   {
-    private readonly int _dummyNukeLeftOverId;
+    public required float CastTime { get; init; }
+    
+    public required int NuclearWarningUnitTypeId { get; init; }
+    
+    public required string WarningSoundPath { get; init; }
+    
+    /// <summary>
+    /// Unit type ID for the unit to be left behind for a bit after the spell casts.
+    /// </summary>
+    public required int DummyNukeLeftOverId { get; init; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="NuclearLaunch"/> class.
     /// </summary>
     /// <param name="id"><inheritdoc /></param>
-    /// <param name="dummyNukeLeftOverId">Unit type ID for the unit to be left behind for a bit after the spell casts.</param>
-    public NuclearLaunch(int id, int dummyNukeLeftOverId) : base(id)
+    public NuclearLaunch(int id) : base(id)
     {
-      _dummyNukeLeftOverId = dummyNukeLeftOverId;
     }
 
     /// <inheritdoc />
     public override void OnCast(unit caster, unit target, Point targetPoint)
     {
-      var dummyNukeLeftover = CreateUnit(caster.OwningPlayer(), _dummyNukeLeftOverId, targetPoint.X, targetPoint.Y, 0);
+      var dummyNukeLeftover = CreateUnit(caster.OwningPlayer(), DummyNukeLeftOverId, targetPoint.X, targetPoint.Y, 0);
       UnitApplyTimedLife(dummyNukeLeftover, 0, 3);
-      dummyNukeLeftover.IssueOrder("flamestrike", targetPoint);
+      dummyNukeLeftover.IssueOrder(OrderId("flamestrike"), targetPoint);
+    }
+
+
+    /// <inheritdoc />
+    public void OnStartChannel(unit caster, Point targetPoint)
+    {
+      var sound = SoundUtils.CreateNormalSound(WarningSoundPath);
+      StartSound(sound);
+      var dummyNukeWarning =
+        CreateUnit(caster.OwningPlayer(), NuclearWarningUnitTypeId, targetPoint.X, targetPoint.Y, 0);
+      
+      UnitApplyTimedLife(dummyNukeWarning, 0, CastTime);
     }
   }
 }
