d5f86e3592de28cb6a41e7eceab7c4d0f6f2e256
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2024-08-02 09:00:02 -0400

    Update Skywall.cs

--- src/WarcraftLegacies.Source/Factions/Skywall.cs
+++ src/WarcraftLegacies.Source/Factions/Skywall.cs
@@ -1,5 +1,4 @@
-﻿using MacroTools.Extensions;
-using MacroTools.FactionSystem;
+﻿using MacroTools.FactionSystem;
 using MacroTools.PassiveAbilities;
 using MacroTools.PassiveAbilitySystem;
 using MacroTools.Powers;
