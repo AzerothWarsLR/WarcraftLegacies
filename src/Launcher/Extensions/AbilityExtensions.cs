5a1d008dbf68e9c32a970ce36d50137567f78a6f
Zakary Boven <zakaryboven@gmail.com>
2024-08-04 11:28:10 +1200

    Orb of Corruption based abilities no longer show in tooltips

--- src/Launcher/Extensions/AbilityExtensions.cs
+++ src/Launcher/Extensions/AbilityExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using War3Api.Object;
+﻿using War3Api.Object;
 using War3Api.Object.Abilities;
 
 namespace Launcher.Extensions
@@ -11,7 +9,7 @@ namespace Launcher.Extensions
     {
       return ability is not (InventoryPackMule or Inventory2SlotUnitHuman or Inventory2SlotUnitOrc or Inventory2SlotUnitUndead
         or Inventory2SlotUnitNightElf or Invulnerable or DefenseBonus1 or Ultravision or SellItem or AlliedBuilding or 
-        PurchaseItem or LightningAttack or Inventory or AttributeModifierSkill);
+        PurchaseItem or LightningAttack or Inventory or AttributeModifierSkill or OrbOfCorruption);
     }
 
     /// <summary>
