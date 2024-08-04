29f3a5f14fe31a0327aab83187894e088740aff5
Zakary Boven <zakaryboven@gmail.com>
2024-08-04 14:46:56 +1200

    cleanup

--- src/WarcraftLegacies.Source/Quests/Zandalar/QuestJinthaAlor.cs
+++ src/WarcraftLegacies.Source/Quests/Zandalar/QuestJinthaAlor.cs
@@ -13,10 +13,10 @@ namespace WarcraftLegacies.Source.Quests.Zandalar
   /// </summary>
   public sealed class QuestJinthaAlor : QuestData
   {
-    private const int _jinthaalorResearch = UPGRADE_R02N_QUEST_COMPLETED_THE_ANCIENT_EGG_WARSONG;
-    private const int _bearRiderId = UNIT_O02K_BEAR_RIDER_ZANDALAR;
-    private const int _trollShrineId = UNIT_O04X_LOA_SHRINE_ZANDALARI_SIEGE;
-    private readonly int goldReward = 50;
+    private const int JinthaalorResearch = UPGRADE_R02N_QUEST_COMPLETED_THE_ANCIENT_EGG_WARSONG;
+    private const int BearRiderId = UNIT_O02K_BEAR_RIDER_ZANDALAR;
+    private const int TrollShrineId = UNIT_O04X_LOA_SHRINE_ZANDALARI_SIEGE;
+    private readonly int _goldReward = 50;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="QuestJinthaAlor"/> class
@@ -35,20 +35,20 @@ namespace WarcraftLegacies.Source.Quests.Zandalar
 
     /// <inheritdoc/>>
     protected override string RewardDescription =>
-      $"Control of Jintha'Alor, {goldReward} gold tribute and the ability to train {GetObjectName(_bearRiderId)}s from the {GetObjectName(_trollShrineId)}";
+      $"Control of Jintha'Alor, {_goldReward} gold tribute and the ability to train {GetObjectName(BearRiderId)}s from the {GetObjectName(TrollShrineId)}";
     /// <inheritdoc/>
     protected override void OnComplete(Faction completingFaction)
     {
       if (completingFaction.Player != null)
       {
-        SetPlayerTechResearched(completingFaction.Player, _jinthaalorResearch, 1);
-        completingFaction.Player.AdjustPlayerState(PLAYER_STATE_RESOURCE_GOLD, goldReward);
+        SetPlayerTechResearched(completingFaction.Player, JinthaalorResearch, 1);
+        completingFaction.Player.AdjustPlayerState(PLAYER_STATE_RESOURCE_GOLD, _goldReward);
       }
     }
     /// <inheritdoc/>
     protected override void OnAdd(Faction whichFaction)
     {
-      whichFaction.ModObjectLimit(_jinthaalorResearch, Faction.UNLIMITED);
+      whichFaction.ModObjectLimit(JinthaalorResearch, Faction.UNLIMITED);
     }
   }
 }
