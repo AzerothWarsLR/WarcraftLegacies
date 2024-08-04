c627b7586f846b300fe5edb1a05f8990aeab5937
SebastianTanoni <47048283+SebastianTanoni@users.noreply.github.com>
2024-08-04 10:04:24 -0400

    Update QuestMawofGorma.cs

--- src/WarcraftLegacies.Source/Quests/BlackEmpire/QuestMawofGorma.cs
+++ src/WarcraftLegacies.Source/Quests/BlackEmpire/QuestMawofGorma.cs
@@ -27,7 +27,8 @@ namespace WarcraftLegacies.Source.Quests.BlackEmpire
       AddObjective(new ObjectiveControlPoint(UNIT_NMOG_MAW_OF_GOR_MA));
       AddObjective(new ObjectiveExpire(660, Title));
       AddObjective(new ObjectiveSelfExists());
-      _rescueUnits = rescueRect.PrepareUnitsForRescue(RescuePreparationMode.HideAll);
+      _rescueUnits = rescueRect.PrepareUnitsForRescue(RescuePreparationMode.HideAll,
+        filterUnit => filterUnit.GetTypeId() != FourCC("ngme"));
       ResearchId = UPGRADE_RBMG_QUEST_COMPLETED_MAW_OF_GOR_MA;
 
     }
