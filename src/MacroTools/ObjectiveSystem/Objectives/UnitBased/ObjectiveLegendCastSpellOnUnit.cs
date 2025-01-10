06a861e1b7391e9bd47447e95fae04cf00e95ad7
Zakary Boven <zakaryboven@gmail.com>
2025-01-10 22:52:20 +1300

    ObjectiveLegendCastSpellOnUnit correctly requires the spell

--- src/MacroTools/ObjectiveSystem/Objectives/UnitBased/ObjectiveLegendCastSpellOnUnit.cs
+++ src/MacroTools/ObjectiveSystem/Objectives/UnitBased/ObjectiveLegendCastSpellOnUnit.cs
@@ -12,6 +12,7 @@ namespace MacroTools.ObjectiveSystem.Objectives.UnitBased
   public sealed class ObjectiveLegendCastSpellOnUnit : Objective
   {
     private readonly LegendaryHero _caster;
+    private readonly int _spellId;
     private readonly unit _target;
 
     /// <summary>
@@ -23,6 +24,7 @@ namespace MacroTools.ObjectiveSystem.Objectives.UnitBased
     public ObjectiveLegendCastSpellOnUnit(LegendaryHero caster, int spellId, unit target)
     {
       _caster = caster;
+      _spellId = spellId;
       _target = target;
       PlayerUnitEvents.Register(SpellEvent.Effect, OnCast, spellId);
       Description = $"{caster.Name} has casted {GetObjectName(spellId)} on {target.GetName()}";
@@ -30,10 +32,8 @@ namespace MacroTools.ObjectiveSystem.Objectives.UnitBased
 
     private void OnCast()
     {
-      if (GetTriggerUnit() == _caster.Unit && GetSpellTargetUnit() == _target)
-      {
+      if (GetTriggerUnit() == _caster.Unit && GetSpellTargetUnit() == _target && GetSpellAbilityId() == _spellId)
         Progress = QuestProgress.Complete;
-      }
     }
   }
 }
