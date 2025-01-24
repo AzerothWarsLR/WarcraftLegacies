fe99727518e22138c2ffdae22fba17550c49ec9a
Zakary Boven <zakaryboven@gmail.com>
2025-01-24 23:56:28 +1300

    Sunwell vial quest correctly fails if someone else gets the Vial

--- src/MacroTools/ObjectiveSystem/Objectives/LegendBased/ObjectiveLegendHasArtifact.cs
+++ src/MacroTools/ObjectiveSystem/Objectives/LegendBased/ObjectiveLegendHasArtifact.cs
@@ -13,11 +13,12 @@ namespace MacroTools.ObjectiveSystem.Objectives.LegendBased
   {
     private readonly Artifact _targetArtifact;
     private readonly Legend _targetLegend;
- 
+    private readonly QuestProgress _progressOnArtifactLoss;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="ObjectiveLegendHasArtifact"/> class.
     /// </summary>
-    public ObjectiveLegendHasArtifact(Legend targetLegend, Artifact targetArtifact)
+    public ObjectiveLegendHasArtifact(Legend targetLegend, Artifact targetArtifact, bool failOnArtifactLoss = false)
     {
       Description = targetLegend is LegendaryHero legendaryHero
         ? $"{legendaryHero.Name} has {GetItemName(targetArtifact.Item)}"
@@ -25,7 +26,7 @@ namespace MacroTools.ObjectiveSystem.Objectives.LegendBased
 
       _targetLegend = targetLegend;
       _targetArtifact = targetArtifact;
-      targetArtifact.PickedUp += OnPickedUp;
+      _progressOnArtifactLoss = failOnArtifactLoss ? QuestProgress.Failed : QuestProgress.Incomplete;
     }
 
     public override void OnAdd(FactionSystem.Faction whichFaction)
@@ -33,14 +34,15 @@ namespace MacroTools.ObjectiveSystem.Objectives.LegendBased
       if (_targetArtifact.OwningUnit != null && _targetArtifact.OwningUnit == _targetLegend.Unit) 
         Progress = QuestProgress.Complete;
 
+      _targetArtifact.OwnerChanged += OnArtifactOwnerChanged;
       _targetArtifact.Disposed += (_, _) => Progress = QuestProgress.Failed;
     }
 
-    private void OnPickedUp(object? sender, Artifact artifact)
+    private void OnArtifactOwnerChanged(object? sender, Artifact artifact)
     {
       Progress =_targetArtifact.OwningUnit != null && _targetArtifact.OwningUnit == _targetLegend.Unit
         ? QuestProgress.Complete
-        : QuestProgress.Incomplete;
+        : _progressOnArtifactLoss;
     }
   }
 }
