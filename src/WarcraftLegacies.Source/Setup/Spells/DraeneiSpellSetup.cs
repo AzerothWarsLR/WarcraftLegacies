297c5097c2cdeb2de84c046498502198aa2c5390
Technopig <127978232+Technopig1992@users.noreply.github.com>
2025-04-13 00:10:41 +0100

    Refactored Slipstream portals to use a random point in a region in region rather than an exact point . (#3292)
    This fixes a bug where Exodar's Slipstream portals would not work after being casted more than once.

--- src/WarcraftLegacies.Source/Setup/Spells/DraeneiSpellSetup.cs
+++ src/WarcraftLegacies.Source/Setup/Spells/DraeneiSpellSetup.cs
@@ -6,53 +6,48 @@ using WarcraftLegacies.Source.Spells.MassiveAttack;
 using WarcraftLegacies.Source.Spells.Slipstream;
 using WCSharp.Shared.Data;
 
+
 namespace WarcraftLegacies.Source.Setup.Spells
 {
-  /// <summary>
-  /// Responsible for setting up all Draenei <see cref="Spell"/>s.
-  /// </summary>
   public static class DraeneiSpellSetup
   {
-    /// <summary>
-    /// Sets up all Draenei <see cref="Spell"/>s.
-    /// </summary>
     public static void Setup()
     {
-      var slipstreamOrigin = new Point(-22259.9f, 7104.2f);
-      
-      //Azuremyst
-      SpellSystem.Register(new SlipstreamSpellSpecificOriginAndDestination(ABILITY_A0P9_PORTAL_TO_AZUREMYST_DRAENEI)
+      Point slipstreamTempestOriginPoint = Regions.SlipstreamTempestOrigin.GetRandomPoint();
+      Point slipstreamTempestTargetPoint = Regions.SlipstreamTempestTarget.GetRandomPoint();
+      Point slipstreamArgusOriginPoint = Regions.SlipstreamArgusOrigin.GetRandomPoint();
+      Point slipstreamArgusTargetPoint = Regions.SlipstreamArgusTarget.GetRandomPoint();
+      Point slipstreamAzuremystTargetPoint = new Point(-20940, 10412); 
+
+     SpellSystem.Register(new SlipstreamSpellSpecificOriginAndDestination(ABILITY_A0P9_PORTAL_TO_AZUREMYST_DRAENEI)
       {
         PortalUnitTypeId = UNIT_N0D9_SLIPSTREAM_PORTAL_STORMWIND_KHADGAR,
         OpeningDelay = 20,
         ClosingDelay = 10,
-        OriginLocation = slipstreamOrigin,
-        TargetLocation = new Point(-20940, 10412),
+        OriginLocation = slipstreamArgusOriginPoint,
+        TargetLocation = slipstreamAzuremystTargetPoint,
         Color = new Color(155, 250, 50, 255)
       });
-
-      //Argus
       SpellSystem.Register(new SlipstreamSpellSpecificOriginAndDestination(ABILITY_A0RB_PORTAL_TO_ARGUS_DRAENEI)
       {
         PortalUnitTypeId = UNIT_N0D9_SLIPSTREAM_PORTAL_STORMWIND_KHADGAR,
         OpeningDelay = 20,
         ClosingDelay = 10,
-        OriginLocation = slipstreamOrigin,
-        TargetLocation = new Point(22010, -26816),
+        OriginLocation = slipstreamArgusOriginPoint,
+        TargetLocation = slipstreamArgusTargetPoint,
         Color = new Color(255, 50, 50, 255)
       });
 
-      //Outland
       SpellSystem.Register(new SlipstreamSpellSpecificOriginAndDestination(ABILITY_A0SR_PORTAL_TO_TEMPEST_KEEP_DRAENEI)
       {
         PortalUnitTypeId = UNIT_N0D9_SLIPSTREAM_PORTAL_STORMWIND_KHADGAR,
         OpeningDelay = 20,
         ClosingDelay = 10,
-        OriginLocation = slipstreamOrigin,
-        TargetLocation = new Point(2943, -21644),
+        OriginLocation = slipstreamTempestOriginPoint,
+        TargetLocation = slipstreamTempestTargetPoint,
         Color = new Color(55, 50, 250, 255)
       });
-      
+
       PassiveAbilityManager.Register(new MassiveAttackAbility(UNIT_N0CX_LIGHTFORGED_WARFRAME_DRAENEI)
       {
         AttackDamagePercentage = 0.3f,
