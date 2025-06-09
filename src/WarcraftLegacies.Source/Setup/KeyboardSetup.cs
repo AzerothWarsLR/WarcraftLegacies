aba5f1863f540eab11412eb0ff4f8c59e8f365f3
Technopig <127978232+Technopig1992@users.noreply.github.com>
2025-06-09 15:48:13 +0100

    f4 key selects fourth hero working, keyboard mapped on game start

---
+++ src/WarcraftLegacies.Source/Setup/KeyboardSetup.cs
@@ -0,0 +1,20 @@
+﻿using WarcraftLegacies.Source.KeyboardManager;
+
+namespace WarcraftLegacies.Source.Setup
+{
+  public static class KeyboardSetup
+  {
+    public static void InitializeKeyboard()
+    {
+      var debugger = new KeyboardDebugger();
+      var hotkeyManager = new HeroHotkeyManager();
+      
+      for (int i = 0; i < 24; i++) 
+      {
+        var player = Player(i);
+        debugger.Initialize(player);
+        hotkeyManager.Initialize(player);
+      }
+    }
+  }
+}
