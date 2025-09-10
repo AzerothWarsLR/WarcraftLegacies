44622954a3cd51b64764f5ce149dceb76fda9662
alrun3 <221875817+alrun3@users.noreply.github.com>
2025-09-10 21:58:40 +0200

    feat: add `csharp-to-lua` for transpiling C# to WC3 maps in folder mode

---
+++ src/Launcher/Commands/CSharpToLuaCommand.cs
@@ -0,0 +1,45 @@
+﻿#nullable enable
+using Launcher.Services;
+using Launcher.Settings;
+using System.CommandLine;
+using System.IO;
+using War3Net.Build;
+
+namespace Launcher.Commands
+{
+  internal static class CSharpToLuaCommand
+  {
+    public static void RegisterCSharpToLuaCommand(this RootCommand rootCommand)
+    {
+      var command = new Command("csharp-to-lua", "Transpiles C# source code into Lua and writes it into an existing Warcraft 3 map folder.");
+      rootCommand.Add(command);
+
+      var mapNameArgument = new Argument<string>(
+        name: "mapname",
+        description: "The name of the map folder inside the output directory (without extension). Transpiled code will be written into this folder.");
+      command.AddArgument(mapNameArgument);
+
+      var outputDirectoryArgument = new Argument<string>(
+        name: "outputDirectory",
+        description: "The directory containing map folders. The specified map folder will be resolved as a subdirectory of this path.");
+      command.AddArgument(outputDirectoryArgument);
+
+      var sourceCodeFolderArgument = new Argument<string>(
+        name: "sourcecodepath",
+        description: "The directory in which the C# source code is stored. This code will be transpiled into Lua and written into the map folder.");
+      command.AddArgument(sourceCodeFolderArgument);
+
+      command.SetHandler(Run, mapNameArgument, outputDirectoryArgument, sourceCodeFolderArgument);
+    }
+
+    private static void Run(string mapName, string outputDirectory, string sourceCodeFolderPath)
+    {
+      var compilerSettings = new CompilerSettings
+      {
+        ArtifactsPath = $"{Path.Combine(outputDirectory, mapName)}.w3x"
+      };
+
+      AdvancedMapBuilder.AddCSharpCode(Map.Open(compilerSettings.ArtifactsPath), sourceCodeFolderPath, compilerSettings);
+    }
+  }
+}
