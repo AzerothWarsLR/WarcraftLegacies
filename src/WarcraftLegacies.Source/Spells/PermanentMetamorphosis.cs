a8df533078e1a028f83b3e641d41e183bd0bbc4a
Zakary Boven <zakaryboven@gmail.com>
2025-09-22 19:50:43 +1200

    Added object data for passive Metamorphosis

---
+++ src/WarcraftLegacies.Source/Spells/PermanentMetamorphosis.cs
@@ -0,0 +1,22 @@
+﻿using MacroTools.SpellSystem;
+
+namespace WarcraftLegacies.Source.Spells;
+
+public sealed class PermanentMetamorphosis : IEffectOnLearn
+{
+  /// <summary>
+  /// The unit is transformed to look like this unit type when the ability is learned.
+  /// </summary>
+  public int DemonFormTypeId { get; init; }
+
+
+
+  /// <inheritdoc />
+  public void OnLearn(unit learner)
+  {
+    if (learner.Skin != DemonFormTypeId)
+    {
+      learner.Skin = DemonFormTypeId;
+    }
+  }
+}
