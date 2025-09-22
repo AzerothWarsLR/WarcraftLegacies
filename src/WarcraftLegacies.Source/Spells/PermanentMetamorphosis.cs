6de7e837f8e33ef9a0ca626b18903e6688aa693e
Zakary Boven <zakaryboven@gmail.com>
2025-09-22 19:57:45 +1200

    Added base ability Permanent Metamorphosis

--- src/WarcraftLegacies.Source/Spells/PermanentMetamorphosis.cs
+++ src/WarcraftLegacies.Source/Spells/PermanentMetamorphosis.cs
@@ -1,22 +1,46 @@
-﻿using MacroTools.SpellSystem;
+﻿using MacroTools.Data;
+using MacroTools.LegendSystem;
+using MacroTools.SpellSystem;
 
 namespace WarcraftLegacies.Source.Spells;
 
-public sealed class PermanentMetamorphosis : IEffectOnLearn
+/// <summary>
+/// When learned, permanently turns the hero into a demon with a ranged attack.
+/// </summary>
+public sealed class PermanentMetamorphosis : Spell, IEffectOnLearn
 {
-  /// <summary>
-  /// The unit is transformed to look like this unit type when the ability is learned.
-  /// </summary>
-  public int DemonFormTypeId { get; init; }
+  public PermanentMetamorphosis(int id) : base(id)
+  {
+  }
 
+  public LeveledAbilityField<int> HitPointBonus { get; init; } = new();
 
+  public int[] UnitTypeIdsByLevel { get; init; } = System.Array.Empty<int>();
+
+  public string? LearnEffectPath { get; init; }
 
   /// <inheritdoc />
   public void OnLearn(unit learner)
   {
-    if (learner.Skin != DemonFormTypeId)
+    var level = learner.GetAbilityLevel(Id);
+
+    var legend = LegendaryHeroManager.GetFromUnit(learner);
+    if (legend == null)
     {
-      learner.Skin = DemonFormTypeId;
+      return;
     }
+
+    legend.UnitType = UnitTypeIdsByLevel[level-1];
+
+    if (level == 1)
+    {
+      if (LearnEffectPath != null)
+      {
+        var learnEffect = AddSpecialEffect(LearnEffectPath, learner.X, learner.Y);
+        learnEffect.Dispose();
+      }
+    }
+
+    learner.MaxLife += HitPointBonus.Base + HitPointBonus.PerLevel;
   }
 }
