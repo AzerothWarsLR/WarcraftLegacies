e2a8eca7fe80a0a958fb466410acac56f7823ebd
Zakary Boven <zakaryboven@gmail.com>
2025-09-22 21:09:00 +1200

    Added method to add abilities to unit and trigger OnLearn

---
+++ src/MacroTools/SpellSystem/UnitExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace MacroTools.SpellSystem;
+
+public static class UnitExtensions
+{
+  public static void SetAbilityLevelWithEvents(this unit unit, int abilityId, int level)
+  {
+    var currentLevel = unit.GetAbilityLevel(abilityId);
+    if (SpellSystem.TryGetSpellByAbilityId(abilityId, out var spell))
+    {
+      if (spell is IEffectOnLearn effectOnLearn)
+      {
+        if (level > currentLevel)
+        {
+          for (var i = currentLevel; i < level; i++)
+          {
+            effectOnLearn.OnLearn(unit);
+          }
+        }
+      }
+    }
+    unit.SetAbilityLevel(abilityId, level);
+  }
+}
