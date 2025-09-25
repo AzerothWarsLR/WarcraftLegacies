b903d38327e627da009cfdebc9f227468b405f88
Zakary Boven <zakaryboven@gmail.com>
2025-09-25 18:30:58 +1200

    Removed special handling of Neutral Victim player slot

--- src/WarcraftLegacies.Source/Setup/NeutralVictimAndPassiveSetup.cs
+++ src/WarcraftLegacies.Source/Setup/NeutralVictimAndPassiveSetup.cs
@@ -1,34 +1,19 @@
-﻿using System.Collections.Generic;
-using MacroTools.Extensions;
+﻿using MacroTools.Extensions;
 using WCSharp.Shared;
 
 namespace WarcraftLegacies.Source.Setup;
 
 public static class NeutralVictimAndPassiveSetup
 {
-  private static List<unit> _hideUnit;
-
-  private static void Unally(player sourcePlayer, player otherPlayer)
-  {
-    sourcePlayer.SetAlliance(otherPlayer, alliancetype.Passive, false);
-    sourcePlayer.SetAlliance(otherPlayer, alliancetype.HelpRequest, false);
-    sourcePlayer.SetAlliance(otherPlayer, alliancetype.HelpResponse, false);
-    sourcePlayer.SetAlliance(otherPlayer, alliancetype.SharedExperience, false);
-    sourcePlayer.SetAlliance(otherPlayer, alliancetype.SharedSpells, false);
-  }
-
   public static void Setup()
   {
-    _hideUnit = Regions.HideUnitBottomLeft.PrepareUnitsForRescue(RescuePreparationMode.HideAll);
+    Regions.HideUnitBottomLeft.PrepareUnitsForRescue(RescuePreparationMode.HideAll);
     foreach (var player in Util.EnumeratePlayers())
     {
-      Unally(player, player.NeutralVictim);
-      Unally(player.NeutralVictim, player);
       player.SetAlliance(player.Create(27), alliancetype.Passive, true);
       player.SetAlliance(player.Create(27), alliancetype.SharedVision, false);
       player.SetAlliance(player.Create(26), alliancetype.Passive, true);
       player.SetAlliance(player.Create(26), alliancetype.SharedVision, false);
-
     }
   }
 }
