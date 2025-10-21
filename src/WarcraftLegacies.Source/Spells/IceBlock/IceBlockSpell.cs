1bb8592aa1e92185215b60c72cfe05d661bd3d41
Zakary Boven <zakaryboven@gmail.com>
2025-10-22 06:45:48 +1300

    Cleaned up two special effect creation leaks from Ice Block

--- src/WarcraftLegacies.Source/Spells/IceBlock/IceBlockSpell.cs
+++ src/WarcraftLegacies.Source/Spells/IceBlock/IceBlockSpell.cs
@@ -61,6 +61,6 @@ public sealed class IceBlockSpell : Spell
 
     BuffSystem.Add(iceBlockBuff);
 
-    effect.Create(CastEffectPath, targetPoint.X, targetPoint.Y);
+    effect.Create(CastEffectPath, targetPoint.X, targetPoint.Y).Dispose();
   }
 }
