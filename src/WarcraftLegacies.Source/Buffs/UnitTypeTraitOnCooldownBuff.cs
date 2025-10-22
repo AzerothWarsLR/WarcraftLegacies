3d282f1b9b136c3c3888389164c4d3a0ac9bbc8c
Zakary Boven <zakaryboven@gmail.com>
2025-10-23 10:08:00 +1300

    Remove UnitEventSystem and refactor SpellOnAttack to use BuffSystem for cooldowns

---
+++ src/WarcraftLegacies.Source/Buffs/UnitTypeTraitOnCooldownBuff.cs
@@ -0,0 +1,21 @@
+﻿using MacroTools.UnitTypeTraits;
+using WCSharp.Buffs;
+
+namespace WarcraftLegacies.Source.Buffs;
+
+/// <summary>
+/// Marker buff to indicate that any instance of a <see cref="UnitTypeTrait"/>
+/// can't be applied to this unit for the duration.
+/// </summary>
+public sealed class UnitTypeTraitOnCooldownBuff : PassiveBuff
+{
+  /// <summary>
+  /// The trait on cooldown.
+  /// </summary>
+  public required UnitTypeTrait Trait { get; init; }
+
+  /// <inheritdoc />
+  public UnitTypeTraitOnCooldownBuff(unit caster, unit target) : base(caster, target)
+  {
+  }
+}
