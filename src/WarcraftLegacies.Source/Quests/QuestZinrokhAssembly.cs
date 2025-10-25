3effeffb949fadad416c00c167a247e9ff87bf84
Zakary Boven <zakaryboven@gmail.com>
2025-10-26 12:21:55 +1300

    Move Titanforging mechanic from MacroTools to WarcraftLegacies.Source

--- src/WarcraftLegacies.Source/Quests/QuestZinrokhAssembly.cs
+++ src/WarcraftLegacies.Source/Quests/QuestZinrokhAssembly.cs
@@ -49,10 +49,7 @@ public sealed class QuestZinrokhAssembly : QuestData
       ArtifactManager.Destroy(artifact);
     }
 
-    var zinrokh = new Artifact(item.Create(ITEM_I016_ZIN_ROKH_DESTROYER_OF_WORLDS, 0, 0))
-    {
-      TitanforgedAbility = ABILITY_A0VM_TITANFORGED_9_STRENGTH
-    };
+    var zinrokh = new Artifact(item.Create(ITEM_I016_ZIN_ROKH_DESTROYER_OF_WORLDS, 0, 0));
     ArtifactManager.Register(zinrokh);
     azureFragmentHolder?.AddItemSafe(zinrokh.Item);
   }
--- src/WarcraftLegacies.Source/Setup/Spells/IronforgeSpellSetup.cs
+++ src/WarcraftLegacies.Source/Setup/Spells/IronforgeSpellSetup.cs
@@ -1,4 +1,5 @@
-﻿using MacroTools.Spells;
+﻿using System.Collections.Generic;
+using MacroTools.Spells;
 using MacroTools.UnitTypeTraits;
 using WarcraftLegacies.Source.Spells;
 using WarcraftLegacies.Source.UnitTypeTraits;
@@ -12,7 +13,16 @@ public static class IronforgeSpellSetup
   /// </summary>
   public static void Setup()
   {
-    SpellRegistry.Register(new TitanForgeArtifact(ABILITY_A0T3_TITANFORGE_ARTIFACT_IRONFORGE, 0));
+    SpellRegistry.Register(new TitanForgeArtifact(ABILITY_A0T3_TITANFORGE_ARTIFACT_IRONFORGE)
+    {
+      DefaultTitanforgedAbility = FourCC("A0VJ"),
+      UniqueTitanforgedAbilitiesByItemTypeId = new Dictionary<int, int>
+      {
+        { ITEM_I016_ZIN_ROKH_DESTROYER_OF_WORLDS, ABILITY_A0VM_TITANFORGED_9_STRENGTH },
+        { ITEM_I015_XAL_ATATH_BLADE_OF_THE_BLACK_EMPIRE, ABILITY_A0VM_TITANFORGED_9_STRENGTH }
+      },
+      GoldCost = 25
+    });
 
     UnitTypeTraitRegistry.Register(new SpellOnAttack(ABILITY_A10J_MASTER_OF_LIGHTNING_STORMRIDERS)
     {
