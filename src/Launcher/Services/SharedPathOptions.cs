1e6312975c18a283b4c5a620781bad85e9736984
Zakary Boven <zakaryboven@gmail.com>
2025-12-19 21:13:59 +1300

    Add ConstantsGenerator to generate Constants and Regions files from map data

--- src/Launcher/Services/SharedPathOptions.cs
+++ src/Launcher/Services/SharedPathOptions.cs
@@ -21,6 +21,8 @@ public sealed class SharedPathOptions
       ScriptArtifactPath = Path.Combine(rootPath, PathConventions.ArtifactsPath),
       Warcraft3ExecutablePath = settings.CompilerSettings.Warcraft3ExecutablePath,
       MapDataPath = Path.Combine(settings.CompilerSettings.RootPath, PathConventions.MapDataPath, mapName),
+      SourceProjectPath = Path.Combine(rootPath, PathConventions.SrcPath, $"{mapName}{PathConventions.SourceProjectSuffix}"),
+      SharedProjectPath = Path.Combine(rootPath, PathConventions.SrcPath, $"{mapName}{PathConventions.SharedProjectSuffix}")
     };
     return options;
   }
@@ -28,35 +30,45 @@ public sealed class SharedPathOptions
   /// <summary>
   /// Path to the folder in which to write the fully built map.
   /// </summary>
-  public required string W3XFolderPath { get; set; }
+  public required string W3XFolderPath { get; init; }
 
   /// <summary>
   /// Path to the file in which to write published, ready-to-release maps.
   /// </summary>
-  public required string PublishPath { get; set; }
+  public required string PublishPath { get; init; }
 
   /// <summary>
   /// Location of the main .NET project containing the map's code.
   /// </summary>
-  public required string CsProjPath { get; set; }
+  public required string CsProjPath { get; init; }
 
   /// <summary>
   /// Path to the preferred backup folder.
   /// </summary>
-  public required string BackupPath { get; set; }
+  public required string BackupPath { get; init; }
 
   /// <summary>
   /// Path to the folder the war3map.lua should be copied to for later inspection.
   /// </summary>
-  public required string ScriptArtifactPath { get; set; }
+  public required string ScriptArtifactPath { get; init; }
 
   /// <summary>
   /// Path to the folder where this project's Json data is stored.
   /// </summary>
-  public required string MapDataPath { get; set; }
+  public required string MapDataPath { get; init; }
 
   /// <summary>
   /// Path to the Warcraft III executable.
   /// </summary>
-  public required string Warcraft3ExecutablePath { get; set; }
+  public required string Warcraft3ExecutablePath { get; init; }
+
+  /// <summary>
+  ///  Folder path in which the map's main C# files are stored.
+  /// </summary>
+  public required string SourceProjectPath { get; init; }
+
+  /// <summary>
+  ///  Folder path in which the map's shared C# files are stored.
+  /// </summary>
+  public required string SharedProjectPath { get; init; }
 }
