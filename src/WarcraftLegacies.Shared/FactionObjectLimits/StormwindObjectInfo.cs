a219cc7dc61076964679098decf5a283e70b3953
Zakary Boven <zakaryboven@gmail.com>
2026-01-02 22:13:41 +1300

    Add unit categories to all Stormwind units

--- src/WarcraftLegacies.Shared/FactionObjectLimits/StormwindObjectInfo.cs
+++ src/WarcraftLegacies.Shared/FactionObjectLimits/StormwindObjectInfo.cs
@@ -1,4 +1,5 @@
 ﻿using MacroTools.Shared;
+using static MacroTools.Shared.UnitCategory;
 
 namespace WarcraftLegacies.Shared.FactionObjectLimits;
 
@@ -8,39 +9,40 @@ public static class StormwindObjectInfo
 
   public static IEnumerable<ObjectInfo> GetAllObjectLimits()
   {
-    yield return new(UNIT_H06K_TOWN_HALL_STORMWIND_T1, Unlimited);
-    yield return new(UNIT_H06M_STORMWIND_T2, Unlimited);
-    yield return new(UNIT_H06N_CASTLE_STORMWIND_T3, Unlimited);
-    yield return new(UNIT_H072_HOMESTEAD_STORMWIND_FARM, Unlimited);
-    yield return new(UNIT_H06T_ALTAR_OF_KINGS_STORMWIND_ALTAR, Unlimited);
-    yield return new(UNIT_H06E_BARRACKS_STORMWIND_BARRACKS, Unlimited);
-    yield return new(UNIT_H06F_BLACKSMITH_STORMWIND_RESEARCH, Unlimited);
-    yield return new(UNIT_H06A_SMITHY_STORMWIND_SPECIALIST, Unlimited);
-    yield return new(UNIT_HARS_ARCANE_SANCTUM_STORMWIND_MAGIC, Unlimited);
-    yield return new(UNIT_H06V_SCOUT_TOWER_STORMWIND_TOWER, Unlimited);
-    yield return new(UNIT_H06W_GUARD_TOWER_STORMWIND_TOWER, Unlimited);
-    yield return new(UNIT_H070_IMPROVED_GUARD_TOWER_STORMWIND_TOWER, Unlimited);
-    yield return new(UNIT_H06X_CANNON_TOWER_STORMWIND_TOWER, Unlimited);
-    yield return new(UNIT_H071_IMPROVED_CANNON_TOWER_STORMWIND_TOWER, Unlimited);
-    yield return new(UNIT_N07T_AUCTION_HOUSE_STORMWIND_SHOP, Unlimited);
-    yield return new(UNIT_H06D_ROYAL_HARBOUR_STORMWIND_SHIPYARD, Unlimited, UnitCategory.Shipyard);
-    yield return new(UNIT_H06Y_ARCANE_TOWER_STORMWIND_TOWER, Unlimited);
-    yield return new(UNIT_H06Z_IMPROVED_ARCANE_TOWER_STORMWIND_TOWER, Unlimited);
-    yield return new(UNIT_H024_LIGHT_HOUSE_STORMWIND_OTHER, 10); //Light House
-    yield return new(UNIT_H05J_CHAMPION_S_HALL_STORMWIND_OTHER, 1);
-    yield return new(UNIT_H05A_WIZARD_S_SANCTUM_STORMWIND_OTHER, 1);
-    yield return new(UNIT_HPEA_PEASANT_LORDAERON_STORMWIND_WORKER, Unlimited);
-    yield return new(UNIT_H02O_BLADESMAN_STORMWIND, Unlimited);
-    yield return new(UNIT_H03K_MARSHAL_STORMWIND, 12);
-    yield return new(UNIT_H03Z_STORMRIDER_IRONFORGE, 12);
-    yield return new(UNIT_H00A_SPEARMAN_STORMWIND, Unlimited);
-    yield return new(UNIT_H01B_OUTRIDER_STORMWIND, Unlimited);
-    yield return new(UNIT_H05F_STORMWIND_CHAMPION_STORMWIND_ELITE, 6);
-    yield return new(UNIT_N05L_CONJURER_STORMWIND_ELITE, 6);
-    yield return new(UNIT_H00J_CLERGYMAN_STORMWIND, Unlimited);
-    yield return new(UNIT_N06N_GUNSHIP_STORMWIND, 6);
-    yield return new(UNIT_N093_CHAPLAIN_STORMWIND, Unlimited);
-    yield return new(UNIT_O06K_SIEGE_TOWER_STORMWIND, 5);
+    yield return new(UNIT_H06K_TOWN_HALL_STORMWIND_T1, Unlimited, TownHall);
+    yield return new(UNIT_H06M_STORMWIND_T2, Unlimited, TownHall);
+    yield return new(UNIT_H06N_CASTLE_STORMWIND_T3, Unlimited, TownHall);
+    yield return new(UNIT_H072_HOMESTEAD_STORMWIND_FARM, Unlimited, Farm);
+    yield return new(UNIT_H06T_ALTAR_OF_KINGS_STORMWIND_ALTAR, Unlimited, Altar);
+    yield return new(UNIT_H06E_BARRACKS_STORMWIND_BARRACKS, Unlimited, Barracks);
+    yield return new(UNIT_H06F_BLACKSMITH_STORMWIND_RESEARCH, Unlimited, Research);
+    yield return new(UNIT_H06A_SMITHY_STORMWIND_SPECIALIST, Unlimited, Specialist);
+    yield return new(UNIT_HARS_ARCANE_SANCTUM_STORMWIND_MAGIC, Unlimited, Magic);
+    yield return new(UNIT_H06V_SCOUT_TOWER_STORMWIND_TOWER, Unlimited, Tower);
+    yield return new(UNIT_H06W_GUARD_TOWER_STORMWIND_TOWER, Unlimited, Tower);
+    yield return new(UNIT_H070_IMPROVED_GUARD_TOWER_STORMWIND_TOWER, Unlimited, Tower);
+    yield return new(UNIT_H06X_CANNON_TOWER_STORMWIND_TOWER, Unlimited, Tower);
+    yield return new(UNIT_H071_IMPROVED_CANNON_TOWER_STORMWIND_TOWER, Unlimited, Tower);
+    yield return new(UNIT_N07T_AUCTION_HOUSE_STORMWIND_SHOP, Unlimited, Shop);
+    yield return new(UNIT_H06D_ROYAL_HARBOUR_STORMWIND_SHIPYARD, Unlimited, Shipyard);
+    yield return new(UNIT_H06Y_ARCANE_TOWER_STORMWIND_TOWER, Unlimited, Tower);
+    yield return new(UNIT_H06Z_IMPROVED_ARCANE_TOWER_STORMWIND_TOWER, Unlimited, Tower);
+    yield return new(UNIT_H024_LIGHT_HOUSE_STORMWIND_OTHER, 10, Tower);
+    yield return new(UNIT_H05J_CHAMPION_S_HALL_STORMWIND_OTHER, 1, Research);
+    yield return new(UNIT_H05A_WIZARD_S_SANCTUM_STORMWIND_OTHER, 1, Research);
+
+    yield return new(UNIT_HPEA_PEASANT_LORDAERON_STORMWIND_WORKER, Unlimited, Worker);
+    yield return new(UNIT_H02O_BLADESMAN_STORMWIND, Unlimited, Tank);
+    yield return new(UNIT_H03K_MARSHAL_STORMWIND, 12, Tank);
+    yield return new(UNIT_H00A_SPEARMAN_STORMWIND, Unlimited, Marksman);
+    yield return new(UNIT_H01B_OUTRIDER_STORMWIND, Unlimited, Fighter);
+    yield return new(UNIT_H05F_STORMWIND_CHAMPION_STORMWIND_ELITE, 6, new List<UnitCategory> { Elite, Fighter, Support });
+    yield return new(UNIT_N05L_CONJURER_STORMWIND_ELITE, 6, new List<UnitCategory> { Destroyer, Support, Summoner });
+    yield return new(UNIT_H00J_CLERGYMAN_STORMWIND, Unlimited, Support);
+    yield return new(UNIT_N06N_GUNSHIP_STORMWIND, 6, new List<UnitCategory> { Flyer, Marksman, Support });
+    yield return new(UNIT_N093_CHAPLAIN_STORMWIND, Unlimited, Support);
+    yield return new(UNIT_O06K_SIEGE_TOWER_STORMWIND, 5, Siege);
+
     yield return new(UNIT_HBOT_TRANSPORT_SHIP_ALLIANCE, Unlimited);
     yield return new(UNIT_H0AR_SCOUT_SHIP_ALLIANCE, Unlimited);
     yield return new(UNIT_H0AX_FRIGATE_ALLIANCE, Unlimited);
@@ -50,10 +52,12 @@ public static class StormwindObjectInfo
     yield return new(UNIT_H0AN_JUGGERNAUT_ALLIANCE, Unlimited);
     yield return new(UNIT_H0B7_BOMBARD_ALLIANCE, 6);
     yield return new(UNIT_H060_CARRIER_FLAGSHIP_STORMWIND, 3);
-    yield return new(UNIT_H00R_KING_OF_STORMWIND_STORMWIND, 1);
-    yield return new(UNIT_H017_HIGHLORD_OF_THE_ALLIANCE_STORMWIND, 1);
-    yield return new(UNIT_H05Y_LORD_WIZARD_STORMWIND, 1);
-    yield return new(UNIT_H00Z_CROWN_PRINCE_OF_STROMGARDE_STORMWIND, 1);
+
+    yield return new(UNIT_H00R_KING_OF_STORMWIND_STORMWIND, 1, new List<UnitCategory> { Fighter, Support });
+    yield return new(UNIT_H017_HIGHLORD_OF_THE_ALLIANCE_STORMWIND, 1, new List<UnitCategory> { Tank, Support });
+    yield return new(UNIT_H05Y_LORD_WIZARD_STORMWIND, 1, new List<UnitCategory> { Destroyer, Support });
+    yield return new(UNIT_H00Z_CROWN_PRINCE_OF_STROMGARDE_STORMWIND, 1, new List<UnitCategory> { Tank, Support });
+
     yield return new(UPGRADE_R02E_LIGHT_S_PRAISE_ADEPT_TRAINING_ARATHOR_LORDAERON, Unlimited);
     yield return new(UPGRADE_R005_CLERGYMAN_ADEPT_TRAINING_ARATHOR, Unlimited);
     yield return new(UPGRADE_R02B_CHAINMAIL_LAYERING_ARATHOR, Unlimited);
