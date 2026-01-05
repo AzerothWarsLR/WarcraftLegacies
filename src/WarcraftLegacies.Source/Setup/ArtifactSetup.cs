ae4bc33ec7638c5d16fb436dd1d9baf492402a89
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-01-05 19:00:40 +0100

    perf: replace more FourCCs with numeric constants

--- src/WarcraftLegacies.Source/Setup/ArtifactSetup.cs
+++ src/WarcraftLegacies.Source/Setup/ArtifactSetup.cs
@@ -90,34 +90,34 @@ public sealed class ArtifactSetup
   public ArtifactSetup()
   {
     CrownOfStormwind = new Artifact(item.Create(FourCC("I002"), DummyX, DummyY));
-    PreplacedWidgets.Units.Get(FourCC("n021")).AddItem(CrownOfStormwind.Item); //Hogger
+    PreplacedWidgets.Units.Get(UNIT_N021_HOGGER).AddItem(CrownOfStormwind.Item);
     ArtifactManager.Register(CrownOfStormwind);
 
-    ScarabGong = new Artifact(item.Create(FourCC("ISGC"), DummyX, DummyY)); //Scarab Gong
-    PreplacedWidgets.Units.Get(FourCC("nJTR")).AddItem(ScarabGong.Item); //Scarab Gong
+    ScarabGong = new Artifact(item.Create(FourCC("ISGC"), DummyX, DummyY));
+    PreplacedWidgets.Units.Get(UNIT_NJTR_JONATHAN_THE_REVELATOR).AddItem(ScarabGong.Item);
     ArtifactManager.Register(ScarabGong);
 
     EyeOfSargeras = new Artifact(item.Create(ITEM_I003_EYE_OF_SARGERAS, -77.9f, 10910.4f));
     ArtifactManager.Register(EyeOfSargeras);
 
-    HelmOfDomination = new Artifact(item.Create(FourCC("I01Y"), DummyX, DummyY)); //Helm of Domination
-    PreplacedWidgets.Units.Get(FourCC("u000_0649")).AddAbility(Artifact.ArtifactHolderAbilId); //Frozen Throne
-    PreplacedWidgets.Units.Get(FourCC("u000")).AddItem(HelmOfDomination.Item);
+    HelmOfDomination = new Artifact(item.Create(FourCC("I01Y"), DummyX, DummyY));
+    PreplacedWidgets.Units.Get(UNIT_U000_FROZEN_THRONE_SCOURGE_MAIN).AddAbility(Artifact.ArtifactHolderAbilId);
+    PreplacedWidgets.Units.Get(UNIT_U000_FROZEN_THRONE_SCOURGE_MAIN).AddItem(HelmOfDomination.Item);
     ArtifactManager.Register(HelmOfDomination);
 
-    CrownOfLordaeron = new Artifact(item.Create(FourCC("I001"), DummyX, DummyY)); //Crown of Lordaeron
-    PreplacedWidgets.Units.Get(FourCC("nemi_0019")).AddAbility(Artifact.ArtifactHolderAbilId); //King Terenas
-    PreplacedWidgets.Units.Get(FourCC("nemi")).AddItem(CrownOfLordaeron.Item);
+    CrownOfLordaeron = new Artifact(item.Create(FourCC("I001"), DummyX, DummyY));
+    PreplacedWidgets.Units.Get(UNIT_NEMI_KING_TERENAS_MENETHIL_LORDAERON).AddAbility(Artifact.ArtifactHolderAbilId);
+    PreplacedWidgets.Units.Get(UNIT_NEMI_KING_TERENAS_MENETHIL_LORDAERON).AddItem(CrownOfLordaeron.Item);
     ArtifactManager.Register(CrownOfLordaeron);
 
     var tempArtifact = new Artifact(item.Create(FourCC("klmm"), DummyX, DummyY)); //Killmaim
-    PreplacedWidgets.Units.Get(UNIT_H0BD_RAMZES_THE_HORROR_CREEP_DESOLACE).AddAbility(Artifact.ArtifactHolderAbilId); //Ramzes the Horror
+    PreplacedWidgets.Units.Get(UNIT_H0BD_RAMZES_THE_HORROR_CREEP_DESOLACE).AddAbility(Artifact.ArtifactHolderAbilId);
     PreplacedWidgets.Units.Get(UNIT_H0BD_RAMZES_THE_HORROR_CREEP_DESOLACE).AddItem(tempArtifact.Item);
     ArtifactManager.Register(tempArtifact);
 
     ScepterOfTheQueen = new Artifact(item.Create(FourCC("I00I"), DummyX, DummyY));
-    PreplacedWidgets.Units.Get(FourCC("n085_2846")).AddAbility(Artifact.ArtifactHolderAbilId); //The Atheneum
-    PreplacedWidgets.Units.Get(FourCC("n085")).AddItem(ScepterOfTheQueen.Item);
+    PreplacedWidgets.Units.Get(UNIT_N085_THE_ATHENAEUM_SENTINELS_OTHER).AddAbility(Artifact.ArtifactHolderAbilId);
+    PreplacedWidgets.Units.Get(UNIT_N085_THE_ATHENAEUM_SENTINELS_OTHER).AddItem(ScepterOfTheQueen.Item);
     ArtifactManager.Register(ScepterOfTheQueen);
 
     BookOfMedivh = new Artifact(item.Create(ITEM_I006_BOOK_OF_MEDIVH, DummyX, DummyY));
@@ -142,8 +142,8 @@ public sealed class ArtifactSetup
     PreplacedWidgets.Units.Get(UNIT_O06W_GAL_DARAH_CREEP_ZUL_DRAK).AddItem(AzureFragment.Item);
     ArtifactManager.Register(AzureFragment);
 
-    EssenceofMurmur = new Artifact(item.Create(FourCC("I00K"), DummyX, DummyY)); //Essence
-    PreplacedWidgets.Units.Get(FourCC("n03T")).AddItem(EssenceofMurmur.Item); //Murmur
+    EssenceofMurmur = new Artifact(item.Create(FourCC("I00K"), DummyX, DummyY));
+    PreplacedWidgets.Units.Get(UNIT_N03T_MURMUR_CREEP).AddItem(EssenceofMurmur.Item);
     ArtifactManager.Register(EssenceofMurmur);
 
     EmeraldFragment = new Artifact(item.Create(ITEM_I01K_EMERALD_FRAGMENT, DummyX, DummyY));
@@ -151,17 +151,17 @@ public sealed class ArtifactSetup
     ArtifactManager.Register(EmeraldFragment);
 
     tempArtifact = new Artifact(item.Create(FourCC("arsh"), DummyX, DummyY)); //Shroud of Nozdormuru
-    PreplacedWidgets.Units.Get(UNIT_O070_OCCULUS_CREEP_CAVERNS).AddAbility(Artifact.ArtifactHolderAbilId); //Occulus
+    PreplacedWidgets.Units.Get(UNIT_O070_OCCULUS_CREEP_CAVERNS).AddAbility(Artifact.ArtifactHolderAbilId);
     PreplacedWidgets.Units.Get(UNIT_O070_OCCULUS_CREEP_CAVERNS).AddItem(tempArtifact.Item);
     ArtifactManager.Register(tempArtifact);
 
     HornOfCenarius = new Artifact(item.Create(FourCC("cnhn"), DummyX, DummyY));
-    PreplacedWidgets.Units.Get(FourCC("nhcn_2597")).AddAbility(Artifact.ArtifactHolderAbilId); //Horn of Cenarius Pedestal
-    PreplacedWidgets.Units.Get(FourCC("nhcn")).AddItem(HornOfCenarius.Item);
+    PreplacedWidgets.Units.Get(UNIT_NHCN_HORN_OF_CENARIUS_PEDESTAL_CREEP).AddAbility(Artifact.ArtifactHolderAbilId);
+    PreplacedWidgets.Units.Get(UNIT_NHCN_HORN_OF_CENARIUS_PEDESTAL_CREEP).AddItem(HornOfCenarius.Item);
     ArtifactManager.Register(HornOfCenarius);
 
     tempArtifact = new Artifact(item.Create(FourCC("kgal"), DummyX, DummyY)); //Keg of Thunderwater
-    PreplacedWidgets.Units.GetClosest(FourCC("hmtm"), 15109, -895).AddItem(tempArtifact.Item);
+    PreplacedWidgets.Units.GetClosest(UNIT_HMTM_MORTAR_TEAM_IRONFORGE, 15109, -895).AddItem(tempArtifact.Item);
     ArtifactManager.Register(tempArtifact);
 
     SunwellVial = new Artifact(item.Create(ITEM_I018_VIAL_OF_THE_SUNWELL, DummyX, DummyX));
