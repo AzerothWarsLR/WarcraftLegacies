f5487d41738a5a20eac0adcf7a1dc39a6aaa0db0
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-01-08 17:43:13 +0000

    Update EliteNameSystem.cs

--- src/WarcraftLegacies.Source/UniqueUnitNames/EliteNameSystem.cs
+++ src/WarcraftLegacies.Source/UniqueUnitNames/EliteNameSystem.cs
@@ -10,80 +10,80 @@ namespace WarcraftLegacies.Source.UniqueUnitNames;
 /// </summary>
 public static class EliteNameSystem
 {
-    private static readonly Random Random = new Random();
-    private static readonly Dictionary<int, HashSet<string>> NamesInUse = new();
+  private static readonly Random Random = new Random();
+  private static readonly Dictionary<int, HashSet<string>> NamesInUse = new();
 
-    /// <summary>
-    /// Sets up the system to automatically apply unique names when elite units are created or die.
-    /// Only registers events for unit types that have names defined.
-    /// </summary>
-    public static void Setup()
+  /// <summary>
+  /// Sets up the system to automatically apply unique names when elite units are created or die.
+  /// Only registers events for unit types that have names defined.
+  /// </summary>
+  public static void Setup()
+  {
+    EliteNames.Init();
+
+    foreach (var unitType in EliteNames.NamePool.Keys)
     {
-        EliteNames.Init();
+      PlayerUnitEvents.Register(UnitTypeEvent.IsCreated, OnUnitCreated, unitType);
+      PlayerUnitEvents.Register(UnitTypeEvent.Dies, OnUnitDeath, unitType);
+    }
+  }
 
-        foreach (var unitType in EliteNames.NamePool.Keys)
-        {
-            PlayerUnitEvents.Register(UnitTypeEvent.IsCreated, OnUnitCreated, unitType);
-            PlayerUnitEvents.Register(UnitTypeEvent.Dies, OnUnitDeath, unitType);
-        }
+  private static void OnUnitCreated()
+  {
+    var unit = @event.Unit;
+    if (unit == null)
+    {
+      return;
     }
 
-    private static void OnUnitCreated()
-    {
-        var unit = @event.Unit;
-        if (unit == null)
-        {
-            return;
-        }
+    AssignName(unit);
+  }
 
-        AssignName(unit);
+  private static void OnUnitDeath()
+  {
+    var unit = @event.Unit;
+    if (unit == null)
+    {
+      return;
     }
 
-    private static void OnUnitDeath()
+    if (!NamesInUse.ContainsKey(unit.UnitType))
     {
-        var unit = @event.Unit;
-        if (unit == null)
-        {
-            return;
-        }
-
-        if (!NamesInUse.ContainsKey(unit.UnitType))
-        {
-            return;
-        }
-
-        NamesInUse[unit.UnitType].Remove(unit.Name);
+      return;
     }
 
-    private static void AssignName(unit unit)
+    NamesInUse[unit.UnitType].Remove(unit.Name);
+  }
+
+  private static void AssignName(unit unit)
+  {
+    if (!EliteNames.NamePool.ContainsKey(unit.UnitType))
     {
-        if (!EliteNames.NamePool.ContainsKey(unit.UnitType))
-        {
-            return;
-        }
-
-        if (!NamesInUse.ContainsKey(unit.UnitType))
-        {
-            NamesInUse[unit.UnitType] = new HashSet<string>();
-        }
-
-        var availableNames = new List<string>();
-        foreach (var name in EliteNames.NamePool[unit.UnitType])
-        {
-            if (!NamesInUse[unit.UnitType].Contains(name))
-            {
-                availableNames.Add(name);
-            }
-        }
-
-        if (availableNames.Count == 0)
-        {
-            unit.Name = "";
-            return;
-        }
-
-        var chosenName = availableNames[Random.Next(availableNames.Count)];
-        unit.Name = chosenName;
-        NamesInUse[unit.UnitType].Add(chosenName);
+      return;
     }
+
+    if (!NamesInUse.ContainsKey(unit.UnitType))
+    {
+      NamesInUse[unit.UnitType] = new HashSet<string>();
+    }
+
+    var availableNames = new List<string>();
+    foreach (var name in EliteNames.NamePool[unit.UnitType])
+    {
+      if (!NamesInUse[unit.UnitType].Contains(name))
+      {
+        availableNames.Add(name);
+      }
+    }
+
+    if (availableNames.Count == 0)
+    {
+      unit.Name = "";
+      return;
+    }
+
+    var chosenName = availableNames[Random.Next(availableNames.Count)];
+    unit.Name = chosenName;
+    NamesInUse[unit.UnitType].Add(chosenName);
+  }
 }
