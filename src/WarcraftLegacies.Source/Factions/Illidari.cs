7c405e4d5f59ba32a06a1567dbca494da310201c
Madsen <benmadse@gmail.com>
2026-01-26 01:42:00 +0100

    Update Illidari.cs

--- src/WarcraftLegacies.Source/Factions/Illidari.cs
+++ src/WarcraftLegacies.Source/Factions/Illidari.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using MacroTools.DialogueSystem;
 using MacroTools.Extensions;
 using MacroTools.FactionSystem;
