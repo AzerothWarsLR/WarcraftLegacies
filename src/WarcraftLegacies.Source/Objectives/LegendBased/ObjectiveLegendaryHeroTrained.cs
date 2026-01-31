136cb9354926fea92dd19fb61b31ad3ec425cd88
Zakary Boven <zakaryboven@gmail.com>
2026-02-01 00:39:38 +1300

    Add Objective that completes when a specific LegendaryHero is trained

---
+++ src/WarcraftLegacies.Source/Objectives/LegendBased/ObjectiveLegendaryHeroTrained.cs
@@ -0,0 +1,23 @@
+﻿using MacroTools.Legends;
+using MacroTools.Quests;
+
+namespace WarcraftLegacies.Source.Objectives.LegendBased;
+
+/// <summary>
+/// Completed when the <see cref="LegendaryHero"/> is trained, by anyone.
+/// </summary>
+public sealed class ObjectiveLegendaryHeroTrained : Objective
+{
+  /// <summary>
+  /// Initializes a new instance of the <see cref="ObjectiveLegendaryHeroTrained"/> class.
+  /// </summary>
+  public ObjectiveLegendaryHeroTrained(LegendaryHero target)
+  {
+    target.ChangedOwner += OnTargetChangeOwner;
+  }
+
+  private void OnTargetChangeOwner(object? sender, LegendChangeOwnerEventArgs e)
+  {
+    Progress = QuestProgress.Complete;
+  }
+}
