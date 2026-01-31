6eb0efcc661c2880b293a1c09b8650d3c578bb16
Zakary Boven <zakaryboven@gmail.com>
2026-02-01 00:48:24 +1300

    Add option to ObjectiveLegendDead to only credit the killer

--- src/WarcraftLegacies.Source/Objectives/LegendBased/ObjectiveLegendDead.cs
+++ src/WarcraftLegacies.Source/Objectives/LegendBased/ObjectiveLegendDead.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MacroTools.Factions;
 using MacroTools.Legends;
 using MacroTools.Quests;
 using WCSharp.Shared.Data;
@@ -10,6 +11,8 @@ namespace WarcraftLegacies.Source.Objectives.LegendBased;
 /// </summary>
 public sealed class ObjectiveLegendDead : Objective
 {
+  private readonly LegendaryHero _target;
+
   /// <summary>
   /// An optional filter that is applied against the <see cref="LegendaryHero"/> when it dies. The <see cref="Objective"/>
   /// will only be completed if the filter passes.
@@ -22,13 +25,19 @@ public sealed class ObjectiveLegendDead : Objective
   public bool PermanentOnly { get; init; } = true;
 
   /// <summary>
+  /// If true, the objective will only be completed if the killer is one of the objective holders.
+  /// </summary>
+  public bool OnlyCreditKiller { get; init; } = false;
+
+  /// <summary>
   /// Initializes a new instance of the <see cref="ObjectiveLegendDead"/> class.
   /// </summary>
   /// <param name="target">The <see cref="Legend"/> that has to die for this objective to be completed.</param>
   public ObjectiveLegendDead(LegendaryHero target)
   {
+    _target = target;
     TargetWidget = target.Unit;
-    Description = PermanentOnly ? $"{target.Name} is permanently dead" : $"{target.Name} is dead";
+
     target.Died += OnDeath;
 
     if (target.Unit.Owner != null && target.Unit.Owner == player.NeutralAggressive)
@@ -38,9 +47,20 @@ public sealed class ObjectiveLegendDead : Objective
     }
   }
 
+  /// <inheritdoc />
+  public override void OnAdd(Faction faction)
+  {
+    Description = CalculateDescription(_target);
+  }
+
   private void OnDeath(object? sender, LegendDiedEventArgs eventArgs)
   {
     if (PermanentOnly && !eventArgs.Permanent)
+    {
+      return;
+    }
+
+    if (OnlyCreditKiller && !EligibleFactions.Contains(@event.KillingUnit.Owner))
     {
       return;
     }
@@ -49,5 +69,15 @@ public sealed class ObjectiveLegendDead : Objective
     {
       Progress = QuestProgress.Complete;
     }
+  }
+
+  private string CalculateDescription(LegendaryHero target)
+  {
+    if (PermanentOnly)
+    {
+      return OnlyCreditKiller ? $"Permanently kill {target.Name}" : $"{target.Name} is permanently dead";
+    }
+
+    return OnlyCreditKiller ? $"Kill {target.Name}" : $"{target.Name} is dead";
   }
 }
