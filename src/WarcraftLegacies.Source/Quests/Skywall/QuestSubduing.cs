7d4d916501eb94e2672b06ca50bd0ad9d70ef515
Madsen <105180704+Ripzss@users.noreply.github.com>
2026-02-04 11:41:56 +0100

    Significantly nerf income from central islands
    SA cp changes
    Jaguero isle 10 gpm -> 15
    Booty bay 10 gpm -> 25
    Stranglethorn 10 gpm -> 15
    Western - Stranglethorn 10 gpm -> 15
    Zul’Gurub 10 gpm -> 25
    Blasted lands 10 gpm -> 15
    Nethergard Keep 10 gpm -> 15
    Swapm of Sorrows 10 gpm -> 15
    Deadwind pass 10 -> 15
    total = 65gpm
    
    Islands
    Nazjatar 25 --> 10
    The Abyss 25 --> 10
    Removed Gishan Caverns and Mak'ara, renamed Drowned Reaches to Maelstrom
    Reduced all Broken Isles CPs to 10
    Removed Vol'dun
    Zuldazar 20 --> 15
    Closes #3650

--- src/WarcraftLegacies.Source/Quests/Skywall/QuestSubduing.cs
+++ src/WarcraftLegacies.Source/Quests/Skywall/QuestSubduing.cs
@@ -18,9 +18,8 @@ public sealed class QuestSubduing : QuestData
     @"ReplaceableTextures\CommandButtons\BTNTidelord.blp")
   {
     AddObjective(new ObjectiveControlLevel(UNIT_N00P_THE_ABYSS, 5));
-    AddObjective(new ObjectiveControlLevel(UNIT_N02P_MAK_ARA, 5));
-    AddObjective(new ObjectiveControlLevel(UNIT_N04B_GISHAN_CAVERNS, 5));
     AddObjective(new ObjectiveControlLevel(UNIT_N028_DROWNED_REACHES, 5));
+    AddObjective(new ObjectiveControlLevel(UNIT_N04Y_NAZJATAR, 5));
     AddObjective(new ObjectiveSelfExists());
     ResearchId = UPGRADE_RSW3_QUEST_COMPLETED_SUBDUING_NEPTULON;
   }
