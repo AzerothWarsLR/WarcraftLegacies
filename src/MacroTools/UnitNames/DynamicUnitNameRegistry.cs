b6cda57241a44ae8e3d63d3520975c859f66da76
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-02-08 13:50:25 +0000

    format

--- src/MacroTools/UnitNames/DynamicUnitNameRegistry.cs
+++ src/MacroTools/UnitNames/DynamicUnitNameRegistry.cs
@@ -1,67 +1,67 @@
 ﻿using System.Collections.Generic;
-using WCSharp.Events;
 using MacroTools.PreplacedWidgets;
+using WCSharp.Events;
 
 namespace MacroTools.UnitNames;
 
 public static class DynamicUnitNameRegistry
 {
-    private static readonly Dictionary<int, NamePool> _pools = new();
+  private static readonly Dictionary<int, NamePool> _pools = new();
 
-    public static void Setup(Dictionary<int, List<string>> nameData)
+  public static void Setup(Dictionary<int, List<string>> nameData)
+  {
+    foreach (var kvp in nameData)
     {
-        foreach (var kvp in nameData)
-        {
-            _pools[kvp.Key] = new NamePool(kvp.Value);
-            PlayerUnitEvents.Register(UnitTypeEvent.IsCreated, OnUnitCreated, kvp.Key);
-            PlayerUnitEvents.Register(UnitTypeEvent.Dies, OnUnitDeath, kvp.Key);
-        }
-
-        AssignNamesToPreplacedUnits();
+      _pools[kvp.Key] = new NamePool(kvp.Value);
+      PlayerUnitEvents.Register(UnitTypeEvent.IsCreated, OnUnitCreated, kvp.Key);
+      PlayerUnitEvents.Register(UnitTypeEvent.Dies, OnUnitDeath, kvp.Key);
     }
 
-    private static void OnUnitCreated()
+    AssignNamesToPreplacedUnits();
+  }
+
+  private static void OnUnitCreated()
+  {
+    var unit = @event.Unit;
+    if (unit != null && _pools.TryGetValue(unit.UnitType, out var pool))
     {
-        var unit = @event.Unit;
-        if (unit != null && _pools.TryGetValue(unit.UnitType, out var pool))
-        {
-            pool.TryAssign(unit);
-        }
+      pool.TryAssign(unit);
     }
+  }
 
-    private static void OnUnitDeath()
+  private static void OnUnitDeath()
+  {
+    var unit = @event.Unit;
+    if (unit != null && _pools.TryGetValue(unit.UnitType, out var pool))
     {
-        var unit = @event.Unit;
-        if (unit != null && _pools.TryGetValue(unit.UnitType, out var pool))
-        {
-            pool.TryRelease(unit);
-        }
+      pool.TryRelease(unit);
     }
+  }
 
-    private static void AssignNamesToPreplacedUnits()
+  private static void AssignNamesToPreplacedUnits()
+  {
+    foreach (var kvp in _pools)
     {
-        foreach (var kvp in _pools)
+      var unitType = kvp.Key;
+      NamePool pool = kvp.Value;
+
+      List<unit> preplacedUnits;
+      try
+      {
+        preplacedUnits = AllPreplacedWidgets.Units.GetAll(unitType);
+      }
+      catch (KeyNotFoundException)
+      {
+        continue;
+      }
+
+      foreach (var unit in preplacedUnits)
+      {
+        if (true)
         {
-            int unitType = kvp.Key;
-            NamePool pool = kvp.Value;
-
-            List<unit> preplacedUnits;
-            try
-            {
-                preplacedUnits = AllPreplacedWidgets.Units.GetAll(unitType);
-            }
-            catch (KeyNotFoundException)
-            {
-                continue;
-            }
-
-            foreach (var unit in preplacedUnits)
-            {
-                if (true)
-                {
-                    pool.TryAssign(unit);
-                }
-            }
+          pool.TryAssign(unit);
         }
+      }
     }
+  }
 }
