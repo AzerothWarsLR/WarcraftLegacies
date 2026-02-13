b0410146ac3bbc2c1d00550305f8b5ee80de4fbe
Zakary Boven <zakaryboven@gmail.com>
2026-02-13 21:01:10 +1300

    Add Split Healing Wave research to Illidari Seer

---
+++ src/WarcraftLegacies.Source/Researches/ReplaceAbilityResearch.cs
@@ -0,0 +1,38 @@
+﻿using MacroTools.Extensions;
+using MacroTools.Researches;
+
+namespace WarcraftLegacies.Source.Researches;
+
+/// <summary>
+/// Replaces an ability with another ability.
+/// </summary>
+public sealed class ReplaceAbilityResearch : Research
+{
+  /// <inheritdoc />
+  public ReplaceAbilityResearch(int researchTypeId, int goldCost) : base(researchTypeId, goldCost)
+  {
+  }
+
+  /// <summary>
+  /// The ability to remove from the player.
+  /// </summary>
+  public required int OldResearchTypeId { get; init; }
+
+  /// <summary>
+  /// The ability to add to the player.
+  /// </summary>
+  public required int NewAbilityTypeId { get; init; }
+
+  /// <inheritdoc />
+  public override void OnResearch(player player)
+  {
+    var faction = player.GetPlayerData().Faction;
+    if (faction == null)
+    {
+      return;
+    }
+
+    faction.ModAbilityAvailability(OldResearchTypeId, -1);
+    faction.ModAbilityAvailability(NewAbilityTypeId, 1);
+  }
+}
---
+++ src/WarcraftLegacies.Source/Spells/SplitHealingWave.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using MacroTools.DummyCasters;
+using MacroTools.Spells;
+using MacroTools.Utils;
+using WCSharp.Shared.Data;
+
+namespace WarcraftLegacies.Source.Spells;
+
+/// <summary>
+/// Casts a Healing Wave dummy spell on a friendly unit near the target.
+/// </summary>
+public sealed class SplitHealingWave : Spell
+{
+  /// <summary>
+  /// The dummy spell to cast.
+  /// </summary>
+  public required int DummyAbilityId { get; init; }
+
+  /// <summary>
+  /// The order string used to cast the dummy spell.
+  /// </summary>
+  public required int DummyAbilityOrderId { get; init; }
+
+  /// <summary>
+  /// How close to the original target the secondary target must be.
+  /// </summary>
+  public required float Radius { get; init; }
+
+  /// <inheritdoc />
+  public SplitHealingWave(int id) : base(id)
+  {
+  }
+
+  /// <inheritdoc />
+  public override void OnCast(unit caster, unit primaryTarget, Point targetPoint)
+  {
+    var casterOwner = caster.Owner;
+
+    var secondaryTarget = GlobalGroup.EnumUnitsInRange(primaryTarget.X, primaryTarget.Y, Radius)
+      .Where(x => IsValidTarget(casterOwner, x) && x != primaryTarget)
+      .OrderBy(x => x.MaxLife - x.Life)
+      .FirstOrDefault();
+
+    if (secondaryTarget == null)
+    {
+      return;
+    }
+
+    DummyCasterManager
+      .GetGlobalDummyCaster()
+      .CastUnit(caster, DummyAbilityId, DummyAbilityOrderId, GetAbilityLevel(caster), secondaryTarget, DummyCastOriginType.Caster);
+  }
+
+  private static bool IsValidTarget(player casterOwner, unit target)
+  {
+    return target.Alive &&
+           target.Owner == casterOwner &&
+           !target.IsUnitType(unittype.Structure) &&
+           !target.IsUnitType(unittype.Ancient) &&
+           !target.IsUnitType(unittype.Mechanical) &&
+           !target.IsUnitType(unittype.MagicImmune);
+  }
+}
