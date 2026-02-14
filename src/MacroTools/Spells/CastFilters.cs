abb2f353e9d3026315ee3516830a3c4aec1a0ea5
Chucky <ryancollett@hotmail.co.uk>
2026-02-14 19:26:50 +0000

    and again...

--- src/MacroTools/Spells/CastFilters.cs
+++ src/MacroTools/Spells/CastFilters.cs
@@ -31,8 +31,17 @@ public static class CastFilters
            (target.IsEnemyTo(casterPlayer) || target.Owner == player.NeutralPassive) &&
            target.Alive && target.IsInvulnerable == false;
   }
-  
-  // Used for spells where you only want them to affect enemy ground units. Flying units and structures are not affected.
+
+  /// <summary>
+  /// Used for spells where you only want them to affect enemy ground units. Flying units and structures are not affected.
+  /// </summary>
+   public static bool IsTargetEnemyAliveAndGround(unit caster, unit target)
+  {
+    var casterPlayer = caster.Owner;
+    return caster != target &&
+           (target.IsEnemyTo(casterPlayer) || target.Owner == player.NeutralPassive) &&
+           target.Alive && target.IsInvulnerable == false && !target.IsUnitType(unittype.Structure) && !target.IsUnitType(unittype.Flying);
+  }
   public static bool IsTargetEnemyAliveAndGroundUnits(unit caster, unit target)
   {
     var casterPlayer = caster.Owner;
@@ -41,7 +50,9 @@ public static class CastFilters
            target.Alive && target.IsInvulnerable == false && !target.IsUnitType(unittype.Structure) && !target.IsUnitType(unittype.Flying);
   }
 
-  // Used for spells where you only want them to affect all enemy units (including flying units). Structures are not affected.
+  /// <summary>
+  /// Used for spells where you only want them to affect all enemy units (including flying units). Structures are not affected.
+  /// </summary>
   public static bool IsTargetEnemyAndAliveUnits(unit caster, unit target)
   {
     var casterPlayer = caster.Owner;
