1982f7d8101cc5cc3b2bbca08079db528c7508d0
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-02-17 17:57:13 +0100

    chore: convert MacroTools.TestSupport to a test project

--- test/MacroTools.TestSupport/GameTimeManagerCollection.cs
+++ test/MacroTools.TestSupport/GameTimeManagerCollection.cs
@@ -1,6 +1,4 @@
-﻿using Xunit;
-
-namespace MacroTools.TestSupport;
+﻿namespace MacroTools.TestSupport;
 
 [CollectionDefinition(nameof(GameTimeManagerCollection), DisableParallelization = true)]
 public class GameTimeManagerCollection;
