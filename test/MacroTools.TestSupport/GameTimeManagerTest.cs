aa01e3f12290f9a52778ffa341d6a25d1008bc09
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-03-01 14:03:41 +0100

    refactor: expose internals to `MacroTools.TestSupport`

--- test/MacroTools.TestSupport/GameTimeManagerTest.cs
+++ test/MacroTools.TestSupport/GameTimeManagerTest.cs
@@ -1,21 +1,13 @@
-﻿using System.Runtime.CompilerServices;
-using MacroTools.GameTime;
+﻿using MacroTools.GameTime;
 
 namespace MacroTools.TestSupport;
 
 public static class GameTimeManagerTest
 {
-  private const string Assembly = "MacroTools";
-  private const string Namespace = $"{nameof(MacroTools)}.{nameof(MacroTools.GameTime)}";
-  private const string Type = nameof(MacroTools.GameTime.GameTimeManager);
-
-  [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "set_Turn")]
-  private static extern void TurnSetter([UnsafeAccessorType($"{Namespace}.{Type}, {Assembly}")] object? _, int value);
-
   public static int Turn
   {
     get => GameTimeManager.Turn;
-    set => TurnSetter(null, value);
+    set => GameTimeManager.SetTurn(value);
   }
 
   public static void Reset()
