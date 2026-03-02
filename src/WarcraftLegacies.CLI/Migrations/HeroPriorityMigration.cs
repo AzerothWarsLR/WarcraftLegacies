62e398d80da337df0cd1af0338f5e0f52f1a80bf
Zakary Boven <zakaryboven@gmail.com>
2026-03-02 22:47:45 +1300

    Add a migration forcing all hero priorities to be at least 16

---
+++ src/WarcraftLegacies.CLI/Migrations/HeroPriorityMigration.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using War3Api.Object;
+using War3Net.Build;
+using Warcraft.Cartographer.Migrations;
+
+namespace WarcraftLegacies.CLI.Migrations;
+
+/// <summary>
+/// Sets the priority of every hero to a minimum value.
+/// </summary>
+public sealed class HeroPriorityMigration : IMapMigration
+{
+  private const int MinimumHeroPriority = 16;
+
+  /// <inheritdoc />
+  public void Migrate(Map map, ObjectDatabase objectDatabase)
+  {
+    var units = objectDatabase.GetUnits();
+
+    foreach (var unit in units)
+    {
+      if (unit.AbilitiesHero.Any() && unit.StatsPriority < MinimumHeroPriority)
+      {
+        unit.StatsPriority = MinimumHeroPriority;
+      }
+    }
+
+    var unitData = objectDatabase.GetAllData().UnitData;
+    map.UnitObjectData = unitData;
+    map.UnitSkinObjectData = unitData;
+  }
+}
