e92187a56111c69efde9cbfede3c36644e0b3294
Zakary Boven <zakaryboven@gmail.com>
2026-03-03 18:34:37 +1300

    Add XML comments to MassAnySpell

--- src/WarcraftLegacies.Source/Spells/MassAnySpell.cs
+++ src/WarcraftLegacies.Source/Spells/MassAnySpell.cs
@@ -6,32 +6,74 @@ using WCSharp.Shared.Data;
 
 namespace WarcraftLegacies.Source.Spells;
 
+/// <summary>
+/// Deals damage and/or casts a dummy spell at each unit in the area.
+/// </summary>
 public sealed class MassAnySpell : Spell
 {
+  /// <summary>
+  /// The ID of the single-target ability to cast on units in the area.
+  /// </summary>
   public int DummyAbilityId { get; init; }
 
+  /// <summary>
+  /// The order string used to cast <see cref="DummyAbilityId"/>.
+  /// </summary>
   public int DummyAbilityOrderId { get; init; }
 
+  /// <summary>
+  /// The radius in which units are affected.
+  /// </summary>
   public float Radius { get; init; }
 
+  /// <summary>
+  /// A filter that units must pass to be considered eligible targets for the spell.
+  /// </summary>
   public required DummyCasterManager.CastFilter CastFilter { get; init; }
 
+  /// <summary>
+  /// What kind of thing the spell targets.
+  /// </summary>
   public SpellTargetType TargetType { get; init; } = SpellTargetType.None;
 
+  /// <summary>
+  /// What kind of dummy caster is used to cast the spell.
+  /// <remarks>Use <see cref="DummyCasterType.AbilitySpecific"/> if the spell being cast will break if it is removed from
+  /// the dummy unit before its effect takes place, e.g. Death Coil.</remarks>
+  /// <remarks>Currently does not support <see cref="DummyCasterType.LongLived"/>.</remarks>
+  /// </summary>
   public DummyCasterType DummyCasterType { get; init; } = DummyCasterType.Global;
 
+  /// <summary>
+  /// The chance of each unit in the area being affected.
+  /// </summary>
   public float Chance { get; init; } = 1.0f;
 
+  /// <summary>
+  /// Where each dummy spell should be cast from.
+  /// <remarks>Defaults to <see cref="DummyCastOriginType.Target"/>, which causes each spell to cast from each target's positions.</remarks>
+  /// </summary>
   public DummyCastOriginType DummyCastOriginType { get; init; } = DummyCastOriginType.Target;
 
-  public float DamageBase { get; init; } = 0f;
+  /// <summary>
+  /// How much damage the spell does to each affected unit, ignoring any levels.
+  /// </summary>
+  public float DamageBase { get; init; }
 
-  public float DamageLevel { get; init; } = 0f;
+  /// <summary>
+  /// How much damage the spell does to each affected unit per spell level.
+  /// </summary>
+  public float DamageLevel { get; init; }
 
+  /// <summary>
+  /// Initializes a new instance of the <see cref="MassAnySpell"/> class.
+  /// </summary>
+  /// <inheritdoc />
   public MassAnySpell(int id) : base(id)
   {
   }
 
+  /// <inheritdoc />
   public override void OnCast(unit caster, unit target, Point targetPoint)
   {
     var center = TargetType == SpellTargetType.None ? new Point(caster.X, caster.Y) : targetPoint;
