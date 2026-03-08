8ddda40648de2aae3d4d074c8f04c8fe4dde941c
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-03-08 08:12:49 +0100

    refactor: align remaining event handlers

--- src/MacroTools/ControlPoints/ControlPointDefenderManager.cs
+++ src/MacroTools/ControlPoints/ControlPointDefenderManager.cs
@@ -26,7 +26,7 @@ public static class ControlPointDefenderManager
   public static void Initialize(ControlPointManager controlPointManager, ControlPointDefenderSettings controlPointSettings)
   {
     _settings = controlPointSettings;
-    controlPointManager.ControlPointCreated += (_, controlPoint) =>
+    controlPointManager.ControlPointCreated += controlPoint =>
     {
       if (controlPoint.UseControlLevels)
       {
