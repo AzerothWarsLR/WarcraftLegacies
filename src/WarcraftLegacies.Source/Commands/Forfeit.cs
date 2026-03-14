afe69c3b6193d92316a03672c2a634edd2751b75
Madsen <benmadse@gmail.com>
2026-03-14 01:47:50 +0100

    forfeit command qol

--- src/WarcraftLegacies.Source/Commands/Forfeit.cs
+++ src/WarcraftLegacies.Source/Commands/Forfeit.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using MacroTools.Commands;
 using MacroTools.Extensions;
 using MacroTools.Factions;
@@ -21,6 +22,94 @@ public sealed class Forfeit : Command
   public override CommandType Type => CommandType.Normal;
   public override string Description => "Vote to forfeit the game.";
 
+
+  public override void OnRegister()
+  {
+    try
+    {
+      trigger trigger = trigger.Create();
+      foreach (var player in Util.EnumeratePlayers())
+      {
+        trigger.RegisterPlayerEvent(player, playerevent.Leave);
+      }
+      trigger.AddAction(() =>
+      {
+        RegisterLeaveVote(@event.Player);
+      });
+
+      // Check a faction change aka -obs
+      foreach (var faction in FactionManager.GetAllFactions())
+      {
+        faction.ScoreStatusChanged += _ => OnFactionScoreStatusChanged(faction);
+      };
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine("wow an error.");
+    }
+  }
+
+
+  /// <summary>
+  /// Gets Called when a factions score status changes aka -obs
+  /// </summary>
+  private static void OnFactionScoreStatusChanged(Faction faction)
+  {
+    foreach (var player in Util.EnumeratePlayers())
+    {
+      var playerData = player.GetPlayerData();
+      if (playerData.Faction == faction)
+      {
+        RegisterLeaveVote(player);
+      }
+    }
+  }
+
+  /// <summary>
+  /// Gets called when a player leaves the game.
+  /// </summary>
+  public static void RegisterLeaveVote(player leavingPlayer)
+  {
+    var team = leavingPlayer.GetPlayerData().Team;
+    if (team == null)
+    {
+      return;   
+    }
+    if (!_ffVotesByTeam.TryGetValue(team, out var votes))
+    {
+      votes = new HashSet<player>();
+      _ffVotesByTeam[team] = votes;
+    }
+    if (!votes.Add(leavingPlayer))
+    {
+      return;
+    }
+
+    var currentVotes = votes.Count;
+    var playerFaction = leavingPlayer.GetPlayerData().Faction;
+    var prefixCol = playerFaction?.PrefixCol ?? "";
+    team.DisplayText(
+        $"{prefixCol}{leavingPlayer.Name}|r has left the game and counts as a forfeit vote. ({currentVotes}/{VotesRequired})."
+    );
+
+    if (currentVotes >= VotesRequired)
+    {
+      team.DisplayText("Forfeit vote passed.");
+      foreach (var p in Util.EnumeratePlayers())
+      {
+        p.DisplayTextTo($"{team.Name} has forfeited the game.|cFFFF0000The game will end in 10 seconds.");
+      }
+      WCSharp.Events.PeriodicEvents.AddPeriodicEvent(() =>
+      {
+        EndGame(true);
+        return false;
+      }, 10f);
+    }
+  }
+
+  /// <summary>
+  /// Gets called when a player types the -ff command ingame.
+  /// </summary>
   public override string Execute(player commandUser, params string[] parameters)
   {
     var team = commandUser.GetPlayerData().Team;
@@ -37,9 +126,12 @@ public sealed class Forfeit : Command
     {
       return "You have already voted to forfeit.";
     }
+
     var currentVotes = votes.Count;
+    var playerFaction = commandUser.GetPlayerData().Faction;
+    var prefixCol = playerFaction?.PrefixCol ?? "";
     team.DisplayText(
-        $"{commandUser.Name} voted to forfeit ({currentVotes}/{VotesRequired})."
+        $"{prefixCol}{commandUser.Name}|r voted to forfeit ({currentVotes}/{VotesRequired})."
     );
 
     if (currentVotes >= VotesRequired)
@@ -47,7 +139,7 @@ public sealed class Forfeit : Command
       team.DisplayText("Forfeit vote passed.");
       foreach (var player in Util.EnumeratePlayers())
       {
-        player.DisplayTextTo($"{team.Name} has forfeited the game. The game will end in 10 seconds.");
+        player.DisplayTextTo($"|cFFFF0000{team.Name}|r has forfeited the game. The game will end in 10 seconds.");
       }
       WCSharp.Events.PeriodicEvents.AddPeriodicEvent(() =>
       {
