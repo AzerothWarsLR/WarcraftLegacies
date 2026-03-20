37f6fdcc9840570e51dfa1852dc80d2a12ec7124
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-03-20 20:47:11 +0100

    refactor: move spells and traits to faction root (Legion)

---
+++ src/WarcraftLegacies.Source/Factions/Legion/LegionTraits.cs
@@ -0,0 +1,39 @@
+﻿using MacroTools.Spells;
+using MacroTools.UnitTraits;
+using WarcraftLegacies.Source.UnitTypeTraits;
+
+namespace WarcraftLegacies.Source.Factions.Legion;
+
+public static class LegionTraits
+{
+  public static void Setup()
+  {
+    UnitTypeTraitRegistry.Register(new RestoreHealthFromEachTargetDamaged(ABILITY_VP02_VAMPIRIC_SIPHON_LEGION_DREADLORDS)
+    {
+      HealthPerTarget = new LeveledAbilityField<int>
+      {
+        Base = -5,
+        PerLevel = 10
+      },
+      HealthPerLevel = 1,
+      Effect = @"Abilities\Spells\Human\Heal\HealTarget.mdl"
+    }, new[]
+    {
+      UNIT_UMAL_THE_CUNNING_LEGION,
+      UNIT_UTIC_THE_DARKENER_LEGION,
+      UNIT_U00L_ENVOY_OF_ARCHIMONDE_LEGION
+    });
+
+    UnitTypeTraitRegistry.Register(
+      new RestoreHealthFromEachTargetDamaged(ABILITY_VP08_VAMPIRIC_SIPHON_LEGION_ELITES)
+      {
+        HealthPerTarget = new LeveledAbilityField<int> { Base = -5, PerLevel = 10 },
+        HealthPerLevel = 1,
+        Effect = @"Abilities\Spells\Human\Heal\HealTarget.mdl"
+      }, new[]
+      {
+        UNIT_U007_DREADLORD_LEGION_ELITE,
+        UNIT_N04O_DOOM_LORD_LEGION
+      });
+  }
+}
