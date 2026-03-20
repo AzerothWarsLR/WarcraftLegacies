2c019b80e23199be2149e06961581cd81ec2920d
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-03-20 21:12:55 +0100

    refactor: move spells and traits to faction root (Ahnqiraj)

---
+++ src/WarcraftLegacies.Source/Factions/Ahnqiraj/AhnqirajSpells.cs
@@ -0,0 +1,65 @@
+﻿using MacroTools.Spells;
+using WarcraftLegacies.Source.Spells;
+
+namespace WarcraftLegacies.Source.Factions.Ahnqiraj;
+
+public static class AhnqirajSpells
+{
+  public static void Setup()
+  {
+    SpellRegistry.Register(new InstantKill(ABILITY_ZBBS_HATCH_INCUBATE)
+    {
+      Target = InstantKill.KillTarget.Self
+    });
+
+    SpellRegistry.Register(new InspireMadness(ABILITY_ZBIM_INSPIRE_MADNESS_C_THUN)
+    {
+      Radius = 300,
+      CountBase = 14,
+      Duration = 30,
+      EffectTarget = @"Abilities\Spells\Other\Charm\CharmTarget.mdl",
+      EffectScaleTarget = 0.5f
+    });
+
+    SpellRegistry.Register(new UnstableEvolution(ABILITY_ZBUE_UNSTABLE_EVOLUTION_C_THUN)
+    {
+      Radius = 300,
+      Duration = 30,
+      AttackDamageMultiplier = new LeveledAbilityField<float>
+      {
+        Base = 1.5f,
+        PerLevel = 0.5f
+      },
+      AttackSpeedMultiplier = new LeveledAbilityField<float>
+      {
+        Base = 2
+      },
+      MaxHealthMultiplier = new LeveledAbilityField<float>
+      {
+        Base = 1.25f,
+        PerLevel = 0.25f
+      },
+      EffectTarget = @"Abilities\Spells\Human\Feedback\ArcaneTowerAttack.mdl",
+      EffectScaleTarget = 1
+    });
+
+    SpellRegistry.Register(new SpawnTentacle(ABILITY_ZBST_SPAWN_TENTACLE_C_THUN)
+    {
+      HitPoints = new LeveledAbilityField<int>
+      {
+        Base = 500,
+        PerLevel = 100
+      },
+      AttackDamageBase = new LeveledAbilityField<int>
+      {
+        Base = 20,
+        PerLevel = 20
+      },
+      UnitTypeId = UNIT_ZBTH_TENTACLE_SPAWN_TENTACLE_C_THUN,
+      Duration = new LeveledAbilityField<float>
+      {
+        Base = 30
+      }
+    });
+  }
+}
