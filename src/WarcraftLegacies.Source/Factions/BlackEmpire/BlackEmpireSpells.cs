47de7b9336b16676e1e6c66089809524326b43bf
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-03-20 21:10:07 +0100

    refactor: move spells and traits to faction root (BlackEmpire)

---
+++ src/WarcraftLegacies.Source/Factions/BlackEmpire/BlackEmpireSpells.cs
@@ -0,0 +1,64 @@
+﻿using MacroTools.DummyCasters;
+using MacroTools.Spells;
+using WarcraftLegacies.Source.Spells;
+
+namespace WarcraftLegacies.Source.Factions.BlackEmpire;
+
+public static class BlackEmpireSpells
+{
+  public static void Setup()
+  {
+    var poisonYor = new MassAnySpell(ABILITY_ABNT_VOID_TOXIN_BLACK_EMPIRE)
+    {
+      Radius = 500,
+      Damage = new LeveledAbilityField<float>
+      {
+        Base = 60,
+        PerLevel = 20
+      },
+      DummyAbilityId = ABILITY_ABSS_SHADOW_STRIKE_VOID_TOXIN_REAL,
+      DummyAbilityOrderId = ORDER_SHADOWSTRIKE,
+      SpecialEffect = @"Abilities\Weapons\ChimaeraAcidMissile\ChimaeraAcidMissile.mdl",
+      CastFilter = CastFilters.IsTargetEnemyAliveAndGroundUnits,
+      DummyCastOriginType = DummyCastOriginType.Caster,
+      TargetType = SpellTargetType.None
+    };
+    SpellRegistry.Register(poisonYor);
+
+    var massBanish = new MassAnySpell(ABILITY_MD29_MASS_BANISH_BLACK_EMPIRE_ZA_QUL)
+    {
+      DummyAbilityId = ABILITY_MD30_MASS_BANISH_BLACK_EMPIRE_ZA_QUL_DUMMY_CASTER,
+      DummyAbilityOrderId = ORDER_BANISH,
+      Radius = 250,
+      CastFilter = CastFilters.IsTargetOrganicAndAlive,
+      TargetType = SpellTargetType.Point
+    };
+    SpellRegistry.Register(massBanish);
+
+    var siphoningRitual = new SiphoningRitual(ABILITY_MD24_SIPHONING_RITUAL_BLACK_EMPIRE_ZON_OOZ)
+    {
+      TargetCountBase = 24,
+      TargetCountLevel = 0,
+      LifeDrainedPerSecondBase = 30,
+      LifeDrainedPerSecondLevel = 10,
+      ManaDrainedPerSecondBase = 15,
+      ManaDrainedPerSecondLevel = 5,
+      Range = 800,
+      Radius = 225,
+      Interval = 0.1f
+    };
+    SpellRegistry.Register(siphoningRitual);
+
+    SpellRegistry.Register(new ChainManaBurn(ABILITY_MD23_CHAIN_MANA_BURN_ZA_QUL)
+    {
+      ManaBurned = new LeveledAbilityField<int>
+      {
+        Base = 100,
+        PerLevel = 75
+      },
+      MaximumBounces = 5,
+      BurnReductionPerBounce = 0.15f,
+      MaximumBounceRadius = 500
+    });
+  }
+}
---
+++ src/WarcraftLegacies.Source/Factions/BlackEmpire/BlackEmpireTraits.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using MacroTools.Spells;
+using MacroTools.UnitTraits;
+using WarcraftLegacies.Source.UnitTypeTraits;
+
+namespace WarcraftLegacies.Source.Factions.BlackEmpire;
+
+public static class BlackEmpireTraits
+{
+  public static void Setup()
+  {
+    UnitTypeTraitRegistry.Register(new HideousAppendages
+    {
+      TentacleUnitTypeId = UNIT_N098_NZOTHTENTACLE_HIDEOUS_APPENDAGES_N_ZOTH,
+      TentacleCount = 9,
+      RadiusOffset = 520
+    }, UNIT_U01Z_OLD_GOD_NZOTH);
+
+    UnitTypeTraitRegistry.Register(new InfiniteInfluence
+    {
+      Radius = 800
+    }, UNIT_U01Z_OLD_GOD_NZOTH);
+
+    UnitTypeTraitRegistry.Register(new SpellOnCast(ABILITY_AXK2_VOID_RIFT_ICON_XKORR)
+    {
+      DummyAbilityId = ABILITY_AXK1_VOIDBOLTDUMMY_X_KORR_DUMMY_SPELL,
+      DummyOrderId = ORDER_FAN_OF_KNIVES,
+      AbilityWhitelist = new List<int>
+      {
+        ABILITY_A032_ARCANE_BOMBARDMENT_ORANGE_ANTONIDAS_MEDIVH,
+        ABILITY_ABEH_HEALING_WAVE_BLACK_EMPIRE,
+        ABILITY_A10U_MANA_BURN_DALARAN_YOGG,
+        ABILITY_A11O_BLACK_HOLE_KHADGAR,
+      },
+      TargetType = SpellTargetType.None
+    }, UNIT_E01D_HARBINGER_OF_NY_ALOTHA_NZOTH);
+
+    UnitTypeTraitRegistry.Register(new SpellOnAttack(ABILITY_ABES_GENESIS_ATTACK_ICON_STYGIAN_HULK)
+    {
+      DummyAbilityId = ABILITY_ABEG_PARASITE_GENESIS_ATTACK_REAL,
+      DummyOrderId = ORDER_PARASITE,
+      ProcChance = 1.0f
+    }, UNIT_U029_STYGIAN_HULK_NZOTH);
+
+    UnitTypeTraitRegistry.Register(new SpellOnAttack(ABILITY_ABPF_PARALYSING_FEAR_ICON)
+    {
+      DummyAbilityId = ABILITY_ABSF_SLOW_PARALYSING_FEAR,
+      DummyOrderId = ORDER_SLOW,
+      ProcChance = 0.2f
+    }, UNIT_O01G_BRUTE_NZOTH);
+
+    UnitTypeTraitRegistry.Register(new SpellOnAttack(ABILITY_ABGP_GREATER_PARALYSING_FEAR_ICON)
+    {
+      DummyAbilityId = ABILITY_ABSG_SLOW_GREATER_PARALYSING_FEAR,
+      DummyOrderId = ORDER_SLOW,
+      ProcChance = 0.4f
+    }, UNIT_H09F_DEEP_FIEND_NZOTH);
+  }
+}
