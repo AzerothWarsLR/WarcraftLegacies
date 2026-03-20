13ce909cac5a6b43ba35b7de0a65c8b8d9d74965
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-03-20 20:40:50 +0100

    refactor: move spells and traits to faction root (Quelthalas)

--- src/WarcraftLegacies.Source/Factions/Quelthalas/QuelthalasSpells.cs
+++ src/WarcraftLegacies.Source/Factions/Quelthalas/QuelthalasSpells.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using MacroTools.Spells;
-using MacroTools.UnitTraits;
+﻿using MacroTools.Spells;
 using WarcraftLegacies.Source.Spells;
-using WarcraftLegacies.Source.UnitTypeTraits;
 
 namespace WarcraftLegacies.Source.Factions.Quelthalas;
 
@@ -20,22 +17,6 @@ public static class QuelthalasSpells
       Effect = @"war3mapImported\Earth NovaTarget.mdx"
     };
     SpellRegistry.Register(summonGraniteGolems);
-
-    UnitTypeTraitRegistry.Register(new DefensiveOrbs(ABILITY_A055_DEFENSIVE_ORBS_QUEL_THALAS_ANASTERIAN)
-    {
-      OrbitRadius = 350,
-      OrbitalPeriod = 4,
-      OrbEffectPath = @"war3mapImported\OrbFireX.mdx",
-      Damage = new LeveledAbilityField<float> { Base = 20, PerLevel = 10 },
-      CollisionRadius = new LeveledAbilityField<float> { Base = 100, PerLevel = 0 },
-      OrbDuration = 30,
-      AbilityWhitelist = new List<int>
-      {
-        ABILITY_A04J_ARCANE_BURST_HIGH_ELVES_ANASTERIAN,
-        ABILITY_A013_DEVOUR_MAGIC_ANASTERIAN,
-        ABILITY_AHPX_ASHES_OF_AL_AR_QUEL_THALAS_ANASTERIAN_KAEL_THAS
-      }
-    }, UNIT_H00Q_KING_OF_QUEL_THALAS_QUELTHALAS);
 
     SpellRegistry.Register(new ChainManaBurn(ABILITY_ZBCM_CHAIN_MANA_BURN_ROMMATH)
     {
