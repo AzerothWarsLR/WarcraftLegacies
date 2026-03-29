5d1780e56ff35939bc373867159db7597215d1b0
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-03-29 15:41:27 +0200

    test: validate tooltip tags instead of exempting via fourcc scan

---
+++ test/WarcraftLegacies.Map.Tests/TestSupport/MapDataRegex.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WarcraftLegacies.Map.Tests.TestSupport;
+
+public static partial class MapDataRegex
+{
+  [GeneratedRegex(@"(?<![A-Z\d])[A-Z\d]{4}(?![A-Z\d])", RegexOptions.IgnoreCase)]
+  public static partial Regex ParseFourCcs();
+
+  [GeneratedRegex(@"<(?<fourcc>[A-Z\d]{4}),(?<field>[^,>]+)(?:,(?<format>[^>]+))?>", RegexOptions.IgnoreCase)]
+  public static partial Regex ParseTags();
+}
