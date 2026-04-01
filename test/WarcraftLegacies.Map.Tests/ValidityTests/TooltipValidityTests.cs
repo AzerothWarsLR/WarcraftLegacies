d4987752922586dbfd509adfdc6745c68c1e717c
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-04-01 19:20:14 +0200

    test: validate ability tooltips reference existing objects

--- test/WarcraftLegacies.Map.Tests/ValidityTests/TooltipValidityTests.cs
+++ test/WarcraftLegacies.Map.Tests/ValidityTests/TooltipValidityTests.cs
@@ -1,5 +1,7 @@
-﻿using War3Net.Common.Extensions;
+﻿using War3Api.Object;
+using War3Net.Common.Extensions;
 using Warcraft.Cartographer.Extensions;
+using Warcraft.Integrity;
 using WarcraftLegacies.Map.Tests.TestSupport;
 
 namespace WarcraftLegacies.Map.Tests.ValidityTests;
@@ -27,6 +29,38 @@ public sealed class TooltipValidityTests(MapTestFixture fixture)
       .ToList();
 
     ValidityTestHelpers.ThrowIfAny(issues);
+  }
+
+  [Fact]
+  public void AllAbilities_HaveValidTooltipReferences()
+  {
+    var issues = fixture.ObjectDatabase.GetAbilities().ToList()
+      .SelectMany(GetAbilityTooltipIssues)
+      .ToList();
+
+    ValidityTestHelpers.ThrowIfAny(issues);
+  }
+
+  private IEnumerable<string> GetAbilityTooltipIssues(Ability ability)
+  {
+    var id = ability.GetReadableId();
+
+    var leveledIssues = Enumerable.Range(1, ability.StatsLevels)
+      .SelectMany(level => new[]
+        {
+          (IsModified: ability.IsTextTooltipNormalExtendedModified[level], Property: ability.TextTooltipNormalExtended),
+          (IsModified: ability.IsTextTooltipTurnOffExtendedModified[level], Property: ability.TextTooltipTurnOffExtended),
+        }
+        .Where(x => x.IsModified)
+        .Select(x => x.Property.TryGetStringAtLevel(level, x.IsModified))
+        .OfType<string>()
+        .SelectMany(tooltip => GetTooltipIssues(id, tooltip)));
+
+    var learnIssues = ability.IsTextTooltipLearnExtendedModified
+      ? GetTooltipIssues(id, ability.TextTooltipLearnExtended)
+      : [];
+
+    return leveledIssues.Concat(learnIssues);
   }
 
   private IEnumerable<string> GetTooltipIssues(string ownerId, string tooltip)
