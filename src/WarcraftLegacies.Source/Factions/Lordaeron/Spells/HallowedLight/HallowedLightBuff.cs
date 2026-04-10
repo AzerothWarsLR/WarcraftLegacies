c7056492495303c986bd69af19b716538570ee4e
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-04-10 22:01:30 +0100

    2 x buff applicators

--- src/WarcraftLegacies.Source/Factions/Lordaeron/Spells/HallowedLight/HallowedLightBuff.cs
+++ src/WarcraftLegacies.Source/Factions/Lordaeron/Spells/HallowedLight/HallowedLightBuff.cs
@@ -1,4 +1,5 @@
-﻿using WCSharp.Buffs;
+﻿using System.Collections.Generic;
+using WCSharp.Buffs;
 
 namespace WarcraftLegacies.Source.Factions.Lordaeron.Spells.HallowedLight;
 
@@ -6,6 +7,9 @@ public sealed class HallowedLightBuff : BoundBuff
 {
   public int BuffId { get; init; }
   public int ArmorAbilityId { get; init; }
+  public int ApplicatorAbilityId { get; init; }
+
+  private static readonly Dictionary<int, effect> ActiveFx = new();
   private effect fx;
 
   public HallowedLightBuff(unit caster, unit target)
@@ -17,17 +21,29 @@ public sealed class HallowedLightBuff : BoundBuff
   public override void OnApply()
   {
     Target.AddAbility(ArmorAbilityId);
-    BindAura(ArmorAbilityId, BuffId);
+    BindAura(ApplicatorAbilityId, BuffId);
+
+    var id = GetHandleId(Target);
+    if (ActiveFx.TryGetValue(id, out var existing) && existing != null)
+    {
+      DestroyEffect(existing);
+    }
+
     fx = AddSpecialEffectTarget(EffectString, Target, EffectAttachmentPoint);
+    ActiveFx[id] = fx;
+
     base.OnApply();
   }
 
   public override void OnDispose()
   {
+    var id = GetHandleId(Target);
     if (fx != null)
     {
       DestroyEffect(fx);
     }
+
+    ActiveFx.Remove(id);
 
     Target.RemoveAbility(ArmorAbilityId);
     base.OnDispose();
