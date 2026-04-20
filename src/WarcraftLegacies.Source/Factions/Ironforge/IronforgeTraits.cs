480b991e245a8c0a574d7e8d031d6144990802b7
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-04-20 21:39:26 +0100

    Flamethrow Flame Tank
    SpellOnAttackPointCast created
    Dummy Flamethrower ability
    Icon Flamethrower ability
    Removed old unused Flamethrower ability
    corrected Electric Strike typo

--- src/WarcraftLegacies.Source/Factions/Ironforge/IronforgeTraits.cs
+++ src/WarcraftLegacies.Source/Factions/Ironforge/IronforgeTraits.cs
@@ -13,5 +13,14 @@ public static class IronforgeTraits
       DummyOrderId = ORDER_FORKED_LIGHTNING,
       ProcChance = 0.2f
     }, UNIT_H03Z_STORMRIDER_IRONFORGE);
+
+    UnitTypeTraitRegistry.Register(new SpellOnAttackPointCast(ABILITY_TP27_FLAMETHROWER_FLAME_TANK)
+    {
+      DummyAbilityId = ABILITY_TP26_FLAMETHROWER_FLAME_TANK_DUMMY,
+      DummyOrderId = ORDER_BREATH_OF_FIRE,
+      ProcChance = 1.0f,
+      Cooldown = 0,
+      CastDistance = 600
+    }, UNIT_H01P_STEAM_TANK_IRONFORGE);
   }
 }
