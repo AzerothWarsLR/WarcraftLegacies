df239d1bf301737b024f684e3e1e6792c577ee21
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-07-22 15:28:27 +0100

    dotnet format

--- src/WarcraftLegacies.Source/Factions/Ironforge/IronforgeTraits.cs
+++ src/WarcraftLegacies.Source/Factions/Ironforge/IronforgeTraits.cs
@@ -10,12 +10,12 @@ public static class IronforgeTraits
 {
   public static void Setup()
   {
-   // UnitTypeTraitRegistry.Register(new SpellOnAttack(ABILITY_A10J_MASTER_OF_LIGHTNING_STORMRIDERS)
-   //{
-   // DummyAbilityId = ABILITY_ACFL_FORKED_LIGHTNING_LIGHT_BLUE_HIGHBORNE,
-   // DummyOrderId = ORDER_FORKED_LIGHTNING,
-   // ProcChance = 0.2f
-   // }, UNIT_HGRY_GRYPHON_RIDER_IRONFORGE);
+    // UnitTypeTraitRegistry.Register(new SpellOnAttack(ABILITY_A10J_MASTER_OF_LIGHTNING_STORMRIDERS)
+    //{
+    // DummyAbilityId = ABILITY_ACFL_FORKED_LIGHTNING_LIGHT_BLUE_HIGHBORNE,
+    // DummyOrderId = ORDER_FORKED_LIGHTNING,
+    // ProcChance = 0.2f
+    // }, UNIT_HGRY_GRYPHON_RIDER_IRONFORGE);
 
     UnitTypeTraitRegistry.Register(new SpellOnAttackConeCast(ABILITY_TP27_FLAMETHROWER_FLAME_TANK)
     {
