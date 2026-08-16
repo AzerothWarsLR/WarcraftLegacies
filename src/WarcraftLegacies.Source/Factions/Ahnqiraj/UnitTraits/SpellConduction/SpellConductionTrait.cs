f644b44019a550ebc839ac762ba145295c811383
grolmash <141155688+grolmash@users.noreply.github.com>
2026-08-16 21:14:35 +0200

    replace Spell Conduction buffs with damage-time checks (#4080)
    * make spell conduction not blow up cpu
    
    * rename _eradicators list to _conductors + dedup
    
    ---------
    
    Co-authored-by: Grolmash <grolmash@users.noreply.github.com>

--- src/WarcraftLegacies.Source/Factions/Ahnqiraj/UnitTraits/SpellConduction/SpellConductionTrait.cs
+++ src/WarcraftLegacies.Source/Factions/Ahnqiraj/UnitTraits/SpellConduction/SpellConductionTrait.cs
@@ -1,10 +1,16 @@
-﻿using MacroTools.UnitTraits;
-using WCSharp.Buffs;
+﻿using System.Collections.Generic;
+using System.Linq;
+using MacroTools.Extensions;
+using MacroTools.Spells;
+using MacroTools.UnitTraits;
+using WCSharp.Events;
 
 namespace WarcraftLegacies.Source.Factions.Ahnqiraj.UnitTraits.SpellConduction;
 
 public sealed class SpellConductionTrait : UnitTrait, IEffectOnCreated
 {
+  private readonly List<unit> _conductors = new();
+
   /// <summary>
   /// The research required for this ability to work.
   /// </summary>
@@ -25,16 +31,65 @@ public sealed class SpellConductionTrait : UnitTrait, IEffectOnCreated
   /// </summary>
   public required int Radius { get; init; }
 
+  public SpellConductionTrait()
+  {
+    PlayerUnitEvents.Register(UnitTypeEvent.IsDamaged, OnDamageTaken);
+  }
+
   /// <inheritdoc />
   public void OnCreated(unit createdUnit)
   {
-    var aura = new SpellConductionAura(createdUnit)
+    if (!_conductors.Contains(createdUnit))
     {
-      RedirectionPercentage = RedirectionPercentage,
-      RedirectableAttackTypes = RedirectableAttackTypes,
-      RequiredResearch = RequiredResearch,
-      Radius = Radius
-    };
-    AuraSystem.Add(aura);
+      _conductors.Add(createdUnit);
+    }
+  }
+
+  private void OnDamageTaken()
+  {
+    var target = @event.Unit;
+    var attackType = @event.AttackType;
+    if (_conductors.Count == 0 ||
+        !RedirectableAttackTypes.Contains(attackType) ||
+        UnitTypeTraitRegistry.UnitHasTrait(target, typeof(SpellConductionTrait)))
+    {
+      return;
+    }
+
+    var damageSource = @event.DamageSource;
+    var damageType = @event.DamageType;
+    var weaponType = @event.WeaponType;
+    var eventDamage = @event.Damage;
+
+    for (var i = 0; i < _conductors.Count;)
+    {
+      var conductor = _conductors[i];
+      if (!conductor.Alive)
+      {
+        _conductors.RemoveAt(i);
+        continue;
+      }
+
+      i++;
+      if (conductor.Owner.GetTechResearched(RequiredResearch, false) <= 0 ||
+          !CastFilters.IsTargetAllyAndAlive(conductor, target) ||
+          !IsInRange(conductor, target))
+      {
+        continue;
+      }
+
+      var redirectedDamage = eventDamage * RedirectionPercentage;
+      eventDamage *= 1 - RedirectionPercentage;
+      @event.Damage = eventDamage;
+      conductor.TakeDamage(damageSource, redirectedDamage, false, true,
+        attackType, damageType, weaponType);
+    }
+  }
+
+  private bool IsInRange(unit conductor, unit target)
+  {
+    var deltaX = conductor.X - target.X;
+    var deltaY = conductor.Y - target.Y;
+    return deltaX * deltaX + deltaY * deltaY <= Radius * Radius;
   }
 }
