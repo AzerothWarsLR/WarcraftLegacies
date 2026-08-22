2a2190c064552f863f4899a0e38f8e6da5f1bbc6
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-08-22 13:43:23 +0200

    chore: hide Sulfuron Spire in Ashenvale until a future event (#4109)

---
+++ src/WarcraftLegacies.Source/Setup/SulfuronSpireSetup.cs
@@ -0,0 +1,14 @@
+﻿using MacroTools.PreplacedWidgets;
+
+namespace WarcraftLegacies.Source.Setup;
+
+public static class SulfuronSpireSetup
+{
+  /// <summary>
+  /// Hides the preplaced Sulfuron Spire in Ashenvale Forest until a later event reveals it.
+  /// </summary>
+  public static void Setup()
+  {
+    AllPreplacedWidgets.Units.Get(UNIT_SWSS_SULFURON_SPIRE_SCOURGE_T3).IsVisible = false;
+  }
+}
