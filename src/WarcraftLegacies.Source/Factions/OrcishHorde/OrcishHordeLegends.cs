f9e362a2ed60e169aaa7d2aa564f861a17b8b2cb
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-08-31 18:51:55 +0100

    Rebuild Thrall as a real hero and add Tiny Building rewards

--- src/WarcraftLegacies.Source/Factions/OrcishHorde/OrcishHordeLegends.cs
+++ src/WarcraftLegacies.Source/Factions/OrcishHorde/OrcishHordeLegends.cs
@@ -7,7 +7,18 @@ namespace WarcraftLegacies.Source.Factions.OrcishHorde;
 /// </summary>
 public sealed class OrcishHordeLegends
 {
+  public LegendaryHero Thrall { get; }
+
+  public OrcishHordeLegends()
+  {
+    Thrall = new LegendaryHero("Thrall")
+    {
+      UnitType = UNIT_TP52_WARCHIEF_OF_THE_HORDE_ORCISH_HORDE
+    };
+  }
+
   public void RegisterLegends()
   {
+    LegendaryHeroManager.Register(Thrall);
   }
 }
