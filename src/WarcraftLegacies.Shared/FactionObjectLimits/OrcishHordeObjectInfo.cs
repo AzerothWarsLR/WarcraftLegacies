3e7378534cd64cc7f616ffee3806747db080cb36
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-09-01 21:52:23 +0100

    Add Great Hall tiers and War Mill for Orcish Horde

--- src/WarcraftLegacies.Shared/FactionObjectLimits/OrcishHordeObjectInfo.cs
+++ src/WarcraftLegacies.Shared/FactionObjectLimits/OrcishHordeObjectInfo.cs
@@ -10,8 +10,11 @@ public static class OrcishHordeObjectInfo
   public static IEnumerable<ObjectInfo> GetAllObjectLimits()
   {
     yield return new(UNIT_O078_GREAT_HALL_ORCISH_HORDE_T1, Unlimited, TownHall);
+    yield return new(UNIT_O077_STRONGHOLD_ORCISH_HORDE_T2, Unlimited, Keep);
+    yield return new(UNIT_O07G_FORTRESS_ORCISH_HORDE_T3, Unlimited, Castle);
     yield return new(UNIT_O076_ALTAR_OF_STORMS_ORCISH_HORDE, Unlimited, Altar);
     yield return new(UNIT_O075_WAR_CAMP_ORCISH_HORDE, Unlimited, Barracks);
+    yield return new(UNIT_O07H_WAR_MILL_ORCISH_HORDE_RESEARCH, Unlimited, Research);
     yield return new(UNIT_O07E_WATCH_TOWER_ORCISH_HORDE, Unlimited, Tower);
     yield return new(UNIT_O07F_BURROW_ORCISH_HORDE, Unlimited, Farm);
 
