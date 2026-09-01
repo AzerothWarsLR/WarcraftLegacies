1c1ba45c09399804d506ce620326863ad61390c9
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-09-01 18:49:09 +0100

    Block the land entrances into South Kalimdor with rocks until turn 15

--- src/WarcraftLegacies.Source/Setup/RockSetup.cs
+++ src/WarcraftLegacies.Source/Setup/RockSetup.cs
@@ -1,4 +1,5 @@
 ﻿using WarcraftLegacies.Source.GameLogic.Rocks;
+using WarcraftLegacies.Source.GameLogic.SouthKalimdorGuard;
 
 namespace WarcraftLegacies.Source.Setup;
 
@@ -22,5 +23,8 @@ public static class RockSetup
 
     //Bridge Rocks
     RockSystem.Register(new RockGroup(Regions.BridgeAmbient, _rockChunkId, 30));
+
+    RockSystem.Register(new RockGroup(Regions.SouthKalimdor2, _rockChunkId, SouthKalimdorGuardSystem.UnlockTurn));
+    RockSystem.Register(new RockGroup(Regions.SouthKalimdor3, _rockChunkId, SouthKalimdorGuardSystem.UnlockTurn));
   }
 }
