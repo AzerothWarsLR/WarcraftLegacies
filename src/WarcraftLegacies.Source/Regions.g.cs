109ab82849e163667e43d2f2f7831341876628bc
Technopig <127978232+Technopigg@users.noreply.github.com>
2026-09-08 21:22:58 +0100

    Herr dave south kalimdor terrain borders (#4126)
    * Add South Kalimdor regions
    
    * Stop players landing in South Kalimdor early, push them back with a warning until turn 15
    
    * Block the land entrances into South Kalimdor with rocks until turn 15
    
    * Fix CI formatting errors in the South Kalimdor guard file
    
    * Pathing Blockers and regions to remove them
    
    - Terrain: Fixed the east coast cliff exploit in Eastern Kingdoms
    - Terrain: Brushed up Thunder Bluff a little bit
    - Added blockers between Stonetalon Peak and Desolace
    - Added blockers between Feralas and Thousand Needles
    - Added blockers between
    - New Region: Blockers Stonetalon Peak
    - New Region: Blockers Feralas
    
    * Wire up the new South Kalimdor rock blockers
    
    * Remove empty South Kalimdor rock groups now that real ones exist
    
    * Extend South Kalimdor 3 region further north to cover the Stonetalon Peak entrance
    
    ---------
    
    Co-authored-by: Technopig <127978232+Technopig1992@users.noreply.github.com>
    Co-authored-by: HerrDave <HerrDave.Matthewg@protonmail.com>

--- src/WarcraftLegacies.Source/Regions.g.cs
+++ src/WarcraftLegacies.Source/Regions.g.cs
@@ -44,6 +44,9 @@ public static class Regions
   public static Rectangle BlackrockUnlock { get; set; } = new Rectangle(11840f, -12224f, 13632f, -10208f);
   public static Rectangle BlademasterUnlock { get; set; } = new Rectangle(4352f, -10496f, 5024f, -10112f);
   public static Rectangle BlastedlandAmbient { get; set; } = new Rectangle(14912f, -22208f, 20224f, -16896f);
+  public static Rectangle Blockers_Feralas { get; set; } = new Rectangle(-14752f, -9984f, -13536f, -7904f);
+  public static Rectangle Blockers_Stonetalon_Peak { get; set; } = new Rectangle(-19264f, -2176f, -17600f, -544f);
+  public static Rectangle Blockers_Tanaris { get; set; } = new Rectangle(-10496f, -13216f, -9280f, -12448f);
   public static Rectangle BookOfMedivhDalaran { get; set; } = new Rectangle(9312f, 4864f, 9600f, 5216f);
   public static Rectangle BookOfMedivhGilneas { get; set; } = new Rectangle(7840f, 448f, 8032f, 640f);
   public static Rectangle BootyBayQuest { get; set; } = new Rectangle(9696f, -24160f, 12032f, -22304f);
@@ -280,6 +283,9 @@ public static class Regions
   public static Rectangle SlipstreamTempestOrigin { get; set; } = new Rectangle(-22304f, 6816f, -22112f, 7392f);
   public static Rectangle SlipstreamTempestTarget { get; set; } = new Rectangle(2752f, -21920f, 3200f, -21504f);
   public static Rectangle Solliden_Farmstead { get; set; } = new Rectangle(6400f, 8992f, 7808f, 10592f);
+  public static Rectangle SouthKalimdor1 { get; set; } = new Rectangle(-24352f, -20704f, -7744f, -12576f);
+  public static Rectangle SouthKalimdor2 { get; set; } = new Rectangle(-17408f, -12576f, -14080f, -8320f);
+  public static Rectangle SouthKalimdor3 { get; set; } = new Rectangle(-24000f, -12416f, -17408f, -1024f);
   public static Rectangle South_EK_Ships { get; set; } = new Rectangle(7872f, -18240f, 18880f, -12288f);
   public static Rectangle SouthshoreAmbient { get; set; } = new Rectangle(9216f, 1216f, 14176f, 3520f);
   public static Rectangle SouthshoreAmbient2 { get; set; } = new Rectangle(10336f, 3936f, 13568f, 4608f);
