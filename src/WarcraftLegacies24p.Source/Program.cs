bb562e30e8480b50bef37cb7f9bf47e610e884ab
alrun3 <221875817+alrun3@users.noreply.github.com>
2026-09-09 03:01:45 +0200

    chore: setup WarcraftLegacies24p project (#4136)

---
+++ src/WarcraftLegacies24p.Source/Program.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+namespace WarcraftLegacies24p;
+
+public static class Program
+{
+  public static void Main()
+  {
+    var startTimer = timer.Create();
+    startTimer.Start(0.01f, false, () =>
+    {
+      startTimer.Dispose();
+      Start();
+    });
+  }
+
+  private static void Start()
+  {
+    player.LocalPlayer.DisplayTextTo("Hello World!");
+  }
+}
