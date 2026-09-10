0a159eb12ffca3faf5b66f92e6b989c785d46ffa
Technopig <127978232+Technopigg@users.noreply.github.com>
2026-09-10 20:35:55 +0100

    add execute to blademaster

--- src/WarcraftLegacies.Source/Factions/OrcishHorde/OrcishHordeTraits.cs
+++ src/WarcraftLegacies.Source/Factions/OrcishHorde/OrcishHordeTraits.cs
@@ -14,6 +14,12 @@ public static class OrcishHordeTraits
       DamageMultResistant = 1.5f
     }, UNIT_OGRH_CHIEFTAIN_OF_THE_WARSONG_CLAN_ORCISH_HORDE);
 
+    UnitTypeTraitRegistry.Register(new Execute
+    {
+      DamageMultNonResistant = 3,
+      DamageMultResistant = 1.15f
+    }, UNIT_O00G_BLADEMASTER_ORCISH_HORDE);
+
     UnitTypeTraitRegistry.Register(new ResoluteHeart(ABILITY_A0TY_RESOLUTE_HEART_ICON)
     {
       Radius = 300f,
