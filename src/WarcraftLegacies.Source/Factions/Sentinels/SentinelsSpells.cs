b1bfa1fbfc254329d3ad1b06ef6bf249771f2743
Technopig <127978232+Technopigg@users.noreply.github.com>
2026-09-20 18:17:07 +0100

    Add Lunar Sanctuary spell for Tyrande

--- src/WarcraftLegacies.Source/Factions/Sentinels/SentinelsSpells.cs
+++ src/WarcraftLegacies.Source/Factions/Sentinels/SentinelsSpells.cs
@@ -1,4 +1,5 @@
 ﻿using MacroTools.Spells;
+using WarcraftLegacies.Source.Factions.Sentinels.Spells;
 using WarcraftLegacies.Source.Shared.Spells;
 
 namespace WarcraftLegacies.Source.Factions.Sentinels;
@@ -17,5 +18,18 @@ public static class SentinelsSpells
     };
     SpellRegistry.Register(elunesGaze);
 
+    var lunarSanctuary = new LunarSanctuarySpell(ABILITY_A01D_LUNAR_SANCTUARY_LIGHT_BLUE_TYRANDE)
+    {
+      Radius = 350,
+      Duration = 10,
+      Period = 1,
+      HealPerPeriod = new LeveledAbilityField<float> { Base = 5, PerLevel = 5 },
+      ManaPerPeriod = new LeveledAbilityField<float> { Base = 1, PerLevel = 1 },
+      SlowAuraAbilityId = ABILITY_A14C_SLOW_AURA_LUNAR_SANCTUARY,
+      TrueSightAbilityId = ABILITY_A14D_TRUE_SIGHT_LUNAR_SANCTUARY,
+      SanctuaryEffectPath = @"Abilities\Spells\NightElf\Tranquility\TranquilityTarget.mdl",
+      HealEffectPath = @"Abilities\Spells\NightElf\Rejuvenation\RejuvenationTarget.mdl"
+    };
+    SpellRegistry.Register(lunarSanctuary);
   }
 }
---
+++ src/WarcraftLegacies.Source/Factions/Sentinels/Spells/LunarSanctuarySpell.cs
@@ -0,0 +1,102 @@
+﻿using MacroTools.DummyCasters;
+using MacroTools.Hazards;
+using MacroTools.Spells;
+using MacroTools.Utils;
+using WCSharp.Shared.Data;
+
+namespace WarcraftLegacies.Source.Factions.Sentinels.Spells;
+
+public sealed class LunarSanctuarySpell : Spell
+{
+  public required float Radius { get; init; }
+  public required float Duration { get; init; }
+  public required float Period { get; init; }
+  public required LeveledAbilityField<float> HealPerPeriod { get; init; }
+  public required LeveledAbilityField<float> ManaPerPeriod { get; init; }
+  public required int SlowAuraAbilityId { get; init; }
+  public required int TrueSightAbilityId { get; init; }
+  public string? SanctuaryEffectPath { get; init; }
+  public string? HealEffectPath { get; init; }
+
+  public LunarSanctuarySpell(int id) : base(id)
+  {
+  }
+
+  public override void OnCast(unit caster, unit target, Point targetPoint)
+  {
+    var level = GetAbilityLevel(caster);
+    HazardSystem.Add(new LunarSanctuaryHazard(caster, targetPoint.X, targetPoint.Y)
+    {
+      Radius = Radius,
+      Heal = HealPerPeriod.GetValue(level),
+      Mana = ManaPerPeriod.GetValue(level),
+      AbilityLevel = level,
+      SlowAuraAbilityId = SlowAuraAbilityId,
+      TrueSightAbilityId = TrueSightAbilityId,
+      SanctuaryEffectPath = SanctuaryEffectPath,
+      HealEffectPath = HealEffectPath,
+      Interval = Period,
+      Duration = Duration
+    });
+  }
+}
+
+public sealed class LunarSanctuaryHazard : Hazard
+{
+  private readonly unit _caster;
+  private unit? _auraCarrier;
+  private effect? _sanctuaryEffect;
+
+  public float Radius { get; init; }
+  public float Heal { get; init; }
+  public float Mana { get; init; }
+  public int AbilityLevel { get; init; }
+  public int SlowAuraAbilityId { get; init; }
+  public int TrueSightAbilityId { get; init; }
+  public string? SanctuaryEffectPath { get; init; }
+  public string? HealEffectPath { get; init; }
+
+  public LunarSanctuaryHazard(unit caster, float x, float y) : base(caster, x, y)
+  {
+    _caster = caster;
+  }
+
+  public override void OnCreate()
+  {
+    _auraCarrier = unit.Create(_caster.Owner, DummyCasterManager.UnitTypeId, Position.X, Position.Y, 0);
+    _auraCarrier.AddAbility(SlowAuraAbilityId);
+    _auraCarrier.SetAbilityLevel(SlowAuraAbilityId, AbilityLevel);
+    _auraCarrier.AddAbility(TrueSightAbilityId);
+
+    if (SanctuaryEffectPath != null)
+    {
+      _sanctuaryEffect = effect.Create(SanctuaryEffectPath, Position.X, Position.Y);
+    }
+  }
+
+  protected override void OnPeriodic()
+  {
+    foreach (var target in GlobalGroup.EnumUnitsInRange(Position.X, Position.Y, Radius))
+    {
+      if (!CastFilters.IsTargetAllyAndAlive(_caster, target))
+      {
+        continue;
+      }
+
+      var wasHurt = target.Life < target.MaxLife;
+      target.Life += Heal;
+      target.Mana += Mana;
+
+      if (wasHurt && HealEffectPath != null)
+      {
+        effect.Create(HealEffectPath, target, "origin").Dispose();
+      }
+    }
+  }
+
+  protected override void OnDispose()
+  {
+    _sanctuaryEffect?.Dispose();
+    _auraCarrier?.Dispose();
+  }
+}
