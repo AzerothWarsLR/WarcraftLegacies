96117dee8381427fe59151d6a9b8d58659c8acbb
Technopig <127978232+Technopigg@users.noreply.github.com>
2026-09-22 23:54:51 +0100

    add darkspear trolls quest for orcish horde

---
+++ src/WarcraftLegacies.Source/Factions/OrcishHorde/Quests/QuestSenjinIsles.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using MacroTools.Extensions;
+using MacroTools.Factions;
+using MacroTools.Quests;
+using WarcraftLegacies.Source.Objectives.FactionBased;
+using WarcraftLegacies.Source.Objectives.TurnBased;
+using WarcraftLegacies.Source.Objectives.UnitBased;
+
+namespace WarcraftLegacies.Source.Factions.OrcishHorde.Quests;
+
+public sealed class QuestSenjinIsles : QuestData
+{
+  private readonly List<unit> _rescueUnits;
+
+  public QuestSenjinIsles() : base("The Darkspear Trolls",
+    "Mere months ago, Thrall's forces saved the Darkspear tribe from the brink of extinction at the hands of constant murloc raids. They have recently made a new home on the Echo Isles, and could prove formidable allies in the invasion of Kalimdor.",
+    @"ReplaceableTextures\CommandButtons\BTNWitchDoctor.blp")
+  {
+    _rescueUnits = Regions.EchoUnlock.PrepareUnitsForRescue(RescuePreparationMode.HideNonStructures);
+    AddObjective(new ObjectiveAnyUnitInRect(Regions.EchoUnlock, "Echo Isles", true));
+    AddObjective(new ObjectiveSelfExists());
+    AddObjective(new ObjectiveExpire(20, Title));
+
+    ResearchId = UPGRADE_R032_QUEST_COMPLETED_THE_DARKSPEAR_TROLLS;
+  }
+
+  public override string RewardFlavour =>
+    "Vol'jin, foremost Shadow Hunter of the Darkspear Tribe, welcomes Thrall to his village with open arms. The trolls of the Echo Isles unanimously agree to join the Horde.";
+
+  protected override string RewardDescription =>
+    "You gain control of Echo Isles, and learn to train Vol'jin from the Altar of Storms";
+
+  protected override void OnFail(Faction completingFaction)
+  {
+    var rescuer = completingFaction.ScoreStatus == ScoreStatus.Defeated
+      ? player.NeutralAggressive
+      : completingFaction.Player;
+
+    rescuer.RescueGroup(_rescueUnits);
+  }
+
+  protected override void OnComplete(Faction completingFaction) =>
+    completingFaction.Player.RescueGroup(_rescueUnits);
+}
