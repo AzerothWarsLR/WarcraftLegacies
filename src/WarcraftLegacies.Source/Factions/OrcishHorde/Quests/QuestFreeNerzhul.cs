78a032c8b143099d74ef40172e95dda6373cdba7
Technopig <127978232+Technopigg@users.noreply.github.com>
2026-09-22 23:54:55 +0100

    add free nerzhul quest for orcish horde

---
+++ src/WarcraftLegacies.Source/Factions/OrcishHorde/Quests/QuestFreeNerzhul.cs
@@ -0,0 +1,44 @@
+﻿using MacroTools.Extensions;
+using MacroTools.Factions;
+using MacroTools.Legends;
+using MacroTools.Quests;
+using WarcraftLegacies.Source.Factions.Scourge.Mechanics;
+using WarcraftLegacies.Source.Objectives;
+using WarcraftLegacies.Source.Objectives.LegendBased;
+using WarcraftLegacies.Source.Objectives.MetaBased;
+using WarcraftLegacies.Source.Objectives.QuestBased;
+
+namespace WarcraftLegacies.Source.Factions.OrcishHorde.Quests;
+
+public sealed class QuestFreeNerzhul : QuestData
+{
+  private readonly LegendaryHero _thrall;
+  private readonly ObjectiveFrozenThroneState _objectiveFrozenThroneState;
+
+  public QuestFreeNerzhul(Capital theFrozenThrone, LegendaryHero thrall, QuestData warsongHold, QuestData kalimdorAblaze) : base("Jailor of the Damned",
+    "Before he became the Lich King, Ner'zhul was the chieftain and elder shaman of the Shadowmoon Clan. Perhaps something of his former self still survives within the Frozen Throne.",
+    @"ReplaceableTextures\CommandButtons\BTNLichKing.blp")
+  {
+    _thrall = thrall;
+    _objectiveFrozenThroneState = new ObjectiveFrozenThroneState(FrozenThroneState.Ruptured);
+    AddObjective(new ObjectiveEitherOf(new ObjectiveCapitalDead(theFrozenThrone), _objectiveFrozenThroneState));
+    AddObjective(new ObjectiveLegendInRect(thrall, Regions.Ice_Crown, "Icecrown"));
+    AddObjective(new ObjectiveAnyQuestComplete(warsongHold, kalimdorAblaze)
+    {
+      Progress = QuestProgress.Undiscovered,
+      ShowsInQuestLog = false,
+      ShowsInPopups = false
+    });
+  }
+
+  public override string RewardFlavour => _objectiveFrozenThroneState.State == FrozenThroneState.Ruptured
+    ? "The Frozen Throne has been ruptured beyond repair, but Ner'zhul's mangled soul remains imprisoned within. Perhaps the old Shaman will never know peace."
+    : "It seems Ner'zhul is finally free from his tortured existence as the bearer of the Helm of Domination.";
+
+  protected override string RewardDescription => "Thrall gains 10 Strength, 10 Agility, and 10 Intelligence";
+
+  protected override void OnComplete(Faction completingFaction)
+  {
+    _thrall.Unit?.AddHeroAttributes(10, 10, 10);
+  }
+}
