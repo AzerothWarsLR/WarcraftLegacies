5dade390abad2d9292926d8016a2b680130b550c
Technopig <127978232+Technopigg@users.noreply.github.com>
2026-09-24 01:54:02 +0100

    make the new tauren unit abilities work

--- src/WarcraftLegacies.Source/Factions/TaurenTribes/TaurenTribesSpells.cs
+++ src/WarcraftLegacies.Source/Factions/TaurenTribes/TaurenTribesSpells.cs
@@ -24,20 +24,10 @@ public static class TaurenTribesSpells
     };
     SpellRegistry.Register(warStompChieftainOgreLord);
 
-    var warStompSunwalkerChampion = new MassAnySpell(ABILITY_AT15_WAR_STOMP_TAUREN_TRIBES_SUNWALKER_CHAMPION)
+    SpellRegistry.Register(new ChallengingTaunt(ABILITY_A15C_CHALLENGING_TAUNT_TAUREN_TRIBES_OGRE_LORD)
     {
-      Radius = 300,
-      Damage = new LeveledAbilityField<float>
-      {
-        PerLevel = 25
-      },
-      DummyAbilityId = ABILITY_AT11_STUN_UNIT_TAUREN_TRIBES,
-      DummyAbilityOrderId = ORDER_THUNDERBOLT,
-      SpecialEffect = @"Abilities\Spells\Orc\WarStomp\WarStompCaster.mdl",
-      CastFilter = CastFilters.IsTargetEnemyAliveAndGroundUnits,
-      TargetType = SpellTargetType.None
-    };
-    SpellRegistry.Register(warStompSunwalkerChampion);
+      ArmorAbilityId = ABILITY_A15D_CHALLENGING_TAUNT_ARMOR_TAUREN_TRIBES_OGRE_LORD_DUMMY
+    });
 
     SpellRegistry.Register(new SpiritMend(ABILITY_A14Y_SPIRIT_MEND_TAUREN_TRIBES_MAGATHA)
     {
